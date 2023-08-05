5c82358a8979e54d469bd4a3e46b087e1b54def3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:20 +0000

    Add package peewee-jsonfield/0.0.2/peewee-jsonfield-0.0.2.tar.gz

---
+++ packages/peewee-jsonfield/peewee-jsonfield-0.0.2.tar.gz/peewee-jsonfield-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='peewee-jsonfield',
+    version='0.0.2',
+    description='JSONField for Peewee',
+    packages=['jsonfield'],
+    install_requires=[
+        'peewee',
+        'pymysql'
+    ],
+    license='MIT License',
+    platforms=['any'],
+    author='Mark Smirnov',
+    author_email='mark@mark99.ru'
+)
