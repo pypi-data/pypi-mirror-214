e2e6c007db0b24c4d025d9a068f6ecb02958f6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:53 +0000

    Add package grai-source-flat-file/0.0.11/grai_source_flat_file-0.0.11.tar.gz

---
+++ packages/grai-source-flat-file/grai_source_flat_file-0.0.11.tar.gz/grai_source_flat_file-0.0.11/src/grai_source_flat_file/base.py
@@ -0,0 +1,52 @@
+from typing import List, Literal, Optional, Tuple
+
+from grai_client.endpoints.client import BaseClient
+from grai_client.update import update
+
+from grai_source_flat_file.adapters import adapt_to_client
+from grai_source_flat_file.loader import build_nodes_and_edges
+
+
+def get_nodes_and_edges(file_name: str, namespace: str, version: str = "v1"):
+    """
+
+    Args:
+        file_name (str):
+        namespace (str):
+        version (str, optional):  (Default value = "v1")
+
+    Returns:
+
+    Raises:
+
+    """
+    if version != "v1":
+        raise NotImplementedError(f"No implementation for version {version}")
+
+    nodes, edges = build_nodes_and_edges(file_name, namespace)
+    nodes = adapt_to_client(nodes)
+    edges = adapt_to_client(edges)
+    return nodes, edges
+
+
+def update_server(
+    client: BaseClient,
+    file_name: str,
+    namespace: Optional[str] = None,
+):
+    """
+
+    Args:
+        client (BaseClient):
+        file_name (str):
+        namespace (Optional[str], optional):  (Default value = None)
+
+    Returns:
+
+    Raises:
+
+    """
+    nodes, edges = get_nodes_and_edges(file_name, namespace, client.id)
+
+    update(client, nodes)
+    update(client, edges)
