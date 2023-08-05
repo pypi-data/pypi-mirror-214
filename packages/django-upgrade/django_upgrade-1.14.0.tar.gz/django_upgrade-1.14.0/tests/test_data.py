937f8e80f8299fbb23e4d723758ba8cf3c749309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:30 +0000

    Add package django-upgrade/1.14.0/django_upgrade-1.14.0.tar.gz

---
+++ packages/django-upgrade/django_upgrade-1.14.0.tar.gz/django_upgrade-1.14.0/tests/test_data.py
@@ -0,0 +1,272 @@
+from __future__ import annotations
+
+from collections import defaultdict
+
+import pytest
+
+from django_upgrade.data import Settings
+from django_upgrade.data import State
+
+settings = Settings(target_version=(4, 0))
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "admin.py",
+        "myapp/admin.py",
+        "myapp\\admin.py",
+        "myapp/admin/file.py",
+        "myapp\\admin\\file.py",
+        "myapp/blog/admin/article.py",
+        "myapp\\blog\\admin\\article.py",
+        "myapp/custom_admin.py",
+        "myapp\\custom_admin.py",
+        "myapp/custom_admin/file.py",
+        "myapp\\custom_admin\\file.py",
+        "myapp/admin_custom.py",
+        "myapp\\admin_custom.py",
+        "myapp/admin_custom/file.py",
+        "myapp\\admin_custom\\file.py",
+    ),
+)
+def test_looks_like_admin_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_admin_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "administrator.py",
+        "blog/adm/article.py",
+        "blog\\adm\\article.py",
+    ),
+)
+def test_looks_like_admin_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_admin_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "management/commands/test.py",
+        "management\\commands\\test.py",
+        "myapp/management/commands/test.py",
+        "myapp\\management\\commands\\test.py",
+        "myapp/subapp/management/commands/test.py",
+        "myapp\\subapp\\management\\commands\\test.py",
+        "myapp/subapp/management/commands/test/subcommand.py",
+        "myapp\\subapp\\management\\commands\\test\\subcommand.py",
+    ),
+)
+def test_looks_like_command_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_command_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "test.py",
+        "management/commands.py",
+        "management\\commands.py",
+        "myapp/management/commands.py",
+        "myapp\\management\\commands.py",
+        "myapp/mgmt/commands.py",
+        "myapp\\mgmt\\commands.py",
+        "myapp/management/something/commands/example.py",
+        "myapp\\management\\something\\commands\\example.py",
+        "myapp/commands/management/example.py",
+        "myapp\\commands\\management\\example.py",
+    ),
+)
+def test_looks_like_command_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_command_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "__init__.py",
+        "package/__init__.py",
+        "package\\__init__.py",
+        "project/package/__init__.py",
+        "project\\package\\__init__.py",
+    ),
+)
+def test_looks_like_dunder_init_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_dunder_init_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "__thing__init__.py",
+        "thing-__init__.py",
+        "__init___py",
+        "_init_.py",
+        "__init.py",
+        "init__.py",
+    ),
+)
+def test_looks_like_dunder_init_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_dunder_init_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "project/migrations/0238_auto_20200424_1249.py",
+        "project\\migrations\\0238_auto_20200424_1249.py",
+        "another_project/migrations/0001_initial.py",
+        "another_project\\migrations\\0001_initial.py",
+    ),
+)
+def test_looks_like_migrations_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_migrations_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "0238_auto_20200424_1249.py",
+        "package/0001_initial.py",
+        "package\\0001_initial.py",
+        "migration/0001_initial.py",
+        "migration\\0001_initial.py",
+    ),
+)
+def test_looks_like_migrations_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_migrations_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "settings.py",
+        "myapp/settings.py",
+        "myapp\\settings.py",
+        "myapp/settings/prod.py",
+        "myapp\\settings\\prod.py",
+        "myapp/prod_settings.py",
+        "myapp\\prod_settings.py",
+        "myapp/local_settings.py",
+        "myapp\\local_settings.py",
+        "myapp/settings_tests.py",
+        "myapp\\settings_tests.py",
+    ),
+)
+def test_looks_like_settings_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_settings_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "upsettings.py",
+        "settingsprod.py",
+    ),
+)
+def test_looks_like_settings_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_settings_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "test_example.py",
+        "example_test.py",
+        "test.py",
+        "tests.py",
+        "myapp/test.py",
+        "myapp\\test.py",
+        "myapp/tests.py",
+        "myapp\\tests.py",
+        "myapp/tests/base.py",
+        "myapp\\tests\\base.py",
+        "myapp/tests/__init__.py",
+        "myapp\\tests\\__init__.py",
+        "myapp/test_example.py",
+        "myapp\\test_example.py",
+        "myapp/tests_example.py",
+        "myapp\\tests_example.py",
+        "myapp/example_test.py",
+        "myapp\\example_test.py",
+        "myapp/example_tests.py",
+        "myapp\\example_tests.py",
+    ),
+)
+def test_looks_like_test_file_true(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert state.looks_like_test_file
+
+
+@pytest.mark.parametrize(
+    "filename",
+    (
+        "conftest.py",
+        "protester.py",
+        "myapp/protests/models.py",
+        "myapp\\protests\\models.py",
+    ),
+)
+def test_looks_like_test_file_false(filename: str) -> None:
+    state = State(
+        settings=settings,
+        filename=filename,
+        from_imports=defaultdict(set),
+    )
+    assert not state.looks_like_test_file
---
+++ packages/django-upgrade/django_upgrade-1.14.0.tar.gz/django_upgrade-1.14.0/tests/test_main.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+import io
+import re
+import sys
+from textwrap import dedent
+from unittest import mock
+
+import pytest
+from tokenize_rt import src_to_tokens
+from tokenize_rt import UNIMPORTANT_WS
+
+from django_upgrade import __main__  # noqa: F401
+from django_upgrade.main import fixup_dedent_tokens
+from django_upgrade.main import main
+from django_upgrade.tokens import DEDENT
+
+
+def test_main_no_files(capsys):
+    """
+    Main should fail without any files as argument
+    """
+    with pytest.raises(SystemExit) as excinfo:
+        main([])
+
+    assert excinfo.value.code == 2
+    out, err = capsys.readouterr()
+    assert "error: the following arguments are required: filenames\n" in err
+    assert out == ""
+
+
+def test_main_help():
+    with pytest.raises(SystemExit) as excinfo:
+        main(["--help"])
+
+    assert excinfo.value.code == 0
+
+
+def test_main_version(capsys):
+    with pytest.raises(SystemExit) as excinfo:
+        main(["--version"])
+
+    out, err = capsys.readouterr()
+
+    assert excinfo.value.code == 0
+    assert re.fullmatch(r"__main__\.py \d+\.\d+\.\d+\n", out)
+    assert err == ""
+
+
+def test_main_syntax_error(tmp_path):
+    path = tmp_path / "example.py"
+    path.write_text("print 1\n")
+
+    result = main([str(path)])
+
+    assert result == 0
+
+
+def test_main_non_utf8_bytes(tmp_path, capsys):
+    path = tmp_path / "example.py"
+    path.write_bytes("# -*- coding: cp1252 -*-\nx = €\n".encode("cp1252"))
+
+    result = main([str(path)])
+
+    assert result == 1
+    out, err = capsys.readouterr()
+    assert out == f"{path} is non-utf-8 (not supported)\n"
+    assert err == ""
+
+
+def test_main_file(tmp_path, capsys):
+    path = tmp_path / "example.py"
+    path.write_text("from django.core.paginator import QuerySetPaginator\n")
+
+    result = main([str(path)])
+
+    assert result == 1
+    out, err = capsys.readouterr()
+    assert err == f"Rewriting {path}\n"
+    assert path.read_text() == "from django.core.paginator import Paginator\n"
+
+
+def test_main_exit_zero_even_if_changed(tmp_path, capsys):
+    path = tmp_path / "example.py"
+    path.write_text("from django.core.paginator import QuerySetPaginator\n")
+
+    result = main(["--exit-zero-even-if-changed", str(path)])
+
+    assert result == 0
+    out, err = capsys.readouterr()
+    assert err == f"Rewriting {path}\n"
+    assert path.read_text() == "from django.core.paginator import Paginator\n"
+
+
+def test_main_stdin_no_changes(capsys):
+    stdin = io.TextIOWrapper(io.BytesIO(b'print("hi")\n'), "UTF-8")
+
+    with mock.patch.object(sys, "stdin", stdin):
+        result = main(["-"])
+
+    assert result == 0
+    out, err = capsys.readouterr()
+    assert out == 'print("hi")\n'
+    assert err == ""
+
+
+def test_main_stdin_with_changes(capsys):
+    input_ = "from django.core.paginator import QuerySetPaginator\n"
+    stdin = io.TextIOWrapper(io.BytesIO(input_.encode()), "UTF-8")
+
+    with mock.patch.object(sys, "stdin", stdin):
+        result = main(["-"])
+
+    assert result == 1
+    out, err = capsys.readouterr()
+    assert out == "from django.core.paginator import Paginator\n"
+    assert err == ""
+
+
+def test_fixup_dedent_tokens():
+    code = dedent(
+        """\
+        if True:
+            if True:
+                pass
+            else:
+                pass
+        """
+    )
+    tokens = src_to_tokens(code)
+
+    assert tokens[14].name == UNIMPORTANT_WS
+    assert tokens[15].name == DEDENT
+
+    fixup_dedent_tokens(tokens)
+
+    assert tokens[14].name == DEDENT
+    assert tokens[15].name == UNIMPORTANT_WS
