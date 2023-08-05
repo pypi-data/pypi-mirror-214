dc281ede9e47876dcc24c46e0d72ddfe03d70226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:00 +0000

    Add package girlfriendpy/0.2/girlfriendpy-0.2.tar.gz

---
+++ packages/girlfriendpy/girlfriendpy-0.2.tar.gz/girlfriendpy-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "girlfriendpy",
+    version = "0.2",
+    py_modules = ["gf"],
+    install_requires = [],
+    description = "i think i have a gf i dont entirely know how a gf is defined but i think she is so i made this library to help me"
+)
