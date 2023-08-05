18f9b7326f309aaa0825ce7b5cb1d066ec9c6534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:03 +0000

    Add package oleo/0.0.9/oleo-0.0.9-py3-none-any.whl

---
+++ packages/oleo/oleo-0.0.9-py3-none-any.whl/oleo/_version.py
@@ -0,0 +1,4 @@
+# Module version
+__version__ = "0.0.9"
+
+NPM_PACKAGE_RANGE = "0.0.9"
---
+++ packages/oleo/oleo-0.0.9-py3-none-any.whl/oleo/netlist_graph.py
@@ -0,0 +1,25 @@
+import ipywidgets as widgets
+import traitlets
+from ._version import NPM_PACKAGE_RANGE
+from .utils import convert_gdsfactory_netlist
+
+
+@widgets.register
+class NetlistGraph(widgets.DOMWidget):
+    """
+    Widget element that contains the javascript interactive netlist.
+    """
+
+    _view_name = traitlets.Unicode("NetlistGraphView").tag(sync=True)
+    _model_name = traitlets.Unicode("NetlistGraphModel").tag(sync=True)
+    _view_module = traitlets.Unicode("oleo").tag(sync=True)
+    _model_module = traitlets.Unicode("oleo").tag(sync=True)
+    _view_module_version = traitlets.Unicode(NPM_PACKAGE_RANGE).tag(sync=True)
+    _model_module_version = traitlets.Unicode(NPM_PACKAGE_RANGE).tag(sync=True)
+
+    # Widget specific property.
+    # Widget properties are defined as traitlets. Any property tagged with `sync=True`
+    # is automatically synced to the frontend *any* time it changes in Python.
+    # It is synced back to Python from the frontend *any* time the model is touched.
+    netlist_graph_data = traitlets.Dict().tag(sync=True)
+    value = netlist_graph_data
