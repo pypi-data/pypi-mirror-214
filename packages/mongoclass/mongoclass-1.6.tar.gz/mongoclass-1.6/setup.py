9880c4fc3dd691addc983d39fcb98903dc680a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:18 +0000

    Add package mongoclass/1.6/mongoclass-1.6.tar.gz

---
+++ packages/mongoclass/mongoclass-1.6.tar.gz/mongoclass-1.6/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from pathlib import Path
+
+install_requires = ["dnspython==2.2.1", "mongita==1.1.1"]
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="mongoclass",
+    packages=["mongoclass"],
+    version="1.6",
+    license="MIT",
+    description="A basic ORM like interface for mongodb in python that uses dataclasses.",
+    author="Philippe Mathew",
+    author_email="philmattdev@gmail.com",
+    url="https://github.com/bossauh/mongoclass",
+    download_url="https://github.com/bossauh/mongoclass/archive/refs/tags/v_16.tar.gz",
+    keywords=["pymongo", "orm"],
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
