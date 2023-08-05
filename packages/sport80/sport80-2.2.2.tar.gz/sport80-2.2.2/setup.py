0bb15e391e4095d16d484cfe15fda569094f0925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:43 +0000

    Add package sport80/2.2.2/sport80-2.2.2.tar.gz

---
+++ packages/sport80/sport80-2.2.2.tar.gz/sport80-2.2.2/setup.py
@@ -0,0 +1,19 @@
+""" Setup file for PyPi """
+from setuptools import setup
+
+setup(
+    name="sport80",
+    version="2.2.2",
+    description="Python API interface for the Sport80 sites",
+    long_description='Intentionally empty',
+    url="https://github.com/euanwm/sport80_api",
+    author="Euan Meston",
+    author_email="euanmeston@gmail.com",
+    license="BSD",
+    install_requires=["requests",
+                      "beautifulsoup4",
+                      "js2py"],
+    classifiers=["Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9"],
+    python_requires='>=3.8'
+)
---
+++ packages/sport80/sport80-2.2.2.tar.gz/sport80-2.2.2/sport80/sport80.py
@@ -0,0 +1,32 @@
+""" Main file """
+import logging
+from typing import Union
+from .sport80_http_client import SportEightyHTTP
+
+
+class SportEighty:
+    """
+    This class enables a variety of functions that can be carried out with a sport80 subdomain.
+    """
+    def __init__(self, subdomain: str, return_dict=True, debug: logging = logging.WARNING):
+        self.__http_client = SportEightyHTTP(subdomain, return_dict=return_dict, debug_lvl=debug)
+
+    def event_index(self, year: int) -> dict:
+        """ Now working """
+        return self.__http_client.get_event_index(year)
+
+    def event_results(self, event_dict: dict) -> Union[list, dict]:
+        """ Now working """
+        return self.__http_client.get_event_results(event_dict)
+
+    def upcoming_events(self) -> Union[list, dict]:
+        """ Now working """
+        return self.__http_client.get_upcoming_events()
+
+    def start_list(self, event_id: str) -> Union[list, dict]:
+        """ Now working """
+        return self.__http_client.get_start_list(event_id)
+
+    def rankings_index(self) -> dict:
+        """ Returns a dict containing endpoints for all available ranking categories """
+        return self.__http_client.get_ranking_index()
