38dd813045ca60697db4281c926b46ec8d2555b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:34 +0000

    Add package bs-orm/0.8.7.post20230615/bs_orm-0.8.7.post20230615.tar.gz

---
+++ packages/bs-orm/bs_orm-0.8.7.post20230615.tar.gz/bs_orm-0.8.7.post20230615/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='bs_orm',
+    version='0.8.7',
+    description='fix bug',
+    packages=['bs_orm'],
+    author_email='mr.z.75@mail.ru',
+    zip_safe=False
+    )
