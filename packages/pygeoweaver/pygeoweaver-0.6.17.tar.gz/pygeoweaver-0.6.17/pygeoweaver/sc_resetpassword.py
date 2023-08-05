831438f0c214058af59539819f3c85f0d1b67d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:21 +0000

    Add package pygeoweaver/0.6.17/pygeoweaver-0.6.17.tar.gz

---
+++ packages/pygeoweaver/pygeoweaver-0.6.17.tar.gz/pygeoweaver-0.6.17/pygeoweaver/sc_resetpassword.py
@@ -0,0 +1,23 @@
+import subprocess
+from pygeoweaver.utils import (
+    download_geoweaver_jar,
+    get_geoweaver_jar_path,
+    get_java_bin_path,
+    get_root_dir,
+)
+
+
+def reset_password():
+    """
+    Usage: <main class> resetpassword
+    Reset password for localhost
+    """
+    download_geoweaver_jar()
+    subprocess.run(
+        [
+            get_java_bin_path(),
+            "-jar",
+            get_geoweaver_jar_path(),
+            "resetpassword",
+        ]
+    )
