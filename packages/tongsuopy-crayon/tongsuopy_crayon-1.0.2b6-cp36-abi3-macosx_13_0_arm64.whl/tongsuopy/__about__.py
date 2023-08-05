a2959d28c3c94cb9594be6ac6f4a65d6c3da54af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:09 +0000

    Add package tongsuopy-crayon/1.0.2b6/tongsuopy_crayon-1.0.2b6-cp36-abi3-macosx_13_0_arm64.whl

---
+++ packages/tongsuopy-crayon/tongsuopy_crayon-1.0.2b6-cp36-abi3-macosx_13_0_arm64.whl/tongsuopy/__about__.py
@@ -0,0 +1,14 @@
+# Licensed under the Apache License 2.0 (the "License").  You may not use
+# this file except in compliance with the License. See the LICENSE file
+# in the root of this repository for complete details.
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__version__ = "1.0.2-beta.6"
+
+__author__ = "The Tongsuo Project Authors"
+__copyright__ = "Copyright 2023 {}. All Rights Reserved.".format(__author__)
