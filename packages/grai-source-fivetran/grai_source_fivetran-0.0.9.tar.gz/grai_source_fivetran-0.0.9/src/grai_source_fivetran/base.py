4dace17c6ec19679faa6d237f27af4c957c97bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:53 +0000

    Add package grai-source-fivetran/0.0.9/grai_source_fivetran-0.0.9.tar.gz

---
+++ packages/grai-source-fivetran/grai_source_fivetran-0.0.9.tar.gz/grai_source_fivetran-0.0.9/src/grai_source_fivetran/base.py
@@ -0,0 +1,97 @@
+from functools import partial
+from typing import List, Literal, Optional, Tuple
+
+from grai_client.endpoints.client import BaseClient
+from grai_client.update import update
+from grai_schemas.base import Edge, Node
+
+from grai_source_fivetran.adapters import adapt_to_client
+from grai_source_fivetran.loader import FivetranConnector, NamespaceTypes
+
+
+def validate_nodes_and_edges(nodes: List[Node], edges: List[Edge]):
+    """
+
+    Args:
+        nodes (List[Node]):
+        edges (List[Edge]):
+
+    Returns:
+
+    Raises:
+
+    """
+    node_hashes = [hash(node) for node in nodes]
+    if (n_hashes := len(set(node_hashes))) == len(nodes):
+        message = (
+            f"The Fivetran connection generated {len(nodes) - n_hashes} duplicated nodes. "
+            f"This is likely because there are multiple Fivetran tables with the same name. "
+            f"You can disambiguate these tables by identifying them with different namespaces. Please "
+            f"see the documentation for more information. https://docs.grai.io/tooling/github-actions#fivetran"
+        )
+        raise ValueError(message)
+
+
+def get_nodes_and_edges(connector: FivetranConnector, version: Literal["v1"]) -> Tuple[List[Node], List[Edge]]:
+    """
+
+    Args:
+        connector (FivetranConnector):
+        version (Literal["v1"]):
+
+    Returns:
+
+    Raises:
+
+    """
+    nodes, edges = connector.get_nodes_and_edges()
+
+    nodes = adapt_to_client(nodes, version)
+    edges = adapt_to_client(edges, version)
+    validate_nodes_and_edges(nodes, edges)
+    return nodes, edges
+
+
+def update_server(
+    client: BaseClient,
+    namespaces: Optional[NamespaceTypes] = None,
+    default_namespace: Optional[str] = None,
+    api_key: Optional[str] = None,
+    api_secret: Optional[str] = None,
+    endpoint: Optional[str] = None,
+    limit: Optional[int] = None,
+    parallelization: Optional[int] = None,
+):
+    """
+
+    Args:
+        client (BaseClient):
+        namespaces (Optional[NamespaceTypes], optional):  (Default value = None)
+        default_namespace (Optional[str], optional):  (Default value = None)
+        api_key (Optional[str], optional):  (Default value = None)
+        api_secret (Optional[str], optional):  (Default value = None)
+        endpoint (Optional[str], optional):  (Default value = None)
+        limit (Optional[int], optional):  (Default value = None)
+        parallelization (Optional[int], optional):  (Default value = None)
+
+    Returns:
+
+    Raises:
+
+    """
+    kwargs = {
+        "namespaces": namespaces,
+        "default_namespace": default_namespace,
+        "api_key": api_key,
+        "api_secret": api_secret,
+        "endpoint": endpoint,
+        "limit": limit,
+        "parallelization": parallelization,
+    }
+    kwargs = {k: v for k, v in kwargs.items() if v is not None}
+
+    conn = FivetranConnector(**kwargs)
+    nodes, edges = get_nodes_and_edges(conn, client.id)
+
+    update(client, nodes)
+    update(client, edges)
