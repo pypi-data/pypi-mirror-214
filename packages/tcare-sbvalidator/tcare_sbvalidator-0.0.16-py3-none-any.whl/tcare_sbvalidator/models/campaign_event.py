3f0889b5ca6746911243602283f5cfa4bc752cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:28 +0000

    Add package tcare-sbvalidator/0.0.16/tcare_sbvalidator-0.0.16-py3-none-any.whl

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.16-py3-none-any.whl/tcare_sbvalidator/models/campaign_event.py
@@ -0,0 +1,14 @@
+from .tcare_cloud_event import TcareCloudEvent
+from pydantic import BaseModel
+
+# SMS
+class CampaignEventData(BaseModel):
+    type: str
+    trigger: str
+    action: str # i.e., start, end, etc.
+    campaign_id: str
+    case_id: str
+    user_id: str
+
+class CampaignEventServiceBusMessage(TcareCloudEvent):
+    data: CampaignEventData
