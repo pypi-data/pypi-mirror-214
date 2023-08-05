8d4d4adf01389ad8b64aecd77353cf8ba674112c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:39 +0000

    Add package rigatoni/0.2.2/rigatoni-0.2.2-py3-none-any.whl

---
+++ packages/rigatoni/rigatoni-0.2.2-py3-none-any.whl/rigatoni/__init__.py
@@ -0,0 +1,34 @@
+"""Python Server Library for NOODLES Protocol
+
+This server library implements the NOODLES messaging protocol and provides objects for 
+maintaining a scene in state. The server uses a websocket connection to send CBOR encoded 
+messages. To customize its implementation, the library provides convenient interface 
+methods to assist the user in writing their own methods for the server. The user can also 
+add custom delegates to add additionally functionality to any of the standard components.
+
+Modules:
+    core.py
+    geometry/
+        geometry_creation.py
+        geometry_objects.py
+    delegates.py
+    noodle_objects.py
+    server.py
+"""
+
+
+__version__ = "0.2.2"
+
+from .core import Server
+from .noodle_objects import *
+
+# Ensure that dependencies are installed for optional module
+try:
+    import numpy
+    import meshio
+    optionals = True
+except ImportError:
+    optionals = False
+
+if optionals:
+    from . import geometry
