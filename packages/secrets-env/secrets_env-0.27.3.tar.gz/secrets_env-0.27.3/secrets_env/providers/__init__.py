cee6b8f9b0a3fc8cd7ed902f6d302c160c1ed1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:32 +0000

    Add package secrets-env/0.27.3/secrets_env-0.27.3.tar.gz

---
+++ packages/secrets-env/secrets_env-0.27.3.tar.gz/secrets_env-0.27.3/secrets_env/providers/__init__.py
@@ -0,0 +1,28 @@
+import typing
+
+import secrets_env.exceptions
+
+if typing.TYPE_CHECKING:
+    from secrets_env.provider import ProviderBase
+
+DEFAULT_PROVIDER = "vault"
+
+
+def get_provider(data: dict) -> "ProviderBase":
+    type_raw = data.get("type", DEFAULT_PROVIDER)
+    type_ = type_raw.lower()
+
+    # builtin first
+    # fmt: off
+    if type_.startswith("teleport+"):
+        from . import teleport
+        return teleport.get_provider(type_, data)
+    if type_ == "null":
+        from . import null
+        return null.get_provider(type_, data)
+    if type_ == "vault":
+        from . import vault
+        return vault.get_provider(type_, data)
+    # fmt: on
+
+    raise secrets_env.exceptions.ConfigError("Unknown provider type {}", type_raw)
