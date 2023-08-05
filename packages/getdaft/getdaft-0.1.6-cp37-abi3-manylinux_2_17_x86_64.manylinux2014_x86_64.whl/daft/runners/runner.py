2ecacc6c5ba9cb088d5010e12142c5f71f477647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:59 +0000

    Add package getdaft/0.1.6/getdaft-0.1.6-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/getdaft/getdaft-0.1.6-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/daft/runners/runner.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+
+from daft.logical.logical_plan import LogicalPlan
+from daft.runners.partitioning import (
+    PartitionCacheEntry,
+    PartitionSet,
+    PartitionSetCache,
+)
+from daft.runners.runner_io import RunnerIO
+
+
+class Runner:
+    def __init__(self) -> None:
+        self._part_set_cache = PartitionSetCache()
+
+    def get_partition_set_from_cache(self, pset_id: str) -> PartitionCacheEntry:
+        return self._part_set_cache.get_partition_set(pset_id=pset_id)
+
+    def put_partition_set_into_cache(self, pset: PartitionSet) -> PartitionCacheEntry:
+        return self._part_set_cache.put_partition_set(pset=pset)
+
+    @abstractmethod
+    def runner_io(self) -> RunnerIO:
+        ...
+
+    @abstractmethod
+    def run(self, plan: LogicalPlan) -> PartitionCacheEntry:
+        ...
+
+    def optimize(self, plan: LogicalPlan) -> LogicalPlan:
+        return plan
