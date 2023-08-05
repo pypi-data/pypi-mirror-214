2054e27118077ec2d919370cf8fa439ef81abf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:50 +0000

    Add package proxidize-wrapper/0.1.0/proxidize_wrapper-0.1.0.tar.gz

---
+++ packages/proxidize-wrapper/proxidize_wrapper-0.1.0.tar.gz/proxidize_wrapper-0.1.0/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use proxidize_wrapper in a project::
+
+    import proxidize_wrapper
---
+++ packages/proxidize-wrapper/proxidize_wrapper-0.1.0.tar.gz/proxidize_wrapper-0.1.0/tests/test_proxidize_wrapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `proxidize_wrapper` package."""
+
+
+import unittest
+
+from proxidize_wrapper import proxidize_wrapper
+
+
+class TestProxidize_wrapper(unittest.TestCase):
+    """Tests for `proxidize_wrapper` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
