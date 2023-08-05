e67c9ba389a829cc75f106a3524556faf767e9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:22 +0000

    Add package laminci/0.6.7/laminci-0.6.7.tar.gz

---
+++ packages/laminci/laminci-0.6.7.tar.gz/laminci-0.6.7/noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.run("pre-commit", "install")
+    session.run("pre-commit", "run", "--all-files")
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    session.install(".[dev,test]")
+    session.run(
+        "pytest",
+        "-s",
+        "--cov=laminci",
+        "--cov-append",
+        "--cov-report=term-missing",
+    )
