f601306958d823512a423d35c1b3bcbdd1907dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:50 +0000

    Add package erdgen/0.1.2/erdgen-0.1.2.tar.gz

---
+++ packages/erdgen/erdgen-0.1.2.tar.gz/erdgen-0.1.2/erdgen/erdgen.py
@@ -0,0 +1,14 @@
+"""cli"""
+import click
+
+from erdgen.src.erdgen import generate_dbml_schema
+
+
+@click.command()
+@click.option("--directory", default=".", help="Directory to search for yml files")
+@click.option(
+    "--include_non_join_keys", default=False, help="Include non join keys", type=bool
+)
+def main(directory: str, include_non_join_keys: bool) -> None:
+    """main"""
+    print(generate_dbml_schema(directory, include_non_join_keys))
---
+++ packages/erdgen/erdgen-0.1.2.tar.gz/erdgen-0.1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Andrew Moss",
+    author_email="andrew.moss@neofinancial.com",
+    python_requires=">=3.6",
+    name="erdgen",
+    version="0.1.2",
+    description="Generate a DBML ERD from DBT YML relationships",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
+    license="MIT license",
+    packages=find_packages(include=["erdgen", "erdgen.*"]),
+    url="https://github.com/neo-andrew-moss/erdgen",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["click", "PyYAML", "Jinja2", "typing_extensions"],
+    tests_require=["pytest"],
+)
