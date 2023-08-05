54fae8789128de6f555a5ef6f1e86676ebc21bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package Flask-PyTelegramBotAPI/0.0.3/Flask-PyTelegramBotAPI-0.0.3.tar.gz

---
+++ packages/Flask-PyTelegramBotAPI/Flask-PyTelegramBotAPI-0.0.3.tar.gz/Flask-PyTelegramBotAPI-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Flask-PyTelegramBotAPI",
+    version='0.0.3',
+    author='kuzmichus',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=['pytelegrambotapi', "Flask < 3"],
+    description='Интеграция Flask c PyTelegramBotAPI'
+)
