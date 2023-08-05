1616a3004ac9220d75b7998d8986ddc045e9973c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package pocketbase/0.9.1/pocketbase-0.9.1-py3-none-any.whl

---
+++ packages/pocketbase/pocketbase-0.9.1-py3-none-any.whl/pocketbase/models/record.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from pocketbase.models.utils.base_model import BaseModel
+from pocketbase.utils import camel_to_snake
+
+
+class Record(BaseModel):
+    collection_id: str
+    collection_name: str
+    expand: dict
+
+    def load(self, data: dict) -> None:
+        super().load(data)
+        self.expand = {}
+        for key, value in data.items():
+            key = camel_to_snake(key).replace("@", "")
+            setattr(self, key, value)
+        self.load_expanded()
+
+    @classmethod
+    def parse_expanded(cls, data: dict):
+        return cls(data)
+
+    def load_expanded(self) -> None:
+        for key, value in self.expand.items():
+            self.expand[key] = self.parse_expanded(value)
