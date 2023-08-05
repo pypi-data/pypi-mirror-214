824964c4f210ad2b569de7da0f66e625093d9eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package psbs/0.1.0/psbs-0.1.0.tar.gz

---
+++ packages/psbs/psbs-0.1.0.tar.gz/psbs-0.1.0/psbs/extension.py
@@ -0,0 +1,18 @@
+class Extension:
+    def __init__(self, config):
+        self.methods = {}
+        self.config = config
+        if self.get_config():
+            for key, value in self.get_config().items():
+                if key not in self.config:
+                    self.config[key] = None
+                if not self.config[key]:
+                    self.config[key] = value
+
+    def register(self, name, function):
+        if name not in self.methods:
+            self.methods[name] = function
+
+    @staticmethod
+    def get_config():
+        return None
