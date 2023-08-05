1b7cb276bc252ffe0b6ca6b1a4a799d1687b0954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:52 +0000

    Add package holamundoplayerM67/0.0.1/holamundoplayerM67-0.0.1.tar.gz

---
+++ packages/holamundoplayerM67/holamundoplayerM67-0.0.1.tar.gz/holamundoplayerM67-0.0.1/holamundoplayerM67/player.py
@@ -0,0 +1,28 @@
+"""
+Este es el modulo que incluye la clase 
+de reproductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor
+    de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibe como
+        parametro
+
+        Parameters:
+        song (str): este un string con el Path de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito, devuelve 0 fracaso
+        """
+
+        print("Reproduccion cancion")
+
+    def stop(self):
+        print("Stopping")
