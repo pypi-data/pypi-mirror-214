32521d0588732742886be01f574de9e0c1079904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:33 +0000

    Add package mppfc/1.1.2/mppfc-1.1.2-py3-none-any.whl

---
+++ packages/mppfc/mppfc-1.1.2-py3-none-any.whl/mppfc/__init__.py
@@ -0,0 +1,8 @@
+from .mppfc import parse_num_proc
+from .mppfc import ErroneousFunctionCall
+from .mppfc import MultiProcCachedFunction
+from .mppfc import MultiProcCachedFunctionDec
+
+from .cache import CacheFileBased
+from .cache import CacheFileBasedDec
+from .cache import CacheInit
