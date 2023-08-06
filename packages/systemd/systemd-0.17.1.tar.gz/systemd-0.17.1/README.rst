fe79ad06519c3693802742af360dda028265d688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:12 +0000

    Add package systemd/0.17.1/systemd-0.17.1.tar.gz

---
+++ packages/systemd/systemd-0.17.1.tar.gz/systemd-0.17.1/README.rst
@@ -0,0 +1,8 @@
+SystemD for Python
+==================
+
+It's a proxy package for cysystemd package. It's has been renamed to avoid
+of confusions. Please use the `cysystemd` instead of this package.
+
+This package exists only for support users what do not upgrade to
+the new package.
---
+++ packages/systemd/systemd-0.17.1.tar.gz/systemd-0.17.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="systemd",
+    version="0.17.1",
+    packages=['.'],
+    package_dir={'': 'src'},
+    license="Apache",
+    description="systemd wrapper in Cython",
+    long_description=open("README.rst").read(),
+    platforms=["POSIX"],
+    url='http://github.com/mosquito/cysystemd',
+    provides=["systemd"],
+    install_requires=['cysystemd'],
+    keywords="systemd, python, daemon, sd_notify, cython",
+)
