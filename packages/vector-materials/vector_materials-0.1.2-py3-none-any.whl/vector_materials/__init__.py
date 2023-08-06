a6b7691b16f9e450b44f82744633a09a45926174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:00 +0000

    Add package vector-materials/0.1.2/vector_materials-0.1.2-py3-none-any.whl

---
+++ packages/vector-materials/vector_materials-0.1.2-py3-none-any.whl/vector_materials/__init__.py
@@ -0,0 +1,8 @@
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the realpython-reader package
+__version__ = "0.1.1"
