f0f4b6909c69d041eac707ec37ec088b82b86ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package mlexhibit/0.0.1/mlexhibit-0.0.1-py3-none-any.whl

---
+++ packages/mlexhibit/mlexhibit-0.0.1-py3-none-any.whl/mlexhibit/EDA/EDA.py
@@ -0,0 +1,29 @@
+import numpy as np
+from mlexhibit.EDA.utils import meh
+
+class EDA:
+    '''
+    The EDA class provides methods for machine learning exploratory data analysis.
+    '''
+    def __init__(self, name):
+        """
+        Instantiate a class instance
+        
+        :param name: object's name
+        :type name: string
+        """
+        self.name = name
+
+    def example_fun(self, data):
+        """
+        Apply the example function to the given data
+        
+        :param data: An object containing the training data 
+        :type number: string
+    
+        :return: None
+        :rtype: NoneType
+        """
+        rubbish = data
+    
+
