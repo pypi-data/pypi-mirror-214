49edfcd1450660e3eb9afbc02061d6022cef3b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:40 +0000

    Add package tile-coder-rs/0.3.1/tile_coder_rs-0.3.1-cp310-none-win32.whl

---
+++ packages/tile-coder-rs/tile_coder_rs-0.3.1-cp310-none-win32.whl/tile_coder_rs/tile_coder_rs.pyi
@@ -0,0 +1,10 @@
+import numpy as np
+
+def get_tc_indices(
+    dims: int,
+    tiles: np.ndarray,
+    tilings: int,
+    bounds: np.ndarray,
+    offsets: np.ndarray,
+    pos: np.ndarray,
+) -> np.ndarray: ...
