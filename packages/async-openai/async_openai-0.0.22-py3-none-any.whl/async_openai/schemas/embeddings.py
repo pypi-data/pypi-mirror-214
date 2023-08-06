b55fb06563c437770e682f82b7f5ca9c9a01ce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package async-openai/0.0.22/async_openai-0.0.22-py3-none-any.whl

---
+++ packages/async-openai/async_openai-0.0.22-py3-none-any.whl/async_openai/schemas/embeddings.py
@@ -0,0 +1,63 @@
+from typing import Optional, Type, Any, Union, List
+from lazyops.types import validator, lazyproperty
+
+from async_openai.types.options import OpenAIModel
+from async_openai.types.resources import BaseResource
+from async_openai.types.responses import BaseResponse
+from async_openai.types.routes import BaseRoute
+
+
+__all__ = [
+    'EmbeddingData',
+    'EmbeddingObject',
+    'EmbeddingResponse',
+    'EmbeddingRoute',
+]
+
+
+class EmbeddingData(BaseResource):
+    object: Optional[str] = 'embedding'
+    embedding: Optional[List[float]] = []
+    index: Optional[int] = 0
+
+class EmbeddingObject(BaseResource):
+    model: Optional[Union[str, OpenAIModel, Any]] = "ada"
+    input: Optional[Union[List[Any], Any]]
+    user: Optional[str] = None
+
+    @validator('model', pre=True, always=True)
+    def validate_model(cls, v) -> OpenAIModel:
+        """
+        Validate the model
+        """
+        if isinstance(v, OpenAIModel):
+            return v
+        if isinstance(v, dict):
+            return OpenAIModel(**v)
+        return OpenAIModel(value = v, mode = 'embedding')
+
+
+class EmbeddingResponse(BaseResponse):
+    data: Optional[List[EmbeddingData]]
+    data_model: Optional[Type[BaseResource]] = EmbeddingData
+
+    @lazyproperty
+    def embeddings(self) -> List:
+        """
+        Returns the text for the response
+        object
+        """
+        if self.data:
+            return [data.embedding for data in self.data] if self.data else []
+        return None
+
+
+class EmbeddingRoute(BaseRoute):
+    input_model: Optional[Type[BaseResource]] = EmbeddingObject
+    response_model: Optional[Type[BaseResource]] = EmbeddingResponse
+
+    @lazyproperty
+    def api_resource(self):
+        return 'embeddings'
+
+
---
+++ packages/async-openai/async_openai-0.0.22-py3-none-any.whl/async_openai/utils/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from async_openai.utils.logs import logger
+from async_openai.utils.helpers import (
+    is_naive,
+    total_seconds,
+    remove_trailing_slash,
+    
+)
+from async_openai.utils.config import (
+    OpenAISettings, 
+    settings
+)
+
+from async_openai.utils.tokenization import (
+    modelname_to_contextsize,
+    get_token_count,
+    get_max_tokens,
+    get_chat_tokens_count,
+    get_max_chat_tokens,
+)
