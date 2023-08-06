e7f33f1d5fb420a3fa200faac81e3e10dcad233b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:59 +0000

    Add package propeller-design-tools/0.4.1/propeller_design_tools-0.4.1-py3-none-any.whl

---
+++ packages/propeller-design-tools/propeller_design_tools-0.4.1-py3-none-any.whl/propdes/__init__.py
@@ -0,0 +1,7 @@
+from propdes.funcs import *
+from propdes.settings import set_airfoil_database, set_propeller_database, get_foil_db, get_prop_db
+from propdes.airfoil import Airfoil
+from propdes.radialstation import RadialStation
+from propdes.propeller import Propeller
+from propdes.optimizations import DutyCycleDesignOptimization
+from propdes.user_interface import InterfaceMainWindow
