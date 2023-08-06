b1eff6d63cd9cb7793aaac4c0d6cc8866e9c703b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:15 +0000

    Add package my-model/0.3/my-model-0.3.tar.gz

---
+++ packages/my-model/my-model-0.3.tar.gz/my-model-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-model',
+    version='0.3',
+    author='ls',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[
+
+        # 其他依赖
+    ],
+)
