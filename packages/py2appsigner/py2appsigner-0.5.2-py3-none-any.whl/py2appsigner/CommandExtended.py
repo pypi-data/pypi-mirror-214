3d0ec880b6eadfc62e6095378a6a677bc8a2c871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:33 +0000

    Add package py2appsigner/0.5.2/py2appsigner-0.5.2-py3-none-any.whl

---
+++ packages/py2appsigner/py2appsigner-0.5.2-py3-none-any.whl/py2appsigner/CommandExtended.py
@@ -0,0 +1,25 @@
+
+from logging import Logger
+from logging import getLogger
+
+from abc import abstractmethod
+
+from py2appsigner.CommandBasic import CommandBasic
+from py2appsigner.environment.Environment import Environment
+
+
+class CommandExtended(CommandBasic):
+    def __init__(self, environment: Environment):
+
+        super().__init__(environment=environment)
+        self.logger: Logger = getLogger(__name__)
+
+        self._extendedEnvironment: Environment = environment
+        self._pythonVersion:       str         = self._removeDecimalSeparator(self._extendedEnvironment.pythonVersion)
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+    def _removeDecimalSeparator(self, pythonVersion: str):
+        return pythonVersion.replace('.', '')
