1a64eaa3c7a3e38e3ccd7ac72c54aed16bd8e4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:29 +0000

    Add package bonus/0.6.27/bonus-0.6.27.tar.gz

---
+++ packages/bonus/bonus-0.6.27.tar.gz/bonus-0.6.27/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='bonus',
+    version='0.6.27',
+    packages=['bonus'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
