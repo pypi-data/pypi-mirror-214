4e8b97516378fe693638284421a4aa6db2e11749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:43 +0000

    Add package query-profiling-middleware/1.0.0/query-profiling-middleware-1.0.0.tar.gz

---
+++ packages/query-profiling-middleware/query-profiling-middleware-1.0.0.tar.gz/query-profiling-middleware-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='query-profiling-middleware',
+    version='1.0.0',
+    description='Middleware for profiling and logging database queries in a Python web application',
+    author='abc',
+    author_email='abc@example.com',
+    url="https://github.com/Sarvadhi-Solutions/fastapi-sql-profiler.git",
+    install_requires=[
+        "fastapi >= 0.97.0",
+        "SQLAlchemy >= 2.0.16",
+    ],
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Bug Tracking',
+    ]
+)
