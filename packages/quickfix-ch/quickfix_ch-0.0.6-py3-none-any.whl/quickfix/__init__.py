c3425f716810819b5e681cae05f0bf63fdd8b4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:32 +0000

    Add package quickfix-ch/0.0.6/quickfix_ch-0.0.6-py3-none-any.whl

---
+++ packages/quickfix-ch/quickfix_ch-0.0.6-py3-none-any.whl/quickfix/__init__.py
@@ -0,0 +1,10 @@
+from .quickfix import *
+from .quickfix41 import *
+from .quickfix42 import *
+from .quickfix43 import *
+from .quickfix44 import *
+from .quickfix50 import *
+from .quickfix50sp1 import *
+from .quickfix50sp2 import *
+from .quickfixt11 import *
+
