5f3e7bfa6c12dfcbf38f4335f0b7895a76a5bd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:15 +0000

    Add package themoviedb/0.3.4/themoviedb-0.3.4-py3-none-any.whl

---
+++ packages/themoviedb/themoviedb-0.3.4-py3-none-any.whl/themoviedb/schemas/__init__.py
@@ -0,0 +1,78 @@
+from themoviedb.schemas._enums import (
+    CreditType,
+    EpisodeGroupType,
+    ImageType,
+    MediaType,
+    SizeType,
+)
+from themoviedb.schemas._partial import (
+    PartialCollection,
+    PartialCompany,
+    PartialKeyword,
+    PartialMedia,
+    PartialMovie,
+    PartialPerson,
+    PartialTV,
+)
+from themoviedb.schemas._result import Dates, Result, ResultWithID, ResultWithPage
+from themoviedb.schemas.alternative_names import AlternativeName, AlternativeNames
+from themoviedb.schemas.alternative_titles import AlternativeTitle, AlternativeTitles
+from themoviedb.schemas.authentication import (
+    Authentication,
+    GuestAuthentication,
+    Response,
+    Session,
+    TokenAuthentication,
+)
+from themoviedb.schemas.certifications import Certification, Certifications
+from themoviedb.schemas.changes import Change
+from themoviedb.schemas.collections import Collection, Collections
+from themoviedb.schemas.companies import Companies, Company
+from themoviedb.schemas.content_ratings import ContentRating, ContentRatings
+from themoviedb.schemas.credit import Credit
+from themoviedb.schemas.credits import (
+    Cast,
+    CastCombined,
+    CastMovie,
+    CastTV,
+    Credits,
+    CreditsCombined,
+    CreditsMovie,
+    CreditsTV,
+    Crew,
+    CrewCombined,
+    CrewMovie,
+    CrewTV,
+)
+from themoviedb.schemas.episode_groups import EpisodeGroup, EpisodeGroups
+from themoviedb.schemas.episodes import Episode
+from themoviedb.schemas.external_ids import ExternalIDs
+from themoviedb.schemas.genres import Genre, Genres
+from themoviedb.schemas.images import Image, Images, TaggedImage, TaggedImages
+from themoviedb.schemas.keywords import Keyword, Keywords
+from themoviedb.schemas.list import ItemList, ItemsList
+from themoviedb.schemas.movies import Movie, Movies
+from themoviedb.schemas.multi import Multi, MultiResults, Multis
+from themoviedb.schemas.networks import Network
+from themoviedb.schemas.people import People, Person
+from themoviedb.schemas.rated import (
+    RatedEpisode,
+    RatedEpisodes,
+    RatedMovie,
+    RatedMovies,
+    RatedTV,
+    RatedTVs,
+)
+from themoviedb.schemas.regions import Region, Regions
+from themoviedb.schemas.release_date import ReleaseDate, ReleaseDates
+from themoviedb.schemas.reviews import Review, Reviews
+from themoviedb.schemas.seasons import Season
+from themoviedb.schemas.translations import Translation, Translations
+from themoviedb.schemas.tv import TV, Episodes, TVs
+from themoviedb.schemas.videos import Video, Videos
+from themoviedb.schemas.watch_providers import (
+    WatchProvider,
+    WatchProviderData,
+    WatchProviders,
+    WatchProvidersData,
+)
