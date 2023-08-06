55c3444e98d6737738b969a70bb479123f09d674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:15 +0000

    Add package vaultpass/1.0.17/vaultpass-1.0.17.tar.gz

---
+++ packages/vaultpass/vaultpass-1.0.17.tar.gz/vaultpass-1.0.17/vaultpass/__init__.py
@@ -0,0 +1,12 @@
+"""
+vaultpass
+
+VaultPass: A CyberArk Search Script. Search for accounts and retrieve their passwords in the CyberArk elevated access management application. You can only retrieve accounts that you are able to view
+"""
+
+__version__ = "1.0.17"
+__author__ = 'Jack Stewart'
+__credits__ = 'University of Michigan'
+
+./vaultpass.py
+
