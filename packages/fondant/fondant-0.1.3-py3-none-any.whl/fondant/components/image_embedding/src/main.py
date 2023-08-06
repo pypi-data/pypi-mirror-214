ce68e8a2c38e9fe79f02bb5eccd5320038991e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:41 +0000

    Add package fondant/0.1.3/fondant-0.1.3-py3-none-any.whl

---
+++ packages/fondant/fondant-0.1.3-py3-none-any.whl/fondant/components/image_embedding/src/main.py
@@ -0,0 +1,112 @@
+"""This component that embeds images using a model from the Hugging Face hub."""
+import io
+import logging
+import typing as t
+
+import dask.dataframe as dd
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+from transformers import CLIPProcessor, CLIPVisionModelWithProjection
+
+from fondant.component import DaskTransformComponent
+from fondant.logger import configure_logging
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+def process_image(image: bytes, *, processor: CLIPProcessor, device: str) -> torch.Tensor:
+    """
+    Process the image to a tensor.
+
+    Args:
+        image: The input image as a byte string.
+        processor: The processor object for transforming the image.
+        device: The device to move the transformed image to.
+    """
+    def load(img: bytes) -> Image:
+        """Load the bytestring as an image."""
+        bytes_ = io.BytesIO(img)
+        return Image.open(bytes_).convert("RGB")
+
+    def transform(img: Image) -> torch.Tensor:
+        """Transform the image to a tensor using a clip processor and move it to the specified
+        device.
+        """
+        return processor(images=img, return_tensors="pt").to(device)
+
+    return transform(load(image))["pixel_values"]
+
+
+@torch.no_grad()
+def embed_image_batch(image_batch: pd.DataFrame, *, model: CLIPVisionModelWithProjection) -> \
+        pd.Series:
+    """Embed a batch of images."""
+    input_batch = torch.cat(image_batch.tolist())
+    output_batch = model(input_batch)
+    embeddings_batch = output_batch.image_embeds.cpu().tolist()
+    return pd.Series(embeddings_batch, index=image_batch.index)
+
+
+def embed_images(
+    images: pd.Series,
+    *,
+    model: CLIPVisionModelWithProjection,
+    processor: CLIPProcessor,
+    batch_size: int,
+    device: str
+) -> pd.DataFrame:
+    """Embed a pandas series of images."""
+    images = images.apply(process_image, processor=processor, device=device)
+    results: t.List[pd.Series] = []
+    for batch in np.split(images, np.arange(batch_size, len(images), batch_size)):
+        if not batch.empty:
+            results.append(embed_image_batch(batch, model=model).T)
+    return pd.concat(results).to_frame()
+
+
+class EmbedImagesComponent(DaskTransformComponent):
+    """Component that captions images using a model from the Hugging Face hub."""
+
+    def transform(
+        self,
+        dataframe: dd.DataFrame,
+        *,
+        model_id: str,
+        batch_size: int,
+    ) -> dd.DataFrame:
+        """
+        Args:
+            dataframe: Dask dataframe
+            model_id: id of the model on the Hugging Face hub
+            batch_size: batch size to use
+        Returns:
+            Dask dataframe.
+        """
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        logger.info("device used is %s", device)
+
+        logger.info("Initialize model '%s'", model_id)
+        processor = CLIPProcessor.from_pretrained(model_id)
+        model = CLIPVisionModelWithProjection.from_pretrained(model_id)
+        model.to(device)
+        logger.info("Model initialized")
+
+        dataframe = dataframe["images_data"].map_partitions(
+            embed_images,
+            model=model,
+            processor=processor,
+            batch_size=batch_size,
+            device=device,
+            meta={0: object}
+        )
+        dataframe.columns = ["embeddings_data"]
+
+        return dataframe
+
+
+if __name__ == "__main__":
+    component = EmbedImagesComponent.from_args()
+    component.run()
---
+++ packages/fondant/fondant-0.1.3-py3-none-any.whl/fondant/components/load_from_hf_hub/src/main.py
@@ -0,0 +1,68 @@
+"""This component loads a seed dataset from the hub."""
+import io
+import logging
+
+import dask.dataframe as dd
+import numpy as np
+from PIL import Image
+
+from fondant.component import LoadComponent
+from fondant.logger import configure_logging
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+def extract_width(image_bytes):
+    # Decode image bytes to PIL Image object
+    pil_image = Image.open(io.BytesIO(image_bytes))
+    width = pil_image.size[0]
+
+    return np.int16(width)
+
+
+def extract_height(image_bytes):
+    # Decode image bytes to PIL Image object
+    pil_image = Image.open(io.BytesIO(image_bytes))
+    height = pil_image.size[1]
+
+    return np.int16(height)
+
+
+class LoadFromHubComponent(LoadComponent):
+    def load(self, *, dataset_name: str) -> dd.DataFrame:
+        """
+        Args:
+            dataset_name: name of the dataset to load.
+
+        Returns:
+            Dataset: HF dataset
+        """
+        # 1) Load data, read as Dask dataframe
+        logger.info("Loading dataset from the hub...")
+        dask_df = dd.read_parquet(f"hf://datasets/{dataset_name}")
+
+        # 2) Rename columns
+        dask_df = dask_df.rename(
+            columns={"image": "images_data", "text": "captions_data"}
+        )
+
+        # 3) Make sure images are bytes instead of dicts
+        dask_df["images_data"] = dask_df["images_data"].map(
+            lambda x: x["bytes"], meta=("bytes", bytes)
+        )
+
+        # 4) Add width and height columns
+        dask_df["images_width"] = dask_df["images_data"].map(
+            extract_width, meta=("images_width", int)
+        )
+        dask_df["images_height"] = dask_df["images_data"].map(
+            extract_height, meta=("images_height", int)
+        )
+
+        return dask_df
+
+
+if __name__ == "__main__":
+    component = LoadFromHubComponent.from_args()
+    component.run()
