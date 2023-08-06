8d92729219d89734c3fa6ade6fd67c64c6ff5f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:47 +0000

    Add package aiogram-ext/0.8.0/aiogram-ext-0.8.0.tar.gz

---
+++ packages/aiogram-ext/aiogram-ext-0.8.0.tar.gz/aiogram-ext-0.8.0/botty/loader.py
@@ -0,0 +1,26 @@
+import logging
+
+from aiohttp.web import Application
+
+from .bot import Bot
+from .config import BOT_TOKEN, MONGO, APP_URL
+from .deps import MongoStorage
+from .dispatcher import Dispatcher
+
+storage = MongoStorage(
+    db_name=MONGO.DB,
+    host=MONGO.HOST,
+    username=MONGO.USER,
+    password=MONGO.PASSWORD,
+)
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher(bot, storage)
+logger = logging.getLogger()
+app = Application()
+
+Dispatcher.set_current(dp)
+
+
+def run():
+    dp.run_server(APP_URL, app)
