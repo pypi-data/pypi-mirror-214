91933e3241c7c06ced259beb7a3a018511435b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:48 +0000

    Add package yambs/1.8.2/yambs-1.8.2-py3-none-any.whl

---
+++ packages/yambs/yambs-1.8.2-py3-none-any.whl/yambs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=4b8095d54d6068d57f06ca65c0dba991
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Yet another meta build-system."
+PKG_NAME = "yambs"
+VERSION = "1.8.2"
