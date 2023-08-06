55743b6c0be0384723470c079ac6489f6fc00828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:53 +0000

    Add package django-backoffice-dashboard-alpha/0.0.1a1/django-backoffice-dashboard-alpha-0.0.1a1.tar.gz

---
+++ packages/django-backoffice-dashboard-alpha/django-backoffice-dashboard-alpha-0.0.1a1.tar.gz/django-backoffice-dashboard-alpha-0.0.1a1/djbackoffice/__init__.py
@@ -0,0 +1,14 @@
+__title__ = 'Django Backoffice Dashboard'
+__version__ = '0.0.1.a1'
+__author__ = 'irfanpule'
+
+
+from django.utils.module_loading import autodiscover_modules
+from djbackoffice.core import backoffice
+
+
+def autodiscover():
+    """
+    autodiscover_modules all file backoffice.py to module
+    """
+    autodiscover_modules('backoffice', register_to=backoffice)
