a4f31ad8cfd68eaff4440c985e1c1e30cab3c64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:36 +0000

    Add package quickfix-ch/0.0.3/quickfix-ch-0.0.3.tar.gz

---
+++ packages/quickfix-ch/quickfix-ch-0.0.3.tar.gz/quickfix-ch-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.3"
+DESCRIPTION = "A python FIX library that supports ARM Macs"
+LONG_DESCRIPTION = "A python FIX library that supports ARM Macs"
+
+# Setting up
+setup(
+    name="quickfix-ch",
+    version=VERSION,
+    author="Coinhako (Javier)",
+    author_email="<javier.phon@coinhako.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION  ,
+    packages=find_packages(),
+    package_data={
+        "quickfix": ["_quickfix.*"]
+    },
+    include_package_data=True,
+    install_requires=[],
+    keywords=["python", "FIX", "finance"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
