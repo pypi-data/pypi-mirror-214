88ed38e75ee698f484285962055b5c3e0e03b2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:27 +0000

    Add package nombresulibreriai/0.2/nombresulibreriai-0.2.tar.gz

---
+++ packages/nombresulibreriai/nombresulibreriai-0.2.tar.gz/nombresulibreriai-0.2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import find_packages, setup
+setup(name="nombresulibreriai", ##este nombre debe ser unico y no estar indexado
+    version="0.2",
+    packages=find_packages(),
+    py_modules=["nombresulibreriai"]) ##este debe ser el nombre del archivo *.py
