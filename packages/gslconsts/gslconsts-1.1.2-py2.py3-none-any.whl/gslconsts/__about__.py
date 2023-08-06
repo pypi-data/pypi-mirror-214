41ffa0075c61188d573b8d091d9d81941c362490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:11 +0000

    Add package gslconsts/1.1.2/gslconsts-1.1.2-py2.py3-none-any.whl

---
+++ packages/gslconsts/gslconsts-1.1.2-py2.py3-none-any.whl/gslconsts/__about__.py
@@ -0,0 +1,7 @@
+__all__ = ["__title__", "__summary__", "__version__", "__author__", "__copyright__"]
+
+__title__ = "gslconsts"
+__summary__ = "Python project GNU Scientific Library (GSL) constants"
+__version__ = "1.1.2"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2023"
---
+++ packages/gslconsts/gslconsts-1.1.2-py2.py3-none-any.whl/gslconsts/__init__.py
@@ -0,0 +1,6 @@
+"""
+A package to handle GNU Scientific Library (GSL) constants for python.
+"""
+
+import gslconsts.consts
+import gslconsts.math
