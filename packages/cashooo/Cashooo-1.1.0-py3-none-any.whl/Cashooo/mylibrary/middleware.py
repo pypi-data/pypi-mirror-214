7dd082b18b7f3b3f9a2ed1887c27c5cb0f48522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:29 +0000

    Add package cashooo/1.1.0/Cashooo-1.1.0-py3-none-any.whl

---
+++ packages/cashooo/Cashooo-1.1.0-py3-none-any.whl/Cashooo/mylibrary/middleware.py
@@ -0,0 +1,61 @@
+# middleware.py
+import logging
+from django.http import JsonResponse, HttpResponseServerError
+from .custom_error import CustomError
+
+logger = logging.getLogger()
+
+
+class ErrorHandlingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        if isinstance(exception, CustomError):
+            return JsonResponse({'errors': exception.serialize_errors()}, safe=False, status=exception.status_code)
+        else:
+            return JsonResponse([{'message': 'something went wrong.'}], safe=False, status=400)
+
+
+class HealthCheckMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        if request.method == "GET":
+            if request.path == "/health/ready":
+                return self.readiness(request)
+            elif request.path == "/health/live":
+                return self.liveness(request)
+        return self.get_response(request)
+
+    def liveness(self, request):
+        """
+        Returns that the server is alive.
+        """
+        logger.info('Checking the liveness of the application')
+        return JsonResponse({"message": "Application is live."}, status=200)
+
+    def readiness(self, request):
+        # Connect to each database and do a generic standard SQL query
+        # that doesn't write any data and doesn't depend on any tables
+        # being present.
+        logger.info('Checking the readiness of the application')
+        try:
+            from django.db import connections
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute("SELECT 1;")
+                row = cursor.fetchone()
+                if row is None:
+                    return HttpResponseServerError("db: invalid response")
+        except Exception as e:
+            logger.error(str(e))
+            return HttpResponseServerError("db: cannot connect to database.")
+
+        return JsonResponse({"message": "Application is ready."}, status=200)
