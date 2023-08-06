54a2277afbd52be1a60b71a765d4ebb70fcde611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:36 +0000

    Add package tcompress/0.0.5/tcompress-0.0.5-py3-none-any.whl

---
+++ packages/tcompress/tcompress-0.0.5-py3-none-any.whl/tcompress/__init__.py
@@ -0,0 +1,6 @@
+from tcompress.installer import *
+from tcompress.functions.main import *
+from tcompress.functions.manage import *
+from tcompress.functions.random import *
+from tcompress.functions.time import *
+from tcompress.functions.image import *
