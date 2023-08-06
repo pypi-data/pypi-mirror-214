a5018b6da2b931d2eca11d59808962351d576887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:09 +0000

    Add package capybara-spotify/0.1/capybara-spotify-0.1.tar.gz

---
+++ packages/capybara-spotify/capybara-spotify-0.1.tar.gz/capybara-spotify-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="capybara-spotify",
+    version="0.1",
+    description="A package for wrapping spotify API (Personal learning)",
+    url="https://github.com/K-T-Ng/spotify-player",
+    author="KT Capybara",
+    author_email="l19so18h13k10@gmail.com",
+    packages=["capybara-spotify"],
+    zip_safe=False
+)
