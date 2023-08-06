84fb4b26c5f9466f99abbff158e1c9c863d07700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:37 +0000

    Add package blessingpdfs/2.0/blessingpdfs-2.0.tar.gz

---
+++ packages/blessingpdfs/blessingpdfs-2.0.tar.gz/blessingpdfs-2.0/blessingpdf/pdf2image.py
@@ -0,0 +1,10 @@
+"""This is simple programming for converting pdf to string"""
+
+
+def convert(path):
+    """It will convert pdf into string
+
+    parameters:
+    path (str): It is dummy.
+    """
+    print("pdf2image")
