87f4ac5245a9647775cab3455cde4bf555f1eefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:33 +0000

    Add package UnifiAPIWrapper/0.1/UnifiAPIWrapper-0.1.tar.gz

---
+++ packages/UnifiAPIWrapper/UnifiAPIWrapper-0.1.tar.gz/UnifiAPIWrapper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="UnifiAPIWrapper",
+    version="0.1",
+    description="A python module to interact with Ubiquiti's Unifi",
+    author="Preben Bargum",
+    url="https://github.com/triplesix2001",
+    license="MiT",    
+    py_modules=["UnifiAPIWrapper"],
+)
