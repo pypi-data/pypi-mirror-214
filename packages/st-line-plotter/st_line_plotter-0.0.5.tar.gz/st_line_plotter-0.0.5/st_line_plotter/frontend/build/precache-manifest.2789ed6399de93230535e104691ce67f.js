300f97e69de540f3ae8e6a6f81d1d96c72d087ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:47 +0000

    Add package st-line-plotter/0.0.5/st_line_plotter-0.0.5.tar.gz

---
+++ packages/st-line-plotter/st_line_plotter-0.0.5.tar.gz/st_line_plotter-0.0.5/st_line_plotter/frontend/build/precache-manifest.2789ed6399de93230535e104691ce67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3f9c93f7e5c1b82176904686434e71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90e4f6dc3c215611ddff",
+    "url": "./static/js/2.51f30e44.chunk.js"
+  },
+  {
+    "revision": "7ed0796b9c778b5589dc13179d486c0d",
+    "url": "./static/js/2.51f30e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c22262a9afd838e685",
+    "url": "./static/js/main.e8acecfa.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  },
+  {
+    "revision": "0d5856bc3f2ae4d7a9f6288bbd574efc",
+    "url": "./static/media/ground.0d5856bc.png"
+  }
+]);
