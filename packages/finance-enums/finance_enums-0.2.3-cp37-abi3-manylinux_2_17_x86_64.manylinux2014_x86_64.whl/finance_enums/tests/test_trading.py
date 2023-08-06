8f3a588ea30407262ad1c106a913ff74e9f0bd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:56 +0000

    Add package finance-enums/0.2.3/finance_enums-0.2.3-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/finance-enums/finance_enums-0.2.3-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/finance_enums/tests/test_trading.py
@@ -0,0 +1,18 @@
+from finance_enums import OrderType, Side, TimeInForce, OrderFlag, TradingType
+
+
+class TestCurrency:
+    def test_order_type_basic(self):
+        assert len(OrderType.members()) == 3
+
+    def test_side_basic(self):
+        assert len(Side.members()) == 2
+
+    def test_time_in_force_basic(self):
+        assert len(TimeInForce.members()) == 2
+
+    def test_order_flag_basic(self):
+        assert len(OrderFlag.members()) == 4
+
+    def test_trading_type_basic(self):
+        assert len(TradingType.members()) == 4
