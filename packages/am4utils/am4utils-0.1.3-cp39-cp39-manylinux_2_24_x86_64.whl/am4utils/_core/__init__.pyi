f9aa4c14982613c8eaed8082b90549c61169b567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:34 +0000

    Add package am4utils/0.1.3/am4utils-0.1.3-cp39-cp39-manylinux_2_24_x86_64.whl

---
+++ packages/am4utils/am4utils-0.1.3-cp39-cp39-manylinux_2_24_x86_64.whl/am4utils/_core/__init__.pyi
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import am4utils._core
+import typing
+
+__all__ = [
+    "aircraft",
+    "airport",
+    "db",
+    "demand",
+    "route",
+    "ticket",
+    "user"
+]
+
+
+__version__ = '0.1.3'
