4b5e2d33e9133c2bd80fb9cab4752e1356f616ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:37 +0000

    Add package kanglib/0.3/kanglib-0.3.tar.gz

---
+++ packages/kanglib/kanglib-0.3.tar.gz/kanglib-0.3/README.md
@@ -0,0 +1,3 @@
+welcome to Kang digital magic world:
+V0.3：calculate_weighted_value
+thank you for your trust. if you have any questions, please contact me. kangxiaowen@gmail.com
---
+++ packages/kanglib/kanglib-0.3.tar.gz/kanglib-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kanglib',
+    version='0.3',
+    packages=find_packages(),
+    description='Kang lib of Python package: calculate my math function',
+    long_description=open('README.md').read(),
+    url='https://github.com/williampolicy/code_pypi',
+    author='xiaowen kang',
+    author_email='kangxiaowen@gmail.com',
+    license='MIT',
+)
+
+
+
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="kanglib",
+#     version="0.2",
+#     packages=find_packages()
+# )
