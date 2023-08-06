b4e43cf63bc26be941c7eb067a2288c90583aab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:16 +0000

    Add package configuraptor/1.6.0/configuraptor-1.6.0.tar.gz

---
+++ packages/configuraptor/configuraptor-1.6.0.tar.gz/configuraptor-1.6.0/src/configuraptor/loaders/__init__.py
@@ -0,0 +1,38 @@
+"""
+Loads loaders based on Python version.
+"""
+
+import typing
+
+from ._types import T_config
+
+# tomlkit used for every Python version now.
+from .loaders_shared import json, toml, yaml
+
+# if sys.version_info > (3, 11):
+#     from .loaders_311 import toml
+# else:  # pragma: no cover
+#     from .loaders_310 import toml
+
+
+__all__ = ["get", "toml", "json", "yaml"]
+
+T_loader = typing.Callable[[typing.BinaryIO], T_config]
+
+LOADERS: dict[str, T_loader] = {
+    "toml": toml,
+    "json": json,
+    "yml": yaml,
+    "yaml": yaml,
+}
+
+
+def get(extension: str) -> T_loader:
+    """
+    Get the right loader for a specific extension.
+    """
+    extension = extension.removeprefix(".")
+    if loader := LOADERS.get(extension):
+        return loader
+    else:
+        raise ValueError(f"Invalid extension {extension}")
