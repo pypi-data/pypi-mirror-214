37a18a56664b723a63becea4ebebd852e849edfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:10 +0000

    Add package ReFrame-HPC/4.2.2/ReFrame_HPC-4.2.2-py3-none-any.whl

---
+++ packages/ReFrame-HPC/ReFrame_HPC-4.2.2-py3-none-any.whl/reframe/core/settings.py
@@ -0,0 +1,73 @@
+# Copyright 2016-2023 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+#
+# Generic fallback configuration
+#
+
+site_configuration = {
+    'systems': [
+        {
+            'name': 'generic',
+            'descr': 'Generic example system',
+            'hostnames': ['.*'],
+            'partitions': [
+                {
+                    'name': 'default',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin']
+                }
+            ]
+        },
+    ],
+    'environments': [
+        {
+            'name': 'builtin',
+            'cc': 'cc',
+            'cxx': '',
+            'ftn': ''
+        },
+    ],
+    'logging': [
+        {
+            'handlers$': [
+                {
+                    'type': 'stream',
+                    'name': 'stdout',
+                    'level': 'info',
+                    'format': '%(message)s'
+                },
+            ],
+            'handlers': [
+                {
+                    'type': 'file',
+                    'level': 'debug2',
+                    'format': '[%(asctime)s] %(levelname)s: %(check_info)s: %(message)s',   # noqa: E501
+                    'append': False
+                }
+            ],
+            'handlers_perflog': [
+                {
+                    'type': 'filelog',
+                    'prefix': '%(check_system)s/%(check_partition)s',
+                    'level': 'info',
+                    'format': (
+                        '%(check_job_completion_time)s,%(version)s,'
+                        '%(check_display_name)s,%(check_system)s,'
+                        '%(check_partition)s,%(check_environ)s,'
+                        '%(check_jobid)s,%(check_result)s,%(check_perfvalues)s'
+                    ),
+                    'format_perfvars': (
+                        '%(check_perf_value)s,%(check_perf_unit)s,'
+                        '%(check_perf_ref)s,%(check_perf_lower_thres)s,'
+                        '%(check_perf_upper_thres)s,'
+                    ),
+                    'append': True
+                }
+            ]
+        }
+    ]   # end of logging
+}
