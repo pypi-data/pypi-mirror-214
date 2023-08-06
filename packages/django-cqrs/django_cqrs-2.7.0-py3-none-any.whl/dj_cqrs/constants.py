a0cf1ed05ac8b2cc3e532278725019853b49a89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package django-cqrs/2.7.0/django_cqrs-2.7.0-py3-none-any.whl

---
+++ packages/django-cqrs/django_cqrs-2.7.0-py3-none-any.whl/dj_cqrs/constants.py
@@ -0,0 +1,31 @@
+#  Copyright © 2023 Ingram Micro Inc. All rights reserved.
+
+ALL_BASIC_FIELDS = '__all__'
+
+FIELDS_TRACKER_FIELD_NAME = '__fields_tracker'
+TRACKED_FIELDS_ATTR_NAME = '__tracked_fields'
+
+
+class SignalType:
+    """Type of signal that generates this event."""
+
+    SAVE = 'SAVE'
+    """The master model has been saved."""
+
+    DELETE = 'DELETE'
+    """The master model has been deleted."""
+
+    SYNC = 'SYNC'
+    """The master model needs syncronization."""
+
+
+NO_QUEUE = 'None'
+
+DEFAULT_DEAD_MESSAGE_TTL = 864000  # 10 days
+
+DEFAULT_MASTER_AUTO_UPDATE_FIELDS = False
+DEFAULT_MASTER_MESSAGE_TTL = 86400  # 1 day
+
+DEFAULT_REPLICA_MAX_RETRIES = 30
+DEFAULT_REPLICA_RETRY_DELAY = 2  # seconds
+DEFAULT_REPLICA_DELAY_QUEUE_MAX_SIZE = 1000
