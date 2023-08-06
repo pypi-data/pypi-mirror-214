e4064cb4d47ea99d829cbb100425a2aa9f1e9540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:29 +0000

    Add package dolbyio-rest-apis/3.7.1/dolbyio-rest-apis-3.7.1.tar.gz

---
+++ packages/dolbyio-rest-apis/dolbyio-rest-apis-3.7.1.tar.gz/dolbyio-rest-apis-3.7.1/client/src/dolbyio_rest_apis/communications/monitor/recordings.py
@@ -0,0 +1,247 @@
+"""
+dolbyio_rest_apis.communications.monitor.recordings
+~~~~~~~~~~~~~~~
+
+This module contains the functions to work with the monitor API related to recordings.
+"""
+
+from dolbyio_rest_apis.communications.internal.http_context import CommunicationsHttpContext
+from dolbyio_rest_apis.communications.monitor.models import GetRecordingsResponse, Recording, DolbyVoiceRecording
+from dolbyio_rest_apis.core.urls import get_comms_monitor_url
+from typing import Any, List
+
+async def get_recordings(
+        access_token: str,
+        tr_from: int=0,
+        tr_to: int=9999999999999,
+        maximum: int=100,
+        start: str=None,
+        perm: bool=False,
+    ) -> GetRecordingsResponse:
+    r"""
+    Get recording details
+
+    Get a list of the recorded conference metadata, such as duration or size of the recording.
+    This API checks only the recordings that have ended during a specific time range.
+    Recordings are indexed based on the ending time.
+
+    See: https://docs.dolby.io/communications-apis/reference/get-recordings
+
+    Args:
+        access_token: Access token to use for authentication.
+        tr_from: The beginning of the time range (in milliseconds that have elapsed since epoch).
+        tr_to: The end of the time range (in milliseconds that have elapsed since epoch).
+        maximum: The maximum number of displayed results.
+            We recommend setting the proper value of this parameter to shorten the response time.
+        start: When the results span multiple pages, use this option to navigate through pages.
+            By default, only the max number of results is displayed. To see the next results,
+            set the start parameter to the value of the next key returned in the previous response.
+        perm: When set to true, the URL is replaced with a monitor API signed URL with unlimited validity.
+
+    Returns:
+        A :class:`GetRecordingsResponse` object.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+    url = f'{get_comms_monitor_url()}/recordings'
+
+    params = {
+        'from': tr_from,
+        'to': tr_to,
+        'max': maximum,
+        'perm': str(perm).lower(),
+    }
+
+    if not start is None:
+        params['start'] = start
+
+    async with CommunicationsHttpContext() as http_context:
+        json_response = await http_context.requests_get(
+            access_token=access_token,
+            url=url,
+            params=params,
+        )
+
+    return GetRecordingsResponse(json_response)
+
+async def get_all_recordings(
+        access_token: str,
+        tr_from: int=0,
+        tr_to: int=9999999999999,
+        page_size: int=100,
+        perm: bool=False,
+    ) -> List[Recording]:
+    r"""
+    Get all recording details
+
+    Get a list of all the recorded conference metadata, such as duration or size of the recording.
+    This API checks only the recordings that have ended during a specific time range.
+    Recordings are indexed based on the ending time.
+
+    See: https://docs.dolby.io/communications-apis/reference/get-recordings
+
+    Args:
+        access_token: Access token to use for authentication.
+        tr_from: The beginning of the time range (in milliseconds that have elapsed since epoch).
+        tr_to: The end of the time range (in milliseconds that have elapsed since epoch).
+        page_size: (Optional) Number of elements to return per page.
+        perm: When set to true, the URL is replaced with a monitor API signed URL with unlimited validity.
+
+    Returns:
+        A list of :class:`Recording` objects.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+    url = f'{get_comms_monitor_url()}/recordings'
+
+    params = {
+        'from': tr_from,
+        'to': tr_to,
+        'max': page_size,
+        'perm': str(perm).lower(),
+    }
+
+    recordings = []
+
+    async with CommunicationsHttpContext() as http_context:
+        elements: List[Any] = await http_context.requests_get_all(
+            access_token=access_token,
+            url=url,
+            params=params,
+            property_name='recordings',
+            page_size=page_size
+        )
+
+    recordings: List[Recording] = []
+    for element in elements:
+        recording = Recording(element)
+        recordings.append(recording)
+
+    return recordings
+
+async def get_recording(
+        access_token: str,
+        conference_id: str,
+        tr_from: int=0,
+        tr_to: int=9999999999999,
+        page_size: int=100,
+        perm: bool=False,
+    ) -> GetRecordingsResponse:
+    r"""
+    Get the recording of a specific conference
+
+    Get a list of the recorded conference metadata, such as duration or size of the recording.
+    This API checks the recordings that have ended during a specific time range.
+    Recordings are indexed based on the ending time.
+
+    See: https://docs.dolby.io/communications-apis/reference/get-conference-recordings
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+        tr_from: The beginning of the time range (in milliseconds that have elapsed since epoch).
+        tr_to: The end of the time range (in milliseconds that have elapsed since epoch).
+        page_size: (Optional) Number of elements to return per page.
+        perm: When set to true, the URL is replaced with a monitor API signed URL with unlimited validity.
+
+    Returns:
+        A :class:`GetRecordingsResponse` object.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+    url = f'{get_comms_monitor_url()}/conferences/{conference_id}/recordings'
+
+    params = {
+        'from': tr_from,
+        'to': tr_to,
+        'max': page_size,
+        'perm': str(perm).lower(),
+    }
+
+    recordings = []
+
+    async with CommunicationsHttpContext() as http_context:
+        elements: List[Any] = await http_context.requests_get_all(
+            access_token=access_token,
+            url=url,
+            params=params,
+            property_name='recordings',
+            page_size=page_size
+        )
+
+    recordings: List[Recording] = []
+    for element in elements:
+        recording = Recording(element)
+        recordings.append(recording)
+
+    return recordings
+
+async def delete_recording(
+        access_token: str,
+        conference_id: str,
+    ) -> None:
+    r"""
+    Delete recordings
+
+    Delete all recording data related to a specific conference.
+
+    Warning: After deleting the recording, it is not possible to restore the recording data.
+
+    See: https://docs.dolby.io/communications-apis/reference/delete-conference-recordings
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    url = f'{get_comms_monitor_url()}/conferences/{conference_id}/recordings'
+
+    async with CommunicationsHttpContext() as http_context:
+        await http_context.requests_delete(
+            access_token=access_token,
+            url=url,
+        )
+
+async def get_dolby_voice_recordings(
+        access_token: str,
+        conference_id: str,
+    ) -> DolbyVoiceRecording:
+    r"""
+    Get Dolby Voice audio recordings of a conference
+
+    Get details of all Dolby Voice-based audio recordings, and associated split recordings,
+    for a given conference and download the conference recording in the MP3 audio format.
+
+    See: https://docs.dolby.io/communications-apis/reference/get-dolby-voice-audio-recordings
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+
+    Returns:
+        A :class:`DolbyVoiceRecording` object.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    url = f'{get_comms_monitor_url()}/conferences/{conference_id}/recordings/audio'
+
+    async with CommunicationsHttpContext() as http_context:
+        json_response = await http_context.requests_get(
+            access_token=access_token,
+            url=url,
+        )
+
+    return DolbyVoiceRecording(json_response)
---
+++ packages/dolbyio-rest-apis/dolbyio-rest-apis-3.7.1.tar.gz/dolbyio-rest-apis-3.7.1/client/src/dolbyio_rest_apis/communications/streaming.py
@@ -0,0 +1,147 @@
+"""
+dolbyio_rest_apis.communications.streaming
+~~~~~~~~~~~~~~~
+
+This module contains the functions to work with the streaming API.
+"""
+
+from dolbyio_rest_apis.communications.internal.http_context import CommunicationsHttpContext
+from dolbyio_rest_apis.core.helpers import add_if_not_none
+from dolbyio_rest_apis.core.urls import get_comms_url_v2
+
+async def start_rtmp(
+        access_token: str,
+        conference_id: str,
+        rtmp_url: str,
+        layout_url: str=None,
+        layout_name: str=None,
+    ) -> None:
+    r"""
+    Starts the RTMP live stream for the specified conference. Once the Dolby.io Communications APIs service starts
+    streaming to the target url, a `Stream.Rtmp.InProgress` Webhook event will be sent.
+
+    See: https://docs.dolby.io/communications-apis/reference/start-rtmp
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+        rtmp_url: The destination URI provided by the RTMP service.
+        layout_url: Overwrites the layout URL configuration.
+            This field is ignored if it is not relevant regarding recording configuration,
+            for example if live_recording set to false or if the recording is MP3 only.
+            - `null`: uses the layout URL configured in the dashboard (if no URL is set in the dashboard, then uses the Dolby.io default);
+            - `default`: uses the Dolby.io default layout;
+            - URL string: uses this layout URL
+        layout_name: Defines a name for the given layout URL, which makes layout identification
+            easier for customers especially when the layout URL is not explicit.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    payload = {
+        'uri': rtmp_url,
+    }
+    add_if_not_none(payload, 'layoutUrl', layout_url)
+    add_if_not_none(payload, 'layoutName', layout_name)
+
+    async with CommunicationsHttpContext() as http_context:
+        await http_context.requests_post(
+            access_token=access_token,
+            url=f'{get_comms_url_v2()}/conferences/mix/{conference_id}/rtmp/start',
+            payload=payload
+        )
+
+async def stop_rtmp(
+        access_token: str,
+        conference_id: str,
+    ) -> None:
+    r"""
+    Stops the RTMP stream of the specified conference.
+
+    See: https://docs.dolby.io/communications-apis/reference/stop-rtmp
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    async with CommunicationsHttpContext() as http_context:
+        await http_context.requests_post(
+            access_token=access_token,
+            url=f'{get_comms_url_v2()}/conferences/mix/{conference_id}/rtmp/stop'
+        )
+
+async def start_rts(
+        access_token: str,
+        conference_id: str,
+        stream_name: str,
+        publishing_token: str,
+        layout_url: str=None,
+        layout_name: str=None,
+    ) -> None:
+    r"""
+    Starts real-time streaming using Dolby.io Real-time Streaming services (formerly Millicast).
+
+    See: https://docs.dolby.io/communications-apis/reference/start-rts
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+        stream_name: The Dolby.io Real-time Streaming stream name to which the conference is broadcasted.
+        publishing_token: The publishing token used to identify the broadcaster.
+        layout_url: Overwrites the layout URL configuration:
+            null: uses the layout URL configured in the dashboard
+                (if no URL is set in the dashboard, then uses the Dolby.io default)
+            default: uses the Dolby.io default layout
+            URL string: uses this layout URL
+        layout_name: Defines a name for the given layout URL, which makes layout identification
+            easier for customers especially when the layout URL is not explicit.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    payload = {
+        'stream_name': stream_name,
+        'publishingToken': publishing_token,
+    }
+    add_if_not_none(payload, 'layoutUrl', layout_url)
+    add_if_not_none(payload, 'layoutName', layout_name)
+
+    async with CommunicationsHttpContext() as http_context:
+        await http_context.requests_post(
+            access_token=access_token,
+            url=f'{get_comms_url_v2()}/conferences/mix/{conference_id}/rts/start',
+            payload=payload,
+        )
+
+async def stop_rts(
+        access_token: str,
+        conference_id: str,
+    ) -> None:
+    r"""
+    Stops real-time streaming to Dolby.io Real-time Streaming services.
+
+    See: https://docs.dolby.io/communications-apis/reference/stop-rts
+
+    Args:
+        access_token: Access token to use for authentication.
+        conference_id: Identifier of the conference.
+
+    Raises:
+        HttpRequestError: If a client error one occurred.
+        HTTPError: If one occurred.
+    """
+
+    async with CommunicationsHttpContext() as http_context:
+        await http_context.requests_post(
+            access_token=access_token,
+            url=f'{get_comms_url_v2()}/conferences/mix/{conference_id}/rts/stop'
+        )
