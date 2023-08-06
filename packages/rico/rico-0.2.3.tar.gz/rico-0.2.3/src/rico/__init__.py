cf4f4bfa00e3a182e8883ba870b453555b04b67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:01 +0000

    Add package rico/0.2.3/rico-0.2.3.tar.gz

---
+++ packages/rico/rico-0.2.3.tar.gz/rico-0.2.3/src/rico/__init__.py
@@ -0,0 +1,21 @@
+# ruff: noqa: F401
+# pyright: reportUnusedImport=false
+"""Rich content to HTML as easy as `Doc(df, plot)`."""
+
+from rico._config import config_context, get_config, set_config
+from rico._container import Div, Doc
+from rico._content import (
+    Chart,
+    Code,
+    ContentBase,
+    HTML,
+    Image,
+    Markdown,
+    Obj,
+    Script,
+    Style,
+    Tag,
+    Text,
+)
+from rico._html import HTMLParser, indent_html, parse_html, serialize_html, strip_html
+from rico._version import __version__
