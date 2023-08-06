f10db9f771d088f396cff06485c3f4fb3cfc0973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package prodiapy/3.0/prodiapy-3.0-py3-none-any.whl

---
+++ packages/prodiapy/prodiapy-3.0-py3-none-any.whl/prodia/__init__.py
@@ -0,0 +1,10 @@
+from .main import Client, AsyncClient
+from .ext import get_seed, models, names, free_api, endpoint
+
+Client = Client
+AsyncClient = AsyncClient
+get_seed = get_seed
+models = models
+names = names
+free_api = free_api
+endpoint = endpoint
