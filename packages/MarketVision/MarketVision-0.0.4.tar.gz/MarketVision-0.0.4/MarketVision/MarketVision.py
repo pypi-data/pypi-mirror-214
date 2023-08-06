c8b21c4def479c7afac881ab096016a91a379fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:48 +0000

    Add package MarketVision/0.0.4/MarketVision-0.0.4.tar.gz

---
+++ packages/MarketVision/MarketVision-0.0.4.tar.gz/MarketVision-0.0.4/MarketVision/MarketVision.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+import yfinance as yf
+
+getData=yf.download
+
+def MACD(df,a,b,c):
+	df['MA Fast']=df['Close'].ewm(span=a,min_periods=a).mean()
+	df['MA Slow']=df['Close'].ewm(span=b,min_periods=b).mean()
+	df['MACD']=df['MA Fast']-df['MA Slow']
+	df['Signal']=df['MACD'].ewm(span=c,min_periods=c).mean()
+	df['Histrogram']=df['MACD']-df['Signal']
+	return df
+	
+def getTarget(df,next):
+	conditions=[df['Close']==df['Open'],df['Open']>df['Close'],df['Open']<df['Close']]
+	choices=['Equal','Decreasing','Increasing']
+	df['target']=np.select(conditions,choices) 
+	df['target']=df['target'].shift(-next)
+	return df
