dd202b77f10e9c78ba5ccc6018037e2b8f3ad4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package langconv/0.1.0/langconv-0.1.0.tar.gz

---
+++ packages/langconv/langconv-0.1.0.tar.gz/langconv-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['langconv', 'langconv.language']
+
+package_data = \
+{'': ['*'], 'langconv': ['data/zh/*']}
+
+install_requires = \
+['attrs>=23.1.0,<24.0.0', 'iso639-lang>=2.1.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'langconv',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Dianliang233',
+    'author_email': 'dianliang233@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
