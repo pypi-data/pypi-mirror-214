de7081ef48f0ad25c0cbb4cfb291f553adf5ce2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package egcst/0.0.11/egcst-0.0.11.tar.gz

---
+++ packages/egcst/egcst-0.0.11.tar.gz/egcst-0.0.11/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='egcst',
+    packages=['egcst'],
+    version='0.0.11',
+    license='MIT',
+    description='This package performs triangulation for engineering geological cross-sections',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yuryatin/egcst',
+    download_url='https://github.com/yuryatin/egcst/archive/refs/tags/v0.0.11.tar.gz',
+    keywords=['engineering', 'geological', 'cross',
+              'sections', 'mapping', 'triangulation', 'delaunay', 'step', 'stp', 'iso 10303-21'],
+    classifiers=[],
+    install_requires=['numpy', 'matplotlib', 'shapely', 'ground', 'sect']
+)
