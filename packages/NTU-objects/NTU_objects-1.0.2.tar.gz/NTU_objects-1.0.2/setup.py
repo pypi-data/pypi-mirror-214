11fcf9f896cb910fcb66fcf572924bf32b0394a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:23 +0000

    Add package NTU-objects/1.0.2/NTU_objects-1.0.2.tar.gz

---
+++ packages/NTU-objects/NTU_objects-1.0.2.tar.gz/NTU_objects-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NTU_objects',
+    version='1.0.2',
+
+    url='https://github.com/math-econ-code/mec_equil/blob/master/NTU_objects.py',
+    author='Alfred Galichon',
+    author_email='ag133@nyu.edu',
+    license="GNU",
+    python_requires=">=3",
+    py_modules=['NTU_objects'],
+)
