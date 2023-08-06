9e729ba4b71d1842a4702c804e6f09e18221f1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:29 +0000

    Add package classattr/1.0.0/classattr-1.0.0.tar.gz

---
+++ packages/classattr/classattr-1.0.0.tar.gz/classattr-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = 'classattr',
+    version = '1.0.0',
+    author = 'Thierry Dassé',
+    url = 'https://framagit.org/makeforartandscience/classattr',
+    install_requires=[],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
