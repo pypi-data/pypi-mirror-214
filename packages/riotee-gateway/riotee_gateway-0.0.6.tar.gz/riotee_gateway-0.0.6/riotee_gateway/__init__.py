9c350d7ef7b1b15d8484e353db054cc6bf605fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:42 +0000

    Add package riotee-gateway/0.0.6/riotee_gateway-0.0.6.tar.gz

---
+++ packages/riotee-gateway/riotee_gateway-0.0.6.tar.gz/riotee_gateway-0.0.6/riotee_gateway/__init__.py
@@ -0,0 +1,19 @@
+"""Riotee Gateway Python package"""
+
+__version__ = "0.0.6"
+
+import base64
+import numpy as np
+
+from riotee_gateway.client import GatewayClient
+from riotee_gateway.transceiver import Transceiver
+
+def base64_to_numpy(data_b64: bytes, dtype=np.uint8):
+    data_bin = base64.urlsafe_b64decode(data_b64)
+    data_np = np.frombuffer(data_bin, dtype)
+    if len(data_np)==1:
+        return data_np[0]
+    return data_np
+
+def base64_to_devid(dev_id_b64: bytes) -> int:
+    return int(base64_to_numpy(dev_id_b64, np.uint32))
