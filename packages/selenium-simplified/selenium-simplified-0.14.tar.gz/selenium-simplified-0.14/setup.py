40508a14d0e99f4e1f3d6787dc44814a6b12fdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:50 +0000

    Add package selenium-simplified/0.14/selenium-simplified-0.14.tar.gz

---
+++ packages/selenium-simplified/selenium-simplified-0.14.tar.gz/selenium-simplified-0.14/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium-simplified",
+    version="0.14",
+    author="rajnish kumar",
+    author_email="raajrajnish@gmail.com",
+    description="A free, open-source web automation library for the Chrome browser using Selenium Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raajrajnish/SeleniumSimplified.git",
+    packages=setuptools.find_packages(),
+    install_requires=['selenium','bs4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
