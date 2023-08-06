3791773709e5df77f050437a56458e82a48f0d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:14 +0000

    Add package pyhaloxml/2.5.3/pyhaloxml-2.5.3-cp310-abi3-win_amd64.whl

---
+++ packages/pyhaloxml/pyhaloxml-2.5.3-cp310-abi3-win_amd64.whl/pyhaloxml/__init__.py
@@ -0,0 +1,6 @@
+from .HaloXML import HaloXML, HaloXMLFile
+from .Region import Region
+from .Layer import Layer
+from .version import __version__
+
+__all__ = ("HaloXML", "Region", "Layer", "__version__")
