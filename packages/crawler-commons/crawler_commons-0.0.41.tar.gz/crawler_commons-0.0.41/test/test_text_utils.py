0193b71ba98df4c9c43f2dcb0ab60c3122454993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package crawler-commons/0.0.41/crawler_commons-0.0.41.tar.gz

---
+++ packages/crawler-commons/crawler_commons-0.0.41.tar.gz/crawler_commons-0.0.41/test/test_text_utils.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from crawlutils.text_utils import fmt_int, fmt_float
+
+
+class TestTextUtils(unittest.TestCase):
+
+    def test_fmt_int(self):
+        self.assertEqual(fmt_int(1000, use_comma=True, default="-", postfix="원"), "1,000원")
+        self.assertEqual(fmt_int(1000, use_comma=False, default="-", postfix="원"), "1000원")
+        self.assertEqual(fmt_int(1000, use_comma=False, default="-", postfix=""), "1000")
+        self.assertEqual(fmt_int(np.nan, use_comma=True, default="-", postfix="원"), "-")
+        self.assertEqual(fmt_int("-", use_comma=True, default="-", postfix="원"), "-")
+        self.assertEqual(fmt_int("-", use_comma=True, default="*", postfix="원"), "*")
+
+    def test_fmt_float(self):
+        self.assertEqual(fmt_float(1000, use_comma=True, precision=2, default="-", postfix="원"), "1,000.00원")
+        self.assertEqual(fmt_float(1000, use_comma=False, precision=0, default="-", postfix="원"), "1000원")
+        self.assertEqual(fmt_float(1000.12, use_comma=False, precision=1, default="-", postfix=""), "1000.1")
+        self.assertEqual(fmt_float(np.nan, use_comma=True, default="-", postfix="원"), "-")
+        self.assertEqual(fmt_float("-", use_comma=True, default="-", postfix="원"), "-")
+        self.assertEqual(fmt_float("-", use_comma=True, default="*", postfix="원"), "*")
