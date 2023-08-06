6630c94e9bacf8123c2dc57f4aa5d8e35f1413fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:09 +0000

    Add package spithon/0.1.0/spithon-0.1.0-py3-none-any.whl

---
+++ packages/spithon/spithon-0.1.0-py3-none-any.whl/spithon/__init__.py
@@ -0,0 +1,21 @@
+"""Init file for CLI."""
+
+MAJOR = 0
+MINOR = 1
+PATCH = 0
+
+__version__ = f"{MAJOR}.{MINOR}.{PATCH}"
+
+# Project Information
+PROJECT_AUTHOR = "Kevin Fronczak"
+PROJECT_EMAIL = "kfronczak@gmail.com"
+PROJECT_URL = "https://github.com/fronzbot/spithon"
+PROJECT_LICENSE = "A CLI for SPI/GPIO manipulation on a Raspberry Pi."
+PROJECT_DESCRIPTION = "A Raspberry Pi CLI tool for SPI and GPIO communication."
+PROJECT_CLASSIFIERS = [
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+]
