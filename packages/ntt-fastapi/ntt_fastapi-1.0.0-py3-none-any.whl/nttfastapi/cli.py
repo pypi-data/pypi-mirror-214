bf9680a914394cef96a48abd9821317cb5bb5367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:17 +0000

    Add package ntt-fastapi/1.0.0/ntt_fastapi-1.0.0-py3-none-any.whl

---
+++ packages/ntt-fastapi/ntt_fastapi-1.0.0-py3-none-any.whl/nttfastapi/cli.py
@@ -0,0 +1,33 @@
+# ------------ python package ------------- 
+import os
+# ------------ python package ------------- 
+
+# ------------ project package ------------
+from utils import *
+from constants import *
+# ------------ project package ------------
+
+
+def main():
+    parser = NTTArgparser()
+
+    parser.add_action(
+        action_name=CREATE_ACTION_NAME,
+        args={
+            NTTArgparser.PROJECT_NAME_KEY: DEFAULT_PROJECT_FOLDER,
+        },
+        action=create_folder_action,
+    )
+
+    parser.run()
+
+
+def create_folder_action(folder_name: str):
+    NTTFileSystem.create_folder(folder_name=folder_name)
+    VirtualEnv.setup(folder_name=DEFAULT_VENV_FOLDER)
+    ProjectConfiguration.setup()
+    GithubConfiguration.setup()
+
+
+if __name__ == "__main__":
+    main()
