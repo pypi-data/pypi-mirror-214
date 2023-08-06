249ccf02aa8f78b67ffaefc91e914a1391c41c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:03 +0000

    Add package meta-edc/0.2.40/meta-edc-0.2.40.tar.gz

---
+++ packages/meta-edc/meta-edc-0.2.40.tar.gz/meta-edc-0.2.40/meta_ae/pdf_reports/death_report.py
@@ -0,0 +1,7 @@
+from edc_adverse_event.pdf_reports import DeathReport as BaseDeathReport
+
+from .meta_pdf_report_mixin import MetaCrfReportMixin
+
+
+class DeathReport(MetaCrfReportMixin, BaseDeathReport):
+    pass
---
+++ packages/meta-edc/meta-edc-0.2.40.tar.gz/meta-edc-0.2.40/meta_subject/admin/malaria_test_admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from edc_microscopy.modeladmin_mixins import MalariaTestModelAdminMixin
+
+from ..admin_site import meta_subject_admin
+from ..forms import MalariaTestForm
+from ..models import MalariaTest
+from .modeladmin import CrfModelAdmin
+
+
+@admin.register(MalariaTest, site=meta_subject_admin)
+class MalariaTestAdmin(MalariaTestModelAdminMixin, CrfModelAdmin):
+    form = MalariaTestForm
