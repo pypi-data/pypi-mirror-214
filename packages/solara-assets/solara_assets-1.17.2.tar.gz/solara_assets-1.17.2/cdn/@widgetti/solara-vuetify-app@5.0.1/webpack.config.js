11debb92ee4143ce5458767c5d0e824c786346a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:11 +0000

    Add package solara-assets/1.17.2/solara_assets-1.17.2.tar.gz

---
+++ packages/solara-assets/solara_assets-1.17.2.tar.gz/solara_assets-1.17.2/cdn/@widgetti/solara-vuetify-app@5.0.1/webpack.config.js
@@ -0,0 +1,103 @@
+var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var rules = [
+    { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+    // required to load font-awesome
+    {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+    },
+    {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+    },
+    {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+    },
+    {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+    },
+    {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+    }
+];
+
+module.exports = [
+    {
+        plugins: [new MiniCssExtractPlugin()],
+        entry: './src/solara-vuetify-app.js',
+        output: {
+            filename: 'solara-vuetify-app7.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'umd',
+            publicPath: 'auto',
+        },
+        module: {
+            rules: rules
+        },
+        mode: 'production',
+    },
+    {
+        plugins: [new MiniCssExtractPlugin()],
+        entry: './src/solara-vuetify-app.js',
+        output: {
+            filename: 'solara-vuetify-app7.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'umd',
+            publicPath: 'auto',
+            devtoolModuleFilenameTemplate: `webpack://@widgetti/solara-vuetify-app`
+        },
+        module: {
+            rules: rules
+        },
+        mode: 'development',
+    },
+    {
+        plugins: [new MiniCssExtractPlugin()],
+        entry: './src/solara-vuetify-app.js',
+        output: {
+            filename: 'solara-vuetify-app8.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'umd',
+            publicPath: 'auto',
+        },
+        devtool: 'source-map',
+        module: {
+            rules: rules
+        },
+        resolve: {
+            alias: {
+                "@jupyter-widgets/base": "@jupyter-widgets/base8",
+                "@jupyter-widgets/controls": "@jupyter-widgets/controls8",
+                "@jupyter-widgets/jupyterlab-manager": "@jupyter-widgets/jupyterlab-manager8",
+            }
+        },
+        mode: 'production',
+    }, {
+        plugins: [new MiniCssExtractPlugin()],
+        entry: './src/solara-vuetify-app.js',
+        output: {
+            filename: 'solara-vuetify-app8.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'umd',
+            publicPath: 'auto',
+            devtoolModuleFilenameTemplate: `webpack://@widgetti/solara-vuetify-app`
+        },
+        devtool: 'source-map',
+        module: {
+            rules: rules
+        },
+        resolve: {
+            alias: {
+                "@jupyter-widgets/base": "@jupyter-widgets/base8",
+                "@jupyter-widgets/controls": "@jupyter-widgets/controls8",
+                "@jupyter-widgets/jupyterlab-manager": "@jupyter-widgets/jupyterlab-manager8",
+            }
+        },
+        mode: 'development',
+    },
+];
