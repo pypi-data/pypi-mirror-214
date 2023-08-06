cdeb67f4e59c662328932152cbb42f634b1a1657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:24 +0000

    Add package Geoarchpy/1.0.1/Geoarchpy-1.0.1.tar.gz

---
+++ packages/Geoarchpy/Geoarchpy-1.0.1.tar.gz/Geoarchpy-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import sys, platform
+import setuptools
+
+with open("README.md", "r") as file:
+    long_desc = file.read()
+
+setuptools.setup(
+    name='Geoarchpy',
+    version='1.0.1',
+    author="Ludovic Schorpp",
+    author_email="ludovic.schorpp@unine.ch",
+    description="Simpler geological and property models",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url = "https://github.com/randlab/ArchPy",
+    install_requires=['matplotlib',
+                      'numpy',
+                      'pyvista',
+                      'scipy',
+                      'scikit-learn',
+                      'pyyaml',
+                      'pandas',
+                      'shapely < 2.0',
+                       'rasterio',
+                       'geopandas'],  # download geone
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    #data_files=[("lib\\site-packages\\ArchPy\\libraries", ["ArchPy\\libraries\\cov_facies.dll"])],
+    license=open('LICENSE', encoding='utf-8').read()
+)
