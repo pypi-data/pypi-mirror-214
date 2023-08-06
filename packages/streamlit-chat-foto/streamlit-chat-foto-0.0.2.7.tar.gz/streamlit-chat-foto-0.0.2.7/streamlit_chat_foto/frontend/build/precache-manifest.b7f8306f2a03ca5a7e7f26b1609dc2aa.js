cf56b4068d4b33b391f91e224d2ea6df2c953cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:34 +0000

    Add package streamlit-chat-foto/0.0.2.7/streamlit-chat-foto-0.0.2.7.tar.gz

---
+++ packages/streamlit-chat-foto/streamlit-chat-foto-0.0.2.7.tar.gz/streamlit-chat-foto-0.0.2.7/streamlit_chat_foto/frontend/build/precache-manifest.b7f8306f2a03ca5a7e7f26b1609dc2aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ea0a592d9b85cd40268250f9f5a1cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d35bb161d61fcade5aae",
+    "url": "./static/js/2.01941a38.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.01941a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ff34434c16492a54ed",
+    "url": "./static/js/main.8d60b768.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
