a45727e5bb90d052a01e59d94e254bb57ac7e3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:26 +0000

    Add package qctrl-commons/18.2.0/qctrl_commons-18.2.0-py3-none-any.whl

---
+++ packages/qctrl-commons/qctrl_commons-18.2.0-py3-none-any.whl/qctrlcommons/graph.py
@@ -0,0 +1,132 @@
+# Copyright 2023 Q-CTRL. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#    https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+"""Module for custom Graph."""
+import inspect
+import logging
+from typing import (
+    Any,
+    Dict,
+)
+
+from numpydoc.docscrape import NumpyDocString
+
+from qctrlcommons.node.composite.namespace import (
+    COMPOSITE_ATTR,
+    NAMESPACE_ATTR,
+)
+from qctrlcommons.node.composite.registry import (
+    COMPOSITE_NAMESPACE_REGISTRY,
+    COMPOSITE_NODE_REGISTRY,
+)
+from qctrlcommons.node.registry import NODE_REGISTRY
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Graph:
+    """
+    Utility class for representing and building a Q-CTRL data flow graph.
+
+    You can call methods to add nodes to the graph, and use the `operations` attribute to get a
+    dictionary representation of the graph.
+    """
+
+    def __init__(self):
+        self.operations = {}
+        self._initialize_composite_namespaces()
+
+    def __getattr__(self, attr):
+        # We override getattr to stop pylint from complaining about missing attributes for methods
+        # that are added dynamically.
+        raise AttributeError(f"'Graph' object has no attribute '{attr}'.")
+
+    def _initialize_composite_namespaces(self):
+        # We initialize the node namespaces to the graph in this way since they need access
+        # to the initialized class object.
+        for _composite_namespace in COMPOSITE_NAMESPACE_REGISTRY:
+            _namespace = getattr(_composite_namespace, NAMESPACE_ATTR)
+            setattr(self, _namespace, getattr(self, _namespace)(self))
+            LOGGER.debug("adding attr %s to namespace: %s", _namespace, self)
+
+    @classmethod
+    def _from_operations(cls, operations: Dict[str, Any]):
+        """
+        Create a new graph from an existing set of operations.
+
+        Parameters
+        ----------
+        operations : dict[str, Any]
+            The initial dictionary of operations for the graph.
+        """
+        graph = cls()
+        graph.operations = operations
+        return graph
+
+
+def _extend_method(obj, method, method_name):
+    """
+    Extends the specified object by adding methods as attributes.
+
+    Parameters
+    ----------
+    obj : Any
+        The object to which the node should be added.
+    method : Any
+        Method to be added to the object.
+    method_name : str
+        Name of the method to be added.
+    """
+    if hasattr(obj, method_name):
+        raise AttributeError(f"existing attr {method_name} on namespace: {obj}")
+    LOGGER.debug("adding attr %s to namespace: %s", method_name, obj)
+    setattr(obj, method_name, method)
+
+
+def _clean_doc(doc: str) -> str:
+    """
+    Remove the graph parameter from the doc.
+    """
+    doc_obj = NumpyDocString(doc)
+
+    doc_obj["Parameters"] = [
+        item for item in doc_obj["Parameters"] if item.name != "graph"
+    ]
+
+    return str(doc_obj)
+
+
+# Set nodes to Graph.
+for node_cls in NODE_REGISTRY.as_list():
+    node = node_cls.create_graph_method()
+    _extend_method(Graph, node, node.__name__)
+
+# Set composite nodes to Graph.
+for composite_node in COMPOSITE_NODE_REGISTRY:
+    composite_node.__doc__ = _clean_doc(composite_node.__doc__)
+    _extend_method(Graph, composite_node, composite_node.__name__)
+
+# Set composite namespaces to Graph.
+# This import-time binding is only useful for building the docs.
+# The actual composite namespace object is created during the
+# Graph object initialization time.
+for composite_namespace in COMPOSITE_NAMESPACE_REGISTRY:
+    assert hasattr(composite_namespace, COMPOSITE_ATTR)
+    assert inspect.isclass(composite_namespace)
+    assert hasattr(composite_namespace, NAMESPACE_ATTR)
+
+    # Add composite node namespace.
+    _extend_method(
+        Graph,
+        composite_namespace,
+        getattr(composite_namespace, NAMESPACE_ATTR),
+    )
---
+++ packages/qctrl-commons/qctrl_commons-18.2.0-py3-none-any.whl/qctrlcommons/node/documentation.py
@@ -0,0 +1,260 @@
+# Copyright 2023 Q-CTRL. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#    https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+"""
+Tools for organizing the graph documentation.
+"""
+
+from dataclasses import dataclass
+from enum import (
+    Enum,
+    auto,
+)
+from typing import (
+    Dict,
+    List,
+)
+
+
+@dataclass
+class DocumentationSection:
+    """
+    A section of the graphs documentation.
+
+    Parameters
+    ----------
+    title : str
+        The title of the section.
+    description : str
+        The description of the section.
+    operations : str
+        The names of the operations to be listed in the section.
+    subsections : list[DocumentationSection]
+        The subsections that should be nested inside the section.
+    """
+
+    title: str
+    description: str
+    operations: List[str]
+    subsections: List["DocumentationSection"]
+
+
+class Category(Enum):
+    """
+    The different categories of graph operations.
+    """
+
+    ARITHMETIC_FUNCTIONS = auto()
+    BASIC_FUNCTIONS = auto()
+    BUILDING_PIECEWISE_CONSTANT_HAMILTONIANS = auto()
+    BUILDING_SMOOTH_HAMILTONIANS = auto()
+    COMPLEX_NUMBERS = auto()
+    DEPRECATED_OPERATIONS = auto()
+    DERIVATIVES = auto()
+    FILTERING_AND_DISCRETIZING = auto()
+    HYPERBOLIC_FUNCTIONS = auto()
+    LARGE_SYSTEMS = auto()
+    LINEAR_ALGEBRA = auto()
+    MANIPULATING_TENSORS = auto()
+    MOLMER_SORENSEN = auto()
+    OPTIMAL_AND_ROBUST_CONTROL = auto()
+    OPTIMIZATION_VARIABLES = auto()
+    OTHER_OPERATIONS = auto()
+    QUANTUM_INFORMATION = auto()
+    RANDOM_OPERATIONS = auto()
+    TIME_EVOLUTION = auto()
+    TRIGONOMETRIC_FUNCTIONS = auto()
+
+
+def create_documentation_sections(
+    operations: Dict[str, List[Category]]
+) -> List[DocumentationSection]:
+    """
+    Create sections containing the full operations documentation.
+
+    Parameters
+    ----------
+    operations : dict[str, list[Category]]
+        The operations and their associated categories.
+
+    Returns
+    -------
+    list[DocumentationSection]
+        A list of objects describing the top-level operations documentation sections. Each category
+        appears exactly once in the nested structure of sections.
+    """
+    excluded_node_names = ["getattr", "getitem"]
+    categories_map: Dict[Category, List[str]] = {category: [] for category in Category}
+
+    for name, categories in sorted(operations.items()):
+        for category in categories:
+            if name not in excluded_node_names:
+                categories_map[category].append(name)
+
+    sections = [
+        DocumentationSection(
+            "Optimization variables",
+            "When performing optimizations, you can use these operations to create the optimizable "
+            "variables that can be tuned by the optimizer in order to minimize your cost function.",
+            categories_map.pop(Category.OPTIMIZATION_VARIABLES),
+            [],
+        ),
+        DocumentationSection(
+            "Building Hamiltonians",
+            "You can use these operations to build graphs representing Hamiltonians. Hamiltonians "
+            "are represented as tensor-valued functions of time. Tensor-valued functions of time "
+            "can be either piecewise-constant (PWCs) or smooth (STFs, which stands for sampleable "
+            "tensor functions). You can manipulate PWCs and STFs either by using the operations in "
+            "this section or by applying most "
+            ":ref:`mathematical functions <Mathematical functions>`. You can also convert PWCs "
+            "into STFs by applying linear filters, and can convert STFs into PWCs by discretizing.",
+            [],
+            [
+                DocumentationSection(
+                    "Working with piecewise-constant tensor functions (PWCs)",
+                    "",
+                    categories_map.pop(
+                        Category.BUILDING_PIECEWISE_CONSTANT_HAMILTONIANS
+                    ),
+                    [],
+                ),
+                DocumentationSection(
+                    "Working with sampleable tensor functions (STFs)",
+                    "You can use these functions to create and manipulate STFs. Note that a "
+                    "powerful way to create analytic STFs is to start with "
+                    ":py:obj:`~Graph.identity_stf` and then apply a sequence of "
+                    ":ref:`mathematical functions <Mathematical functions>`.",
+                    categories_map.pop(Category.BUILDING_SMOOTH_HAMILTONIANS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Filtering and discretizing",
+                    "",
+                    categories_map.pop(Category.FILTERING_AND_DISCRETIZING),
+                    [],
+                ),
+            ],
+        ),
+        DocumentationSection(
+            "Quantum information",
+            "Use these operations to calculate common operations and metrics "
+            "from quantum information theory.",
+            categories_map.pop(Category.QUANTUM_INFORMATION),
+            [],
+        ),
+        DocumentationSection(
+            "Time evolution",
+            "You can use these operations to calculate the time evolution of your open or closed "
+            "quantum system, either for simulations or optimizations.",
+            categories_map.pop(Category.TIME_EVOLUTION),
+            [],
+        ),
+        DocumentationSection(
+            "Optimal and robust control",
+            "You can use these operations, together with the operations for creating "
+            ":ref:`optimization variables<Optimization variables>` and "
+            ":ref:`Hamiltonians <Building Hamiltonians>`, "
+            "to define convenient cost functions for optimal and robust control.",
+            categories_map.pop(Category.OPTIMAL_AND_ROBUST_CONTROL),
+            [],
+        ),
+        DocumentationSection(
+            "Large systems",
+            "You can use these operations, together with those for building "
+            ":ref:`Hamiltonians <Building Hamiltonians>`, to build graphs that efficiently handle "
+            "large quantum systems.",
+            categories_map.pop(Category.LARGE_SYSTEMS),
+            [],
+        ),
+        DocumentationSection(
+            "Mølmer–Sørensen gates",
+            "You can use these operations to efficiently model systems described by "
+            "Mølmer–Sørensen interactions.",
+            categories_map.pop(Category.MOLMER_SORENSEN),
+            [],
+        ),
+        DocumentationSection(
+            "Random operations",
+            "You can use these operations to create random quantities, which take different values "
+            "each time they are evaluated. These operations are most useful in simulations and "
+            "stochastic optimizations.",
+            categories_map.pop(Category.RANDOM_OPERATIONS),
+            [],
+        ),
+        DocumentationSection(
+            "Manipulating tensors",
+            "You can use these operations to manipulate the structures of tensors.",
+            categories_map.pop(Category.MANIPULATING_TENSORS),
+            [],
+        ),
+        DocumentationSection(
+            "Mathematical functions",
+            "You can use these operations to perform standard mathematical computations.",
+            [],
+            [
+                DocumentationSection(
+                    "Arithmetic",
+                    "",
+                    categories_map.pop(Category.ARITHMETIC_FUNCTIONS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Linear algebra",
+                    "",
+                    categories_map.pop(Category.LINEAR_ALGEBRA),
+                    [],
+                ),
+                DocumentationSection(
+                    "Basic functions",
+                    "",
+                    categories_map.pop(Category.BASIC_FUNCTIONS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Trigonometric functions",
+                    "",
+                    categories_map.pop(Category.TRIGONOMETRIC_FUNCTIONS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Hyperbolic functions",
+                    "",
+                    categories_map.pop(Category.HYPERBOLIC_FUNCTIONS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Handling complex numbers",
+                    "",
+                    categories_map.pop(Category.COMPLEX_NUMBERS),
+                    [],
+                ),
+                DocumentationSection(
+                    "Derivatives", "", categories_map.pop(Category.DERIVATIVES), []
+                ),
+            ],
+        ),
+        DocumentationSection(
+            "Other operations",
+            "You typically do not need to use these operations directly.",
+            categories_map.pop(Category.OTHER_OPERATIONS),
+            [],
+        ),
+        DocumentationSection(
+            "Deprecated operations",
+            "These operations are deprecated and will be removed in the future.",
+            categories_map.pop(Category.DEPRECATED_OPERATIONS),
+            [],
+        ),
+    ]
+    # Verify that we dealt with all the categories.
+    assert not categories_map
+    return sections
