3b337649e002daff3464e5f08cfffbcf30d0f01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:36 +0000

    Add package azdev/0.1.49/azdev-0.1.49-py3-none-any.whl

---
+++ packages/azdev/azdev-0.1.49-py3-none-any.whl/azdev/operations/linter/rules/command_coverage_rules.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
+from ..rule_decorators import CommandCoverageRule
+from ..linter import RuleError, LinterSeverity
+
+
+@CommandCoverageRule(LinterSeverity.HIGH)
+def missing_command_test_coverage(linter):
+    if not linter.get_command_test_coverage():
+        raise RuleError('Missing Command Test Coverage')
