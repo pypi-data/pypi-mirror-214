3929f4de5677004330a88b6ed511868aa5d9f138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package pygame-ui-controls/1.0.1/pygame_ui_controls-1.0.1.tar.gz

---
+++ packages/pygame-ui-controls/pygame_ui_controls-1.0.1.tar.gz/pygame_ui_controls-1.0.1/README.md
@@ -0,0 +1,19 @@
+# PygameUI
+
+This code was extracted and simplified from [TowerDefenseV2](https://github.com/ArthurLeFloch/TowerDefenseV2).
+
+# Presentation
+This code allows to create these UI elements :
+- Button
+- ImageButton
+- Slider
+- CheckBox
+- Text
+
+# Callbacks
+These elements come with callbacks :
+- Button : `on_click`
+- ImageButton : `on_click`
+- Slider : `on_value_changed` (takes value as argument)
+- CheckBox : `on_check`, `on_uncheck`, `on_action` (check or uncheck)
+- Text : nothing
---
+++ packages/pygame-ui-controls/pygame_ui_controls-1.0.1.tar.gz/pygame_ui_controls-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pygame_ui_controls',
+    version='1.0.1',
+    author='Arthur Le Floch',
+    author_email='alf.github@gmail.com',
+    description='Pygame UI controls',
+    long_description='UI controls for Pygame (Button, ImageButton, CheckBox, Slider, Text)',
+    url='https://github.com/ArthurLeFloch/PygameUI',
+    packages=[''],
+    package_dir={'': 'src'},
+    install_requires=[
+        'pygame'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
