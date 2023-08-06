ea0b1f5fdf50fbcc8ea5ed329bfedc8b4d197d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:02 +0000

    Add package turbo-docs/0.9.3/turbo_docs-0.9.3-py3-none-any.whl

---
+++ packages/turbo-docs/turbo_docs-0.9.3-py3-none-any.whl/turbo_docs/utils/cli_options.py
@@ -0,0 +1,26 @@
+import click
+from typing import Callable
+
+def copy(func: Callable) -> Callable:
+    return click.option(
+        '--copy',
+        default=False,
+        is_flag=True,
+        help='Copy the directory text to clipboard'
+    )(func)
+
+def readme(func: Callable) -> Callable:
+    return click.option(
+        '--readme',
+        default=False,
+        is_flag=True,
+        help='Generate README.md file'
+    )(func)
+
+def gpt3(func: Callable) -> Callable:
+    return click.option(
+        '--gpt3',
+        default=False,
+        is_flag=True,
+        help='Use the GPT-3.5 Turbo model'
+    )(func)
