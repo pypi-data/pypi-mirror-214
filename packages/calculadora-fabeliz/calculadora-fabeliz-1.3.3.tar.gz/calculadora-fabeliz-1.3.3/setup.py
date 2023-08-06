c5917e11428a5265c2821da21b92d2925ce1bbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:57 +0000

    Add package calculadora-fabeliz/1.3.3/calculadora-fabeliz-1.3.3.tar.gz

---
+++ packages/calculadora-fabeliz/calculadora-fabeliz-1.3.3.tar.gz/calculadora-fabeliz-1.3.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from pathlib import Path
+
+
+long_desc = Path("README.md").read_text()
+
+setuptools.setup(
+    name="calculadora-fabeliz",
+    version="1.3.3",
+    description="""Calculadora Matematica / Math Calculator""",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Fabeliz Irene Alvarez Salazar',
+    author_email='irenex2018@gmail.com',
+)
