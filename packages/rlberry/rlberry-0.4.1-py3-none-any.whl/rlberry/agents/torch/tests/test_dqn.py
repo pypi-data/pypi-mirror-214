adbe2567f8cf27e5563b7785cbf913657626c09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:00 +0000

    Add package rlberry/0.4.1/rlberry-0.4.1-py3-none-any.whl

---
+++ packages/rlberry/rlberry-0.4.1-py3-none-any.whl/rlberry/agents/torch/tests/test_dqn.py
@@ -0,0 +1,40 @@
+import pytest
+from rlberry.envs import gym_make
+from rlberry.agents.torch.dqn import DQNAgent
+from rlberry.agents.torch.utils.training import model_factory
+
+
+@pytest.mark.parametrize(
+    "use_double_dqn, use_prioritized_replay", [(False, False), (True, True)]
+)
+def test_dqn_agent(use_double_dqn, use_prioritized_replay):
+    env = gym_make("CartPole-v1")
+    agent = DQNAgent(
+        env,
+        learning_starts=5,
+        eval_interval=75,
+        train_interval=2,
+        gradient_steps=-1,
+        use_double_dqn=use_double_dqn,
+        use_prioritized_replay=use_prioritized_replay,
+    )
+    agent.fit(budget=500)
+
+    model_configs = {
+        "type": "MultiLayerPerceptron",
+        "layer_sizes": (5, 5),
+        "reshape": False,
+    }
+
+    def mlp(env, **kwargs):
+        """
+        Returns a default Q value network.
+        """
+        kwargs["in_size"] = env.observation_space.shape[0]
+        kwargs["out_size"] = env.action_space.n
+        return model_factory(**kwargs)
+
+    new_agent = DQNAgent(
+        env, q_net_constructor=mlp, q_net_kwargs=model_configs, learning_starts=100
+    )
+    new_agent.fit(budget=2000)
