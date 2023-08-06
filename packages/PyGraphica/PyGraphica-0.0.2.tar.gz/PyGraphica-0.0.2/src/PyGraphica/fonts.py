50c6790ba3c56ec5d22ffd4671de1b2bc44e02b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:06 +0000

    Add package PyGraphica/0.0.2/PyGraphica-0.0.2.tar.gz

---
+++ packages/PyGraphica/PyGraphica-0.0.2.tar.gz/PyGraphica-0.0.2/src/PyGraphica/fonts.py
@@ -0,0 +1,22 @@
+Arial = "PyGraphica/my_fonts/arial.ttf"
+#Arial font is owned by Monotype imaging
+Baskerville = "PyGraphica/my_fonts/baskerville.ttf"
+#Baskerville font was designed by JohnBaskerville in the 1750s
+BrushScript = "PyGraphica/my_fonts/brushscript.otf"
+#BrushScript font is owned by Adobe Originals
+Calibri = "PyGraphica/my_fonts/calibri.ttf"
+#Calibri is owned by Microsoft
+Courier = "PyGraphica/my_fonts/courier.ttf"
+#Courier is owned  by IBM
+Garamond = "PyGraphica/my_fonts/garamond.ttf"
+#Garamond is owned by Monotype imaging
+Helvetica  = "PyGraphica/my_fonts/helvetica.ttf"
+#Helvetica is owned by Monotype imaging
+Impact = "PyGraphica/my_fonts/impact.ttf"
+#Impact is owned by Monotype imaging
+OpenDyslexic = "PyGraphica/my_fonts/opendyslexic.otf"
+#OpenDyslexic is owned by Abelardo Gonzales
+TimesNewRoman= "PyGraphica/my_fonts/timesnewroman.ttf"
+#TimesNewRoman is owned by Monotype Imagine
+Trebuchet = "PyGraphica/my_fonts/timesnewroman.ttf"
+#Trebuchet is owned by Microsoft
