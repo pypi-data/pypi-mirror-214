1791fe90f959692bcff07bf7862c00626b3b9f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package pydantic/2.0b3/pydantic-2.0b3-py3-none-any.whl

---
+++ packages/pydantic/pydantic-2.0b3-py3-none-any.whl/pydantic/version.py
@@ -0,0 +1,33 @@
+"""The `version` module holds the version information for Pydantic."""
+__all__ = 'VERSION', 'version_info'
+
+VERSION = '2.0b3'
+"""The version of Pydantic."""
+
+
+def version_info() -> str:
+    """Return complete version information for Pydantic and its dependencies."""
+    import platform
+    import sys
+    from importlib import import_module
+    from pathlib import Path
+
+    import pydantic_core._pydantic_core as pdc
+
+    optional_deps = []
+    for p in 'devtools', 'email-validator', 'typing-extensions':
+        try:
+            import_module(p.replace('-', '_'))
+        except ImportError:  # pragma: no cover
+            continue
+        optional_deps.append(p)
+
+    info = {
+        'pydantic version': VERSION,
+        'pydantic-core version': f'{pdc.__version__} {pdc.build_profile} build profile',
+        'install path': Path(__file__).resolve().parent,
+        'python version': sys.version,
+        'platform': platform.platform(),
+        'optional deps. installed': optional_deps,
+    }
+    return '\n'.join('{:>30} {}'.format(k + ':', str(v).replace('\n', ' ')) for k, v in info.items())
