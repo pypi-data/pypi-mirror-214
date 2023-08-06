123fa149c015b902bd9a99d4cd12cd56240d7d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:33 +0000

    Add package insurance/0.0.2/insurance-0.0.2.tar.gz

---
+++ packages/insurance/insurance-0.0.2.tar.gz/insurance-0.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'insurance'
+DESCRIPTION = "A package that calculates insurance cost from a set of features: age, sex, body mass index, number of children, whether the person smokes or not, and the region in the US."
+REQUIRES_PYTHON = ">=3.6.0"
+
+long_description = DESCRIPTION
+
+# Load the package's VERSION file as a dictionary.
+about = {}
+ROOT_DIR = Path(__file__).resolve().parent
+REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
+PACKAGE_DIR = ROOT_DIR / 'my_model'
+with open(PACKAGE_DIR / "VERSION") as f:
+    _version = f.read().strip()
+    about["__version__"] = _version
+
+# What packages are required for this module to be executed?
+def list_reqs(fname="requirements.txt"):
+    with open(REQUIREMENTS_DIR / fname) as fd:
+        return fd.read().splitlines()
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=("tests",)),
+    package_data={"my_model": ["VERSION"]},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True
+)
