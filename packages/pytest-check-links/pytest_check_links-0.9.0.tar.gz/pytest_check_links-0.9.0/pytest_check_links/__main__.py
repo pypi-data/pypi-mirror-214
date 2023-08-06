e31d03c1d03913b761699163627f8613f01859ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:18 +0000

    Add package pytest-check-links/0.9.0/pytest_check_links-0.9.0.tar.gz

---
+++ packages/pytest-check-links/pytest_check_links-0.9.0.tar.gz/pytest_check_links-0.9.0/pytest_check_links/__main__.py
@@ -0,0 +1,26 @@
+"""
+This script will run pytest with our plugin enabled,
+and with the normal python plugin disabled. This means collection
+of normal tests will be disabled, unless other plugins are set
+to collect further tests. If that is the case, you can disable
+these plugins with the py.test command line option
+"-p no:<plugin-name-here>".
+"""
+# pragma: no cover
+
+import subprocess
+import sys
+
+
+def main(args=None):
+    """Main function."""
+    if args is None:
+        args = sys.argv[1:]
+
+    return subprocess.call(
+        [sys.executable, "-m", "pytest", "--check-links", "-p", "no:python", *args]  # noqa
+    )
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/pytest-check-links/pytest_check_links-0.9.0.tar.gz/pytest_check_links-0.9.0/pytest_check_links/args.py
@@ -0,0 +1,47 @@
+"""Argparse handlers."""
+import argparse
+import json
+
+
+class StoreExtensionsAction(argparse.Action):
+    """Store extensions action."""
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        """Initialize the action."""
+        if nargs is not None:
+            msg = "nargs not allowed"
+            raise ValueError(msg)
+        super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        """Evaluate the action."""
+        values = self.parse_extensions(values)
+        setattr(namespace, self.dest, values)
+
+    def parse_extensions(self, csv):
+        """Parse extensions."""
+        return {".%s" % ext.lstrip(".") for ext in csv.split(",")}
+
+
+class StoreCacheAction(argparse.Action):
+    """Build the cache session kwargs"""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        """Evalutate the action."""
+        ns_name = "check_links_cache_kwargs"
+        if not hasattr(namespace, ns_name):
+            setattr(namespace, ns_name, {})
+        dest = self.dest.replace("check_links_cache_", "")
+        kwargs = namespace.check_links_cache_kwargs
+        if dest == "name":
+            kwargs["cache_name"] = values
+        elif dest == "expire_after":
+            kwargs["expire_after"] = float(values)
+        elif dest == "backend_opt":
+            key, value = str(values).split(":", 1)
+            try:
+                kwargs[key] = json.loads(value)
+            except Exception:
+                kwargs[key] = value
+        else:
+            kwargs[dest] = values
