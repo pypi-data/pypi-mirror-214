31d432f2a47e35d9dc958c07793b062ff42e7c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:45 +0000

    Add package acore-paths/0.1.1/acore_paths-0.1.1-py3-none-any.whl

---
+++ packages/acore-paths/acore_paths-0.1.1-py3-none-any.whl/acore_paths/api.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .acore_paths import dir_home
+from .acore_paths import dir_azeroth_server
+from .acore_paths import dir_server_bin
+from .acore_paths import path_azeroth_server_authserver_conf_dist
+from .acore_paths import path_azeroth_server_worldserver_conf_dist
+from .acore_paths import path_azeroth_server_authserver_conf
+from .acore_paths import path_azeroth_server_worldserver_conf
+from .acore_paths import dir_azeroth_server_data
+from .acore_paths import dir_azeroth_server_data_dot_zip
+from .acore_paths import dir_azeroth_server_logs
+from .acore_paths import dir_modules
+from .acore_paths import path_mod_eluna_conf_dist
+from .acore_paths import path_mod_eluna_conf
+from .acore_paths import dir_server_lua_scripts
