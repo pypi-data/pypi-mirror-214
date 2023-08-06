00f0a14f767f57f04ba0b637d964af3e5b2dc821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:54 +0000

    Add package embykeeper/2.2.0/embykeeper-2.2.0-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-2.2.0-py3-none-any.whl/embykeeper/telechecker/bots/ljyy.py
@@ -0,0 +1,19 @@
+from .base import AnswerBotCheckin
+
+from pyrogram.errors import BadRequest
+
+
+class LJYYCheckin(AnswerBotCheckin):
+    name = "垃圾影音"
+    bot_username = "zckllflbot"
+    bot_captcha_len = 4
+    bot_use_history = 20
+    bot_text_ignore = "下列选项"
+
+    async def retry(self):
+        if self.message:
+            try:
+                await self.message.click()
+            except (BadRequest, TimeoutError):
+                pass
+        await super().retry()
