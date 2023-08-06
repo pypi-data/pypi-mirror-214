75822ae6a2c995ff21fcd66b230554557a65ff8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:09 +0000

    Add package ABOTP/1.0/ABOTP-1.0.tar.gz

---
+++ packages/ABOTP/ABOTP-1.0.tar.gz/ABOTP-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ABOTP',
+    version='1.0',
+    description='A protocol based on TCP sockets for transmitting arrays of byte objects.',
+    packages=['ABOTP'],
+    author_email='i@savnil.ru',
+    author='mister_svinia',
+    zip_safe=False
+)
