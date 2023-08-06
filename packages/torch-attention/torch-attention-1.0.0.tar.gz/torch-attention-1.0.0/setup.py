07644ad43f74fc670ea9f83e0633b1664f8d8117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:14 +0000

    Add package torch-attention/1.0.0/torch-attention-1.0.0.tar.gz

---
+++ packages/torch-attention/torch-attention-1.0.0.tar.gz/torch-attention-1.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="torch-attention",
+    version="1.0.0",
+    author="changzy00",
+    author_email="changzy@pku.org.cn",
+    description=(
+        "Pytorch implementation of popular Attention Mechanisms, Vision Transformers, MLP-Like models and CNNs."
+    ),
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords=(
+        "Attention"
+        "CNNs"
+        "MLPs"
+        "ViTs"
+    ),
+    license="Apache",
+    url="https://github.com/changzy00/pytorch-attention",
+    package_dir={"": "."},
+    packages=find_packages("."),
+    # entry_points={
+    #     "console_scripts": [
+    #         "huggingface-cli=huggingface_hub.commands.huggingface_cli:main"
+    #     ]
+    # },
+    python_requires=">=3.7.0",
+    # install_requires=install_requires,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
