eca7631435fa9c576d82574d0000ee6bb977c4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:46 +0000

    Add package leanit-mweb/23.6.12/leanit-mweb-23.6.12.tar.gz

---
+++ packages/leanit-mweb/leanit-mweb-23.6.12.tar.gz/leanit-mweb-23.6.12/leanit_mweb/jinja_template/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import logging
+
+from starlette.datastructures import URL
+from starlette.routing import Router
+from starlette.templating import pass_context
+
+logger = logging.getLogger(__name__)
+
+from typing import TYPE_CHECKING, Any
+from fastapi.templating import Jinja2Templates as FastAPIJinja2Templates
+
+if TYPE_CHECKING:
+    pass
+
+class Jinja2Templates(FastAPIJinja2Templates):
+
+    def _create_env(self, *args, **kwargs) -> "jinja2.Environment":
+        from leanit_mweb import config
+        cache_size = config.get("mweb", {}).get("templates", {}).get("cache_size", 1000)
+        kwargs["cache_size"] = cache_size
+
+        env = super()._create_env(*args, **kwargs)
+
+        @pass_context
+        def url(context: dict, name: str, **path_params: Any) -> URL:
+            request = context["request"]
+            router: Router = request.scope["router"]
+            url_path = router.url_path_for(name, **path_params)
+            return url_path
+
+        env.globals["url"] = url
+        return env
