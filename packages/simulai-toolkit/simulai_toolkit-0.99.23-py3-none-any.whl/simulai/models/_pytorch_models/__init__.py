7f4da31cdbacd2f2c9907ca39d92e1ca28ce08aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:01 +0000

    Add package simulai-toolkit/0.99.23/simulai_toolkit-0.99.23-py3-none-any.whl

---
+++ packages/simulai-toolkit/simulai_toolkit-0.99.23-py3-none-any.whl/simulai/models/_pytorch_models/__init__.py
@@ -0,0 +1,8 @@
+from ._autoencoder import (
+    AutoencoderCNN,
+    AutoencoderKoopman,
+    AutoencoderMLP,
+    AutoencoderVariational,
+)
+from ._deeponet import DeepONet, FlexibleDeepONet, ImprovedDeepONet, ResDeepONet
+from ._miscellaneous import ImprovedDenseNetwork, MetaModel, ModelMaker, MoEPool
