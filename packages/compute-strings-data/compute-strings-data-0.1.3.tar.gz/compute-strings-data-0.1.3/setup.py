c7a4623a927b1792e999824dabcd003734ab411e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:28 +0000

    Add package compute-strings-data/0.1.3/compute-strings-data-0.1.3.tar.gz

---
+++ packages/compute-strings-data/compute-strings-data-0.1.3.tar.gz/compute-strings-data-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="compute-strings-data",
+    version="0.1.3",
+    url="https://github.com/davitkv8/challenges/",
+    author="Davit Kvaratskhelia",
+    author_email="davit.kv8@gmail.com",
+    description="compute text based data",
+    long_description="Package includes some tools to compute string data in the shortest possible time.",
+    packages=find_packages(),
+    install_requires=[],
+)
