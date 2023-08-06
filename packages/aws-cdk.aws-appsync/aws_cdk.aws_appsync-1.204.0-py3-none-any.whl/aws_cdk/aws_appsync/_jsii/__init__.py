07aba18ec0e9ef096658493820db7c2133443706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:41 +0000

    Add package aws-cdk.aws-appsync/1.204.0/aws_cdk.aws_appsync-1.204.0-py3-none-any.whl

---
+++ packages/aws-cdk.aws-appsync/aws_cdk.aws_appsync-1.204.0-py3-none-any.whl/aws_cdk/aws_appsync/_jsii/__init__.py
@@ -0,0 +1,35 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_elasticsearch._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_opensearchservice._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/aws-appsync", "1.204.0", __name__[0:-6], "aws-appsync@1.204.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
