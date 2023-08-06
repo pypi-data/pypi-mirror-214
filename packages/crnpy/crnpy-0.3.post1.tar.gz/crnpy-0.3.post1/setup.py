71fa81363602c714ba0815c0b2cfa5894d6e3e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:24 +0000

    Add package crnpy/0.3.post1/crnpy-0.3.post1.tar.gz

---
+++ packages/crnpy/crnpy-0.3.post1.tar.gz/crnpy-0.3.post1/setup.py
@@ -0,0 +1,12 @@
+# contents of setup.py
+import setuptools
+
+setuptools.setup(
+    name="crnpy",
+    version="0.3-1",
+    packages=['crnpy'],
+    package_dir = {"": "src"},
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
