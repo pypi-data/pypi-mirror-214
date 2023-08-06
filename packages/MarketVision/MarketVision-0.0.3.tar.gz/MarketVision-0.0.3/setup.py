46bbf57912ffbbc33be6265f1737ef9b0f53a79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:26 +0000

    Add package MarketVision/0.0.3/MarketVision-0.0.3.tar.gz

---
+++ packages/MarketVision/MarketVision-0.0.3.tar.gz/MarketVision-0.0.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="MarketVision",
+    version="0.0.3",
+    author="Kai-Wei Tian",
+    author_email="m.123051427@gmail.com",
+    url = "https://github.com/m21248074/MarketVision",
+    install_requires=['yfinance'],
+    packages=["MarketVision"]
+)
