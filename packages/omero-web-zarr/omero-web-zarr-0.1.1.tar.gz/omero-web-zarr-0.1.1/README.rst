31a2076463568b5f65841171be5f85ed6de49e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:48 +0000

    Add package omero-web-zarr/0.1.1/omero-web-zarr-0.1.1.tar.gz

---
+++ packages/omero-web-zarr/omero-web-zarr-0.1.1.tar.gz/omero-web-zarr-0.1.1/README.rst
@@ -0,0 +1,88 @@
+.. image:: https://github.com/ome/omero-web-zarr/workflows/OMERO/badge.svg
+    :target: https://github.com/ome/omero-web-zarr/actions
+
+.. image:: https://badge.fury.io/py/omero-web-zarr.svg
+    :target: https://badge.fury.io/py/omero-web-zarr
+
+omero-web-zarr
+==============
+
+OMERO.web plugin for OME-Zarr.
+
+Implementation of [OME-NGFF](https://ngff.openmicroscopy.org/latest/) API with an omero-web app.
+
+This plugin supports OME-NGFF v0.1, v03, v0.4.
+For a given Image ID in OMERO, the following URL will refer to an OME-NGFF image::
+
+    https://[omero-server]/zarr/image/ID.zarr/
+
+Note: This app has not been extensively tested and should **not** be considered "production ready".
+
+Currently supports [OME-NGFF v0.3](https://ngff.openmicroscopy.org/0.3/index.html) and
+[OME-NGFF v0.4](https://ngff.openmicroscopy.org/0.4/index.html).
+
+Development
+-----------
+
+Install with::
+
+    $ pip install -e .
+
+Configuration
+
+::
+
+    $ omero config append omero.web.apps '"omero_web_zarr"'
+
+    # Allow to open-with Vizarr
+
+    $ omero config append omero.web.open_with '["web_zarr_vizarr", "omero_web_zarr_index", {"supported_objects":["image"], "label": "Vizarr", "script_url": "omero_web_zarr/openwith.js"}]'
+
+    # Open with ome-ngff-validator
+
+    $ omero config append omero.web.open_with '["web_zarr_validator", "omero_web_zarr_index", {"supported_objects":["image"], "label": "NGFF validator", "script_url": "omero_web_zarr/openwith_validator.js"}]'
+
+
+Then you will be able to access OMERO Images in OME-NGFF format v0.3 or v0.4 with a URLs like::
+
+    # base URL for Image ID
+    [omero-server]/zarr/v0.4/image/[ID].zarr
+
+    # URLS for .zattrs, .zgroup
+    [omero-server]/zarr/v0.4/image/[ID].zarr/.zattrs
+    [omero-server]/zarr/v0.4/image/[ID].zarr/.zgroup
+
+    # .zarray of the dataset at path '0'
+    [omero-server]/zarr/v0.4/image/[ID].zarr/0/.zarray
+
+    # first 3D chunk of the dataset at path '0'
+    [omero-server]/zarr/v0.4/image/[ID].zarr/0/0/0/0
+
+
+You can see this in action using the [Vizarr](https://github.com/hms-dbmi/vizarr/) viewer.
+
+This omero-web app self-hosts Vizarr to avoid CORS issues (delegating to https://hms-dbmi.github.io/vizarr/).
+
+In the webclient UI you can use the context menu to `Open With > Vizarr`, or use your Image ID and go directly to::
+
+    [omero-server]/zarr/vizarr/?source=[omero-server]/zarr/v0.4/image/[ID].zarr
+
+Testing
+-------
+
+To run integration tests (in your omero-web conda environment above) with `pytest`.
+See [OMERO testing docs](https://docs.openmicroscopy.org/latest/omero/developers/testing.html)
+for setting `ICE_CONFIG` and dependencies etc., then::
+
+    $ pytest test/integration/test_ngff.py
+
+License
+-------
+
+The application is released under the AGPL.
+
+Copyright
+---------
+
+2022-2023, The Open Microscopy Environment
+
---
+++ packages/omero-web-zarr/omero-web-zarr-0.1.1.tar.gz/omero-web-zarr-0.1.1/setup.py
@@ -0,0 +1,103 @@
+
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as test_command
+
+VERSION = '0.1.1'
+DESCRIPTION = "OMERO.web plugin for OME-Zarr"
+AUTHOR = "The Open Microscopy Team"
+LICENSE = "AGPL-3.0"
+HOMEPAGE = "https://github.com/ome/omero-web-zarr"
+
+
+class PyTest(test_command):
+
+    def initialize_options(self):
+        test_command.initialize_options(self)
+        self.test_pythonpath = None
+        self.test_string = None
+        self.test_marker = None
+        self.test_path = 'test'
+        self.test_failfast = False
+        self.test_quiet = False
+        self.test_verbose = False
+        self.test_no_capture = False
+        self.junitxml = None
+        self.pdb = False
+        self.test_ice_config = None
+
+    def finalize_options(self):
+        test_command.finalize_options(self)
+        self.test_args = [self.test_path]
+        if self.test_string is not None:
+            self.test_args.extend(['-k', self.test_string])
+        if self.test_marker is not None:
+            self.test_args.extend(['-m', self.test_marker])
+        if self.test_failfast:
+            self.test_args.extend(['-x'])
+        if self.test_verbose:
+            self.test_args.extend(['-v'])
+        if self.test_quiet:
+            self.test_args.extend(['-q'])
+        if self.junitxml is not None:
+            self.test_args.extend(['--junitxml', self.junitxml])
+        if self.pdb:
+            self.test_args.extend(['--pdb'])
+        self.test_suite = True
+        if 'ICE_CONFIG' not in os.environ:
+            os.environ['ICE_CONFIG'] = self.test_ice_config
+
+    def run_tests(self):
+        if self.test_pythonpath is not None:
+            sys.path.insert(0, self.test_pythonpath)
+
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omeroweb.settings")
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='omero-web-zarr',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read('README.rst'),
+          classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: JavaScript',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Internet :: WWW/HTTP :: WSGI',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'Topic :: Software Development :: Libraries :: '
+          'Application Frameworks',
+          'Topic :: Text Processing :: Markup :: HTML'
+      ],  # Get strings from
+          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author=AUTHOR,
+    author_email='ome-devel@lists.openmicroscopy.org.uk',
+    license=LICENSE,
+    url=HOMEPAGE,
+    download_url='%s/archive/v%s.tar.gz' % (HOMEPAGE, VERSION),  # NOQA
+    packages=find_packages(),
+    keywords=['omero', 'zarr', 'ome', 'web'],
+    install_requires=['zarr'],
+    include_package_data=True,
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+    tests_require=['pytest', 'ome_zarr'],
+)
