daa6191243014b0d0f05acd0094f45c400bd061e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:07 +0000

    Add package mdis/0.3/mdis-0.3-py3-none-any.whl

---
+++ packages/mdis/mdis-0.3-py3-none-any.whl/mdis/__init__.py
@@ -0,0 +1,16 @@
+"""Python multi-dispatcher library"""
+
+__version__ = "0.3"
+
+__author__ = "Dmitry Selyutin"
+__copyright__ = "Copyright 2023 Dmitry Selyutin"
+__license__ = "BSD"
+__maintainer__ = "Dmitry Selyutin"
+__email__ = "ghostmansd@gmail.com"
+__status__ = "Prototype"
+__all__ = [
+    "core",
+    "dispatcher",
+    "visitor",
+    "walker",
+]
