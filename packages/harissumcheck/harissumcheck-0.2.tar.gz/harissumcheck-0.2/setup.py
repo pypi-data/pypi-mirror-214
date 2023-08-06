f0f89b838b836fa0362fd640530d99a15db57045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:14 +0000

    Add package harissumcheck/0.2/harissumcheck-0.2.tar.gz

---
+++ packages/harissumcheck/harissumcheck-0.2.tar.gz/harissumcheck-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='harissumcheck',
+    version='0.2',
+    packages=['harissumcheck'],
+    install_requires=[
+    ],
+    author='Haris Kamal',
+    author_email='hariskamal43@gmail.com',
+    description='harissumcheck',
+    url='https://github.com/hariskamal43/typosquatting',
+)
