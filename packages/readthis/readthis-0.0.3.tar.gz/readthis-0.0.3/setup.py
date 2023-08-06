fd37e2048737a47ced00d95128ad6a2dd7fc2ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:01 +0000

    Add package readthis/0.0.3/readthis-0.0.3.tar.gz

---
+++ packages/readthis/readthis-0.0.3.tar.gz/readthis-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='readthis',
+    version='0.0.3',
+    description='readthis - A command line tool to read a text file aloud',
+    author='Emy Canton',
+    author_email='emy.canton@proton.me',
+    url='https://github.com/entropyqueen/readthis',
+    license='MIT',
+    scripts=['readthis'],
+    setup_requires=[
+        'setuptools',
+        'wheel',
+    ],
+    install_requires=[
+        'gTTS==2.3.2',
+        'pydub==0.25.1',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
