47b2d5640e24fb84eca9e09bad753f056cea7f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:52 +0000

    Add package stackoverflowapithingy/0.1.6/stackoverflowapithingy-0.1.6.tar.gz

---
+++ packages/stackoverflowapithingy/stackoverflowapithingy-0.1.6.tar.gz/stackoverflowapithingy-0.1.6/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="stackoverflowapithingy",
+    version="0.1.6",
+    packages=[],
+    install_requires=["requests"],
+    download_url="https://github.com/hyphen-slash/stackoverflowapithingy/archive/refs/tags/0.1.3.tar.gz"
+)
