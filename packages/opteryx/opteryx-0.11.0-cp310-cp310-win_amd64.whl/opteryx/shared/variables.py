0aa0c84b2a37c2e05c985b99b956554c15f53cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:22 +0000

    Add package opteryx/0.11.0/opteryx-0.11.0-cp310-cp310-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.11.0-cp310-cp310-win_amd64.whl/opteryx/shared/variables.py
@@ -0,0 +1,122 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+
+Owner meanings:
+    SERVER - can only be set at the server level at start up (set in config)
+    INTERNAL - the system can update this as it runs (defaulted in config)
+    USER - the user can update this value (defaulted in config)
+"""
+
+
+import typing
+from enum import Enum
+
+from opteryx.constants.character_set import CharacterSet
+from opteryx.constants.character_set import Collation
+from opteryx.exceptions import VariableNotFoundError
+
+
+class VariableOwner(int, Enum):
+    # Manually assign numbers because USER < INTERNAL < SERVER
+    SERVER = 30
+    INTERNAL = 20
+    USER = 10
+
+
+VariableSchema = typing.Tuple[typing.Type, typing.Any, VariableOwner]
+
+# fmt: off
+SYSTEM_VARIABLES_DEFAULTS: typing.Dict[str, VariableSchema] = {
+    # name: (type, default, owner, description)
+
+    # These are the MySQL set of variables - we don't use all of them
+    "auto_increment_increment": (int, 1, VariableOwner.INTERNAL),
+    "autocommit": (bool, True, VariableOwner.SERVER),
+    "character_set_client": (str, CharacterSet.utf8mb4.name, VariableOwner.SERVER),
+    "character_set_connection": (str, CharacterSet.utf8mb4.name, VariableOwner.SERVER),
+    "character_set_database": (str, CharacterSet.utf8mb4.name, VariableOwner.SERVER),
+    "character_set_results": (str, CharacterSet.utf8mb4.name, VariableOwner.SERVER),
+    "character_set_server": (str, CharacterSet.utf8mb4.name, VariableOwner.SERVER),
+    "collation_connection": (str, Collation.utf8mb4_general_ci.name, VariableOwner.SERVER),
+    "collation_database": (str, Collation.utf8mb4_general_ci.name, VariableOwner.SERVER),
+    "collation_server": (str, Collation.utf8mb4_general_ci.name, VariableOwner.SERVER),
+    "external_user": (str, "", VariableOwner.INTERNAL),
+    "init_connect": (str, "", VariableOwner.SERVER),
+    "interactive_timeout": (int, 28800, VariableOwner.SERVER),
+    "license": (str, "MIT", VariableOwner.SERVER),
+    "lower_case_table_names": (int, 0, VariableOwner.SERVER),
+    "max_allowed_packet": (int, 67108864, VariableOwner.SERVER),
+    "max_execution_time": (int, 0, VariableOwner.SERVER),
+    "net_buffer_length": (int, 16384, VariableOwner.SERVER),
+    "net_write_timeout": (int, 28800, VariableOwner.SERVER),
+    "performance_schema": (bool, False, VariableOwner.SERVER),
+    "sql_auto_is_null": (bool, False, VariableOwner.SERVER),
+    "sql_mode": (str, "ANSI", VariableOwner.SERVER),
+    "sql_select_limit": (int, None, VariableOwner.SERVER),
+    "system_time_zone": (str, "UTC", VariableOwner.SERVER),
+    "time_zone": (str, "UTC", VariableOwner.SERVER),
+    "transaction_read_only": (bool, False, VariableOwner.SERVER),
+    "transaction_isolation": (str, "READ-COMMITTED", VariableOwner.SERVER),
+    "version": (str, "8.0.29", VariableOwner.SERVER),
+    "version_comment": (str, "mesos", VariableOwner.SERVER),
+    "wait_timeout": (int, 28800, VariableOwner.SERVER),
+    "event_scheduler": (str, "OFF", VariableOwner.SERVER),
+    "default_storage_engine": (str, "opteryx", VariableOwner.SERVER),
+    "default_tmp_storage_engine": (str, "opteryx", VariableOwner.SERVER),
+
+    # these are Opteryx specific variables
+    "max_cache_evictions": (int, 32, VariableOwner.USER),
+    "max_size_single_cache_item": (int, 1024 * 1024, VariableOwner.SERVER),
+    "local_buffer_pool_size": (int, 256, VariableOwner.SERVER),
+    "disable_high_priority": (bool, False, VariableOwner.SERVER),
+    "morsel_size": (int, 64 * 1024 * 1024, VariableOwner.USER)
+}
+# fmt: on
+
+
+class SystemVariablesContainer:
+    def __init__(self, owner: VariableOwner = VariableOwner.USER):
+        self._variables = SYSTEM_VARIABLES_DEFAULTS.copy()
+        self._owner = owner
+
+    def __getitem__(self, key: str) -> typing.Any:
+        if key not in self._variables:
+            raise VariableNotFoundError(key)
+        return self._variables[key][1]
+
+    def __setitem__(self, key: str, value: typing.Any) -> None:
+        if key not in self._variables:
+            raise VariableNotFoundError(key)
+        variable_type, _, owner = self._variables[key]
+        if not isinstance(value, variable_type):
+            raise ValueError(f"Invalid type for {key}.")
+        if owner > self._owner:
+            raise PermissionError("Not enough permissions to set variable")
+        self._variables[key] = (variable_type, value, owner)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._variables
+
+    def __iter__(self):
+        return iter(self._variables)
+
+    def __len__(self):
+        return len(self._variables)
+
+    def copy(self, owner: VariableOwner = VariableOwner.USER) -> "SystemVariablesContainer":
+        return SystemVariablesContainer(owner)
+
+
+# load the base set
+SystemVariables = SystemVariablesContainer(VariableOwner.INTERNAL)
---
+++ packages/opteryx/opteryx-0.11.0-cp310-cp310-win_amd64.whl/opteryx/third_party/pyarrow_ops/join.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+from cjoin import cython_inner_join
+from cjoin import cython_left_join
+
+from .helpers import columns_to_array_denulled
+from .helpers import groupify_array
+
+
+def align_tables(t1, t2, l1, l2):
+    # Align tables
+    table = t1.take(l1)
+    # added for opteryx - deal with empty lists
+    if len(l1) == 0 or len(l2) == 0:
+        return table
+    for c in t2.column_names:
+        if c not in t1.column_names:
+            table = table.append_column(c, t2.column(c).take(l2))
+    return table
+
+
+def inner_join(left, right, left_on, right_on):
+    # Gather join columns - create arrays of the hashes of the values in the column
+    # updated for Opteryx
+    l_array, r_array = columns_to_array_denulled(left, left_on), columns_to_array_denulled(
+        right, right_on
+    )
+
+    # Groupify the join array, this generates a set of data about the array
+    # including the unique values in the array, and the sort order for the array.
+    l_distinct, lc, l_sort_idxs, lbi = groupify_array(l_array)
+    r_distinct, rc, r_sort_idxs, rbi = groupify_array(r_array)
+
+    # Create the list of unique values combining the column from the left and the right
+    # tables
+    unique, inv = np.unique(np.concatenate([l_distinct, r_distinct]), return_inverse=True)
+
+    # Align Left side
+    # the inv array the positions in the unique list of the combined left and right list,
+    # because we build this using np.concat, we know the first set of records is from the
+    # left list.
+    linv = inv[: l_distinct.shape[0]]
+    # this creates empty masks
+    lcc, lbic = np.zeros_like(unique), np.zeros_like(unique)
+    # this sets the values at the positions in linv to the count values from groupify above
+    lcc[linv] = lc
+    # this sets the values at the positions in linv to the begin indexes of the groups
+    lbic[linv] = lbi
+
+    # Align right side
+    # the inv array the positions in the unique list of the combined left and right list,
+    # because we build this using np.concat, we know the end set of records is from the
+    # right list.
+    rinv = inv[l_distinct.shape[0] :]
+    # this creates empty masks
+    rcc, rbic = np.zeros_like(unique), np.zeros_like(unique)
+    # this sets the values at the positions in rinv to the count values from groupify above
+    rcc[rinv] = rc
+    # this sets the values at the positions in rinv to the begin indexes of the groups
+    rbic[rinv] = rbi
+
+    # Perform cjoin
+    left_align, right_align = cython_inner_join(
+        l_sort_idxs.astype(np.int64),
+        r_sort_idxs.astype(np.int64),
+        lcc.astype(np.int64),
+        rcc.astype(np.int64),
+        lbic.astype(np.int64),
+        rbic.astype(np.int64),
+    )
+
+    return align_tables(left, right, left_align, right_align)
+
+
+def left_join(left, right, left_on, right_on):  # pragma: no cover - currently not called
+    # Gather join columns - create arrays of the hashes of the values in the column
+    # new for Opteryx
+    l_array, r_array = columns_to_array_denulled(left, left_on), columns_to_array_denulled(
+        right, right_on
+    )
+
+    # Groupify the join array, this generates a set of data about the array
+    # including the unique values in the array, and the sort order for the array.
+    l_distinct, lc, l_sort_idxs, lbi = groupify_array(l_array)
+    r_distinct, rc, r_sort_idxs, rbi = groupify_array(r_array)
+
+    # Create the list of unique values combining the column from the left and the right
+    # tables
+    unique, inv = np.unique(np.concatenate([l_distinct, r_distinct]), return_inverse=True)
+
+    # Align Left side
+    # the inv array the positions in the unique list of the combined left and right list,
+    # because we build this using np.concat, we know the first set of records is from the
+    # left list.
+    linv = inv[: l_distinct.shape[0]]
+    # this creates empty masks
+    lcc, lbic = np.zeros_like(unique), np.zeros_like(unique)
+    # this sets the values at the positions in linv to the count values from groupify above
+    lcc[linv] = lc
+    # this sets the values at the positions in linv to the begin indexes of the groups
+    lbic[linv] = lbi
+
+    # Align right side
+    # the inv array the positions in the unique list of the combined left and right list,
+    # because we build this using np.concat, we know the end set of records is from the
+    # right list.
+    rinv = inv[l_distinct.shape[0] :]
+    # this creates empty masks
+    rcc, rbic = np.zeros_like(unique), np.zeros_like(unique)
+    # this sets the values at the positions in rinv to the count values from groupify above
+    rcc[rinv] = rc
+    # this sets the values at the positions in rinv to the begin indexes of the groups
+    rbic[rinv] = rbi
+
+    rows = len(l_array) * len(r_array)
+    left_align, right_align = np.empty(rows, dtype=np.int64), np.empty(rows, dtype=np.int64)
+
+    # Perform cjoin
+    left_align, right_align = cython_left_join(
+        l_sort_idxs.astype(np.int64),
+        r_sort_idxs.astype(np.int64),
+        lcc.astype(np.int64),
+        rcc.astype(np.int64),
+        lbic.astype(np.int64),
+        rbic.astype(np.int64),
+    )
+
+    return align_tables(left, right, left_align, right_align)
