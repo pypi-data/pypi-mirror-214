7287e04fd49c7fd64ee635a48a85d9017002e657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:18 +0000

    Add package cashooo/1.0.0/Cashooo-1.0.0.tar.gz

---
+++ packages/cashooo/Cashooo-1.0.0.tar.gz/Cashooo-1.0.0/Cashooo/mylibrary/__init__.py
@@ -0,0 +1,4 @@
+from custom_error import CustomError, IErrorStruct
+from database_connection_error import DatabaseConnectionError
+from middleware import ErrorHandlingMiddleware, HealthCheckMiddleware
+from views import index
---
+++ packages/cashooo/Cashooo-1.0.0.tar.gz/Cashooo-1.0.0/Cashooo/mylibrary/views.py
@@ -0,0 +1,10 @@
+import logging
+from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse
+
+logger = logging.getLogger("ashura_app")
+
+def index(request):
+    logger.info("Index view requested.")
+    return JsonResponse({ "message": "OK"}, status=200)
