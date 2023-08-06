dbd65537c52d6bb0e97732929635f9e3b6705820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:41 +0000

    Add package MYPACKAGE123dpp/1.4/MYPACKAGE123dpp-1.4.tar.gz

---
+++ packages/MYPACKAGE123dpp/MYPACKAGE123dpp-1.4.tar.gz/MYPACKAGE123dpp-1.4/ChatAPI/__init__.py
@@ -0,0 +1,31 @@
+import requests
+
+class ChatAPI:
+    def __init__(self, api_key):
+        self.url = "https://openai80.p.rapidapi.com/chat/completions"
+        self.headers = {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": api_key,
+            "X-RapidAPI-Host": "openai80.p.rapidapi.com"
+        }
+
+    def send_message(self, user_input):
+        payload = {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": user_input
+                }
+            ]
+        }
+
+        response = requests.post(self.url, json=payload, headers=self.headers)
+        data = response.json()
+        content = data["choices"][0]["message"]["content"]
+        return content
+
+
+
+api_key = "d9050d5653msh75d645c395b0db8p19a102jsn44253b504e66"
+
---
+++ packages/MYPACKAGE123dpp/MYPACKAGE123dpp-1.4.tar.gz/MYPACKAGE123dpp-1.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='MYPACKAGE123dpp',
+      version='1.4',
+      author='Aleksy Mięki',
+      author_email='259183@student.pwr.edu.pl',
+      packages=find_packages(),
+      )
