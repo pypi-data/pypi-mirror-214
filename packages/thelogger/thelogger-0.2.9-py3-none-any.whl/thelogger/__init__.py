926fe6bb99e468949f8826fa9be769954d285cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:48 +0000

    Add package thelogger/0.2.9/thelogger-0.2.9-py3-none-any.whl

---
+++ packages/thelogger/thelogger-0.2.9-py3-none-any.whl/thelogger/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 04:36:47 2019
+
+Easy logging, timing and email notifications of code execution.
+
+@author: tommy
+"""
+
+from .logger import logger, log, lg
+from .notify import notify
+from .dirty_timer import beg, end
+
+from ._ver import __version__
