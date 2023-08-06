d0572dcfe611326505e8fb68f5cf993591e7d0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:26 +0000

    Add package pipeline-data/1.2.3/pipeline-data-1.2.3.tar.gz

---
+++ packages/pipeline-data/pipeline-data-1.2.3.tar.gz/pipeline-data-1.2.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+# @Time      :2022/10/21 14:26
+# @Author    :huangkewei
+
+import setuptools
+
+setuptools.setup(
+    name='pipeline-data',
+    version='1.2.3',
+    author='zhongbiao',
+    description='数据同步',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'sqlalchemy~=1.4.0',
+        'pottery==3.0.0',
+        'werkzeug',
+        'regex==2022.7.25',
+        'cchardet==2.1.7',
+        'pymysql==0.9.3'
+    ]
+)
+
+# print(setuptools.find_packages())
+
+
