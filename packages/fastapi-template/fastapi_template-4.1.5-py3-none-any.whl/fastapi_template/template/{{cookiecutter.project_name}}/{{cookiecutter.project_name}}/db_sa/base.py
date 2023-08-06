3b9c62c4d7f0c133ad1be0ed2e74234265aedcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:20 +0000

    Add package fastapi-template/4.1.5/fastapi_template-4.1.5-py3-none-any.whl

---
+++ packages/fastapi-template/fastapi_template-4.1.5-py3-none-any.whl/fastapi_template/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/db_sa/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import  DeclarativeBase
+
+from {{cookiecutter.project_name}}.db.meta import meta
+
+
+class Base(DeclarativeBase):
+    """Base for all models."""
+
+    metadata = meta
+
