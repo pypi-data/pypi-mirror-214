54e77c66ac96ddd6822eb08ffeab67b9b37acb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:42 +0000

    Add package tator/1.0.5/tator-1.0.5.tar.gz

---
+++ packages/tator/tator-1.0.5.tar.gz/tator-1.0.5/test/test_local_transcode.py
@@ -0,0 +1,89 @@
+import time
+import subprocess
+
+import tator
+import uuid
+import json
+import subprocess
+
+def _get_stream_info(path):
+    cmd = [
+        "ffprobe",
+        "-v","error",
+        "-show_entries", "stream",
+        "-print_format", "json",
+        "-select_streams", "v",
+        path,
+    ]
+    p = subprocess.run(cmd, stdout=subprocess.PIPE)
+    return json.loads(p.stdout.decode())['streams'][0]
+
+def test_local_transcode(host, token, project, video_type, video_file):
+    unique_name = f"{uuid.uuid4()}.mp4"
+    cmd = [
+        'python3', '-m', 'tator.transcode', video_file,
+        '--host', host,
+        '--token', token,
+        '--project', str(project),
+        '--type', str(video_type),
+        '--section', 'Locally transcoded',
+        '--name', unique_name
+    ]
+    subprocess.run(cmd, check=True)
+    subprocess.run(cmd, check=True)
+    api = tator.get_api(host, token)
+    media_obj = api.get_media_list(project, name=unique_name, presigned=3600)[0]
+
+    stream_info = _get_stream_info(media_obj.media_files.archival[0].path)
+    assert stream_info['pix_fmt'] == 'yuv420p'
+    stream_info = _get_stream_info(media_obj.media_files.streaming[0].path)
+    assert stream_info['pix_fmt'] == 'yuv420p'
+
+def test_local_transcode_yuv444p(host, token, project, yuv444p_video_type, video_file):
+    unique_name = f"{uuid.uuid4()}.mp4"
+    cmd = [
+        'python3', '-m', 'tator.transcode', video_file,
+        '--host', host,
+        '--token', token,
+        '--project', str(project),
+        '--type', str(yuv444p_video_type),
+        '--section', 'Locally transcoded (yuv444p)',
+        '--name', unique_name
+    ]
+    subprocess.run(cmd, check=True)
+    api = tator.get_api(host, token)
+    media_obj = api.get_media_list(project, name=unique_name, presigned=3600)[0]
+
+    stream_info = _get_stream_info(media_obj.media_files.archival[0].path)
+    assert stream_info['pix_fmt'] == 'yuv444p'
+    stream_info = _get_stream_info(media_obj.media_files.streaming[0].path)
+    assert stream_info['pix_fmt'] == 'yuv444p'
+
+def test_bad_file(host, token, project, video_type, image_file):
+    failed = False
+    try:
+        cmd = [
+            'python3', '-m', 'tator.transcode', image_file,
+            '--host', host,
+            '--token', token,
+            '--project', str(project),
+            '--type', str(video_type),
+            '--section', 'Bad transcodes',
+        ]
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as cpe:
+        failed = True
+    assert(failed)
+    time.sleep(2)
+    # Make sure media file is gone.
+    api = tator.get_api(host, token)
+    sections = api.get_section_list(project)
+    section_obj = [s for s in sections if s.name == 'Bad transcodes'][0]
+
+    medias = api.get_media_list(project, attribute=[f'tator_user_sections::{section_obj.tator_user_sections}'])
+    print(medias)
+    assert(len(medias) == 0)
+
+    medias = api.get_media_list(project, section=section_obj.id)
+    print(medias)
+    assert(len(medias) == 0)
---
+++ packages/tator/tator-1.0.5.tar.gz/tator-1.0.5/test/test_media.py
@@ -0,0 +1,178 @@
+from datetime import datetime
+import os
+import tempfile
+from time import sleep
+from uuid import uuid1
+
+import tator
+
+def test_get_file(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        outpath = os.path.join(temp_dir, "video.mp4")
+        for progress in tator.download_media(tator_api, video_obj, outpath):
+            print(f"Video download progress: {progress}%")
+        assert(os.path.exists(outpath))
+
+def test_get_audio(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+
+    audio = video_obj.media_files.audio
+    assert len(audio) > 0
+    assert audio[0].codec == 'aac'
+
+def test_get_by_id(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+    other_obj = tator_api.get_media_list_by_id(project, {'ids': [video]})[0]
+    assert video_obj.id == other_obj.id
+    count = tator_api.get_media_count_by_id(project, {'ids': [video]})
+    assert(count == 1)
+
+def test_archive(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+    video_obj = tator_api.get_media(video)
+
+    # Test default value of `archived` is "live"
+    assert video_obj.archive_state == "live"
+
+    # Test default `get_media_list` filters on `archive_lifecycle == "all"`
+    response = tator_api.get_media_list(project, media_id=[video])
+    assert len(response) == 1
+    assert response[0].archive_state == "live"
+
+    # Test `get_media_list` with `archive_lifecycle="archived"` doesn't return "live" objects
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="archived")
+    assert len(response) == 0
+
+    # Test returning subset of media that is live
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="live")
+    assert len(response) == 1
+    assert response[0].archive_state == "live"
+
+    # Test returning subset of media that has any `archive_lifecycle` state
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="all")
+    assert len(response) == 1
+    assert response[0].archive_state == "live"
+
+    # Mark the video to archive
+    tator_api.update_media_list(project, {"archive_state": "to_archive", "ids": [video]})
+
+    # Wait for update to propagate to ES
+    sleep(2)
+
+    # Test default `get_media_list` filters on `archive_lifecycle == "all"`
+    response = tator_api.get_media_list(project, media_id=[video])
+    assert len(response) == 1
+    assert response[0].archive_state == "to_archive"
+
+    # Test `get_media_list` with `archive_lifecycle="archived"` returns archived objects
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="archived")
+    assert len(response) == 1
+    assert response[0].archive_state == "to_archive"
+
+    # Test returning subset of media that is live
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="live")
+    assert len(response) == 0
+
+    # Test returning subset of media that has any `archive_state` state
+    response = tator_api.get_media_list(project, media_id=[video], archive_lifecycle="all")
+    assert len(response) == 1
+    assert response[0].archive_state == "to_archive"
+
+    # Additional attempts to set the state to `to_archive` will not change anything
+    tator_api.update_media(video, {"archive_state": "to_archive"})
+    video_obj = tator_api.get_media(video)
+    assert video_obj.archive_state == "to_archive"
+
+    # Setting the `archive_state` to `to_live` when a media is in the state `to_archive` should
+    # result in the media's state changing to `live`
+    tator_api.update_media(video, {"archive_state": "to_live"})
+    video_obj = tator_api.get_media(video)
+    assert video_obj.archive_state == "live"
+
+def test_section(host, token, project, video):
+    tator_api = tator.get_api(host, token)
+
+    # Create test section
+    section_spec = {"name": "Test Section", "tator_user_sections": str(uuid1())}
+    response = tator_api.create_section(project, section_spec=section_spec)
+
+    # Update media `tator_user_sections` attribute
+    update_spec = {"attributes": {"tator_user_sections": section_spec["tator_user_sections"]}}
+    response = tator_api.update_media(
+        video, update_spec
+    )
+    media = tator_api.get_media(video)
+    assert media.attributes["tator_user_sections"] == section_spec["tator_user_sections"]
+
+    # Unset media `tator_user_sections` attribute
+    response = tator_api.update_media(
+        video, {"attributes": {"tator_user_sections": ""}}
+    )
+    media = tator_api.get_media(video)
+    assert media.attributes["tator_user_sections"] == ""
+
+    # Update media `tator_user_sections` attribute with a bulk update
+    response = tator_api.update_media_list(project, media_id=[video], media_bulk_update=update_spec)
+    media = tator_api.get_media(video)
+    assert media.attributes["tator_user_sections"] == section_spec["tator_user_sections"]
+
+    # Unset media `tator_user_sections` attribute with a bulk update
+    response = tator_api.update_media_list(
+        project,
+        media_id=[video],
+        media_bulk_update={"attributes": {"tator_user_sections": ""}},
+    )
+    media = tator_api.get_media(video)
+    assert media.attributes["tator_user_sections"] == ""
+
+
+def test_import_multiple_images(host, token, project, image_type):
+    tator_api = tator.get_api(host, token)
+    image_url = "https://www.gstatic.com/webp/gallery/1.jpg"
+    n_images = 5
+    uuid = str(uuid1())
+
+    project_media_count = tator_api.get_media_count(project, type=image_type)
+
+    media_specs = [
+        {
+            "type": image_type,
+            "section": "Multiple image upload",
+            "name": f"{uuid}_{idx}.jpg",
+            "url": image_url,
+            "md5": "",
+        }
+        for idx in range(n_images)
+    ]
+
+    start = datetime.now()
+    response = tator_api.create_media_list(project, body=media_specs)
+    duration = (datetime.now() - start).total_seconds()
+    assert duration < 5
+
+    assert str(len(media_specs)) in response.message
+
+    new_project_media_count = -1
+    desired_project_media_count = project_media_count + len(media_specs)
+    for _ in range(30):
+        sleep(1)
+        new_project_media_count = tator_api.get_media_count(project, type=image_type)
+        if new_project_media_count == desired_project_media_count:
+            break
+    assert new_project_media_count == desired_project_media_count
+    for _ in range(30):
+        sleep(1)
+        media_list = tator_api.get_media_list(project, type=image_type)
+
+        n_with_media_files = 0
+        for media in media_list:
+            if media.name.startswith(uuid) and media.media_files is not None:
+                n_with_media_files += 1
+        if n_with_media_files == n_images:
+            break
+    assert n_with_media_files == n_images
