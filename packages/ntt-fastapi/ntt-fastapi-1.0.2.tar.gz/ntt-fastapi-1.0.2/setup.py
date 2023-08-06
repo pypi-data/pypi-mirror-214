f18f2c21e2f341fed17036ae7c042fca1e8eb8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:34 +0000

    Add package ntt-fastapi/1.0.2/ntt-fastapi-1.0.2.tar.gz

---
+++ packages/ntt-fastapi/ntt-fastapi-1.0.2.tar.gz/ntt-fastapi-1.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='ntt-fastapi',
+    version='1.0.2',
+    author='threezinedine',
+    author_email='threezinedine@email.com',
+    description='The Framework which helps developers work with FastAPI easier',
+    packages=['nttfastapi'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/threezinedine/ntt-fastapi',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'nttfastapi = nttfastapi.cli'
+        ],
+    },
+)
