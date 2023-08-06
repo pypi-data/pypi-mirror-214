043b2a2ffe08ab13bd7142bd7bb7242cccc808eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:59 +0000

    Add package nuovoLIRA/0.5.0/nuovoLIRA-0.5.0.tar.gz

---
+++ packages/nuovoLIRA/nuovoLIRA-0.5.0.tar.gz/nuovoLIRA-0.5.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text() 
+
+setup(
+    name='nuovoLIRA',
+    packages=find_packages(),
+    version='0.5.0',
+    description='A Bayesian procedure to delineate the boundary of an extended astronomical object',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Brendan Martin',
+    license='MIT',
+)
