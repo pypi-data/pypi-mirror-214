95d30493dcb5623dcbe7542f94b2889d00113801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:23 +0000

    Add package calculadora-fabeliz/0.0.3/calculadora-fabeliz-0.0.3.tar.gz

---
+++ packages/calculadora-fabeliz/calculadora-fabeliz-0.0.3.tar.gz/calculadora-fabeliz-0.0.3/calculadorafabeliz/basicOperations.py
@@ -0,0 +1,54 @@
+"""
+Esta es un modulo que contiene las operaciones basicas de la calculadora
+"""
+
+
+class calculadora:
+    """
+    Clase Calculadora
+    """
+
+    def suma(self, num1, num2):
+        """
+        Metodo para retornar al suma
+
+        Parametros: num1 Valor float o int
+                    num2 Valor float o int
+
+        Return: Valor de la sumatoria floar o int            
+        """
+        return num1 + num2
+
+    def resta(self, num1, num2):
+        """
+        Metodo para retornar la resta
+
+        Parametros: num1 Valor float o int
+                    num2 Valor float o int
+
+        Return: Valor de la sumatoria floar o int            
+        """
+        return num1 - num2
+
+    def multiplicacion(self, num1, num2):
+        """
+        Metodo para retornar la multiplicacion
+
+        Parametros: num1 Valor float o int
+                    num2 Valor float o int
+
+        Return: Valor de la sumatoria floar o int            
+        """
+        return num1 * num2
+
+    def division(self, num1, num2):
+        """
+        Metodo para retornar la multiplicacion
+
+        Parametros: num1 Valor float o int
+                    num2 Valor float o int
+
+        Return: Valor de la sumatoria floar o int            
+        """
+        return num1 / num2
+    
