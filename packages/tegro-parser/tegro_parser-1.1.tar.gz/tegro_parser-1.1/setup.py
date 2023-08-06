bad83e3a365fb8311fd54892adbc12657e066e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:50 +0000

    Add package tegro-parser/1.1/tegro_parser-1.1.tar.gz

---
+++ packages/tegro-parser/tegro_parser-1.1.tar.gz/tegro_parser-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tegro_parser',
+    version='1.1',
+    description='TegroParser - module for working with tegro.money API',
+    long_description='TegroParser - module for working with https://tegro.money API \nMade by @PyDevNik',
+    packages=['tegro_parser'],
+    install_requires=[
+        'requests', 'typing', 'pydantic'
+    ],
+)
---
+++ packages/tegro-parser/tegro_parser-1.1.tar.gz/tegro_parser-1.1/tegro_parser/__init__.py
@@ -0,0 +1,2 @@
+from .tegro_parser import TegroMoneyParser
+from .tegro_config import TegroConfig
