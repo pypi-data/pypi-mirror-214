2677178dc34034f2af7a104b13dfecb3182dff2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:22 +0000

    Add package joes-giant-toolbox/0.3.1/joes_giant_toolbox-0.3.1.tar.gz

---
+++ packages/joes-giant-toolbox/joes_giant_toolbox-0.3.1.tar.gz/joes_giant_toolbox-0.3.1/joes_giant_toolbox/text.py
@@ -0,0 +1,14 @@
+"""Tools for working with text data
+
+Available modules: 
+    - RegexRulesClassifier
+    - longest_sentence_subsequence_plagiarism_detector
+    - StringCleaner
+"""
+
+from joes_giant_toolbox.all.regex_rules_classifier import RegexRulesClassifier
+
+from joes_giant_toolbox.all.longest_sentence_subsequence_plagiarism_detector import (
+    longest_sentence_subsequence_plagiarism_detector,
+)
+from joes_giant_toolbox.all.string_cleaner import StringCleaner
