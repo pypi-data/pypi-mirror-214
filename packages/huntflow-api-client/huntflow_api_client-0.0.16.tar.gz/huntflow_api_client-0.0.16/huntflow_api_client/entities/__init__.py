0b343c0614296e08c91b2ffc62fcc8504d4b6e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:30 +0000

    Add package huntflow-api-client/0.0.16/huntflow_api_client-0.0.16.tar.gz

---
+++ packages/huntflow-api-client/huntflow_api_client-0.0.16.tar.gz/huntflow_api_client-0.0.16/huntflow_api_client/entities/__init__.py
@@ -0,0 +1,47 @@
+from huntflow_api_client.entities.account_offers import AccountOffer
+from huntflow_api_client.entities.account_vacancy_request import AccountVacancyRequest
+from huntflow_api_client.entities.accounts import Account
+from huntflow_api_client.entities.applicant_logs import ApplicantLog
+from huntflow_api_client.entities.applicant_offers import ApplicantOffer
+from huntflow_api_client.entities.applicant_on_vacancy import ApplicantOnVacancy
+from huntflow_api_client.entities.applicant_on_vacancy_status import ApplicantOnVacancyStatus
+from huntflow_api_client.entities.applicants import Applicant
+from huntflow_api_client.entities.coworkers import Coworker
+from huntflow_api_client.entities.delayed_tasks import DelayedTask
+from huntflow_api_client.entities.dictionaries import Dictionary
+from huntflow_api_client.entities.divisions import AccountDivision
+from huntflow_api_client.entities.email_templates import MailTemplate
+from huntflow_api_client.entities.production_calendars import ProductionCalendar
+from huntflow_api_client.entities.questionary import ApplicantsQuestionary
+from huntflow_api_client.entities.rejection_reason import RejectionReason
+from huntflow_api_client.entities.resume import Resume
+from huntflow_api_client.entities.tags import AccountTag
+from huntflow_api_client.entities.users import User
+from huntflow_api_client.entities.vacancies import Vacancy
+from huntflow_api_client.entities.vacancy_requests import VacancyRequest
+from huntflow_api_client.entities.webhooks import Webhook
+
+__all__ = (
+    "Account",
+    "AccountDivision",
+    "AccountOffer",
+    "AccountTag",
+    "AccountVacancyRequest",
+    "Applicant",
+    "ApplicantLog",
+    "ApplicantOffer",
+    "ApplicantOnVacancy",
+    "ApplicantOnVacancyStatus",
+    "ApplicantsQuestionary",
+    "Coworker",
+    "DelayedTask",
+    "Dictionary",
+    "MailTemplate",
+    "ProductionCalendar",
+    "RejectionReason",
+    "Resume",
+    "User",
+    "Vacancy",
+    "VacancyRequest",
+    "Webhook",
+)
