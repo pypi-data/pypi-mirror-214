057b017ba93957cd29ea726c43532386fe1b3e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:53 +0000

    Add package llamabot/0.0.35/llamabot-0.0.35.tar.gz

---
+++ packages/llamabot/llamabot-0.0.35.tar.gz/llamabot-0.0.35/llamabot/cli/git.py
@@ -0,0 +1,39 @@
+"""Git subcommand for LlamaBot CLI."""
+
+import git
+import pyperclip
+from typer import Typer
+
+from llamabot.code_manipulation import get_git_diff
+from llamabot.prompt_library.git import commitbot, write_commit_message
+
+from .utils import get_valid_input
+
+gitapp = Typer()
+
+
+@gitapp.command()
+def commit_message(autocommit: bool = True):
+    """Generate a commit message.
+
+    :param autocommit: Whether to automatically commit the changes.
+    """
+    repo = git.Repo(search_parent_directories=True)
+
+    bot = commitbot()
+
+    while True:
+        diff = get_git_diff()
+        if not diff:
+            print(
+                "I don't see any staged changes to commit! Please stage some files before running me again."
+            )
+            return
+        message = bot(write_commit_message(diff))
+        print("\n\n")
+        user_response = get_valid_input("Do you accept this commit message? (y/n) ")
+        if user_response == "y":
+            break
+    if autocommit:
+        repo.index.commit(message.content)
+    pyperclip.copy(message.content)
