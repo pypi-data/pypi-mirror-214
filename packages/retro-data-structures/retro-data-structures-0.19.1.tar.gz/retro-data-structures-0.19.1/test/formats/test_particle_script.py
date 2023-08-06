464c0520d92302afdb17bb90e47a15320918d8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:49 +0000

    Add package retro-data-structures/0.19.1/retro-data-structures-0.19.1.tar.gz

---
+++ packages/retro-data-structures/retro-data-structures-0.19.1.tar.gz/retro-data-structures-0.19.1/test/formats/test_particle_script.py
@@ -0,0 +1,27 @@
+from test import test_lib
+
+from retro_data_structures.asset_manager import AssetManager
+
+
+def test_particles_elsc(prime2_asset_manager: AssetManager, elsc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, elsc_asset_id)
+
+
+def test_particles_swhc(prime2_asset_manager: AssetManager, swhc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, swhc_asset_id)
+
+
+def test_particles_crsc(prime2_asset_manager: AssetManager, crsc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, crsc_asset_id)
+
+
+def test_particles_wpsc(prime2_asset_manager: AssetManager, wpsc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, wpsc_asset_id)
+
+
+def test_particles_dpsc(prime2_asset_manager: AssetManager, dpsc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, dpsc_asset_id)
+
+
+def test_particles_srsc(prime2_asset_manager: AssetManager, srsc_asset_id):
+    test_lib.parse_and_build_compare_auto_manager(prime2_asset_manager, srsc_asset_id)
