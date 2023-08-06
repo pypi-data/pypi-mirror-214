9f5c77d33f1bfa4e0052fac5af91e1b26472ee82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:01 +0000

    Add package tcare-sbvalidator/0.0.42/tcare_sbvalidator-0.0.42.tar.gz

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.42.tar.gz/tcare_sbvalidator-0.0.42/tests/campaign_event_test.py
@@ -0,0 +1,32 @@
+import asyncio, sys
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+sys.path.append("..")
+from src.tcare_sbvalidator.handlers import CampaignEvent
+from src.tcare_sbvalidator.models.campaign_event import CampaignEventData
+
+
+NAMESPACE = config['NAMESPACELOCAL']
+SMS_RECIPIENT = config['TEST_SMS_RECIPIENT']
+EMAIL_RECIPIENT = config['TEST_EMAIL_RECIPIENT']
+
+async def main():
+    handler = CampaignEvent()
+    handler.connectWithCred(NAMESPACE)
+    
+    campaign_event_data = CampaignEventData(
+      source="assist",
+      campaign="elevance-1",
+      case_id="1",
+      caregiver_id="1",
+      first_name="John",
+      last_name="Doe",
+      action="start",
+      email=EMAIL_RECIPIENT,
+      phone=SMS_RECIPIENT,
+    )
+    
+    await handler.publish_message(campaign_event_data)
+
+asyncio.run(main())
