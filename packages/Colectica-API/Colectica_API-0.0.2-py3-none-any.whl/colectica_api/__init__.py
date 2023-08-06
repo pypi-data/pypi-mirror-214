2e43215fb5e1edb196eb75a7b66431825ff6b185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:24 +0000

    Add package Colectica-API/0.0.2/Colectica_API-0.0.2-py3-none-any.whl

---
+++ packages/Colectica-API/Colectica_API-0.0.2-py3-none-any.whl/colectica_api/__init__.py
@@ -0,0 +1,9 @@
+# what things to expose to users
+
+"""
+The collectica_api module exposes a ColecticaObject which
+handles communication with a Colectica servers (using its
+REST api).
+"""
+
+from .colectica import ColecticaObject
