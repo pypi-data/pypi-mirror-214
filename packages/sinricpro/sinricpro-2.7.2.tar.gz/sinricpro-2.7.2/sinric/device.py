e39fe713b6cdeffd87a3e342888d400104099ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:50 +0000

    Add package sinricpro/2.7.2/sinricpro-2.7.2.tar.gz

---
+++ packages/sinricpro/sinricpro-2.7.2.tar.gz/sinricpro-2.7.2/sinric/device.py
@@ -0,0 +1,11 @@
+class Device:
+    """
+    Base class for SinricPro devices
+    """
+    def __init__(self, device_id):
+            self._device_id = device_id
+
+    
+
+
+#https://github.com/ZacheryThomas/smartrent.py/blob/aea0eabd72bc073b51ebfa15471b47c1b71d2aa2/smartrent/device.py
