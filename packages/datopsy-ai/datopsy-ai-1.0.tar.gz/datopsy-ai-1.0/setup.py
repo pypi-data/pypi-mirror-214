33e98de39c3dcebee0f1b3098ad41c575b38a038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:54 +0000

    Add package datopsy-ai/1.0/datopsy-ai-1.0.tar.gz

---
+++ packages/datopsy-ai/datopsy-ai-1.0.tar.gz/datopsy-ai-1.0/setup.py
@@ -0,0 +1,53 @@
+# Copyright 2023 The Datopsy Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import setuptools
+
+with open("README.md", mode="r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datopsy-ai",
+    version="1.0",
+    author="Udyam AI",
+    author_email="",
+    description="A scalable and configurable DataOps framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/udyam-ai/datopsy-ai",
+    license="Apache-2.0",
+    packages=setuptools.find_packages(exclude=[".github", "metadata", "notebooks"]),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "numpy"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
