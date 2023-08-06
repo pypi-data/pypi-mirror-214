df698c295d4d3f1587df69093a366a259087ac5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:20 +0000

    Add package spaces/0.3.0/spaces-0.3.0-py3-none-any.whl

---
+++ packages/spaces/spaces-0.3.0-py3-none-any.whl/spaces/gpu/decorator.py
@@ -0,0 +1,31 @@
+"""
+"""
+from __future__ import annotations
+
+import inspect
+from typing import Callable
+
+from ..utils import CallableT
+from .wrappers import regular_function_wrapper
+from .wrappers import generator_function_wrapper
+
+
+already_decorated: set[Callable] = set()
+
+
+def GPU(task: CallableT) -> CallableT:
+
+    if task in already_decorated:
+        return task
+
+    if inspect.iscoroutinefunction(task):
+        raise NotImplementedError
+
+    if inspect.isgeneratorfunction(task):
+        decorated = generator_function_wrapper(task)
+    else:
+        decorated = regular_function_wrapper(task)
+
+    already_decorated.add(decorated)
+
+    return decorated # type: ignore
