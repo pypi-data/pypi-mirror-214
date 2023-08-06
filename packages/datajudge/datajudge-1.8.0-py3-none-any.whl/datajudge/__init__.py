aaa859e660f52907b12ceaa3e826895b5ebc3b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:45 +0000

    Add package datajudge/1.8.0/datajudge-1.8.0-py3-none-any.whl

---
+++ packages/datajudge/datajudge-1.8.0-py3-none-any.whl/datajudge/__init__.py
@@ -0,0 +1,22 @@
+"""datajudge allows to assess  whether data from database complies with reference
+information."""
+
+
+import pkg_resources
+
+from .constraints.base import Constraint
+from .db_access import Condition
+from .requirements import BetweenRequirement, Requirement, WithinRequirement
+
+__all__ = [
+    "BetweenRequirement",
+    "Condition",
+    "Constraint",
+    "Requirement",
+    "WithinRequirement",
+]
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    __version__ = "1.8.0"
