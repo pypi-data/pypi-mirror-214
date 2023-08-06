e57882c17fd166063087394b1fa96611f21744fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:50 +0000

    Add package DeepLogTimedelta/0.0.2/DeepLogTimedelta-0.0.2.tar.gz

---
+++ packages/DeepLogTimedelta/DeepLogTimedelta-0.0.2.tar.gz/DeepLogTimedelta-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DeepLogTimedelta',
+    version='0.0.2',
+    description='a deeplog implementation for timedelta',
+    license='MIT',
+    packages=['DeepLogTimedelta'],
+    author='Rustam Zhientaev',
+    email = 'zhientaev322@gmail.com',
+    url='https://github.com/sasniy/DeeplogTimedelta'
+)
