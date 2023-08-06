75805317c97dc15c98ae5a2985e4570387473c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:01 +0000

    Add package MouseTracks-Generate/1.4/MouseTracks_Generate-1.4.tar.gz

---
+++ packages/MouseTracks-Generate/MouseTracks_Generate-1.4.tar.gz/MouseTracks_Generate-1.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('readme.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='MouseTracks_Generate',
+    version='1.4',
+    author='momo',
+    long_description=long_description,
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'keras',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
