ff6e7e5b4533e6f6150aefdf8a64f4911739426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package netbox-cisco-maintenance/0.0.218/netbox-cisco-maintenance-0.0.218.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.218.tar.gz/netbox-cisco-maintenance-0.0.218/netbox_cisco_support_plugin/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm
+from .models import CiscoDeviceSupport
+
+
+class CiscoDeviceSupportFilterForm(NetBoxModelFilterSetForm):
+    model = CiscoDeviceSupport
+
+    fieldsets = (
+        ("SNI", ("sr_no_owner", "is_covered",)),
+        ("EOX", ("eox_has_error",)),
+    )
+
+    sr_no_owner =  forms.BooleanField(
+        required=False,
+    )
+
+    is_covered =  forms.BooleanField(
+        required=False,
+    )
+
+    eox_has_error =  forms.BooleanField(
+        required=False,
+    )
