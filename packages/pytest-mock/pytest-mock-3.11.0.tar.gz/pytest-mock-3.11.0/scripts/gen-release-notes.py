971f68890bcb22433ebaa64024ab4399dff40fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:39 +0000

    Add package pytest-mock/3.11.0/pytest-mock-3.11.0.tar.gz

---
+++ packages/pytest-mock/pytest-mock-3.11.0.tar.gz/pytest-mock-3.11.0/scripts/gen-release-notes.py
@@ -0,0 +1,36 @@
+"""
+Generates the release notes for the latest release, in Markdown.
+
+Convert CHANGELOG.rst to Markdown, and extracts just the latest release.
+
+Writes to ``scripts/latest-release-notes.md``, which can be
+used with https://github.com/softprops/action-gh-release.
+"""
+from pathlib import Path
+
+import pypandoc
+
+this_dir = Path(__file__).parent
+rst_text = (this_dir.parent / "CHANGELOG.rst").read_text(encoding="UTF-8")
+md_text = pypandoc.convert_text(
+    rst_text, "md", format="rst", extra_args=["--wrap=preserve"]
+)
+
+output_lines = []
+first_heading_found = False
+for line in md_text.splitlines():
+    if line.startswith("# "):
+        # Skip the first section (normally # Releases).
+        pass
+    elif line.startswith("## "):
+        # First second-level section, note it and skip the text,
+        # as we are only interested in the individual release items.
+        if first_heading_found:
+            break
+        first_heading_found = True
+    else:
+        output_lines.append(line)
+
+output_fn = this_dir / "latest-release-notes.md"
+output_fn.write_text("\n".join(output_lines), encoding="UTF-8")
+print(output_fn, "generated.")
