4ae1577589ed59b818fbb1d995288ab79490b012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:08 +0000

    Add package liquer-framework/0.9.3/liquer-framework-0.9.3.tar.gz

---
+++ packages/liquer-framework/liquer-framework-0.9.3.tar.gz/liquer-framework-0.9.3/liquer/util.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+def format_datetime(dt):
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def format_time(dt):
+    return dt.strftime("%H:%M:%S")
+
+
+def now():
+    return format_datetime(datetime.now())
+
+
+def timestamp():
+    date = datetime.now()
+    return date.isoformat()
+
+
+def to_datetime(x):
+    if isinstance(x, datetime):
+        return x
+    if isinstance(x, str):
+        try:
+            return datetime.fromisoformat(x)
+        except:
+            return datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
