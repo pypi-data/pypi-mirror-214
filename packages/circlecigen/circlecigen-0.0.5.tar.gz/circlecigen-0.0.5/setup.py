7dba86757e49c6c23e259074132b32e5252877dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:17 +0000

    Add package circlecigen/0.0.5/circlecigen-0.0.5.tar.gz

---
+++ packages/circlecigen/circlecigen-0.0.5.tar.gz/circlecigen-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="circlecigen",
+    author="Nic Cheneweth",
+    author_email="nchenewe@thoughtworks.com",
+    description="Opinionated generation of continuation pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThoughtWorks-DPS/circlecigen",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.10",
+    entry_points='''
+        [console_scripts]
+        circlecigen=src.circlecigen:cli
+    '''
+)
