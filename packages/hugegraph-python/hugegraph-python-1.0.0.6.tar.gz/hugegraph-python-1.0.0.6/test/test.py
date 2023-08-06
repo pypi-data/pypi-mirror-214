b0184ac3d4e0a63731872b5a398365f1c7fcca29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package hugegraph-python/1.0.0.6/hugegraph-python-1.0.0.6.tar.gz

---
+++ packages/hugegraph-python/hugegraph-python-1.0.0.6.tar.gz/hugegraph-python-1.0.0.6/test/test.py
@@ -0,0 +1,29 @@
+from hugegraph import PyHugeGraph
+
+if __name__ == '__main__':
+    client = PyHugeGraph("10.21.3.10", "8080", user="admin", pwd="admin", graph="hugegraph")
+
+    """system"""
+    res = client.get_graphinfo()
+    print(res)
+
+    res = client.get_all_graphs()
+    print(res)
+
+    res = client.get_version()
+    print(res)
+
+    res = client.get_graph_config()
+    print(res)
+
+    """schema"""
+    schema = client.schema()
+    res = schema.getVertexLabels()
+    print(res)
+
+    """graph"""
+    g = client.graph()
+    res = g.getVertexById("1:tom").label
+    # g.removeVertexById("1:vadas")
+    print(res)
+    g.close()
