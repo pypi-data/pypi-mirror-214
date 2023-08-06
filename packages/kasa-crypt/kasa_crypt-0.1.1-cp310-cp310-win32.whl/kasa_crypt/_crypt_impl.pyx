3c46ad0859c6f695e8d90a0d7e63d02ad9a570be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:44 +0000

    Add package kasa-crypt/0.1.1/kasa_crypt-0.1.1-cp310-cp310-win32.whl

---
+++ packages/kasa-crypt/kasa_crypt-0.1.1-cp310-cp310-win32.whl/kasa_crypt/_crypt_impl.pyx
@@ -0,0 +1,29 @@
+import cython
+
+from libc.stdlib cimport malloc
+
+
+cdef extern from "crypt_wrapper.h":
+    void _encrypt_into(const char * unencrypted, char * encrypted)
+    void _decrypt_into(const char * encrypted, char * unencrypted)
+
+cdef char* _decrypt(const char *encrypted):
+    cdef char* unencrypted = <char *> malloc(sizeof(encrypted))
+    if not unencrypted:
+        return NULL  # malloc failed
+    _decrypt_into(encrypted, unencrypted)
+    return unencrypted
+
+cdef char* _encrypt(const char *unencrypted):
+    cdef char* encrypted = <char *> malloc(sizeof(unencrypted))
+    if not encrypted:
+        return NULL  # malloc failed
+    _encrypt_into(unencrypted, encrypted)
+    return encrypted
+
+def encrypt(string: str) -> bytes:
+    return _encrypt(string.encode('utf-8'))
+
+
+def decrypt(string: bytes) -> str:
+    return _decrypt(string).decode('utf-8')
