d2ecba7a34bd55f832f0ada2018567f0e35f2bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:56 +0000

    Add package serverless-sdk/0.4.13/serverless_sdk-0.4.13-py3-none-any.whl

---
+++ packages/serverless-sdk/serverless_sdk-0.4.13-py3-none-any.whl/sls_sdk/base.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import List, Union
+from pathlib import Path
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Final
+else:
+    from typing_extensions import Final
+
+SLS_ORG_ID: Final[str] = "SLS_ORG_ID"
+
+# module metadata
+__name__ = "serverless-sdk"
+with open(Path(__file__).parent / "VERSION") as version_file:
+    __version__ = version_file.read().strip()
+
+
+TraceId = str
+Nanoseconds = int
+
+TagType = Union[str, int, float, bool, datetime]
+TagList = List[TagType]
+ValidTags = Union[TagType, TagList]
