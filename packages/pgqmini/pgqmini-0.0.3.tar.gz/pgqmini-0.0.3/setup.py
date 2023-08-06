f7a095f348db1d17f535cacd6a5f6a3703e0bd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:58 +0000

    Add package pgqmini/0.0.3/pgqmini-0.0.3.tar.gz

---
+++ packages/pgqmini/pgqmini-0.0.3.tar.gz/pgqmini-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+setup(
+    name="pgqmini",
+    version="0.0.3",
+    description="pgqmini is a lightweight, easy-to-use Python library for managing PostgreSQL message queues.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/over-engineers/pgqmini",
+    author="Kajago",
+    author_email="jangsc0000@gmail.com",
+    license="MIT",
+    packages=["pgqmini"],
+    zip_safe=False,
+    install_requires=["psycopg2-binary"],
+)
