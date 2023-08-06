2ab1b2b1ff67ed8770b9e6a27874017d1f73515e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package sumo-rl/1.4.3/sumo-rl-1.4.3.tar.gz

---
+++ packages/sumo-rl/sumo-rl-1.4.3.tar.gz/sumo-rl-1.4.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name="sumo-rl", version="1.4.3", long_description=open("README.md").read())
+
+# python setup.py sdist
+# python setup.py bdist_wheel
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# twine upload dist/*
+
+# https://towardsdatascience.com/create-your-own-python-package-and-publish-it-into-pypi-9306a29bc116
