5021dee1671cae9b3444dca4bb6e09da4c600fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:20 +0000

    Add package splitwise/3.0.0/splitwise-3.0.0.tar.gz

---
+++ packages/splitwise/splitwise-3.0.0.tar.gz/splitwise-3.0.0/splitwise/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'splitwise'
+__description__ = 'Splitwise Python SDK'
+__version__ = '3.0.0'
+__url__ = 'https://github.com/namaggarwal/splitwise'
+__download_url__ = 'https://github.com/namaggarwal/splitwise/tarball/v'+__version__
+__build__ = 0x022400
+__author__ = 'Naman Aggarwal'
+__author_email__ = 'aggarwal.nam@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Naman Aggarwal'
