380304fe21f0141fd708ad39997fc08271fcfbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:25 +0000

    Add package joes-giant-toolbox/0.3.2/joes_giant_toolbox-0.3.2.tar.gz

---
+++ packages/joes-giant-toolbox/joes_giant_toolbox-0.3.2.tar.gz/joes_giant_toolbox-0.3.2/joes_giant_toolbox/web.py
@@ -0,0 +1,17 @@
+from joes_giant_toolbox.all.anonymous_view_public_linkedin_page import (
+    anonymous_view_public_linkedin_page,
+)
+
+from joes_giant_toolbox.all.duckduckgo_search_multipage import (
+    duckduckgo_search_multipage,
+)
+
+from joes_giant_toolbox.all.make_url_request import make_url_request
+
+from joes_giant_toolbox.all.scrape_webpage_and_all_linked_webpages import (
+    scrape_webpage_and_all_linked_webpages,
+)
+
+from joes_giant_toolbox.all.url_to_filename_to_url_mapper import (
+    url_to_filename_to_url_mapper,
+)
