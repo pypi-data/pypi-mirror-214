5d2494a027e4c3250ae61d54b9e44385e6ac1f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:05 +0000

    Add package tcare-sbvalidator/0.0.28/tcare_sbvalidator-0.0.28.tar.gz

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.28.tar.gz/tcare_sbvalidator-0.0.28/src/tcare_sbvalidator/sms_handler.py
@@ -0,0 +1,29 @@
+import json
+from datetime import datetime
+from .models.sms import SmsServiceBusMessage
+from .base_handler import BaseHandler
+
+class Sms(BaseHandler):
+    messageSchema = SmsServiceBusMessage
+    topic = "send-sms"
+
+    # Building JSON and publishing are separate functions but could be combined
+    def build_json(self, id, recipient, sender, content, options=None):
+        try:
+            return json.dumps({
+                "specversion": "1.0.0",
+                "type": "servicebusevent",
+                "id": id,
+                "time": datetime.now(),
+                "datacontenttype": "application/json",
+                "data": {
+                    "type": "sms",
+                    "content": content,
+                    "recipient": recipient,
+                    "sender": sender
+                },
+                "options": options,
+            }, default=str)
+        except Exception as e:
+            print(e)
+
