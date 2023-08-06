2a54d1c5e42a95f8c6709c7856093813b53c68d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:39 +0000

    Add package aiovantage/0.2.1/aiovantage-0.2.1.tar.gz

---
+++ packages/aiovantage/aiovantage-0.2.1.tar.gz/aiovantage-0.2.1/src/aiovantage/config_client/errors.py
@@ -0,0 +1,15 @@
+"""Config client exceptions."""
+
+import asyncio
+
+
+class ClientError(Exception):
+    """Base exception for config client."""
+
+
+class ClientConnectionError(ClientError):
+    """Exception for config client connection errors."""
+
+
+class ClientTimeoutError(asyncio.TimeoutError, ClientConnectionError):
+    """Exception for command client connection errors caused by timeouts."""
