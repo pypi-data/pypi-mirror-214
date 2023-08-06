812a5481d703040f5b22dc20c9bafc4c7bb061eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:53 +0000

    Add package mercury-sync/0.1.9/mercury-sync-0.1.9.tar.gz

---
+++ packages/mercury-sync/mercury-sync-0.1.9.tar.gz/mercury-sync-0.1.9/mercury_sync/models/message.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from pydantic import BaseModel, StrictStr, StrictInt
+from typing import Optional
+
+class Message(BaseModel):
+    host: Optional[StrictStr]
+    port: Optional[StrictInt]
+    error: Optional[StrictStr]
+
+    def to_data(self):
+        return {
+            name: value for name, value in self.__dict__.items() if name not in [
+                'host',
+                'port'
+            ]
+        }
