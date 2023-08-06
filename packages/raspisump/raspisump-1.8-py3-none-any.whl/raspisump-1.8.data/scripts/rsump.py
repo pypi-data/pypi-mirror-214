14acb6fb4f37870eeed6419e9c6c05b080870d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package raspisump/1.8/raspisump-1.8-py3-none-any.whl

---
+++ packages/raspisump/raspisump-1.8-py3-none-any.whl/raspisump-1.8.data/scripts/rsump.py
@@ -0,0 +1,34 @@
+#!python
+
+# Raspi-sump, a sump pump monitoring system.
+# Al Audet
+# https://www.linuxnorth.org/raspi-sump/
+#
+# All configuration changes should be done in raspisump.conf
+# MIT License -- https://www.linuxnorth.org/raspi-sump/license.html
+
+import time
+import os
+import configparser
+from raspisump import reading, log
+
+config = configparser.RawConfigParser()
+user = os.getlogin()
+config.read("/home/" + user + "/raspi-sump/raspisump.conf")
+reading_interval = config.getint("pit", "reading_interval")
+
+if reading_interval == 0:
+    try:
+        reading.water_depth()
+    except RuntimeError:
+        print(
+            "ERROR -- Cannot Access gpio pins.  Make sure user is part of the gpio group."
+        )
+        log.log_event(
+            "error_log",
+            "GPIO ERROR -- Cannot Access gpio pins.  Make sure user is part of the gpio group.",
+        )
+else:
+    while True:
+        reading.water_depth()
+        time.sleep(reading_interval)
