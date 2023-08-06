372969e24f431590acdbc0cfe97fea3c223382dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:46 +0000

    Add package KitronikPicoSmartAirQuality/1.0.1/KitronikPicoSmartAirQuality-1.0.1.tar.gz

---
+++ packages/KitronikPicoSmartAirQuality/KitronikPicoSmartAirQuality-1.0.1.tar.gz/KitronikPicoSmartAirQuality-1.0.1/KitronikPicoSmartAirQualityWheelSetup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = "KitronikPicoSmartAirQuality",
+    version = "1.0.1",
+    description = "Kitronik Air Quality Datalogging Board for Pico",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules = ["PicoAirQuality"],
+)
