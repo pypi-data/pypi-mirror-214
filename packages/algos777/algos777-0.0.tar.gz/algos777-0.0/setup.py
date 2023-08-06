090232f76d26d597cab36cf2b8bfb48946ddb857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:38 +0000

    Add package algos777/0.0/algos777-0.0.tar.gz

---
+++ packages/algos777/algos777-0.0.tar.gz/algos777-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='algos777',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['algos777'],
+      author_email='ja.s.p.erx.chu.a@gmail.com',
+      zip_safe=False)
