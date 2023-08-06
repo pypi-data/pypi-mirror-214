dc957e3d2d9daba059e778506de47a63b1b0d9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:45 +0000

    Add package piccolo/0.115.0/piccolo-0.115.0.tar.gz

---
+++ packages/piccolo/piccolo-0.115.0.tar.gz/piccolo-0.115.0/piccolo/query/__init__.py
@@ -0,0 +1,45 @@
+from .base import Query
+from .methods import (
+    Alter,
+    Avg,
+    Count,
+    Create,
+    CreateIndex,
+    Delete,
+    DropIndex,
+    Exists,
+    Insert,
+    Max,
+    Min,
+    Objects,
+    Raw,
+    Select,
+    Sum,
+    TableExists,
+    Update,
+)
+from .methods.select import SelectRaw
+from .mixins import OrderByRaw
+
+__all__ = [
+    "Alter",
+    "Avg",
+    "Count",
+    "Create",
+    "CreateIndex",
+    "Delete",
+    "DropIndex",
+    "Exists",
+    "Insert",
+    "Max",
+    "Min",
+    "Objects",
+    "OrderByRaw",
+    "Query",
+    "Raw",
+    "Select",
+    "SelectRaw",
+    "Sum",
+    "TableExists",
+    "Update",
+]
