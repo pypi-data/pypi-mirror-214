a13283cae9597729ecbc700b568e542e4f779359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:36 +0000

    Add package prostate-nomograms/0.0.7/prostate_nomograms-0.0.7-py3-none-any.whl

---
+++ packages/prostate-nomograms/prostate_nomograms-0.0.7-py3-none-any.whl/examples/ex02-capra.py
@@ -0,0 +1,78 @@
+import env_examples
+
+import os
+
+import pandas as pd
+
+from prostate_nomograms import CapraNomogram, ClassificationOutcome, SurvivalOutcome
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                                Constant                                                     #
+    # ----------------------------------------------------------------------------------------------------------- #
+    DATA_FOLDER_NAME = "data"
+    DATASET_FILENAME = "fake_dataset.xlsx"
+    RESULTS_FILENAME = "results_capra.csv"
+    DATASET_PATH = os.path.join(DATA_FOLDER_NAME, DATASET_FILENAME)
+    RESULTS_PATH = os.path.join(DATA_FOLDER_NAME, RESULTS_FILENAME)
+
+    AGE_COLUMN = "AGE"
+    PSA_COLUMN = "PSA"
+    CLINICAL_STAGE_COLUMN = "CLINICAL_STAGE"
+    GLEASON_PRIMARY_COLUMN = "GLEASON_PRIMARY"
+    GLEASON_SECONDARY_COLUMN = "GLEASON_SECONDARY"
+
+    NUMBER_OF_MONTHS = [60, 120]
+
+    OUTCOMES = {
+        ClassificationOutcome.EXTRACAPSULAR_EXTENSION: "EE",
+        ClassificationOutcome.LYMPH_NODE_INVOLVEMENT: "PN",
+        ClassificationOutcome.SEMINAL_VESICLE_INVASION: "SVI",
+        ClassificationOutcome.ORGAN_CONFINED_DISEASE: "OCD",
+        SurvivalOutcome.PREOPERATIVE_BCR: "BCR",
+        SurvivalOutcome.PREOPERATIVE_PROSTATE_CANCER_DEATH: "DEATH"
+    }
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                                    Data                                                     #
+    # ----------------------------------------------------------------------------------------------------------- #
+    dataframe = pd.read_excel(DATASET_PATH)
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                                   CAPRA                                                     #
+    # ----------------------------------------------------------------------------------------------------------- #
+    for outcome, col_name in OUTCOMES.items():
+        if outcome in SurvivalOutcome:
+            capra_nomogram = CapraNomogram(
+                outcome=outcome,
+                event_indicator_column_name=col_name,
+                event_time_column_name=f"{col_name}_TIME",
+                age_column_name=AGE_COLUMN,
+                psa_column_name=PSA_COLUMN,
+                primary_gleason_column_name=GLEASON_PRIMARY_COLUMN,
+                secondary_gleason_column_name=GLEASON_SECONDARY_COLUMN,
+                clinical_stage_column_name=CLINICAL_STAGE_COLUMN
+            )
+            capra_nomogram.fit(dataframe)
+            dataframe[f"PREDICTED_{outcome.name}_RISK"] = capra_nomogram.predict_risk(dataframe)
+            for number_of_months in NUMBER_OF_MONTHS:
+                column_name = f"PREDICTED_{outcome.name}_{number_of_months}MONTHS"
+                dataframe[column_name] = capra_nomogram.predict_proba(dataframe, number_of_months)
+        else:
+            capra_nomogram = CapraNomogram(
+                outcome=outcome,
+                target_column_name=col_name,
+                age_column_name=AGE_COLUMN,
+                psa_column_name=PSA_COLUMN,
+                primary_gleason_column_name=GLEASON_PRIMARY_COLUMN,
+                secondary_gleason_column_name=GLEASON_SECONDARY_COLUMN,
+                clinical_stage_column_name=CLINICAL_STAGE_COLUMN
+            )
+            capra_nomogram.fit(dataframe)
+            dataframe[f"PREDICTED_{outcome.name}"] = capra_nomogram.predict_proba(dataframe)
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                                  Results                                                    #
+    # ----------------------------------------------------------------------------------------------------------- #
+    dataframe.to_csv(path_or_buf=RESULTS_PATH)
---
+++ packages/prostate-nomograms/prostate_nomograms-0.0.7-py3-none-any.whl/prostate_nomograms/custom/custom.py
@@ -0,0 +1,215 @@
+from typing import List, Optional, Union
+
+import pandas as pd
+import numpy as np
+
+from ..enum import ClassificationOutcome, SurvivalOutcome
+from .base import LogisticRegression, SurvivalRegression
+
+
+class CustomNomogram:
+    """
+    Custom model. It can be used to create a nomogram with a custom model.
+    """
+
+    def __init__(
+            self,
+            outcome: Union[str, ClassificationOutcome, SurvivalOutcome],
+            features_column_names: List[str],
+            target_column_name: Optional[str] = None,
+            event_indicator_column_name: Optional[str] = None,
+            event_time_column_name: Optional[str] = None,
+            random_state: int = 0
+    ):
+        """
+        Initializes columns names.
+
+        Parameters
+        ----------
+        outcome : Optional[Union[str, Outcome]]
+            Name of the outcome.
+        features_column_names : Optional[List[str]]
+            Names of the columns containing the features of the patients.
+        target_column_name : Optional[str]
+            Name of the column containing the target of the patients.
+        event_indicator_column_name : Optional[str]
+            Name of the column containing the event indicator of the patients.
+        event_time_column_name : Optional[str]
+            Name of the column containing the event time of the patients.
+        random_state : int, optional
+            Random state.
+        """
+        if outcome in ClassificationOutcome:
+            self.outcome = ClassificationOutcome(outcome)
+        elif outcome in SurvivalOutcome:
+            self.outcome = SurvivalOutcome(outcome)
+        else:
+            raise ValueError(f"Invalid outcome: {outcome}")
+
+        self.target_column_name = target_column_name
+        self.event_indicator_column_name = event_indicator_column_name
+        self.event_time_column_name = event_time_column_name
+        self.features_column_names = features_column_names
+        self._is_fitted = False
+
+        if self.model_type == "survival":
+            assert self.event_indicator_column_name is not None, (
+                "Event indicator column name must be specified for survival models."
+            )
+            assert self.event_time_column_name is not None, (
+                "Event time column name must be specified for survival models."
+            )
+            regressor_constructor = SurvivalRegression
+        elif self.model_type == "logistic":
+            assert self.target_column_name is not None, (
+                "Target column name must be specified for logistic models."
+            )
+            regressor_constructor = LogisticRegression
+        else:
+            raise ValueError(f"Unknown model type: {self.model_type}")
+
+        self.regressor = regressor_constructor(random_state=random_state)
+
+    @property
+    def model_type(self) -> str:
+        """
+        The type of the model. It is used to determine which model to use for the prediction.
+
+        Returns
+        -------
+        model_type : str
+            The type of the model. It is used to determine which model to use for the prediction.
+        """
+        if self.outcome in list(SurvivalOutcome):
+            return "survival"
+        else:
+            return "logistic"
+
+    @property
+    def cores(self):
+        """
+        Whether the model is for cores or not.
+
+        Returns
+        -------
+        is_cores : bool
+            Whether the model is for cores or not.
+        """
+        if self.outcome.endswith("(Cores)"):
+            return True
+        else:
+            return False
+
+    @property
+    def columns(self) -> List[str]:
+        """
+        Returns the columns of the model.
+
+        Returns
+        -------
+        columns : List[str]
+            The columns of the model.
+        """
+        return self.features_column_names
+
+    def get_features(self, dataframe: pd.DataFrame) -> np.ndarray:
+        """
+        Returns the features of the patients.
+
+        Parameters
+        ----------
+        dataframe : pd.DataFrame
+            Dataframe containing the data of the patients.
+
+        Returns
+        -------
+        features : np.ndarray
+            The features of the patients.
+        """
+        dataframe = dataframe[self.columns]
+        return np.array(dataframe)
+
+    def fit(
+            self,
+            dataset: pd.DataFrame
+    ):
+        """
+        Fits the model.
+
+        Parameters
+        ----------
+        dataset : pd.DataFrame
+            Dataframe containing the data of the patients.
+        """
+        features = self.get_features(dataset)
+        if self.model_type == "survival":
+            self.regressor.fit(
+                features,
+                np.array(dataset[self.event_indicator_column_name], dtype=bool),
+                np.array(dataset[self.event_time_column_name], dtype=float)
+            )
+        else:
+            self.regressor.fit(
+                features,
+                np.array(dataset[self.target_column_name])
+            )
+
+        self._is_fitted = True
+
+    def predict_proba(
+            self,
+            dataframe: pd.DataFrame,
+            number_of_months: Union[np.ndarray, list, float, int] = None
+    ) -> np.ndarray:
+        """
+        Gets the predictions. If the model is survival, the number of years must be given.
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            The dataframe.
+        number_of_months : Union[numpy.ndarray, list, float, int], optional
+            The number of months. It is used only for survival models.
+
+        Returns
+        -------
+        predictions : numpy.ndarray
+            The predictions.
+        """
+        assert self._is_fitted, "Model must be fitted first."
+
+        features = self.get_features(dataframe)
+        if self.model_type == "survival":
+            if number_of_months is None:
+                raise ValueError("Number of months must be given.")
+            else:
+                return self.regressor.get_predicted_survival_probability(features, number_of_months)
+        elif self.model_type == "logistic":
+            return self.regressor.get_predicted_probability(features)
+        else:
+            raise ValueError(f"Model type {self.model_type} doesn't exist.")
+
+    def predict_risk(
+            self,
+            dataframe: pd.DataFrame
+    ) -> np.ndarray:
+        """
+        Gets the risk predictions.
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            The dataframe.
+
+        Returns
+        -------
+        predictions : numpy.ndarray
+            The predictions.
+        """
+        if self.model_type == "survival":
+            features = self.get_features(dataframe)
+            return self.regressor.get_predicted_risk(features)
+        elif self.model_type == "logistic":
+            raise ValueError("Logistic models don't have risk predictions.")
+        else:
+            raise ValueError(f"Model type {self.model_type} doesn't exist.")
