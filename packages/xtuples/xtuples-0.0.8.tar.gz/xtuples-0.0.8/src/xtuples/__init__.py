3268db1179d8963711c6d1080dfdbfd33863fead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:04 +0000

    Add package xtuples/0.0.8/xtuples-0.0.8.tar.gz

---
+++ packages/xtuples/xtuples-0.0.8.tar.gz/xtuples-0.0.8/src/xtuples/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023-present Tom Williams <tomjrw@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .xtuples import (
+    iTuple,
+    nTuple,
+    fDict,
+)
+from .json import (
+    JSONDecoder,
+    JSONEncoder,
+)
