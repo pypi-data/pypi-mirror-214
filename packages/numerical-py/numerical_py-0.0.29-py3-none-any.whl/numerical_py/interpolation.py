abef7eeab640c459dc24fea064f36a7e35c97548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:56 +0000

    Add package numerical-py/0.0.29/numerical_py-0.0.29-py3-none-any.whl

---
+++ packages/numerical-py/numerical_py-0.0.29-py3-none-any.whl/numerical_py/interpolation.py
@@ -0,0 +1,20 @@
+import numpy as np
+import sympy as sp
+
+class Interpolate:
+
+    def __init__(self,x_values: np.ndarray, f_x_values: np.ndarray) -> None:
+        self.x_values = x_values
+        self.f_x_values = f_x_values
+
+    def lagrange_polynom(self) -> sp.Function:
+        n = len(self.x_values)
+        x = sp.Symbol('x')
+        L = 0
+        for i in range(n):
+            l = 1
+            for j in range(n):
+                if j != i:
+                    l *= (x - self.x_values[j])/(self.x_values[i] - self.x_values[j])
+            L += self.f_x_values[i]*l
+        return L.expand()
