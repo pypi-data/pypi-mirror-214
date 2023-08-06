c7f73556b0cfa0f34cf079b4e3a9c2f5118514ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:36 +0000

    Add package capellambse/0.5.26.dev22/capellambse-0.5.26.dev22.tar.gz

---
+++ packages/capellambse/capellambse-0.5.26.dev22.tar.gz/capellambse-0.5.26.dev22/capellambse/svg/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capellambse contributors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Export diagrams to ``.svg`` files."""
+from .generate import SVGDiagram
