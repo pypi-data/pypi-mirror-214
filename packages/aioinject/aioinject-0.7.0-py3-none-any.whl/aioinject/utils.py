7076d76546e02de36f3c6836879fcba82c25b621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:56 +0000

    Add package aioinject/0.7.0/aioinject-0.7.0-py3-none-any.whl

---
+++ packages/aioinject/aioinject-0.7.0-py3-none-any.whl/aioinject/utils.py
@@ -0,0 +1,39 @@
+import inspect
+import typing
+from collections.abc import Callable
+from typing import Any, ParamSpec, TypeVar
+
+from aioinject.markers import Inject
+
+_T = TypeVar("_T")
+_P = ParamSpec("_P")
+
+
+def clear_wrapper(wrapper: Callable[_P, _T]) -> Callable[_P, _T]:
+    inject_annotations = get_inject_annotations(wrapper)
+    signature = inspect.signature(wrapper)
+    new_params = tuple(
+        p
+        for p in signature.parameters.values()
+        if p.name not in inject_annotations
+    )
+    wrapper.__signature__ = signature.replace(  # type: ignore[attr-defined]
+        parameters=new_params,
+    )
+    for name in inject_annotations:
+        del wrapper.__annotations__[name]
+    return wrapper
+
+
+def get_inject_annotations(function: Callable[..., Any]) -> dict[str, Any]:
+    return {
+        name: annotation
+        for name, annotation in typing.get_type_hints(
+            function,
+            include_extras=True,
+        ).items()
+        if any(
+            isinstance(arg, Inject) or arg is Inject
+            for arg in typing.get_args(annotation)
+        )
+    }
