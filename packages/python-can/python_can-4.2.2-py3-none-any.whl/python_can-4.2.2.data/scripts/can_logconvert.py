3a94ae9ca9509148fb9aad73dc4a16f2306922f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:31 +0000

    Add package python-can/4.2.2/python_can-4.2.2-py3-none-any.whl

---
+++ packages/python-can/python_can-4.2.2-py3-none-any.whl/python_can-4.2.2.data/scripts/can_logconvert.py
@@ -0,0 +1,11 @@
+#!python
+
+"""
+See :mod:`can.logconvert`.
+"""
+
+from can.logconvert import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/python-can/python_can-4.2.2-py3-none-any.whl/python_can-4.2.2.data/scripts/can_player.py
@@ -0,0 +1,11 @@
+#!python
+
+"""
+See :mod:`can.player`.
+"""
+
+from can.player import main
+
+
+if __name__ == "__main__":
+    main()
