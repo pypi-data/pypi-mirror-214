7001f016b9678b1b3e71a61794a61ccb69435a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:05 +0000

    Add package fudan-jwc-news/0.5.2/fudan_jwc_news-0.5.2-py3-none-any.whl

---
+++ packages/fudan-jwc-news/fudan_jwc_news-0.5.2-py3-none-any.whl/fudan_jwc_news/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.5.2'
+__app_name__ = 'fudan-jwc-news'
+
+try:
+    # raise ImportError
+    # raise
+    from logging_utils_tddschn import get_logger
+    logger, _ = get_logger(__app_name__)
+except:
+    import logging
+    from logging import NullHandler
+    logger = logging.getLogger(__app_name__)
+    logger.addHandler(NullHandler())
