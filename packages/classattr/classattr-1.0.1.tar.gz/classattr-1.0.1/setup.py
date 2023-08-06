5b250b2c2d04e7450db431f67919313547d31c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package classattr/1.0.1/classattr-1.0.1.tar.gz

---
+++ packages/classattr/classattr-1.0.1.tar.gz/classattr-1.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = 'classattr',
+    version = '1.0.1',
+    author = 'Thierry Dassé',
+    url = 'https://framagit.org/makeforartandscience/classattr',
+    license = 'MIT License',
+    description = 'classattr provides tools to manage class and object attributes',
+    long_description=open('description').read(),
+    install_requires=[],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
