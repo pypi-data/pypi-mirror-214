5f904967bcca182120fdff49a148c1c21cb0e7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:40 +0000

    Add package isTrue/0.0.3/isTrue-0.0.3.tar.gz

---
+++ packages/isTrue/isTrue-0.0.3.tar.gz/isTrue-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="isTrue",
+    version="0.0.3",
+    description="REMAIN CALM THIS IS JUST A TEST",
+    long_description_content_type="text/markdown",
+    keywords="IDK",
+    install_requires=[],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers"
+
+    ],
+)
