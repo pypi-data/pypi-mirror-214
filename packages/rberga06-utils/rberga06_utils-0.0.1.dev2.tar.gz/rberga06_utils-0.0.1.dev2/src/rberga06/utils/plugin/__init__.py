0cf3a9c1f378d430a38de79d97ee3639758ec0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:56 +0000

    Add package rberga06-utils/0.0.1.dev2/rberga06_utils-0.0.1.dev2.tar.gz

---
+++ packages/rberga06-utils/rberga06_utils-0.0.1.dev2.tar.gz/rberga06_utils-0.0.1.dev2/src/rberga06/utils/plugin/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A flexible, robust and easy-to-use plugin system."""
+from __future__ import annotations
+from .spec import *
+from .system import *
