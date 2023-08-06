c329fa64f63e3ef1a74eb4c73d3a63c304dca35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:04 +0000

    Add package mdapackmol-fmt/0.1.2/mdapackmol_fmt-0.1.2-py2.py3-none-any.whl

---
+++ packages/mdapackmol-fmt/mdapackmol_fmt-0.1.2-py2.py3-none-any.whl/mdapackmol/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for MDA Packmol."""
+
+__author__ = """Richard J Gowers"""
+__email__ = 'richardjgowers@gmail.com'
+__version__ = '0.1.0'
+
+from . import mdapackmol
+from . import tools
+from .mdapackmol import packmol, PackmolStructure
