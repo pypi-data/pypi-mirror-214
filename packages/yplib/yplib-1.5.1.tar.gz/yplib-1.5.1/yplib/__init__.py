ed5e17faa1cdde7e2960f355dffe65ee429f23d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:07 +0000

    Add package yplib/1.5.1/yplib-1.5.1.tar.gz

---
+++ packages/yplib/yplib-1.5.1.tar.gz/yplib-1.5.1/yplib/__init__.py
@@ -0,0 +1,15 @@
+# 引入自定义的全部的包
+
+# 基础的，通用的基础包
+from yplib.index import *
+
+# 有关文件操作类的包
+from yplib.file import *
+
+# 有关 图表的 html 代码的包
+from yplib.chart_html import *
+# 有关 图表的 操作类的包
+from yplib.chart import *
+
+# 有关 html 解析类的包
+from yplib.html_parser import *
