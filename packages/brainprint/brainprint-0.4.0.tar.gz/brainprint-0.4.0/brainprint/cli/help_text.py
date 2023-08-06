1a7553f5aa5d619711e38cd0fc3b5694ddca1aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:19 +0000

    Add package brainprint/0.4.0/brainprint-0.4.0.tar.gz

---
+++ packages/brainprint/brainprint-0.4.0.tar.gz/brainprint-0.4.0/brainprint/cli/help_text.py
@@ -0,0 +1,169 @@
+"""
+Help text strings for the :mod:`brainprint.cli` module.
+"""
+CLI_DESCRIPTION: str = (
+    "This program conducts a brainprint analysis of FreeSurfer output."
+)
+
+
+SUBJECT_ID: str = (
+    "Subject ID (FreeSurfer processed directory inside the subjects directory)"
+)
+SUBJECTS_DIR: str = "FreeSurfer subjects directory"
+OUTPUT_DIRECTORY: str = "Output directory (default: <sdir>/<sid>/brainprint)"
+NUM: str = "Number of eigenvalues/vectors to compute (default: 50)"
+EVEC: str = "Switch on eigenvector computation (default: off)"
+SKIP_CORTEX: str = "Skip cortical surfaces (default: off)"
+NORM: str = "Eigenvalues normalization method (default: none)"
+REWEIGHT: str = "Switch on eigenvalue reweighting (default: off)"
+ASYM: str = "Switch on additional asymmetry calculation (default: off)"
+ASYM_DISTANCE: str = (
+    "Distance measurement to use for asymmetry calculation (default: euc)"
+)
+CHOLMOD: str = "Use cholesky decomposition (faster) instead of LU decomposition (slower). May require manual install of scikit-sparse library. Default is LU decomposition."
+KEEP_TEMP: str = (
+    "Whether to keep the temporary files directory or not, by default False"
+)
+HELP: str = "Display this help message and exit"
+MORE_HELP: str = "Display extensive help message and exit"
+
+HELPTEXT: str = """
+brainprint.py
+Author: Martin Reuter, 2015
+
+=======
+SUMMARY
+=======
+
+Computes the BrainPrint for a FreeSurfer subject.
+
+The BrainPrint consists of the shape descriptors (Shape-DNA) [1]
+of a selection of both cortical and subcortical structures [2].
+
+Here is a list of structures and FreeSurfer aseg label ids:
+
+CorpusCallosum                  [251, 252, 253, 254, 255]
+Cerebellum                      [7, 8, 16, 46, 47]
+Ventricles                      [4, 5, 14, 24, 31, 43, 44, 63]
+3rd-Ventricle                   [14, 24]
+4th-Ventricle                   15
+Brain-Stem                      16
+Left-Striatum                   [11, 12, 26]
+Left-Lateral-Ventricle          [4, 5, 31]
+Left-Cerebellum-White-Matter    7
+Left-Cerebellum-Cortex          8
+Left-Thalamus-Proper            10
+Left-Caudate                    11
+Left-Putamen                    12
+Left-Pallidum                   13
+Left-Hippocampus                17
+Left-Amygdala                   18
+Left-Accumbens-area             26
+Left-VentralDC                  28
+Right-Striatum                  [50, 51, 58]
+Right-Lateral-Ventricle         [43, 44, 63]
+Right-Cerebellum-White-Matter   46
+Right-Cerebellum-Cortex         47
+Right-Thalamus-Proper           49
+Right-Caudate                   50
+Right-Putamen                   51
+Right-Pallidum                  52
+Right-Hippocampus               53
+Right-Amygdala                  54
+Right-Accumbens-area            58
+Right-VentralDC                 60
+
+And the following cortical structures:
+
+lh-white-2d    (left white matter surface triangles)
+lh-pial-2d     (left pial surface triangles)
+rh-white-2d    (same for right hemisphere ...)
+rh-pial-2d
+
+Processing of the cortical structures can be skipped (--skipcortex).
+
+Implicit Inputs:
+The mri/aseg.mgz and mri/norm.mgz should be available.
+Also surf/?h.pial and surf/?h.white need to be
+available unless --skipcortex is passed. norm.mgz is not
+absolutely necessary but highly recommended to fix the labels
+and obtain improved meshes.
+
+Output:
+The brainprint CSV table containing column headers for the
+structures, a row of areas, a row of volumes and N rows of
+the first N eigenvalues for each structure. An additional
+output file will be created if the asymmetry calculation is
+performed.
+
+==================
+COMMAND-LINE USAGE
+==================
+
+python3 brainprint.py --sdir <directory> --sid <SubjectID>  [--num <num>]
+                    [--evec] [--skipcortex] [--norm <surface|volume|geometry|none> ]
+                    [--reweight] [--asymmetry] [--cholmod] [--outdir <directory>] 
+                    [--help] [--more-help]
+
+Options:
+    --help           Show this help message and exit
+    --more-help      Show extensive help message and exit
+
+Required options:
+    --sid <SubjectID>
+                    Subject ID (FreeSurfer-processed directory inside the
+                    subjects directory)
+    --sdir <directory>
+                    FreeSurfer subjects directory
+
+Processing directives:
+    --num <num>      Number of eigenvalues/vectors to compute (default: 50)
+    --evec           Switch on eigenvector computation (default: off)
+    --skipcortex     Skip cortical surfaces (default: off)
+    --norm <surface|volume|geometry|none>
+                     Switch on eigenvalue normalization; will be either surface,
+                     volume, or determined by the geometry of the object. Use
+                     "none" or leave out entirely to skip normalization.
+    --reweight       Switch on eigenvalue reweighting (default: off)
+    --asymmetry      Perform left-right asymmetry calculation (default: off)
+    --cholmod        Use cholesky decomposition (faster) instead of LU 
+                     decomposition (slower). May require manual install of the
+                     scikit-sparse library. Default is LU decomposition.
+
+Output parameters:
+    --outdir=OUTDIR  Output directory (default: <sdir>/<sid>/brainprint)
+
+============
+PYTHON USAGE
+============
+
+As an alternative to the command-line usage described above, individual
+functions can also be called within a Python environment as follows:
+
+import lapy
+from brainprint import brainprint
+brainprint.run_brainprint(subjects_dir="/my/subjects/directory", subject_id="my_subject_id")
+
+Additional options are num=<int>, evec=<bool>, skipcortex=<bool>,
+norm=<"surface"|"volume"|"geometry"|"none">, reweight=<bool>, and outdir=<string>.
+
+==========
+REFERENCES
+==========
+
+If used for a publication, please cite both [1] for the shape
+descriptor method and [2] for the application to brain MRI and
+definition of the BrainPrint.
+
+[1] M. Reuter, F.-E. Wolter and N. Peinecke.
+Laplace-Beltrami spectra as "Shape-DNA" of surfaces and solids.
+Computer-Aided Design 38 (4), pp.342-366, 2006.
+http://dx.doi.org/10.1016/j.cad.2005.10.011
+
+[2] C. Wachinger, P. Golland, W. Kremen, B. Fischl, M. Reuter.
+BrainPrint: A discriminative characterization of brain morphology.
+NeuroImage Volume 109, pp.232-248, 2015.
+http://dx.doi.org/10.1016/j.neuroimage.2015.01.032
+"""
+
+# flake8: noqa: E501
---
+++ packages/brainprint/brainprint-0.4.0.tar.gz/brainprint-0.4.0/brainprint/cli/parser.py
@@ -0,0 +1,150 @@
+import argparse
+import pathlib
+import sys
+
+from . import help_text
+
+
+def parse_options():
+    """
+    Initiates the option parser and return the parsed object.
+    """
+    parser = argparse.ArgumentParser(
+        description=help_text.CLI_DESCRIPTION, add_help=False
+    )
+
+    # Required arguments
+    required = parser.add_argument_group(title="Required arguments")
+    required.add_argument(
+        "--sdir",
+        dest="subjects_dir",
+        help=help_text.SUBJECTS_DIR,
+        default=None,
+        metavar="<directory>",
+        required=True,
+        type=pathlib.Path,
+    )
+    required.add_argument(
+        "--sid",
+        dest="subject_id",
+        help=help_text.SUBJECT_ID,
+        default=None,
+        metavar="<string>",
+        required=True,
+    )
+
+    # Optional arguments
+    optional = parser.add_argument_group(title="Processing directives")
+    optional.add_argument(
+        "--num",
+        dest="num",
+        help=help_text.NUM,
+        default=50,
+        metavar="<num>",
+        type=int,
+        required=False,
+    )
+    optional.add_argument(
+        "--evec",
+        dest="keep_eigenvectors",
+        help=help_text.EVEC,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+    optional.add_argument(
+        "--skipcortex",
+        dest="skip_cortex",
+        help=help_text.SKIP_CORTEX,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+    optional.add_argument(
+        "--norm",
+        dest="norm",
+        help=help_text.NORM,
+        default="none",
+        metavar="<surface|volume|geometry|none>",
+        choices=["surface", "volume", "geometry", "none"],
+        required=False,
+    )
+    optional.add_argument(
+        "--reweight",
+        dest="reweight",
+        help=help_text.REWEIGHT,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+    optional.add_argument(
+        "--asymmetry",
+        dest="asymmetry",
+        help=help_text.ASYM,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+    optional.add_argument(
+        "--asym-distance",
+        dest="asymmetry_distance",
+        help=help_text.ASYM_DISTANCE,
+        default="euc",
+        metavar="<euc>",
+        choices=["euc"],
+        required=False,
+    )
+    optional.add_argument(
+        "--cholmod",
+        dest="use_cholmod",
+        help=help_text.CHOLMOD,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+
+    # Output options
+    output = parser.add_argument_group(title="Output parameters")
+    output.add_argument(
+        "--outdir",
+        dest="destination",
+        help=help_text.OUTPUT_DIRECTORY,
+        default=None,
+        metavar="<directory>",
+        required=False,
+        type=pathlib.Path,
+    )
+    output.add_argument(
+        "--keep-temp",
+        dest="keep_temp",
+        help=help_text.KEEP_TEMP,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+
+    # Help
+    help = parser.add_argument_group(title="Getting help")
+    help.add_argument("--help", help=help_text.HELP, action="help")
+    help.add_argument(
+        "--more-help",
+        dest="more_help",
+        help=help_text.MORE_HELP,
+        default=False,
+        action="store_true",
+        required=False,
+    )
+
+    no_input = len(sys.argv) == 1
+    more_help = "--more-help" in sys.argv
+    if no_input:
+        args = parser.parse_args(["--help"])
+        return
+    elif more_help:
+        print(help_text.HELPTEXT)
+        return
+    else:
+        args = vars(parser.parse_args())
+        del args["more_help"]
+
+    return args
