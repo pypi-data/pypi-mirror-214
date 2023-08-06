16be73afb6b2ec8fb255d3adab9f3bfff8130d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:53 +0000

    Add package chatglm-llm/1.4.0/chatglm-llm-1.4.0.tar.gz

---
+++ packages/chatglm-llm/chatglm-llm-1.4.0.tar.gz/chatglm-llm-1.4.0/chatglm_src/cluster_and_smi.py
@@ -0,0 +1,83 @@
+from sklearn.cluster import KMeans, DBSCAN
+
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+from websocket import create_connection
+import datetime
+import time
+from hashlib import md5
+import json
+from termcolor import colored
+import tqdm
+
+TODAY = datetime.datetime.now()
+PASSWORD = "ADSFADSGADSHDAFHDSG@#%!@#T%DSAGADSHDFAGSY@#%@!#^%@#$Y^#$TYDGVDFSGDS!@$!@$" + f"{TODAY.year}-{TODAY.month}"
+
+def cluster(data, n_clusters=2,eps=0.3,min_samples=3, method='kmeans'):
+    
+    if method == 'kmeans':
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(data)
+        return kmeans.labels_
+    elif method == 'dbscan':
+        db = DBSCAN(eps=eps, min_samples=min_samples).fit(data)
+        return db.labels_
+    else:
+        raise ValueError('method must be kmeans or dbscan')
+
+
+def similarity(data, method='cosine'):
+    if method == 'cosine':
+        return cosine_similarity(data).tolist()
+    elif method == 'euclidean':
+        return euclidean_distances(data).tolist()
+    else:
+        raise ValueError('method must be cosine or euclidean')
+
+
+def papers(remote_host,texts, method="dbscan", eps=0.3, min_samples=3, n_clusters=2):
+    """
+    @method : str, default='dbscan' , can used 'kmeans' or 'dbscan' or 'cosine' or 'euclidean'
+        @eps : float, default=0.3, used in dbscan
+        @min_samples : int, default=3, used in dbscan
+        @n_clusters : int, default=2, used in kmeans
+    
+    """
+    ws = create_connection(f"ws://{remote_host}:15000")
+    user_id = md5(time.asctime().encode()).hexdigest()
+    ws.send(json.dumps({"user_id":user_id, "password":PASSWORD}))
+    # time.sleep(0.5)
+    res = ws.recv()
+    if res != "ok":
+        print(colored("[info]:","yellow") ,res)
+        raise Exception("password error")
+    
+    data = json.dumps({
+        "embed_documents":texts,
+        "method":method,
+        "n_clusters":n_clusters,
+        "eps":eps,
+        "min_samples":min_samples
+
+    })
+    msg = send_and_recv(data, ws)
+    return msg
+
+
+def send_and_recv(data, ws):
+    try:
+        for i in tqdm.tqdm(range(0, len(data), 1024*102), desc="sending data"):
+            ws.send(data[i:i+1024*102])
+        ws.send("[STOP]")
+        message = ""
+        total = int(ws.recv())
+        bar = tqdm.tqdm(desc="receiving data", total=total)
+        while 1:
+            res = ws.recv()
+            message += res
+            bar.update(len(res))
+            if message.endswith("[STOP]"):
+                message = message[:-6]
+                break
+        msg = json.loads(message)
+        return msg
+    except Exception as e:
+        raise e
