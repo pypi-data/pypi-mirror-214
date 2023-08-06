ee7324c76156a49ae8fd0d809d798291d0a4e405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:54 +0000

    Add package click-extra/4.4.0/click_extra-4.4.0-py3-none-any.whl

---
+++ packages/click-extra/click_extra-4.4.0-py3-none-any.whl/click_extra/tests/test_version.py
@@ -0,0 +1,143 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from __future__ import annotations
+
+import re
+
+import click
+import pytest
+from pytest_cases import parametrize
+
+from click_extra import Style, echo
+from click_extra.decorators import color_option, extra_group, version_option
+
+from .conftest import command_decorators, skip_windows_colors
+
+
+@skip_windows_colors
+@parametrize("cmd_decorator", command_decorators())
+@parametrize("option_decorator", (version_option, version_option()))
+def test_standalone_version_option(invoke, cmd_decorator, option_decorator):
+    @cmd_decorator
+    @option_decorator
+    def standalone_option():
+        echo("It works!")
+
+    result = invoke(standalone_option, "--version", color=True)
+    assert result.exit_code == 0
+    assert not result.stderr
+    assert re.fullmatch(
+        r"\x1b\[97mstandalone-option\x1b\[0m, version \x1b\[32m\d+.\d+.\d+\x1b\[0m\n",
+        result.output,
+    )
+
+
+@skip_windows_colors
+def test_standalone_version_option_with_env_info(invoke):
+    @click.group
+    @version_option(version="1.2.3.4", print_env_info=True)
+    def color_cli2():
+        echo("It works!")
+
+    # Test default coloring.
+    result = invoke(color_cli2, "--version", color=True)
+    assert result.exit_code == 0
+
+    regex_stdout = (
+        r"\x1b\[97mcolor-cli2\x1b\[0m, version \x1b\[32m1.2.3.4"
+        r"\x1b\[0m\x1b\[90m\n{'.+'}"
+        r"\x1b\[0m\n"
+    )
+    assert re.fullmatch(regex_stdout, result.stdout)
+
+    assert not result.stderr
+
+
+@pytest.mark.xfail(
+    strict=False,
+    reason="version_option always displays click-extra version. See #176.",
+)
+@skip_windows_colors
+@parametrize("cmd_decorator", command_decorators(no_groups=True))
+def test_standalone_version_option_without_env_info(invoke, cmd_decorator):
+    @cmd_decorator
+    @version_option(version="1.2.3.4", print_env_info=False)
+    def color_cli3():
+        echo("It works!")
+
+    # Test default coloring.
+    result = invoke(color_cli3, "--version", color=True)
+    assert result.exit_code == 0
+    assert (
+        result.stdout == "\x1b[97mcolor-cli3\x1b[0m, version \x1b[32m1.2.3.4\x1b[0m\n"
+    )
+    assert not result.stderr
+
+
+@skip_windows_colors
+@pytest.mark.parametrize(
+    "params",
+    (None, "--help", "blah", ("--config", "random.toml")),
+)
+def test_integrated_version_option_precedence(invoke, params):
+    @extra_group(version="1.2.3.4")
+    def color_cli4():
+        echo("It works!")
+
+    result = invoke(color_cli4, "--version", params, color=True)
+    assert result.exit_code == 0
+
+    regex_stdout = (
+        r"\x1b\[97mcolor-cli4\x1b\[0m, version \x1b\[32m1.2.3.4"
+        r"\x1b\[0m\x1b\[90m\n{'.+'}"
+        r"\x1b\[0m\n"
+    )
+    assert re.fullmatch(regex_stdout, result.stdout)
+
+    assert not result.stderr
+
+
+@skip_windows_colors
+def test_color_option_precedence(invoke):
+    """--no-color has an effect on --version, if placed in the right order.
+
+    Eager parameters are evaluated in the order as they were provided on the command
+    line by the user as expleined in:
+    https://click.palletsprojects.com/en/8.0.x/advanced/#callback-evaluation-order
+
+    .. todo::
+
+        Maybe have the possibility to tweak CLI callback evaluation order so we can
+        let the user to have the NO_COLOR env set to allow for color-less ``--version``
+        output.
+    """
+
+    @click.command
+    @color_option
+    @version_option(version="2.1.9")
+    def color_cli6():
+        echo(Style(fg="yellow")("It works!"))
+
+    result = invoke(color_cli6, "--no-color", "--version", "command1", color=True)
+    assert result.exit_code == 0
+    assert result.stdout == "color-cli6, version 2.1.9\n"
+    assert not result.stderr
+
+    result = invoke(color_cli6, "--version", "--no-color", "command1", color=True)
+    assert result.exit_code == 0
+    assert result.stdout == "\x1b[97mcolor-cli6\x1b[0m, version \x1b[32m2.1.9\x1b[0m\n"
+    assert not result.stderr
---
+++ packages/click-extra/click_extra-4.4.0-py3-none-any.whl/click_extra/version.py
@@ -0,0 +1,203 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+"""Extraction of CLI's version and its printing."""
+
+from __future__ import annotations
+
+import inspect
+import re
+from gettext import gettext as _
+from importlib import metadata
+from typing import Sequence
+
+from boltons.ecoutils import get_profile
+
+from . import Context, Parameter, Style, echo
+from .colorize import default_theme
+from .parameters import ExtraOption
+
+
+class VersionOption(ExtraOption):
+    """Prints the colored version of the CLI.
+
+    .. warning::
+        This is a `copy of the standard @click.version_option() decorator
+        <https://github.com/pallets/click/blob/dc918b4/src/click/decorators.py#L399-L466>`_.
+
+        It has been made into a class here, to allow its use with the declarative
+        ``params=`` argument. Which `fixes Click #2324 issue
+        <https://github.com/pallets/click/issues/2324>`_.
+    """
+
+    version: str | None = None
+    package_name: str | None = None
+    prog_name: str | None = None
+    message: str = _("%(prog)s, version %(version)s")
+
+    def guess_package_name(self):
+        package_name = None
+
+        frame = inspect.currentframe()
+        f_back = frame.f_back if frame is not None else None
+        f_globals = f_back.f_globals if f_back is not None else None
+        # break reference cycle
+        # https://docs.python.org/3/library/inspect.html#the-interpreter-stack
+        del frame
+
+        if f_globals is not None:
+            package_name = f_globals.get("__name__")
+
+            if package_name == "__main__":
+                package_name = f_globals.get("__package__")
+
+            if package_name:
+                package_name = package_name.partition(".")[0]
+
+        return package_name
+
+    def print_version(
+        self,
+        ctx: Context,
+        param: Parameter,
+        value: bool,
+        capture_output: bool = False,
+    ) -> str | None:
+        """Prints version and exits.
+
+        Standard callback with an extra ``capture_output`` parameter which returns the
+        output string instead of printing the (colored) version to the console.
+        """
+        if not value or ctx.resilient_parsing:
+            return None
+
+        if self.prog_name is None:
+            self.prog_name = ctx.find_root().info_name
+
+        if self.version is None and self.package_name is not None:
+            try:
+                self.version = metadata.version(self.package_name)
+            except metadata.PackageNotFoundError:
+                msg = (
+                    f"{self.package_name!r} is not installed. Try passing "
+                    "'package_name' instead."
+                )
+                raise RuntimeError(msg) from None
+
+        if self.version is None:
+            msg = (
+                f"Could not determine the version for {self.package_name!r} "
+                "automatically."
+            )
+            raise RuntimeError(msg)
+
+        output = self.message % {
+            "prog": self.prog_name,
+            "package": self.package_name,
+            "version": self.version,
+        }
+
+        if capture_output:
+            return output
+
+        echo(output, color=ctx.color)
+
+        # Do not just ctx.exit() as it will prevent callbacks defined on options
+        # to be called.
+        ctx.close()
+        ctx.exit()
+        return None  # type: ignore[unreachable]
+
+    def __init__(
+        self,
+        param_decls: Sequence[str] | None = None,
+        version: str | None = None,
+        package_name: str | None = None,
+        prog_name: str | None = None,
+        message: str | None = None,
+        print_env_info: bool = False,
+        version_style=Style(fg="green"),
+        package_name_style=default_theme.invoked_command,
+        prog_name_style=default_theme.invoked_command,
+        message_style=None,
+        env_info_style=Style(fg="bright_black"),
+        is_flag=True,
+        expose_value=False,
+        is_eager=True,
+        help=_("Show the version and exit."),
+        **kwargs,
+    ) -> None:
+        """Adds a couple of extra parameters to the standard ``click.version_option``.
+
+        :param version_style: adds environment info at the end of the message. Useful to
+            gather user's details for troubleshooting. Defaults to
+            ``Style(fg="green")``.
+        :param package_name_style: style of the ``version``. Defaults to
+            ``default_theme.invoked_command``.
+        :param prog_name_style: style of the ``prog_name``. Defaults to
+            ``default_theme.invoked_command``.
+        :param message_style: default style of the ``message`` parameter. Defaults to
+            ``None``.
+        :param env_info_style: style of the environment info. Defaults to
+            ``Style(fg="bright_black")``.
+
+        For other params `see Click's version_option decorator
+        <https://click.palletsprojects.com/en/8.1.x/api/#click.version_option>`_.
+        """
+        if not param_decls:
+            param_decls = ("--version",)
+
+        kwargs.setdefault("callback", self.print_version)
+
+        self.version = version
+        self.package_name = package_name
+        self.prog_name = prog_name
+        if message:
+            self.message = message
+
+        if self.version is None and self.package_name is None:
+            self.package_name = self.guess_package_name()
+
+        if print_env_info:
+            env_info = "\n" + str(get_profile(scrub=True))
+            if env_info_style:
+                env_info = env_info_style(env_info)
+            self.message += env_info
+
+        colorized_message = ""
+        for part in re.split(r"(%\(version\)s|%\(package\)s|%\(prog\)s)", self.message):
+            # Skip empty strings.
+            if not part:
+                continue
+            if part == "%(package)s" and package_name_style:
+                part = package_name_style(part)
+            elif part == "%(prog)s" and prog_name_style:
+                part = prog_name_style(part)
+            elif part == "%(version)s" and version_style:
+                part = version_style(part)
+            elif message_style:
+                part = message_style(part)
+            colorized_message += part
+        if colorized_message:
+            self.message = colorized_message
+
+        super().__init__(
+            param_decls=param_decls,
+            is_flag=is_flag,
+            expose_value=expose_value,
+            is_eager=is_eager,
+            help=help,
+            **kwargs,
+        )
