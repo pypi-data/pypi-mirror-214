eac74ec47831d4181b3c46810e5d5d9ea7eaddaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:36 +0000

    Add package pyload-ng/0.5.0b3.dev72/pyload_ng-0.5.0b3.dev72-py3-none-any.whl

---
+++ packages/pyload-ng/pyload_ng-0.5.0b3.dev72-py3-none-any.whl/pyload/plugins/accounts/UptoboxCom.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from ..base.xfs_account import XFSAccount
+
+
+class UptoboxCom(XFSAccount):
+    __name__ = "UptoboxCom"
+    __type__ = "account"
+    __version__ = "0.26"
+    __status__ = "testing"
+
+    __description__ = """Uptobox.com account plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [
+        ("benbox69", "dev@tollet.me"),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
+    ]
+
+    PLUGIN_DOMAIN = "uptobox.eu"
+
+    LOGIN_URL = "https://uptobox.eu/login"
+    LOGIN_SKIP_PATTERN = r"https://uptobox\.eu/logout"
+
+    PREMIUM_PATTERN = r"Premium member"
+
+    VALID_UNTIL_PATTERN = r"class='expiration-date .+?'>(\d{1,2} [\w^_]+ \d{4})"
+
+    def signin(self, user, password, data):
+        html = self.load(self.LOGIN_URL, cookies=self.COOKIES)
+
+        if re.search(self.LOGIN_SKIP_PATTERN, html):
+            self.skip_login()
+
+        html = self.load(
+            self.LOGIN_URL,
+            post={"login": user, "password": password},
+            ref=self.LOGIN_URL,
+            cookies=self.COOKIES,
+        )
+
+        if re.search(self.LOGIN_SKIP_PATTERN, html) is None:
+            self.fail_login()
