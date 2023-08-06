98a44bed4d2f65572ce9871047072abc5143ce3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:04 +0000

    Add package aacommpy/0.1.35/aacommpy-0.1.35.tar.gz

---
+++ packages/aacommpy/aacommpy-0.1.35.tar.gz/aacommpy-0.1.35/aacommpy/settings.py
@@ -0,0 +1,3 @@
+#this array define all of the folder name contain AAComm.dll file of AAComm nuget package
+NET_FRAMEWORK_CHOICES = ['net40', 'net46', 'net48', 'netcoreapp3.1', 'net5.0', 'net6.0','net7.0']
+# there will be 2 constants add into this file name "AACOMM_DLL_PATH" and "AACOMM_SERVER_EXE_PATH" when run "aacommpy install" and "aacommpy update"
