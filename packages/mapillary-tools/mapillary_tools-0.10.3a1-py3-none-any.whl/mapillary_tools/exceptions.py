4c892514794647882d77bd32774231dcbe9dedfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:35 +0000

    Add package mapillary-tools/0.10.3a1/mapillary_tools-0.10.3a1-py3-none-any.whl

---
+++ packages/mapillary-tools/mapillary_tools-0.10.3a1-py3-none-any.whl/mapillary_tools/exceptions.py
@@ -0,0 +1,106 @@
+import typing as T
+
+
+class MapillaryUserError(Exception):
+    exit_code: int
+
+
+class MapillaryBadParameterError(MapillaryUserError):
+    exit_code = 2
+
+
+class MapillaryFileNotFoundError(MapillaryUserError):
+    exit_code = 3
+
+
+class MapillaryInvalidDescriptionFile(MapillaryUserError):
+    exit_code = 4
+
+
+class MapillaryUnknownFileTypeError(MapillaryUserError):
+    exit_code = 5
+
+
+class MapillaryProcessError(MapillaryUserError):
+    exit_code = 6
+
+
+class MapillaryVideoError(MapillaryUserError):
+    exit_code = 7
+
+
+class MapillaryFFmpegNotFoundError(MapillaryUserError):
+    exit_code = 8
+    help = "https://github.com/mapillary/mapillary_tools#video-support"
+
+
+class _MapillaryDescriptionError(Exception):
+    pass
+
+
+class MapillaryGeoTaggingError(_MapillaryDescriptionError):
+    pass
+
+
+class MapillaryGPXEmptyError(_MapillaryDescriptionError, MapillaryUserError):
+    exit_code = 9
+
+
+class MapillaryOutsideGPXTrackError(_MapillaryDescriptionError):
+    def __init__(
+        self, message: str, image_time: str, gpx_start_time: str, gpx_end_time: str
+    ):
+        super().__init__(message)
+        self.image_time = image_time
+        self.gpx_start_time = gpx_start_time
+        self.gpx_end_time = gpx_end_time
+
+
+class MapillaryStationaryVideoError(_MapillaryDescriptionError, MapillaryUserError):
+    exit_code = 10
+
+
+class MapillaryInvalidBlackVueVideoError(
+    _MapillaryDescriptionError, MapillaryUserError
+):
+    exit_code = 11
+
+
+class MapillaryDuplicationError(_MapillaryDescriptionError):
+    def __init__(
+        self,
+        message: str,
+        desc: T.Mapping,
+        distance: float,
+        angle_diff: T.Optional[float],
+    ) -> None:
+        super().__init__(message)
+        self.desc = desc
+        self.distance = distance
+        self.angle_diff = angle_diff
+
+
+class MapillaryUploadedAlreadyError(_MapillaryDescriptionError):
+    def __init__(
+        self,
+        message: str,
+        desc: T.Mapping,
+    ) -> None:
+        super().__init__(message)
+        self.desc = desc
+
+
+class MapillaryEXIFNotFoundError(_MapillaryDescriptionError):
+    pass
+
+
+class MapillaryUploadConnectionError(MapillaryUserError):
+    exit_code = 12
+
+
+class MapillaryUploadTimeoutError(MapillaryUserError):
+    exit_code = 13
+
+
+class MapillaryUploadUnauthorizedError(MapillaryUserError):
+    exit_code = 14
---
+++ packages/mapillary-tools/mapillary_tools-0.10.3a1-py3-none-any.whl/mapillary_tools/utils.py
@@ -0,0 +1,168 @@
+import hashlib
+import os
+import typing as T
+from pathlib import Path
+
+
+# Use "hashlib._Hash" instead of hashlib._Hash because:
+# AttributeError: module 'hashlib' has no attribute '_Hash'
+def md5sum_fp(
+    fp: T.IO[bytes], md5: T.Optional["hashlib._Hash"] = None
+) -> "hashlib._Hash":
+    if md5 is None:
+        md5 = hashlib.md5()
+    while True:
+        buf = fp.read(1024 * 1024 * 32)
+        if not buf:
+            break
+        md5.update(buf)
+    return md5
+
+
+def is_image_file(path: Path) -> bool:
+    return path.suffix.lower() in (".jpg", ".jpeg", ".tif", ".tiff", ".pgm", ".pnm")
+
+
+def is_video_file(path: Path) -> bool:
+    return path.suffix.lower() in (
+        ".mp4",
+        ".avi",
+        ".tavi",
+        ".mov",
+        ".mkv",
+        # GoPro Max video filename extension
+        ".360",
+        # Insta360 X3
+        ".insv",
+    )
+
+
+def iterate_files(
+    root: Path, recursive: bool = False, follow_hidden_dirs: bool = False
+) -> T.Generator[Path, None, None]:
+    for dirpath, dirnames, files in os.walk(root, topdown=True):
+        if not recursive:
+            dirnames.clear()
+        else:
+            if not follow_hidden_dirs:
+                dirnames[:] = [name for name in dirnames if not name.startswith(".")]
+        for file in files:
+            if file.startswith("."):
+                continue
+            yield Path(dirpath).joinpath(file)
+
+
+def filter_video_samples(
+    image_paths: T.Sequence[Path],
+    video_path: Path,
+    skip_subfolders: bool = False,
+) -> T.Generator[Path, None, None]:
+    if video_path.is_dir():
+        video_basenames = set(
+            f.name
+            for f in iterate_files(video_path, not skip_subfolders)
+            if is_video_file(f)
+        )
+    else:
+        video_basenames = {video_path.name}
+
+    for image_path in image_paths:
+        # If you want to walk an arbitrary filesystem path upwards,
+        # it is recommended to first call Path.resolve() so as to resolve symlinks and eliminate “..” components.
+        image_dirname = image_path.resolve().parent.name
+        if image_dirname in video_basenames:
+            root, _ = os.path.splitext(image_dirname)
+            if image_path.name.startswith(root + "_"):
+                yield image_path
+
+
+def deduplicate_paths(paths: T.Iterable[Path]) -> T.Generator[Path, None, None]:
+    resolved_paths: T.Set[Path] = set()
+    for p in paths:
+        resolved = p.resolve()
+        if resolved not in resolved_paths:
+            resolved_paths.add(resolved)
+            yield p
+
+
+def find_images(
+    import_paths: T.Sequence[Path],
+    skip_subfolders: bool = False,
+    check_file_suffix: bool = False,
+) -> T.List[Path]:
+    image_paths: T.List[Path] = []
+    for path in import_paths:
+        if path.is_dir():
+            image_paths.extend(
+                file
+                for file in iterate_files(path, not skip_subfolders)
+                if is_image_file(file)
+            )
+        else:
+            if check_file_suffix:
+                if is_image_file(path):
+                    image_paths.append(path)
+            else:
+                image_paths.append(path)
+    return list(deduplicate_paths(image_paths))
+
+
+def find_videos(
+    import_paths: T.Sequence[Path],
+    skip_subfolders: bool = False,
+    check_file_suffix: bool = False,
+) -> T.List[Path]:
+    video_paths: T.List[Path] = []
+    for path in import_paths:
+        if path.is_dir():
+            video_paths.extend(
+                file
+                for file in iterate_files(path, not skip_subfolders)
+                if is_video_file(file)
+            )
+        else:
+            if check_file_suffix:
+                if is_video_file(path):
+                    video_paths.append(path)
+            else:
+                video_paths.append(path)
+    return list(deduplicate_paths(video_paths))
+
+
+def find_zipfiles(
+    import_paths: T.Sequence[Path],
+    skip_subfolders: bool = False,
+    check_file_suffix: bool = False,
+) -> T.List[Path]:
+    zip_paths: T.List[Path] = []
+    for path in import_paths:
+        if path.is_dir():
+            zip_paths.extend(
+                file
+                for file in iterate_files(path, not skip_subfolders)
+                if file.suffix.lower() in [".zip"]
+            )
+        else:
+            if check_file_suffix:
+                if path.suffix.lower() in [".zip"]:
+                    zip_paths.append(path)
+            else:
+                zip_paths.append(path)
+    return list(deduplicate_paths(zip_paths))
+
+
+def find_xml_files(import_paths: T.Sequence[Path]) -> T.List[Path]:
+    xml_paths: T.List[Path] = []
+    for path in import_paths:
+        if path.is_dir():
+            # XML could be hidden in hidden folders
+            # for example: exiftool -progress -w! /tmp/exiftool_outputs/%d%f.xml -r -n -ee -api LargeFileSupport=1 -X /path/to/.hidden_dirs/images/example.jpg
+            # The XML output will be /tmp/exiftool_outputs/path/to/.hidden_dirs/images/example.jpg
+            xml_paths.extend(
+                file
+                for file in iterate_files(path, recursive=True, follow_hidden_dirs=True)
+                if file.suffix.lower() in [".xml"]
+            )
+        else:
+            xml_paths.append(path)
+    return list(deduplicate_paths(xml_paths))
