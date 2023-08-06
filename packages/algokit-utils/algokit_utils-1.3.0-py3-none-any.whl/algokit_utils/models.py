3ab54680ab56ad24d3ed2dd805a2da606eb45cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:25 +0000

    Add package algokit-utils/1.3.0/algokit_utils-1.3.0-py3-none-any.whl

---
+++ packages/algokit-utils/algokit_utils-1.3.0-py3-none-any.whl/algokit_utils/models.py
@@ -0,0 +1,189 @@
+import dataclasses
+from collections.abc import Sequence
+from typing import Any, Generic, Protocol, TypeAlias, TypedDict, TypeVar
+
+import algosdk.account
+from algosdk import transaction
+from algosdk.abi import Method
+from algosdk.atomic_transaction_composer import (
+    AccountTransactionSigner,
+    AtomicTransactionResponse,
+    SimulateAtomicTransactionResponse,
+    TransactionSigner,
+)
+from algosdk.encoding import decode_address
+
+ReturnType = TypeVar("ReturnType")
+
+
+@dataclasses.dataclass(kw_only=True)
+class Account:
+    """Holds the private_key and address for an account"""
+
+    private_key: str
+    """Base64 encoded private key"""
+    address: str = dataclasses.field(default="")
+    """Address for this account"""
+
+    def __post_init__(self) -> None:
+        if not self.address:
+            self.address = algosdk.account.address_from_private_key(self.private_key)  # type: ignore[no-untyped-call]
+
+    @property
+    def public_key(self) -> bytes:
+        """The public key for this account"""
+        public_key = decode_address(self.address)  # type: ignore[no-untyped-call]
+        assert isinstance(public_key, bytes)
+        return public_key
+
+    @property
+    def signer(self) -> AccountTransactionSigner:
+        """An AccountTransactionSigner for this account"""
+        return AccountTransactionSigner(self.private_key)
+
+    @staticmethod
+    def new_account() -> "Account":
+        private_key, address = algosdk.account.generate_account()  # type: ignore[no-untyped-call]
+        return Account(private_key=private_key)
+
+
+@dataclasses.dataclass(kw_only=True)
+class TransactionResponse:
+    """Response for a non ABI call"""
+
+    tx_id: str
+    """Transaction Id"""
+    confirmed_round: int | None
+    """Round transaction was confirmed, `None` if call was a from a dry-run"""
+
+    @staticmethod
+    def from_atr(
+        result: AtomicTransactionResponse | SimulateAtomicTransactionResponse, transaction_index: int = -1
+    ) -> "TransactionResponse":
+        """Returns either an ABITransactionResponse or a TransactionResponse based on the type of the transaction
+        referred to by transaction_index
+        :param AtomicTransactionResponse result: Result containing one or more transactions
+        :param int transaction_index: Which transaction in the result to return, defaults to -1 (the last transaction)
+        """
+        tx_id = result.tx_ids[transaction_index]
+        abi_result = next((r for r in result.abi_results if r.tx_id == tx_id), None)
+        confirmed_round = None if isinstance(result, SimulateAtomicTransactionResponse) else result.confirmed_round
+        if abi_result:
+            return ABITransactionResponse(
+                tx_id=tx_id,
+                raw_value=abi_result.raw_value,
+                return_value=abi_result.return_value,
+                decode_error=abi_result.decode_error,
+                tx_info=abi_result.tx_info,
+                method=abi_result.method,
+                confirmed_round=confirmed_round,
+            )
+        else:
+            return TransactionResponse(
+                tx_id=tx_id,
+                confirmed_round=confirmed_round,
+            )
+
+
+@dataclasses.dataclass(kw_only=True)
+class ABITransactionResponse(TransactionResponse, Generic[ReturnType]):
+    """Response for an ABI call"""
+
+    raw_value: bytes
+    """The raw response before ABI decoding"""
+    return_value: ReturnType
+    """Decoded ABI result"""
+    decode_error: Exception | None
+    """Details of error that occurred when attempting to decode raw_value"""
+    tx_info: dict
+    """Details of transaction"""
+    method: Method
+    """ABI method used to make call"""
+
+
+ABIArgType = Any
+ABIArgsDict = dict[str, ABIArgType]
+
+
+class ABIReturnSubroutine(Protocol):
+    def method_spec(self) -> Method:
+        ...
+
+
+ABIMethod: TypeAlias = ABIReturnSubroutine | Method | str
+
+
+@dataclasses.dataclass(kw_only=True)
+class TransactionParameters:
+    """Transaction parameters that can be used on ABI and non-ABI calls"""
+
+    signer: TransactionSigner | None = None
+    sender: str | None = None
+    suggested_params: transaction.SuggestedParams | None = None
+    note: bytes | str | None = None
+    lease: bytes | str | None = None
+    boxes: Sequence[tuple[int, bytes | bytearray | str | int]] | None = None
+
+
+@dataclasses.dataclass(kw_only=True)
+class CreateTransactionParameters(TransactionParameters):
+    """Transaction parameters that can be used on ABI and non-ABI create calls"""
+
+    extra_pages: int | None = None
+
+
+@dataclasses.dataclass(kw_only=True)
+class RawTransactionParameters(TransactionParameters):
+    """Transaction parameters that can be used on non-ABI calls"""
+
+    accounts: list[str] | None = None
+    foreign_apps: list[int] | None = None
+    foreign_assets: list[int] | None = None
+
+
+@dataclasses.dataclass(kw_only=True)
+class CommonCallParameters(RawTransactionParameters):
+    """Transaction parameters used when making update, delete, opt_in, close_out or clear_state calls"""
+
+    rekey_to: str | None = None
+
+
+@dataclasses.dataclass(kw_only=True)
+class OnCompleteCallParameters(CommonCallParameters):
+    """Transaction parameters used when making any call to an Application"""
+
+    on_complete: transaction.OnComplete | None = None
+
+
+@dataclasses.dataclass(kw_only=True)
+class CreateCallParameters(OnCompleteCallParameters):
+    """Transaction parameters used when making a create call for Application"""
+
+    extra_pages: int | None = None
+
+
+class CommonCallParametersDict(TypedDict, total=False):
+    """Common transaction parameters used when making update, delete, opt_in, close_out or clear_state calls"""
+
+    signer: TransactionSigner
+    sender: str
+    suggested_params: transaction.SuggestedParams
+    note: bytes | str
+    lease: bytes | str
+    accounts: list[str]
+    foreign_apps: list[int]
+    foreign_assets: list[int]
+    boxes: Sequence[tuple[int, bytes | bytearray | str | int]]
+    rekey_to: str
+
+
+class OnCompleteCallParametersDict(TypedDict, CommonCallParametersDict, total=False):
+    """Transaction parameters used when making any call to an Application"""
+
+    on_complete: transaction.OnComplete
+
+
+class CreateCallParametersDict(TypedDict, OnCompleteCallParametersDict, total=False):
+    """Transaction parameters used when making a create call for Application"""
+
+    extra_pages: int
