b308f8bab2c4c11c586964f3650ed40774370bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:50 +0000

    Add package lispi/0.0.2/lispi-0.0.2.tar.gz

---
+++ packages/lispi/lispi-0.0.2.tar.gz/lispi-0.0.2/src/lispy/__init__.py
@@ -0,0 +1,14 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+
+__all__ = [
+        'slideEdit',
+        'text2audio',
+        'revealjs_template',
+        ]
+
+# Version of the ppr package
+__version__ = "0.0.02"
