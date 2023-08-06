6795140c70b343f6cd44d7d6fc64e0d827b22aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:53 +0000

    Add package forestadmin-datasource-toolkit/0.1.0b35/forestadmin_datasource_toolkit-0.1.0b35-py3-none-any.whl

---
+++ packages/forestadmin-datasource-toolkit/forestadmin_datasource_toolkit-0.1.0b35-py3-none-any.whl/forestadmin/datasource_toolkit/context/agent_context.py
@@ -0,0 +1,14 @@
+from forestadmin.agent_toolkit.utils.context import User
+from forestadmin.datasource_toolkit.context.relaxed_wrappers.collection import RelaxedDatasource
+from forestadmin.datasource_toolkit.interfaces.collections import Collection
+from forestadmin.datasource_toolkit.interfaces.models.collections import Datasource
+
+
+class AgentCustomizationContext:
+    def __init__(self, datasource: Datasource[Collection], caller: User):
+        self._datasource = datasource
+        self._caller = caller
+
+    @property
+    def datasource(self) -> RelaxedDatasource:
+        return RelaxedDatasource(self._datasource)
