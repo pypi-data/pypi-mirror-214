f13f08d6b3df8b9f5e34ee8606aa35201f531b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:56 +0000

    Add package stackoverflowapithingy/0.1.14/stackoverflowapithingy-0.1.14.tar.gz

---
+++ packages/stackoverflowapithingy/stackoverflowapithingy-0.1.14.tar.gz/stackoverflowapithingy-0.1.14/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="stackoverflowapithingy",
+    version="0.1.14",
+    packages=["stackoverflowapithingy"],
+    install_requires=["requests","beautifulsoup4"],
+    download_url="https://github.com/hyphen-slash/stackoverflowapithingy/archive/refs/tags/0.1.14.tar.gz"
+)
