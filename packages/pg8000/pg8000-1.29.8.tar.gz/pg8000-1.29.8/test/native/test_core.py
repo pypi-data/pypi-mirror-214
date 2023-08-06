b9dd3297be4b8741d6810ab10039e58ae9622033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:44 +0000

    Add package pg8000/1.29.8/pg8000-1.29.8.tar.gz

---
+++ packages/pg8000/pg8000-1.29.8.tar.gz/pg8000-1.29.8/test/native/test_core.py
@@ -0,0 +1,51 @@
+from io import BytesIO
+
+import pytest
+
+from pg8000.core import (
+    Context,
+    CoreConnection,
+    NULL_BYTE,
+    PASSWORD,
+    _create_message,
+    _read,
+)
+from pg8000.native import InterfaceError
+
+
+def test_handle_AUTHENTICATION_3(mocker):
+    """Shouldn't send a FLUSH message, as FLUSH only used in extended-query"""
+
+    mocker.patch.object(CoreConnection, "__init__", lambda x: None)
+    con = CoreConnection()
+    password = "barbour".encode("utf8")
+    con.password = password
+    con._sock = mocker.Mock()
+    buf = BytesIO()
+    con._sock.write = buf.write
+    CoreConnection.handle_AUTHENTICATION_REQUEST(con, b"\x00\x00\x00\x03", None)
+    assert buf.getvalue() == _create_message(PASSWORD, password + NULL_BYTE)
+
+
+def test_create_message():
+    msg = _create_message(PASSWORD, "barbour".encode("utf8") + NULL_BYTE)
+    assert msg == b"p\x00\x00\x00\x0cbarbour\x00"
+
+
+def test_handle_ERROR_RESPONSE(mocker):
+    """Check it handles invalid encodings in the error messages"""
+
+    mocker.patch.object(CoreConnection, "__init__", lambda x: None)
+    con = CoreConnection()
+    con._client_encoding = "utf8"
+    data = b"S\xc2err" + NULL_BYTE + NULL_BYTE
+    context = Context(None)
+    CoreConnection.handle_ERROR_RESPONSE(con, data, context)
+    assert str(context.error) == "{'S': '�err'}"
+
+
+def test_read(mocker):
+    mock_socket = mocker.Mock()
+    mock_socket.read = mocker.Mock(return_value=b"")
+    with pytest.raises(InterfaceError, match="network error"):
+        _read(mock_socket, 5)
