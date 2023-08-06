d38efea0ab12ade8178e3022919d54d290b7994b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:34 +0000

    Add package langchain-serve/0.0.46.dev3/langchain-serve-0.0.46.dev3.tar.gz

---
+++ packages/langchain-serve/langchain-serve-0.0.46.dev3.tar.gz/langchain-serve-0.0.46.dev3/lcserve/errors.py
@@ -0,0 +1,16 @@
+class InvalidInstanceError(ValueError):
+    def __init__(self, instance):
+        super().__init__("Invalid instance: {}".format(instance))
+        self.instance = instance
+
+
+class InvalidAutoscaleMinError(ValueError):
+    def __init__(self, min):
+        super().__init__("Invalid autoscale.min: {}".format(min))
+        self.min = min
+
+
+class InvalidDiskSizeError(ValueError):
+    def __init__(self, disk_size):
+        super().__init__("Invalid disk size: {}".format(disk_size))
+        self.disk_size = disk_size
