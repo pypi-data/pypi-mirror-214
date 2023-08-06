07aa7361a18b3fcf6dcbf6567e8b48968ee7d844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:55 +0000

    Add package blendersynth/0.0.2/blendersynth-0.0.2-py3-none-any.whl

---
+++ packages/blendersynth/blendersynth-0.0.2-py3-none-any.whl/blendersynth/run_this_script.py
@@ -0,0 +1,19 @@
+"""Quick utility to run the current script from Blender"""
+import subprocess
+import inspect
+import sys
+from .utils.blender_setup.blender_locator import get_blender_path
+
+def run_this_script(debug=False):
+	"""Run the current script from Blender"""
+	blender_path = get_blender_path()
+	if blender_path != sys.argv[0]:  # if blender is not running this script
+
+		caller_path = inspect.stack()[1].filename # path of script that called this function
+
+		commands = [blender_path] + \
+			['--background'] * (not debug) + \
+			['--python', caller_path]
+
+		subprocess.call(commands)
+		sys.exit() # exit the script once blender is finished
