2ac2208c70b13760b91f42467dd3291afbb974d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package fastgenerateapi/0.0.4/fastgenerateapi-0.0.4-py2.py3-none-any.whl

---
+++ packages/fastgenerateapi/fastgenerateapi-0.0.4-py2.py3-none-any.whl/fastgenerateapi/__init__.py
@@ -0,0 +1,20 @@
+#           dP       oo   dP oo
+#           88            88
+#  .d8888b. 88d888b. dP   88 dP .d8888b. 88d888b. .d8888b.
+#  Y8ooooo. 88'  `88 88   88 88 88'  `88 88'  `88 88'  `88
+#        88 88    88 88   88 88 88.  .88 88    88 88.  .88
+#  `88888P' dP    dP dP   dP dP `88888P8 dP    dP `8888P88
+#                                                      .88
+#                                                  d8888P
+
+
+from fastgenerateapi.settings.register_settings import settings
+from fastgenerateapi.api_view.api_view import APIView, CreateView, GetOneView, GetAllView, UpdateView, DeleteView, \
+    SwitchView
+from fastgenerateapi.api_view.get_tree_view import GetTreeView
+from fastgenerateapi.api_view.delete_tree_view import DeleteTreeView
+from fastgenerateapi.api_view.base_view import BaseView
+from fastgenerateapi.settings.settings import SettingsModel
+
+
+
