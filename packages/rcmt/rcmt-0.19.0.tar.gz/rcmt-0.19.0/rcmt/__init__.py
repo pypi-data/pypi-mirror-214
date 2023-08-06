f27e46d0266da310a2f9f7845a2c91507605b349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:58 +0000

    Add package rcmt/0.19.0/rcmt-0.19.0.tar.gz

---
+++ packages/rcmt/rcmt-0.19.0.tar.gz/rcmt-0.19.0/rcmt/__init__.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+
+from .rcmt import execute, options_from_config
+from .task import Run, Task
+from .verify import execute as execute_verify
+
+__all__ = ["Run", "Task", "execute", "execute_verify", "options_from_config"]
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except Exception:
+    __version__ = "develop"
