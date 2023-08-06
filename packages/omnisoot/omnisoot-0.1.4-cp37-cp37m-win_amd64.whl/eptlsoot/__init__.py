58970cbb0aa365ece8e191d133bab3a3fadc4c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:04 +0000

    Add package omnisoot/0.1.4/omnisoot-0.1.4-cp37-cp37m-win_amd64.whl

---
+++ packages/omnisoot/omnisoot-0.1.4-cp37-cp37m-win_amd64.whl/eptlsoot/__init__.py
@@ -0,0 +1,9 @@
+from .eptlsoot import SootModel, Reactor, ReactorSoot, FlameSolver, FlameSolverOpt
+from .cpfr import CPFR
+from .apps.sootgas import SootGas
+from .apps.sootmodels import MonodisperseSootModel
+from .apps.reactors import PlugFlowReactor, ConstantVolumeReactor, Inlet, PerfectlyStirredReactor
+from .apps.pahgrowth import ReactDim, DimerCoal
+from .apps.flame import TempFlameSolver, TempFlameSolverOpt, FVSolver, FDSolver
+from .apps.sootwrappers import SootWrapper
+from .apps.solutionarray import SolutionArray
