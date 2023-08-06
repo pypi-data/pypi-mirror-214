29c11f6acd9b2ed4ec9b9f1d69afb9e057eaa013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:22 +0000

    Add package numerical-py/0.0.38/numerical_py-0.0.38-py3-none-any.whl

---
+++ packages/numerical-py/numerical_py-0.0.38-py3-none-any.whl/numerical_py/__init__.py
@@ -0,0 +1,6 @@
+from .integrals import Integrate
+from .eigenvals import Eigenvals
+from .equations import LinearSystemEquations, NonLinearSystemEquations, Equations
+from .interpolation import Interpolate
+from .differentional import DifferentialEquation
+from .derivative import Derivative
