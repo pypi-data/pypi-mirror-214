36a497dfef60a49f64bed90e504fe9b87bab0edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package xcoll/0.2.2/xcoll-0.2.2.tar.gz

---
+++ packages/xcoll/xcoll-0.2.2.tar.gz/xcoll-0.2.2/xcoll/__init__.py
@@ -0,0 +1,9 @@
+from .general import _pkg_root
+
+from .beam_elements import BaseCollimator, BlackAbsorber, EverestCollimator, EverestCrystal
+from .scattering_routines.everest import materials
+from .scattering_routines.everest.materials import GeneralMaterial, Material, CrystalMaterial
+from .manager import CollimatorManager
+from .colldb import CollimatorDatabase, load_SixTrack_colldb
+
+__version__ = '0.2.2'
