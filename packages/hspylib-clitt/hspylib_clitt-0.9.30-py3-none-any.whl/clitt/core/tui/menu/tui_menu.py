91234389ba07cbcb2c7030ef4820fad238d6c163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:27 +0000

    Add package hspylib-clitt/0.9.30/hspylib_clitt-0.9.30-py3-none-any.whl

---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.30-py3-none-any.whl/clitt/core/tui/menu/tui_menu.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib
+   @package: clitt.core.tui.menu
+      @file: tui_menu.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2023, HsPyLib team
+"""
+
+from abc import ABC
+from clitt.core.icons.font_awesome.nav_icons import NavIcons
+from clitt.core.tui.tui_component import TUIComponent
+from hspylib.core.tools.commons import sysout
+from hspylib.modules.cli.keyboard import Keyboard
+from typing import Callable, Optional, TypeVar
+
+ON_TRIGGER_CB = TypeVar("ON_TRIGGER_CB", bound=Callable[["TUIMenu"], Optional["TUIMenu"]])
+
+
+class TUIMenu(TUIComponent["TUIMenu"], ABC):
+    """Provide a base class for terminal UI menus."""
+
+    @staticmethod
+    def wait_keystroke(wait_message: str = None) -> None:
+        """Wait for a keypress (blocking).
+        :param wait_message: the message to present to the user.
+        """
+        sysout(wait_message or "%YELLOW%%EOL%Press any key to continue%EOL%%NC%")
+        Keyboard.wait_keystroke()
+
+    def __init__(self, parent: Optional["TUIMenu"] = None, title: Optional[str] = None, tooltip: Optional[str] = None):
+        super().__init__(title or "")
+
+        self._tooltip: str = tooltip or ""
+        self._parent: Optional["TUIMenu"] = parent
+        self._on_trigger: ON_TRIGGER_CB = self._default_trigger_cb
+
+    def __str__(self) -> str:
+        return self._title
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    @property
+    def parent(self) -> Optional["TUIMenu"]:
+        return self._parent
+
+    @property
+    def tooltip(self) -> str:
+        return self._tooltip
+
+    def _handle_keypress(self) -> Keyboard:
+        if keypress := Keyboard.wait_keystroke():
+            match keypress:
+                case Keyboard.VK_ENTER | Keyboard.VK_ESC:
+                    self._done = True
+
+        return keypress
+
+    def _navbar(self, **kwargs) -> str:
+        return (
+            f"%EOL%{self._breadcrumb()}%ED0%%NC%"
+            f"%EOL%{self.prefs.navbar_color.placeholder}%EOL%"
+            f"[Enter] Back  [Esc] Quit  %NC%%EL0%%EOL%%EOL%"
+        )
+
+    def _breadcrumb(self) -> str:
+        """Provide a breadcrumb of menus for navigation purposes."""
+        return (
+            f"{self.prefs.breadcrumb_color.placeholder}"
+            f" {NavIcons.BREADCRUMB} {self._title} %NC%"
+            f"{self.prefs.tooltip_color.placeholder}"
+        )
+
+    def _default_trigger_cb(self, source: Optional["TUIMenu"]) -> Optional["TUIMenu"]:
+        """Provide a default trigger callback when a menu is activated. Provided a source menu, returns it's parent.
+        :param source: the source which invoked this menu.
+        """
+        return self._parent
---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.30-py3-none-any.whl/clitt/core/tui/minput/minput_utils.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib
+   @package: clitt.core.tui.minput
+      @file: minput_utils.py
+   @created: Thu, 20 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2023, HsPyLib team
+"""
+from abc import ABC
+from hspylib.core.exception.exceptions import InvalidInputError
+from hspylib.core.preconditions import check_argument
+from hspylib.core.tools.commons import sysout
+from typing import Any, Optional, Tuple
+
+import re
+
+MASK_SYMBOLS = ["#", "@", "%", "*"]
+
+
+class MInputUtils(ABC):
+    """MenuInput utilities."""
+
+    @staticmethod
+    def detail_len(field: Any) -> int:
+        """Return the length of the details, according to the form field.
+        :param field: the form field to get details.
+        """
+        max_len = len(str(field.max_length))
+        return 1 + (2 * max_len)
+
+    @staticmethod
+    def mi_print(size: int = 0, text: str = None, prepend: str = None, end: str = "") -> None:
+        """Special menu input print.
+        :param size: the length of the field.
+        :param text: the text to be printed.
+        :param prepend the text to prepend to the print.
+        :param end: string appended after the last value, default an empty string.
+        """
+        fmt = ("{}" if prepend else "") + "{:<" + str(size) + "} : "
+        if prepend:
+            sysout(fmt.format(prepend, text), end=end)
+        else:
+            sysout(fmt.format(text or ""), end=end)
+
+    @staticmethod
+    def toggle_selected(tokenized_values: str) -> str:
+        """Toggle the 'select' component value. Selects the next value on the token sequence.
+        :param tokenized_values: the 'select' component tokenized values.
+        """
+        values = re.split("[|,;]", tokenized_values)
+        cur_idx = next((idx for idx, val in enumerate(values) if val.find("<") >= 0), -1)
+        if cur_idx < 0:
+            if len(values) > 1:
+                values[1] = f"<{values[1]}>"
+            else:
+                values[0] = f"<{values[0]}>"
+            return "|".join(values)
+        unselected = list(map(lambda x: x.replace("<", "").replace(">", ""), values))
+        # fmt: off
+        return '|'.join([
+            f'<{val}>'
+            if idx == (cur_idx + 1) or ((cur_idx + 1) >= len(unselected) and idx == 0)
+            else val for idx, val in enumerate(unselected)
+        ])
+        # fmt: on
+
+    @staticmethod
+    def get_selected(tokenized_values: str) -> Optional[Tuple[int, str]]:
+        """Get the selected value from the 'select' component tokens.
+        :param tokenized_values: the 'select' component tokenized values.
+        """
+        values = re.split("[|,;]", tokenized_values)
+        # fmt: off
+        sel_item = next((
+            val.replace('<', '').replace('>', '')
+            for val in values if val.startswith('<') and val.endswith('>')
+        ), values[0])
+        # fmt: on
+        try:
+            return values.index(sel_item), sel_item
+        except ValueError:
+            try:
+                return values.index(f"<{sel_item}>"), sel_item
+            except ValueError:
+                return -1, sel_item
+
+    @staticmethod
+    def unpack_masked(value: str) -> Tuple[str, str]:
+        """Unpack all values of the 'masked' component.
+        :param value: the masked values to unpack.
+        """
+        if value:
+            parts = re.split("[|,;]", value)
+            check_argument(len(parts) == 2, "Invalid masked value: {}", value)
+            return parts[0], parts[1]
+
+        return '', ''
+
+    @staticmethod
+    def append_masked(value: str, mask: str, keypress_value: chr) -> str:
+        """Append a value to the masked field.
+        :param value: the masked field current value.
+        :param mask: the masked field's mask.
+        :param keypress_value: the value to append (it can be a part of the mask itself).
+        """
+        idx = len(value)
+        if keypress_value == mask[idx]:
+            return f"{value}{keypress_value}|{mask}"
+        masked_value = value
+        while idx < len(mask) and mask[idx] not in MASK_SYMBOLS:
+            masked_value += mask[idx]
+            idx += 1
+        if mask and idx < len(mask):
+            mask_regex = mask[idx] \
+                .replace("#", "[0-9]") \
+                .replace("@", "[a-zA-Z]") \
+                .replace("%", "[a-zA-Z0-9]") \
+                .replace("*", ".")
+            if re.search(mask_regex, keypress_value):
+                masked_value += keypress_value
+            else:
+                raise InvalidInputError(f"Value {keypress_value} is not a valid value against mask: {mask}")
+
+        return f"{masked_value}|{mask}"
+
+    @staticmethod
+    def over_masked(value: str, mask: str, placeholder: str = "_") -> str:
+        """Return the value to be printed by a 'masked' input field. A placeholder value will be used for unfilled
+        values.
+        """
+        over_masked_val = ""
+        for idx, element in enumerate(mask):
+            if element in MASK_SYMBOLS:  # Digit, Letter or alphanumeric masks.
+                over_masked_val += value[idx] if idx < len(value) else placeholder
+            else:
+                over_masked_val += element
+
+        return over_masked_val
