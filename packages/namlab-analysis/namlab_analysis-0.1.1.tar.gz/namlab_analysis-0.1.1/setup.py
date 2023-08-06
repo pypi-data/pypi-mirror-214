255c34874905ab1fcab2bb52bc340fe993cac976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:08 +0000

    Add package namlab-analysis/0.1.1/namlab_analysis-0.1.1.tar.gz

---
+++ packages/namlab-analysis/namlab_analysis-0.1.1.tar.gz/namlab_analysis-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='namlab_analysis',
+    version='0.1.1',
+    description='analysis codes for namboodiri lab',
+    author='huijeong',
+    author_email='huijeong.jeong00@gmail.com',
+    url='https://github.com/namboodirilab/general',
+    keywords=['namlab','namboodiri'],
+    packages=find_packages(),
+    package_data={},
+    zip_safe=False,
+    py_modules=[]
+)
