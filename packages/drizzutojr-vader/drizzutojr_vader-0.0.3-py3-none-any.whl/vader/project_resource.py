b121aec56e826ffc2a76fe823f3904c203749cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:42 +0000

    Add package drizzutojr-vader/0.0.3/drizzutojr_vader-0.0.3-py3-none-any.whl

---
+++ packages/drizzutojr-vader/drizzutojr_vader-0.0.3-py3-none-any.whl/vader/project_resource.py
@@ -0,0 +1,14 @@
+from .mongo import MongoResource
+from .general import generate_project_name
+
+
+class ProjectResource(MongoResource):
+    def __init__(
+        self, namespace, app_id, boundary_id, requestor, collection_name, request_data
+    ):
+        super().__init__(app_id, collection_name)
+        self.namespace = namespace
+        self.boundary_id = boundary_id
+        self.project_name = utils.generate_project_name(self.app_id, self.boundary_id)
+        self.requestor = requestor
+        self.request_data = request_data
