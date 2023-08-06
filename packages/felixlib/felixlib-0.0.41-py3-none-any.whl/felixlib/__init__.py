300638c65db69c70d8540e41deb84f255c012a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:10 +0000

    Add package felixlib/0.0.41/felixlib-0.0.41-py3-none-any.whl

---
+++ packages/felixlib/felixlib-0.0.41-py3-none-any.whl/felixlib/__init__.py
@@ -0,0 +1,4 @@
+from .init import *
+
+# normally only need one specific module of this package,
+# so import nothing in `__init__.py` but import everything in `__main__`
---
+++ packages/felixlib/felixlib-0.0.41-py3-none-any.whl/felixlib/__main__.py
@@ -0,0 +1,6 @@
+from .const import README  # needed for `ui_dpg.mvLogger`
+from .main import *
+
+
+if __name__ == '__main__':
+    main()
