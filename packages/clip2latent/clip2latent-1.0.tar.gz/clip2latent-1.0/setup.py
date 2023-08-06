67c41c5aad496643863f381d6f79595d63453c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:32 +0000

    Add package clip2latent/1.0/clip2latent-1.0.tar.gz

---
+++ packages/clip2latent/clip2latent-1.0.tar.gz/clip2latent-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='clip2latent',
+    packages=['clip2latent'],
+    version='1.0',
+    description='Official code for clip2latent',
+    author='Justin Pinkney',
+    package_data = {'clip2latent': ['stylegan3/**/*']}
+)
