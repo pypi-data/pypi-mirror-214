6127305bc5a9428e1dc853a8e4078db93c6b0b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:51 +0000

    Add package halborn-ctf/0.1.15/halborn_ctf-0.1.15.tar.gz

---
+++ packages/halborn-ctf/halborn_ctf-0.1.15.tar.gz/halborn_ctf-0.1.15/src/halborn_ctf/network/filters/_json_rpc/whitelist_json_rpc_method.py
@@ -0,0 +1,43 @@
+from mitmproxy import ctx
+from mitmproxy import http
+import re
+import json
+
+class WhitelistJSONRPCMethod:
+
+    def load(self, loader):
+        loader.add_option(
+            name="methods",
+            typespec=str,
+            default="",
+            help="Methods to whitelist",
+        )
+
+    def request(self, flow):
+        json_dump = json.loads(flow.request.content)
+        is_whitelisted = False
+        if ctx.options.methods:
+            _allmethods = json.loads(ctx.options.methods)
+            for _method in _allmethods:
+                if _method.strip() != '':
+                    if re.search(_method, json_dump['method']) is not None:
+                        is_whitelisted = True
+                        break
+
+        if not is_whitelisted:
+            status = 200
+            content = {
+                    "jsonrpc": "2.0",
+                    "id": json_dump['id'],
+                    "error": {
+                        "code":-32601,
+                        "message":"Method not allowed"
+                        }
+                    }
+            header = {"Content-Type": "Content-Type: application/json"}
+
+            content = json.dumps(content)
+
+            flow.response = http.Response.make(status, content, header)
+
+addons = [WhitelistJSONRPCMethod()]
