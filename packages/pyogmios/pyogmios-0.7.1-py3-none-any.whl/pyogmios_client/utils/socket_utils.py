32b60e897705e970dc680f975ceb1329337550aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:25 +0000

    Add package pyogmios/0.7.1/pyogmios-0.7.1-py3-none-any.whl

---
+++ packages/pyogmios/pyogmios-0.7.1-py3-none-any.whl/pyogmios_client/utils/socket_utils.py
@@ -0,0 +1,12 @@
+from websocket import WebSocketApp
+
+from pyogmios_client.exceptions import WebSocketClosedError
+
+
+async def ensure_socket_is_open(websocket_app: WebSocketApp) -> None:
+    """
+    Ensure the socket is open.
+    :param websocket_app: The websocket app
+    """
+    if not websocket_app.sock.connected:
+        raise WebSocketClosedError()
