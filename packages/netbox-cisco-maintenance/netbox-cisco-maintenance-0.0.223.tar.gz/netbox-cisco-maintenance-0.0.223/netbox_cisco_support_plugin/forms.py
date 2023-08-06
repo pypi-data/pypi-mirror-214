a5a06efb06edc3beb7c3c6cecf0f0c954ad50063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:01 +0000

    Add package netbox-cisco-maintenance/0.0.223/netbox-cisco-maintenance-0.0.223.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.223.tar.gz/netbox-cisco-maintenance-0.0.223/netbox_cisco_support_plugin/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from extras.forms.mixins import SavedFiltersMixin
+from utilities.forms import BOOLEAN_WITH_BLANK_CHOICES, FilterForm
+from .models import CiscoDeviceSupport
+
+
+class CiscoDeviceSupportFilterForm(SavedFiltersMixin, FilterForm):
+    model = CiscoDeviceSupport
+
+    fieldsets = (
+        (None, ("model",)),
+        ("Software Release", ("recommended_release", "desired_release", "current_release",)),
+        ("Device Support", ("sr_no_owner", "is_covered", "coverage_end_date__year",)),
+        ("Device Type Support", ("eox_has_error",)),
+    )
+
+    model = forms.CharField(
+        required=False
+    )
+
+    recommended_release = forms.CharField(
+        required=False
+    )
+
+    desired_release = forms.CharField(
+        required=False
+    )
+
+    current_release = forms.CharField(
+        required=False
+    )
+
+    sr_no_owner = forms.NullBooleanField(
+        required=False,
+        label="Serial Owner",
+        widget=forms.Select(
+            choices=BOOLEAN_WITH_BLANK_CHOICES
+        )
+    )
+
+    is_covered = forms.NullBooleanField(
+        required=False,
+        label="Is Covered",
+        widget=forms.Select(
+            choices=BOOLEAN_WITH_BLANK_CHOICES
+        )
+    )
+
+    eox_has_error = forms.NullBooleanField(
+        required=False,
+        label="Has EoX Error",
+        widget=forms.Select(
+            choices=BOOLEAN_WITH_BLANK_CHOICES
+        )
+    )
+
+    coverage_end_date__year = forms.CharField(
+        required=False
+    )
