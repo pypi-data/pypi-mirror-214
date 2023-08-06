1beff92a18e8cbeb255a8c4235a82c4e4d791bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package slidesgen/0.3/slidesgen-0.3.tar.gz

---
+++ packages/slidesgen/slidesgen-0.3.tar.gz/slidesgen-0.3/setup.py
@@ -0,0 +1,21 @@
+#! -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='slidesgen',
+    version='0.3',
+    description='A simple tool to generate slides from markdown files',
+    author='sjqzhang',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        slidesgen=slidesgen:main
+            
+    ''',
+)
