e22e2b0404273d5a5df2b5931736dbd0596b7da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:34 +0000

    Add package virtualenv/20.23.1/virtualenv-20.23.1.tar.gz

---
+++ packages/virtualenv/virtualenv-20.23.1.tar.gz/virtualenv-20.23.1/tests/unit/config/test_ini.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
+import pytest
+
+from virtualenv.info import fs_supports_symlink
+from virtualenv.run import session_via_cli
+
+
+@pytest.mark.skipif(not fs_supports_symlink(), reason="symlink is not supported")
+def test_ini_can_be_overwritten_by_flag(tmp_path, monkeypatch):
+    custom_ini = tmp_path / "conf.ini"
+    custom_ini.write_text(
+        dedent(
+            """
+        [virtualenv]
+        copies = True
+        """,
+        ),
+        encoding="utf-8",
+    )
+    monkeypatch.setenv("VIRTUALENV_CONFIG_FILE", str(custom_ini))
+
+    result = session_via_cli(["venv", "--symlinks"])
+
+    symlinks = result.creator.symlinks
+    assert symlinks is True
---
+++ packages/virtualenv/virtualenv-20.23.1.tar.gz/virtualenv-20.23.1/tests/unit/test_util.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import concurrent.futures
+import traceback
+
+import pytest
+
+from virtualenv.util.lock import ReentrantFileLock
+from virtualenv.util.subprocess import run_cmd
+
+
+def test_run_fail(tmp_path):
+    code, out, err = run_cmd([str(tmp_path)])
+    assert err
+    assert not out
+    assert code
+
+
+def test_reentrant_file_lock_is_thread_safe(tmp_path):
+    lock = ReentrantFileLock(tmp_path)
+    target_file = tmp_path / "target"
+    target_file.touch()
+
+    def recreate_target_file():
+        with lock.lock_for_key("target"):
+            target_file.unlink()
+            target_file.touch()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        tasks = []
+        for _ in range(4):
+            tasks.append(executor.submit(recreate_target_file))
+        concurrent.futures.wait(tasks)
+        for task in tasks:
+            try:
+                task.result()
+            except Exception:  # noqa: BLE001
+                pytest.fail(traceback.format_exc())
