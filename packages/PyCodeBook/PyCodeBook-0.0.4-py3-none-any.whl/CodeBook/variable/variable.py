f75285ffde550132b09ecd0a69a14503dddd57f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:01 +0000

    Add package PyCodeBook/0.0.4/PyCodeBook-0.0.4-py3-none-any.whl

---
+++ packages/PyCodeBook/PyCodeBook-0.0.4-py3-none-any.whl/CodeBook/variable/variable.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/6/18 01:54
+# @Author  : AI悦创
+# @FileName: variable.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+template = """1. 什么是变量？——变量就是在计算机的内存当中，开辟空间；
+"""
+
+
+def show():
+    """
+    此函数，用于输出 Python 变量的定义与特点。
+
+    """
+    print(template)
+    return template
