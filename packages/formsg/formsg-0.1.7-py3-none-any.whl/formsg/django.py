abbf0a82901746a52f1c460c5fd9a49ed4075fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package formsg/0.1.7/formsg-0.1.7-py3-none-any.whl

---
+++ packages/formsg/formsg-0.1.7-py3-none-any.whl/formsg/django.py
@@ -0,0 +1,26 @@
+__all__ = ['decrypt_django_request']
+import json
+from typing import Any, Mapping, Optional
+
+from .utils import decrypt_content, verify_signature
+
+
+def decrypt_django_request(
+    request: Any, 
+    secret_key: str, 
+    webhook_uri: Optional[str] = None, 
+    signature_expiry_seconds: float = 60,
+):
+    if webhook_uri is None:
+        webhook_uri = request.build_absolute_uri()
+
+    verify_signature(
+        webhook_uri, 
+        request.headers['X-FormSG-Signature'], 
+        signature_expiry_seconds=signature_expiry_seconds,
+    )
+
+    body_json: Mapping[str, Any] = json.loads(request.body)
+
+    return decrypt_content(body_json, secret_key)
+#end def
