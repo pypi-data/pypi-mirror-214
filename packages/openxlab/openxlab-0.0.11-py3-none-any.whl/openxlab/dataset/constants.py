b76b5d609cc616288069900b40cd72ea48cf6d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:47 +0000

    Add package openxlab/0.0.11/openxlab-0.0.11-py3-none-any.whl

---
+++ packages/openxlab/openxlab-0.0.11-py3-none-any.whl/openxlab/dataset/constants.py
@@ -0,0 +1,20 @@
+# odl_clientId = "kmz3bkwzlaa3wrq8pvwa"
+# odl_clientId = "qja9jy5wnjyqwvylmeqw"
+# uaa_url_prefix = "https://sso.openxlab.org.cn/gw/uaa-be"
+# uaa_url_prefix = "https://sso.staging.openxlab.org.cn/gw/uaa-be"
+# baseUrl: https://sso.staging.openxlab.org.cn/gw/uaa-be
+
+# TODO toggle this when publishing
+# production
+# odl_clientId = "kmz3bkwzlaa3wrq8pvwa"
+# staging
+odl_clientId= 'ypkl8bwo0eb5ao1b96no'
+
+computed_url = "/datasets/api/v2/"
+
+endpoint = "http://staging.openxlab.org.cn"
+
+uaa_url_prefix = "https://sso.staging.openxlab.org.cn/gw/uaa-be"
+# uaa_url_prefix = "https://sso.openxlab.org.cn/gw/uaa-be"
+
+FILE_THRESHOLD = 10*1024*1024
