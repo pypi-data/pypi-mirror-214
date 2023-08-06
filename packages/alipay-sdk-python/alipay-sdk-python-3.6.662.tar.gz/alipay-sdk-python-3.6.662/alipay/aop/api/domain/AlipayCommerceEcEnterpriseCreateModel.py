7a90fe958d27c36aff0c9bce195eb0c4c9ebd643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:26 +0000

    Add package alipay-sdk-python/3.6.662/alipay-sdk-python-3.6.662.tar.gz

---
+++ packages/alipay-sdk-python/alipay-sdk-python-3.6.662.tar.gz/alipay-sdk-python-3.6.662/alipay/aop/api/domain/AlipayCommerceEcEnterpriseCreateModel.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+from alipay.aop.api.constant.ParamConstants import *
+
+
+class AlipayCommerceEcEnterpriseCreateModel(object):
+
+    def __init__(self):
+        self._biz_scene = None
+        self._create_iot_group = None
+        self._enterprise_alias = None
+        self._enterprise_name = None
+        self._identity = None
+        self._identity_open_id = None
+        self._identity_type = None
+        self._out_biz_no = None
+
+    @property
+    def biz_scene(self):
+        return self._biz_scene
+
+    @biz_scene.setter
+    def biz_scene(self, value):
+        self._biz_scene = value
+    @property
+    def create_iot_group(self):
+        return self._create_iot_group
+
+    @create_iot_group.setter
+    def create_iot_group(self, value):
+        self._create_iot_group = value
+    @property
+    def enterprise_alias(self):
+        return self._enterprise_alias
+
+    @enterprise_alias.setter
+    def enterprise_alias(self, value):
+        self._enterprise_alias = value
+    @property
+    def enterprise_name(self):
+        return self._enterprise_name
+
+    @enterprise_name.setter
+    def enterprise_name(self, value):
+        self._enterprise_name = value
+    @property
+    def identity(self):
+        return self._identity
+
+    @identity.setter
+    def identity(self, value):
+        self._identity = value
+    @property
+    def identity_open_id(self):
+        return self._identity_open_id
+
+    @identity_open_id.setter
+    def identity_open_id(self, value):
+        self._identity_open_id = value
+    @property
+    def identity_type(self):
+        return self._identity_type
+
+    @identity_type.setter
+    def identity_type(self, value):
+        self._identity_type = value
+    @property
+    def out_biz_no(self):
+        return self._out_biz_no
+
+    @out_biz_no.setter
+    def out_biz_no(self, value):
+        self._out_biz_no = value
+
+
+    def to_alipay_dict(self):
+        params = dict()
+        if self.biz_scene:
+            if hasattr(self.biz_scene, 'to_alipay_dict'):
+                params['biz_scene'] = self.biz_scene.to_alipay_dict()
+            else:
+                params['biz_scene'] = self.biz_scene
+        if self.create_iot_group:
+            if hasattr(self.create_iot_group, 'to_alipay_dict'):
+                params['create_iot_group'] = self.create_iot_group.to_alipay_dict()
+            else:
+                params['create_iot_group'] = self.create_iot_group
+        if self.enterprise_alias:
+            if hasattr(self.enterprise_alias, 'to_alipay_dict'):
+                params['enterprise_alias'] = self.enterprise_alias.to_alipay_dict()
+            else:
+                params['enterprise_alias'] = self.enterprise_alias
+        if self.enterprise_name:
+            if hasattr(self.enterprise_name, 'to_alipay_dict'):
+                params['enterprise_name'] = self.enterprise_name.to_alipay_dict()
+            else:
+                params['enterprise_name'] = self.enterprise_name
+        if self.identity:
+            if hasattr(self.identity, 'to_alipay_dict'):
+                params['identity'] = self.identity.to_alipay_dict()
+            else:
+                params['identity'] = self.identity
+        if self.identity_open_id:
+            if hasattr(self.identity_open_id, 'to_alipay_dict'):
+                params['identity_open_id'] = self.identity_open_id.to_alipay_dict()
+            else:
+                params['identity_open_id'] = self.identity_open_id
+        if self.identity_type:
+            if hasattr(self.identity_type, 'to_alipay_dict'):
+                params['identity_type'] = self.identity_type.to_alipay_dict()
+            else:
+                params['identity_type'] = self.identity_type
+        if self.out_biz_no:
+            if hasattr(self.out_biz_no, 'to_alipay_dict'):
+                params['out_biz_no'] = self.out_biz_no.to_alipay_dict()
+            else:
+                params['out_biz_no'] = self.out_biz_no
+        return params
+
+    @staticmethod
+    def from_alipay_dict(d):
+        if not d:
+            return None
+        o = AlipayCommerceEcEnterpriseCreateModel()
+        if 'biz_scene' in d:
+            o.biz_scene = d['biz_scene']
+        if 'create_iot_group' in d:
+            o.create_iot_group = d['create_iot_group']
+        if 'enterprise_alias' in d:
+            o.enterprise_alias = d['enterprise_alias']
+        if 'enterprise_name' in d:
+            o.enterprise_name = d['enterprise_name']
+        if 'identity' in d:
+            o.identity = d['identity']
+        if 'identity_open_id' in d:
+            o.identity_open_id = d['identity_open_id']
+        if 'identity_type' in d:
+            o.identity_type = d['identity_type']
+        if 'out_biz_no' in d:
+            o.out_biz_no = d['out_biz_no']
+        return o
+
+
