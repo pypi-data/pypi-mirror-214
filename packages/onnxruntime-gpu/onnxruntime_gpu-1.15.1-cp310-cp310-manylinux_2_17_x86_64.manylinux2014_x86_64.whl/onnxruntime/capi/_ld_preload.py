0c537b4714e968ba58b03e6cb48298087870e492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:07 +0000

    Add package onnxruntime-gpu/1.15.1/onnxruntime_gpu-1.15.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/onnxruntime-gpu/onnxruntime_gpu-1.15.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/onnxruntime/capi/_ld_preload.py
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+# This file can be modified by setup.py when building a manylinux2010 wheel
+# When modified, it will preload some libraries needed for the python C extension
+from ctypes import CDLL, RTLD_GLOBAL
+try:
+    _libcublasLt = CDLL("libcublasLt.so.11", mode=RTLD_GLOBAL)
+    _libcublas = CDLL("libcublas.so.11", mode=RTLD_GLOBAL)
+    _libcudnn = CDLL("libcudnn.so.8", mode=RTLD_GLOBAL)
+    _libcurand = CDLL("libcurand.so.10", mode=RTLD_GLOBAL)
+    _libcufft = CDLL("libcufft.so.10", mode=RTLD_GLOBAL)
+    _libcudart = CDLL("libcudart.so.11.0", mode=RTLD_GLOBAL)
+except OSError:
+    import os
+    os.environ["ORT_CUDA_UNAVAILABLE"] = "1"
+from ctypes import CDLL, RTLD_GLOBAL
+try:
+    _libcudnn = CDLL("libcudnn.so.8", mode=RTLD_GLOBAL)
+    _libcublas = CDLL("libcublas.so.11", mode=RTLD_GLOBAL)
+    _libnvinfer = CDLL("libnvinfer.so.8", mode=RTLD_GLOBAL)
+    _libnvinfer_plugin = CDLL("libnvinfer_plugin.so.8", mode=RTLD_GLOBAL)
+    _libnvonnxparser = CDLL("libnvonnxparser.so.8", mode=RTLD_GLOBAL)
+    _libcudart = CDLL("libcudart.so.11.0", mode=RTLD_GLOBAL)
+except OSError:
+    import os
+    os.environ["ORT_TENSORRT_UNAVAILABLE"] = "1"
