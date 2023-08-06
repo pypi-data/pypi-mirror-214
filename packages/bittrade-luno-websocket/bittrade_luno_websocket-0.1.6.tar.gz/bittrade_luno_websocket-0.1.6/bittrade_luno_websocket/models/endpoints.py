4b1908ef861a299954640c0f975b628e06ce3d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:59 +0000

    Add package bittrade-luno-websocket/0.1.6/bittrade_luno_websocket-0.1.6.tar.gz

---
+++ packages/bittrade-luno-websocket/bittrade_luno_websocket-0.1.6.tar.gz/bittrade_luno_websocket-0.1.6/bittrade_luno_websocket/models/endpoints.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Endpoints(Enum):
+    """Enum class for all the endpoints"""
+
+    GET_BALANCES = "/api/1/balance"
+    GET_ORDER_V3 = "/api/exchange/3/order"
+    GET_FEE_INFORMATION = "/api/1/fee_info"
+    POST_MARKET_ORDER = "/api/1/marketorder"
+    POST_LIMIT_ORDER = "/api/1/postorder"
+    ORDERBOOK_TOP = "/api/1/orderbook_top"
