ddd7656360d5113c449adc0d8e4daa8d0cf48863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package examples-snomax/0.1.0/examples_snomax-0.1.0.tar.gz

---
+++ packages/examples-snomax/examples_snomax-0.1.0.tar.gz/examples_snomax-0.1.0/examples/__init__.py
@@ -0,0 +1,4 @@
+# Example function can be imported with from examples import first.
+
+def first():
+    print('First!')
---
+++ packages/examples-snomax/examples_snomax-0.1.0.tar.gz/examples_snomax-0.1.0/examples/first.py
@@ -0,0 +1,4 @@
+# Example function can be imported with: from examples.first import first.
+
+def first():
+    print('First!')
