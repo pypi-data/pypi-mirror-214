3748cf6ff3bc949f4cb1e99a47763141673566cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:21 +0000

    Add package cv3-beta/1.2.0/cv3-beta-1.2.0.tar.gz

---
+++ packages/cv3-beta/cv3-beta-1.2.0.tar.gz/cv3-beta-1.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cv3-beta',
+    version='1.2.0',
+    packages=['cv3'],
+    url='https://github.com/gorodion/pycv',
+    license='Apache 2.0',
+    author='gorodion',
+    author_email='domonion@list.ru',
+    description='Pythonic cv2',
+    install_requires=[
+        'numpy>=1.19.5',
+        'opencv-python>=4.2.0.34'
+    ]
+)
