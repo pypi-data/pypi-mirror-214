c38eead0478ca4aede316090afed94c89f7cc3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package apidev-coop-bar/1.6.1/apidev-coop_bar-1.6.1.tar.gz

---
+++ packages/apidev-coop-bar/apidev-coop_bar-1.6.1.tar.gz/apidev-coop_bar-1.6.1/coop_bar/apps.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Customizable tool bar
+"""
+
+from django.apps import AppConfig
+
+
+class CoopBarAppConfig(AppConfig):
+    name = 'coop_bar'
+    verbose_name = "coop Bar"
