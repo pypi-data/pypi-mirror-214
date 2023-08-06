e6653e4f6ff63ecd74ee0b5e71020308a274efa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:27 +0000

    Add package mixin-python/0.2.10/mixin_python-0.2.10-cp39-cp39-win_amd64.whl

---
+++ packages/mixin-python/mixin_python-0.2.10-cp39-cp39-win_amd64.whl/pymixin/mixin_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#helloworld
+
+"""
+Mixin Config
+get below config from 'https://developers.mixin.one/dashboard'
+code by lee.c
+update at 2018.12.2
+"""
+import os
+import sys
+import json
+import importlib
+
+receivers = [
+    "e07c06fa-084c-4ce1-b14a-66a9cb147b9e",
+    "e0148fc6-0e10-470e-8127-166e0829c839",
+    "3e72ca0c-1bab-49ad-aa0a-4d8471d375e7"
+]
