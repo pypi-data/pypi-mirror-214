5a7e9cefc6585ec0c5a7088a5725ddcce52755cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:49 +0000

    Add package retro-data-structures/0.19.1/retro_data_structures-0.19.1-py3-none-any.whl

---
+++ packages/retro-data-structures/retro_data_structures-0.19.1-py3-none-any.whl/retro_data_structures/formats/dgrp.py
@@ -0,0 +1,36 @@
+import typing
+
+import construct
+from construct import Int32ub, PrefixedArray, Struct
+
+from retro_data_structures.base_resource import AssetType, BaseResource, Dependency
+from retro_data_structures.common_types import FourCC
+from retro_data_structures.game_check import AssetIdCorrect, Game
+
+ConstructDependency = Struct("asset_type" / FourCC, "asset_id" / AssetIdCorrect)
+
+DGRP = PrefixedArray(Int32ub, ConstructDependency)
+
+
+def legacy_dependencies(obj, target_game: Game):
+    for dependency in obj:
+        yield dependency.asset_type, dependency.asset_id
+
+
+class Dgrp(BaseResource):
+    @classmethod
+    def resource_type(cls) -> AssetType:
+        return "DGRP"
+
+    @classmethod
+    def construct_class(cls, target_game: Game) -> construct.Construct:
+        return DGRP
+
+    @property
+    def direct_dependencies(self) -> typing.Iterator[Dependency]:
+        for dep in self.raw:
+            yield dep.asset_type, dep.asset_id
+
+    def dependencies_for(self, is_mlvl: bool = False) -> typing.Iterator[Dependency]:
+        for dependency in self.raw:
+            yield from self.asset_manager.get_dependencies_for_asset(dependency.asset_id, is_mlvl)
