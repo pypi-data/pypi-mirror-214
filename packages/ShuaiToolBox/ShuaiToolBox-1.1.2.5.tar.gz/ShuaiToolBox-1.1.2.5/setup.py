2943cd696ea30e03685a6299b6cd7dba9fa6ada2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:18 +0000

    Add package ShuaiToolBox/1.1.2.5/ShuaiToolBox-1.1.2.5.tar.gz

---
+++ packages/ShuaiToolBox/ShuaiToolBox-1.1.2.5.tar.gz/ShuaiToolBox-1.1.2.5/setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from distutils.core import  setup
+import setuptools
+packages = ['ShuaiToolBox']
+setup(name='ShuaiToolBox',
+	version='1.1.2.5',
+	author='ShuaiZ',
+	description="packages for experiments process",
+    packages=setuptools.find_packages(), 
+    install_requires=['numpy','matplotlib','natsort','opencv-python'],
+    package_dir={'requests': 'requests'},
+    python_requires='>=3.6',
+    )
