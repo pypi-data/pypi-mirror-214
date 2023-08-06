c785b77e924e4f900e59e3098501fba2bbefb0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:16 +0000

    Add package ostorlab/0.14.7/ostorlab-0.14.7-py3-none-any.whl

---
+++ packages/ostorlab/ostorlab-0.14.7-py3-none-any.whl/ostorlab/__init__.py
@@ -0,0 +1,18 @@
+"""Ostorlab main package."""
+import logging
+from rich.logging import RichHandler
+from ostorlab.cli.rootcli import rootcli
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="ERROR",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
+
+logger = logging.getLogger("CLI")
+
+
+def main():
+    rootcli(None)
