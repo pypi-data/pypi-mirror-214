dae96291945463d47fe2626ab44f452cc3a98daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:53 +0000

    Add package odoo14-addon-crm-metadata-rest-api/14.0.1.0.2/odoo14_addon_crm_metadata_rest_api-14.0.1.0.2-py3-none-any.whl

---
+++ packages/odoo14-addon-crm-metadata-rest-api/odoo14_addon_crm_metadata_rest_api-14.0.1.0.2-py3-none-any.whl/odoo/addons/crm_metadata_rest_api/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "crm_metadata_rest_api",
+
+    'summary': """
+        Expose CRM Metadata on the Rest API""",
+
+    'author': "Coopdevs Treball SCCL",
+    'website': "",
+
+    'category': 'api',
+    'version': '14.0.1.0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'crm_metadata',
+        'crm_rest_api'
+    ],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
+}
