69e08a0e4ecc6f4f0843693132b46fe42774aee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:36 +0000

    Add package britishize/0.2.0/britishize-0.2.0.tar.gz

---
+++ packages/britishize/britishize-0.2.0.tar.gz/britishize-0.2.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['britishize']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer>=0.9.0,<0.10.0']
+
+entry_points = \
+{'console_scripts': ['brit = britishize.britishize:main']}
+
+setup_kwargs = {
+    'name': 'britishize',
+    'version': '0.2.0',
+    'description': 'Write poorer english',
+    'long_description': 'Write proper english\n',
+    'author': 'Antonio Feregrino',
+    'author_email': 'antonio.feregrino@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
