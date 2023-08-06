e77276c65171c7b8b947627e134a2a20a81fed08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:34 +0000

    Add package adata/0.0.24b0/adata-0.0.24b0.tar.gz

---
+++ packages/adata/adata-0.0.24b0.tar.gz/adata-0.0.24b0/adata/stock/market/stock_market/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: readme
+@author: 1nchaos
+@time: 2023/6/19
+@log: change log
+"""
+from .stock_market import StockMarket
+from .stock_market_qq import StockMarketQQ
+from .stock_market_sina import StockMarketSina
+from .stock_market_baidu import StockMarketBaiDu
