ce4e6d672573e3a7341ca1d2532ebc51f3d699da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:27 +0000

    Add package pbaa/1.0.0/pbaa-1.0.0-py3-none-any.whl

---
+++ packages/pbaa/pbaa-1.0.0-py3-none-any.whl/pbaa/core/prerequisite.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2023-present dh031200 <imbird0312@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+dependencies = """
+python=3.10
+pip
+wget
+numpy
+opencv-python
+loguru
+torch
+torchvision
+supervision
+transformers
+addict
+yapf
+pycocotools
+timm
+groundingdino
+segment_anything
+"""
