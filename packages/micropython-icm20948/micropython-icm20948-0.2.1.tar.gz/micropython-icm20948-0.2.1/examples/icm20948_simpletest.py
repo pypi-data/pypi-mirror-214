bd28217a15469f500bcff4e32fed398f0cc9ad3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:54 +0000

    Add package micropython-icm20948/0.2.1/micropython-icm20948-0.2.1.tar.gz

---
+++ packages/micropython-icm20948/micropython-icm20948-0.2.1.tar.gz/micropython-icm20948-0.2.1/examples/icm20948_simpletest.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Jose D. Montoya
+
+import time
+from machine import Pin, I2C
+from micropython_icm20948 import icm20948
+
+i2c = I2C(sda=Pin(8), scl=Pin(9))  # Correct I2C pins for UM FeatherS2
+icm = icm20948.ICM20948(i2c)
+
+
+while True:
+    print("Acceleration : ", icm.acceleration)
+    time.sleep(1)
