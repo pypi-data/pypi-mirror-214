30b73ade557650f359b28bffb6a137aee0d8024f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:31 +0000

    Add package pynter-defects/1.0.1/pynter-defects-1.0.1.tar.gz

---
+++ packages/pynter-defects/pynter-defects-1.0.1.tar.gz/pynter-defects-1.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 14:39:37 2023
+
+@author: villa
+"""
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='pynter-defects',
+    version='1.0.1',
+    author='Lorenzo Villa',
+    description='Tools for atomistic calculations',
+    packages=find_namespace_packages(exclude=["pynter.*.tests", "pynter.*.*.tests"]),
+    install_requires=[
+        'ase',
+        'pymatgen>=2023.3.23',
+        'pymatgen-analysis-defects>=2023.3.25',
+        'pymatgen-db',
+        'PyYAML',
+        'schedule'        
+    ],
+    extra_requires={'test':'pytest'},
+    entry_points={
+    "console_scripts": [
+        "pynter = pynter.cli.main:main"]}
+)
+
