3bb037f52dfdcd215376d18e1d2c6266947fe925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:30 +0000

    Add package pytorch-supporter/0.0.18/pytorch-supporter-0.0.18.tar.gz

---
+++ packages/pytorch-supporter/pytorch-supporter-0.0.18.tar.gz/pytorch-supporter-0.0.18/pytorch_supporter/layers/lazily_initialized_linear.py
@@ -0,0 +1,20 @@
+import torch
+
+class LazilyInitializedLinear(torch.nn.Module):
+    def __init__(self, out_features):
+        super().__init__()
+        self.layer = None
+        self.out_features = out_features
+
+    def forward(self, x):
+        if not self.layer:
+            in_features = x.shape[1]
+            self.layer = torch.nn.Linear(in_features=in_features, out_features=self.out_features)
+            device = "cpu"
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif torch.backends.mps.is_available():
+                device = "mps"
+            self.layer = self.layer.to(device)
+        x = self.layer(x)
+        return x
