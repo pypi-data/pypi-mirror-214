f296600855a8728d1e36d93766119388f1535918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:38 +0000

    Add package LinearProgramOptimizer/0.0.1/linearProgramOptimizer-0.0.1.tar.gz

---
+++ packages/LinearProgramOptimizer/linearProgramOptimizer-0.0.1.tar.gz/linearProgramOptimizer-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Optimization models for solving Linear Programming Problems'
+LONG_DESCRIPTION = 'Optimization models for solving Linear Programming Problems'
+
+# Setting up
+setup(
+    name="linearProgramOptimizer",
+    version=VERSION,
+    author="OguntolaIbrahim",
+    author_email="<oibrahimopeyemi@yahoo.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'linear programming','operation research', 'optimization','simplex'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
