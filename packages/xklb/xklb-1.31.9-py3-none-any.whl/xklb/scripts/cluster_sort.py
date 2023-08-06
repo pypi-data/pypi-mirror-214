23925a51ad7b260d7dc6759e2aeb3ca58d393112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:03 +0000

    Add package xklb/1.31.9/xklb-1.31.9-py3-none-any.whl

---
+++ packages/xklb/xklb-1.31.9-py3-none-any.whl/xklb/scripts/cluster_sort.py
@@ -0,0 +1,43 @@
+import argparse, sys
+from pprint import pprint
+
+from xklb import usage, utils
+from xklb.utils import log
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="library cluster-sort", usage=usage.cluster_sort)
+    parser.add_argument("--clusters", "--n-clusters", "-c", type=int, help="Number of KMeans clusters")
+    parser.add_argument("--groups", "-g", action="store_true", help="Show groups")
+    parser.add_argument("--verbose", "-v", action="count", default=0)
+
+    parser.add_argument("input_path", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+    parser.add_argument("output_path", nargs="?")
+    args = parser.parse_args()
+
+    log.info(utils.dict_filter_bool(args.__dict__))
+    return args
+
+
+def cluster_sort() -> None:
+    args = parse_args()
+
+    lines = args.input_path.readlines()
+    args.input_path.close()
+
+    groups = utils.cluster_paths(lines, args.clusters)
+    groups = sorted(groups, key=lambda d: (len(d["grouped_paths"]), -len(d["common_prefix"])))
+
+    if args.groups:
+        pprint(groups)
+    else:
+        lines = utils.flatten(d["grouped_paths"] for d in groups)
+        if args.output_path:
+            with open(args.output_path, "w") as output_fd:
+                output_fd.writelines(lines)
+        else:
+            utils.pipe_lines(lines)
+
+
+if __name__ == "__main__":
+    cluster_sort()
---
+++ packages/xklb/xklb-1.31.9-py3-none-any.whl/xklb/scripts/copy_play_counts.py
@@ -0,0 +1,72 @@
+import argparse
+from pathlib import Path
+
+from xklb import db, usage, utils
+from xklb.utils import log
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="library copy-play-counts", usage=usage.copy_play_counts)
+    parser.add_argument("database")
+    parser.add_argument("source_dbs", nargs="+")
+    parser.add_argument("--source-prefix", default="")
+    parser.add_argument("--target-prefix", default="")
+    parser.add_argument("--db", "-db", help=argparse.SUPPRESS)
+    parser.add_argument("--verbose", "-v", action="count", default=0)
+    args = parser.parse_intermixed_args()
+
+    if args.db:
+        args.database = args.db
+    Path(args.database).touch()
+    args.db = db.connect(args)
+    log.info(utils.dict_filter_bool(args.__dict__))
+
+    return args
+
+
+def copy_play_count(args, source_db) -> None:
+    args.db.attach("src", Path(source_db).resolve())
+
+    copy_counts = args.db.query(
+        """
+        SELECT
+            *
+        FROM
+            src.media
+        WHERE
+            src.media.play_count > 0
+            or
+            src.media.playhead > 0
+        """,
+    )
+
+    modified_row_count = 0
+    with args.db.conn:
+        for d in copy_counts:
+            renamed_path = d["path"].replace(args.source_prefix, args.target_prefix, 1)
+            log.debug(renamed_path)
+
+            sql = """
+            UPDATE
+                main.media
+            SET
+                play_count = :play_count
+                , time_played = :time_played
+                , playhead = :playhead
+            WHERE
+                main.media.path = :renamed_path
+            """
+            cursor = args.db.conn.execute(sql, {**d, "renamed_path": renamed_path})
+            modified_row_count += cursor.rowcount
+
+    log.info("Updated %s rows", modified_row_count)
+
+
+def copy_play_counts() -> None:
+    args = parse_args()
+    for s_db in args.source_dbs:
+        copy_play_count(args, s_db)
+
+
+if __name__ == "__main__":
+    copy_play_counts()
