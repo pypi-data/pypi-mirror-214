26895a57b3d1ffc11ba342cd81547563a680d1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:02 +0000

    Add package selis/3.1/selis-3.1.tar.gz

---
+++ packages/selis/selis-3.1.tar.gz/selis-3.1/selis/utils.py
@@ -0,0 +1,12 @@
+def convert_color(string, style):
+    colors = {
+    "WARNING": '\033[93m',
+    "FAIL": '\033[91m',
+    "ENDC": '\033[0m',
+    "BOLD": '\033[1m'
+    }
+    return colors[style] + string
+
+
+def guide_to_exit():
+    print(convert_color("[+] Press 'Ctrl + C' or 'Enter' to exit", style="ENDC"))
