6334cafe8f045a2e28d299f3153eba3d99cf5c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:26 +0000

    Add package polyfactory/2.3.0/polyfactory-2.3.0-py3-none-any.whl

---
+++ packages/polyfactory/polyfactory-2.3.0-py3-none-any.whl/polyfactory/factories/__init__.py
@@ -0,0 +1,5 @@
+from polyfactory.factories.base import BaseFactory
+from polyfactory.factories.typed_dict_factory import TypedDictFactory
+from polyfactory.factories.dataclass_factory import DataclassFactory
+
+__all__ = ("BaseFactory", "TypedDictFactory", "DataclassFactory")
