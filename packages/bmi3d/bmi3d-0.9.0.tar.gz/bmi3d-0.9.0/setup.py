e975d2daeedbc8eb759b641d43dad58d907fd15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:46 +0000

    Add package bmi3d/0.9.0/bmi3d-0.9.0.tar.gz

---
+++ packages/bmi3d/bmi3d-0.9.0.tar.gz/bmi3d-0.9.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+setuptools.setup(
+    name="bmi3d",
+    version="0.9.0",
+    author="Lots of people",
+    description="electrophysiology experimental rig library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "django",
+        "celery",
+        "jinja2",
+        "scipy",
+        "traits",
+        "pandas",
+        "patsy",
+        "statsmodels",
+        "pygame",
+        "PyOpenGL",
+        "pylibftdi",
+        "sphinx",
+        "numpydoc",
+        "tornado",
+        "tables",
+        "h5py",
+        "pymysql",
+    ]
+)
