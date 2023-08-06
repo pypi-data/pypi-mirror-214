f4848ef0d9b9a9b23409d497385bc7d3bebadf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:09 +0000

    Add package ntt-fastapi/1.0.7/ntt-fastapi-1.0.7.tar.gz

---
+++ packages/ntt-fastapi/ntt-fastapi-1.0.7.tar.gz/ntt-fastapi-1.0.7/nttfastapi/cli.py
@@ -0,0 +1,54 @@
+# ------------ python package ------------- 
+import os
+# import sys 
+# sys.path.append('nttfastapi')
+# ------------ python package ------------- 
+
+# # ------------ project package ------------
+# from .utils import *
+# from .constants import *
+# # ------------ project package ------------
+
+# # ------------ project package ------------
+# from utils import *
+# from constants import *
+# # ------------ project package ------------
+
+# ------------ project package ------------
+from nttfastapi.utils import *
+from nttfastapi.constants import *
+# ------------ project package ------------
+
+
+def main():
+    parser = NTTArgparser()
+
+    parser.add_action(
+        action_name=CREATE_ACTION_NAME,
+        args={
+            NTTArgparser.PROJECT_NAME_KEY: DEFAULT_PROJECT_FOLDER,
+        },
+        action=create_folder_action,
+    )
+
+    parser.add_action(
+        action_name='test',
+        args={},
+        action=test_function,
+    )
+
+    parser.run()
+
+def test_function(*args, **kwargs):
+    print("Testing")
+
+
+def create_folder_action(folder_name: str):
+    NTTFileSystem.create_folder(folder_name=folder_name)
+    VirtualEnv.setup(folder_name=DEFAULT_VENV_FOLDER)
+    ProjectConfiguration.setup()
+    GithubConfiguration.setup()
+
+
+if __name__ == "__main__":
+    main()
