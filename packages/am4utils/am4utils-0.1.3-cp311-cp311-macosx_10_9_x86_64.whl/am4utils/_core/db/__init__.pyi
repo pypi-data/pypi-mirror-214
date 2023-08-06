c81fde33292689eeae57c2006c52f32ca0624b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:54 +0000

    Add package am4utils/0.1.3/am4utils-0.1.3-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/am4utils/am4utils-0.1.3-cp311-cp311-macosx_10_9_x86_64.whl/am4utils/_core/db/__init__.pyi
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import am4utils._core.db
+import typing
+
+__all__ = [
+    "DatabaseException",
+    "init"
+]
+
+
+class DatabaseException(Exception, BaseException):
+    pass
+def _debug_query(arg0: str) -> None:
+    pass
+def init(arg0: str) -> None:
+    pass
