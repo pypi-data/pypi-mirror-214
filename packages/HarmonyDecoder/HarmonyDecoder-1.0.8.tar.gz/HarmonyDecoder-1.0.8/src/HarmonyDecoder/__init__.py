1983206f889c2858081cb6693ce8f9b26af85f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package HarmonyDecoder/1.0.8/HarmonyDecoder-1.0.8.tar.gz

---
+++ packages/HarmonyDecoder/HarmonyDecoder-1.0.8.tar.gz/HarmonyDecoder-1.0.8/src/HarmonyDecoder/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "drawing",
+    "function",
+    "harmony",
+    "interval",
+    "logger",    
+    "note",
+    "possiblenotes",
+    "program",
+    "rules",
+    "scale",
+    "setup",
+    "strdecoder",
+    "ui"
+)
