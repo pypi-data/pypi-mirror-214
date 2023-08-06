c4330dc104433ea8a57869dd6fa6e38b90c2888c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:45 +0000

    Add package hylite/1.23/hylite-1.23.tar.gz

---
+++ packages/hylite/hylite-1.23.tar.gz/hylite-1.23/tests/test_filters.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+from pathlib import Path
+from hylite import io
+
+class MyTestCase(unittest.TestCase):
+    def test_PCA_MNF(self):
+        image = io.load(os.path.join(os.path.join(str(Path(__file__).parent.parent), "test_data"), "image.hdr"))
+        cloud = io.load(os.path.join(os.path.join(str(Path(__file__).parent.parent), "test_data"), "image.hdr"))
+
+        # run mnf on clouds and images
+        from hylite.filter import MNF
+        from hylite.filter import PCA
+        for data in [image, cloud]:
+
+            # test PCA
+            pca, w = PCA( data, bands=10)
+            self.assertEqual(pca.band_count(), 10)
+
+            # run MNF
+            mnf, w = MNF( data, bands=10)
+            self.assertEqual( mnf.band_count(), 10 )
+
+            # run MNF denoising
+            denoise, w = MNF( data, bands=10, denoise=True )
+            self.assertEqual(denoise.band_count(), data.band_count())
+
+        # test hull correction
+        from hylite.correct import get_hull_corrected
+        get_hull_corrected(image) # just run ... is difficult to evaluate if results make sense
+        get_hull_corrected(cloud) # just run ... is difficult to evaluate if results make sense
+if __name__ == '__main__':
+    unittest.main()
