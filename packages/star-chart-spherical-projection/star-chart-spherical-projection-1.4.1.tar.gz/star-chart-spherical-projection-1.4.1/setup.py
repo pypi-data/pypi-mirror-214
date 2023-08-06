c75b6fd6e2e31ae3da8ada82029947331cf7a4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:05 +0000

    Add package star-chart-spherical-projection/1.4.1/star-chart-spherical-projection-1.4.1.tar.gz

---
+++ packages/star-chart-spherical-projection/star-chart-spherical-projection-1.4.1.tar.gz/star-chart-spherical-projection-1.4.1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Python Package Setup
+from setuptools import setup, find_namespace_packages
+
+VERSION="1.4.1"
+DESCRIPTION="A Python package to generate an astronomy star chart based on spherical projection that corrects for distortions with stereographic projection"
+
+with open("README.md", "r") as f:
+	long_description_readme = f.read()
+
+setup(
+	name="star-chart-spherical-projection",
+	version=VERSION,
+	description=DESCRIPTION,
+	long_description=long_description_readme,
+	long_description_content_type='text/markdown',
+	url="https://github.com/cyschneck/Star-Chart-Spherical-Projection",
+	download_url="https://github.com/cyschneck/Star-Chart-Spherical-Projection/archive/refs/tags/v{0}.tar.gz".format(VERSION),
+	author="cyschneck (C. Y. Schneck)",
+	keywords=["astronomy", "python", "star charts", "precession", "proper motion", "spherical projection", "stereographic projection"],
+	license="MIT",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Physics",
+		"Topic :: Scientific/Engineering :: Visualization",
+		"Topic :: Scientific/Engineering :: Astronomy"
+	],
+	packages=find_namespace_packages(include=['star_chart_spherical_projection',
+											'star_chart_spherical_projection.*']),
+	include_package_data=True,
+	install_requires=[
+		"configparser>=5.3.0",
+		"matplotlib>=3.1.0",
+		"numpy>=1.24.3",
+		"pandas>=1.3.5",
+		"pytest>=7.2.2"
+	],
+	python_requires='>=3.7'
+)
---
+++ packages/star-chart-spherical-projection/star-chart-spherical-projection-1.4.1.tar.gz/star-chart-spherical-projection-1.4.1/star_chart_spherical_projection/__init__.py
@@ -0,0 +1,13 @@
+from .error_handling import errorHandling
+
+from .generate_star_chart import getStarList
+from .generate_star_chart import plotStereographicProjection
+from .generate_star_chart import finalPositionOfStars
+
+from .declination_r_axis import calculateRuler
+from .declination_r_axis import calculateRadiusOfCircle
+from .declination_r_axis import calculateLength
+
+from .ra_dec_precession_vondrak import ltp_pbmat
+from .ra_dec_precession_vondrak import pdp
+from .ra_dec_precession_vondrak import ra_dec
