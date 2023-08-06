26f0f20e79c7cff95e4f5c429ebfcc9d80d2ad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:23 +0000

    Add package rss2pdf/0.1.1/rss2pdf-0.1.1.tar.gz

---
+++ packages/rss2pdf/rss2pdf-0.1.1.tar.gz/rss2pdf-0.1.1/tests/test_main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+
+import unittest
+
+from src import *
+
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(0, 0)
+
+if __name__ == "__main__":
+    unittest.main()
