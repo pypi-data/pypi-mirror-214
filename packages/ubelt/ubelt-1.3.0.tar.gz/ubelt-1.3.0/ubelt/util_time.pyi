d56ad5c87cfdb7a555c132c403e4f8ccd63dd5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package ubelt/1.3.0/ubelt-1.3.0.tar.gz

---
+++ packages/ubelt/ubelt-1.3.0.tar.gz/ubelt-1.3.0/ubelt/util_time.pyi
@@ -0,0 +1,45 @@
+import datetime
+from _typeshed import Incomplete
+
+
+def timestamp(datetime: datetime.datetime | datetime.date | None = None,
+              precision: int = 0,
+              default_timezone: str | datetime.timezone = 'local',
+              allow_dateutil: bool = True) -> str:
+    ...
+
+
+def timeparse(stamp: str,
+              default_timezone: str = 'local',
+              allow_dateutil: bool = True) -> datetime.datetime:
+    ...
+
+
+class Timer:
+    elapsed: float
+    tstart: float
+    label: Incomplete
+    verbose: Incomplete
+    newline: Incomplete
+    write: Incomplete
+    flush: Incomplete
+    ns: Incomplete
+
+    def __init__(self,
+                 label: str = ...,
+                 verbose: Incomplete | None = ...,
+                 newline: bool = ...,
+                 ns: bool = ...) -> None:
+        ...
+
+    def tic(self):
+        ...
+
+    def toc(self):
+        ...
+
+    def __enter__(self):
+        ...
+
+    def __exit__(self, ex_type, ex_value, trace):
+        ...
