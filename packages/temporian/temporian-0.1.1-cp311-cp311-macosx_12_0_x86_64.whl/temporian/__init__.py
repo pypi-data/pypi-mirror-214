fc7fa3c490e23a145b01ac41bb6a470ab6ed7c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:32 +0000

    Add package temporian/0.1.1/temporian-0.1.1-cp311-cp311-macosx_12_0_x86_64.whl

---
+++ packages/temporian/temporian-0.1.1-cp311-cp311-macosx_12_0_x86_64.whl/temporian/__init__.py
@@ -0,0 +1,76 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Temporian."""
+
+# NOTE: The API reference documentation reads this file and expects a single
+# import per line. Do not import several symbols from the same module in a
+# single line. Do not allow import to break lines.
+
+# NOTE: If you need to import something here that isn't part of the public API,
+# and therefore shouldn't show up in the documentation, import it with a private
+# name:
+# from temporian.module import submodule as _submodule
+
+# NOTE: Wildcard imports (*) are treated and parsed as part of root-level
+# imports, so same rules apply to modules imported with a wildcard.
+# Also:
+# - Wildcard imports are only allowed from modules explicitly called api_symbols
+# - Modules imported with wildcards are shown as subfolders in the docs, with
+# the folder's name being the api_symbols.py file's parent's name.
+
+# pylint: disable=wrong-import-position
+# pylint: disable=line-too-long
+
+__version__ = "0.0.1"
+
+# Register the ops definitions and implementations.
+from temporian.core.operator_lib import registered_operators as _ops
+from temporian.implementation.numpy import operators as _impls
+
+
+# Actual API
+# ==========
+
+# Nodes and related
+from temporian.core.data.node import Node
+from temporian.core.data.node import input_node
+
+# Dtypes
+from temporian.core.data.dtypes.api_symbols import *
+
+# Schema
+from temporian.core.data.schema import Schema
+
+# Durations
+from temporian.core.data import duration
+
+# Event set
+from temporian.implementation.numpy.data.event_set import EventSet
+from temporian.implementation.numpy.data.io import event_set
+
+# Graph serialization
+from temporian.core.serialization.api_symbols import *
+
+# Graph execution
+from temporian.core.evaluation import evaluate
+
+# Operators
+from temporian.core.operators.api_symbols import *
+
+# IO
+from temporian.io.api_symbols import *
+
+# Plotting
+from temporian.implementation.numpy.data.plotter import plot
---
+++ packages/temporian/temporian-0.1.1-cp311-cp311-macosx_12_0_x86_64.whl/temporian/implementation/numpy/data/test/plotter_test.py
@@ -0,0 +1,58 @@
+from absl.testing import parameterized, absltest
+
+import matplotlib
+
+from temporian.implementation.numpy.data import plotter
+from temporian.implementation.numpy.data.io import event_set
+
+
+class PlotterTest(parameterized.TestCase):
+    def setUp(self):
+        # Make sure that the plot functions don't fail on command line
+        print("Setting matplotlib backend to: agg")
+        matplotlib.use("agg")
+
+    @parameterized.parameters((True,), (False,))
+    def test_plot(self, interactive):
+        try:
+            import IPython.display
+        except ImportError:
+            # IPython is not installed / supported
+            return
+
+        evset = event_set(
+            timestamps=[0.1, 0.2, 0.3, 0.4, 0.5],
+            features={
+                "a": [1, 2, 3, 7, 8],
+                "b": [4, 5, 6, 9, 10],
+                "c": ["X", "Y", "X", "X", "Z"],
+                "x": [1, 1, 1, 2, 2],
+            },
+            index_features=["x"],
+        )
+
+        _ = plotter.plot(
+            evset, indexes=None, interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset, indexes=1, interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset, indexes=[1, 2], interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset,
+            indexes=[(1,), (2,)],
+            interactive=interactive,
+            return_fig=True,
+        )
+
+    def test_is_uniform(self):
+        self.assertTrue(plotter.is_uniform([]))
+        self.assertTrue(plotter.is_uniform([1]))
+        self.assertTrue(plotter.is_uniform([1, 2, 3]))
+        self.assertFalse(plotter.is_uniform([1, 2, 2.5]))
+
+
+if __name__ == "__main__":
+    absltest.main()
