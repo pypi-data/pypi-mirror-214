f42e5131020385326e2e60664794d8afb724ef53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:14 +0000

    Add package wedeliver-core-plus/0.0.4/wedeliver_core_plus-0.0.4-py3-none-any.whl

---
+++ packages/wedeliver-core-plus/wedeliver_core_plus-0.0.4-py3-none-any.whl/wedeliver_core_plus/helpers/topics.py
@@ -0,0 +1,9 @@
+class Topics(object):
+    INTERNAL_NOTIFICATION_MESSAGE = 'internal_notification_message'
+    SEND_MAIL = 'send_mail'
+    SEND_SMS = 'send_sms'
+    LOG_MODEL_CHANGES = 'log_model_changes'
+
+    THRIVVE_SEND_SMS = 'thrivve_send_sms'
+    THRIVVE_SEND_PUSH_NOTIFICATION = 'thrivve_send_push_notification'
+    THRIVVE_THIRD_PARTY_ADD_BULK_TRANSACTIONS = 'thrivve_third_party_add_bulk_transactions'
