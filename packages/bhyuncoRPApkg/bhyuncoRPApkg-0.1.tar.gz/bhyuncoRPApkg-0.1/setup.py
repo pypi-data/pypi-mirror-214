03a07d857a2ff6ac82bff92f72fd31cf14df39d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:30 +0000

    Add package bhyuncoRPApkg/0.1/bhyuncoRPApkg-0.1.tar.gz

---
+++ packages/bhyuncoRPApkg/bhyuncoRPApkg-0.1.tar.gz/bhyuncoRPApkg-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bhyuncoRPApkg",  # 패키지 이름
+    version="0.1",  # 패키지 버전
+    packages=find_packages(),  # 패키지에 포함될 디렉토리 (포함될 파이썬 코드가 있는 디렉토리들)
+)
