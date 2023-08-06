350ce2f2fa834227a4005680927c8c0527f9edc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:11 +0000

    Add package trulia-rapidapi/0.1.1/trulia_rapidapi-0.1.1.tar.gz

---
+++ packages/trulia-rapidapi/trulia_rapidapi-0.1.1.tar.gz/trulia_rapidapi-0.1.1/trulia_rapidapi/src/trulia_models/search_token_model.py
@@ -0,0 +1,22 @@
+from typing import List
+from typing import Optional
+from datetime import datetime
+from pydantic.fields import Field
+from .base import BaseModelORM
+
+class Place(BaseModelORM):
+    type: str
+    title: str
+    search_token: str
+
+class Data(BaseModelORM):
+    places: List[Place]
+
+class SearchTokenModel(BaseModelORM):
+    description: str
+    status: int
+    data: Optional[Data] = None
+    def __str__(self):
+        if len(self.data.places) == 0:
+            return None
+        return self.data.places[0].search_token
