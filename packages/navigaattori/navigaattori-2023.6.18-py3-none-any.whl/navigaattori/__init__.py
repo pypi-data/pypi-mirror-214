67b7807755994aa2c4adb2e0459d642868484543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:55 +0000

    Add package navigaattori/2023.6.18/navigaattori-2023.6.18-py3-none-any.whl

---
+++ packages/navigaattori/navigaattori-2023.6.18-py3-none-any.whl/navigaattori/__init__.py
@@ -0,0 +1,79 @@
+"""Navigator (Finnish: navigaattori) guided by conventions."""
+import datetime as dti
+import logging
+import os
+import pathlib
+from typing import List, no_type_check
+
+# [[[fill git_describe()]]]
+__version__ = '2023.6.18+parent.ee0b6c1b'
+# [[[end]]] (checksum: 4089f19e8bc3ec7819aafb3b79ce17ba)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+
+APP_NAME = 'Navigator (Finnish: navigaattori) guided by conventions.'
+APP_ALIAS = 'navigaattori'
+APP_ENV = 'NAVIGAATTORI'
+COMMA = ','
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = '.navigaattori.json'
+DEFAULT_LF_ONLY = 'YES'
+GUESSED_STRUCTURES_FOLDER_NAME = 'GUESSED_STRUCTURES'
+STRUCTURES_KEY = 'structures'
+HUB_NAME = f'{STRUCTURES_KEY}.yml'
+DEFAULT_STRUCTURE_NAME = 'structure.yml'
+log = logging.getLogger()  # Module level logger is sufficient
+LOG_FOLDER = pathlib.Path('logs')
+LOG_FILE = f'{APP_ALIAS}.log'
+LOG_PATH = pathlib.Path(LOG_FOLDER, LOG_FILE) if LOG_FOLDER.is_dir() else pathlib.Path(LOG_FILE)
+LOG_LEVEL = logging.INFO
+
+TS_FORMAT_LOG = '%Y-%m-%dT%H:%M:%S'
+TS_FORMAT_PAYLOADS = '%Y-%m-%d %H:%M:%S.%f UTC'
+
+__all__: List[str] = [
+    'DEFAULT_STRUCTURE_NAME',
+    'ENCODING',
+    'GUESSED_STRUCTURES_FOLDER_NAME',
+    'HUB_NAME',
+    'STRUCTURES_KEY',
+    'log',
+    'parse_csl',
+]
+
+
+def parse_csl(csl: str) -> List[str]:
+    """DRY."""
+    return [fmt.strip().lower() for fmt in csl.split(COMMA) if fmt.strip()]
+
+
+@no_type_check
+def formatTime_RFC3339(self, record, datefmt=None):  # noqa
+    """HACK A DID ACK we could inject .astimezone() to localize ..."""
+    return dti.datetime.fromtimestamp(record.created, dti.timezone.utc).isoformat()  # pragma: no cover
+
+
+@no_type_check
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global log  # pylint: disable=global-statement
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': TS_FORMAT_LOG,
+        # 'filename': LOG_PATH,
+        'level': LOG_LEVEL if level is None else level,
+    }
+    logging.Formatter.formatTime = formatTime_RFC3339
+    logging.basicConfig(**log_format)
+    log = logging.getLogger(APP_ENV if name is None else name)
+    log.propagate = True
+
+
+init_logger(name=APP_ENV, level=logging.DEBUG if DEBUG else None)
