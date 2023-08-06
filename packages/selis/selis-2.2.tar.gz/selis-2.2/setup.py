5027dd80268a6dbaf52ecd20d53a222511dad941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:01 +0000

    Add package selis/2.2/selis-2.2.tar.gz

---
+++ packages/selis/selis-2.2.tar.gz/selis-2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='selis',
+    version='2.2',
+    packages=['selis'],
+    entry_points={
+        'console_scripts': [
+            'selis=selis.selis:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+    ],
+)
