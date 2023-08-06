aa419fceb2acab7750cf54729f07c31788b52c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package typedpy/2.24.0/typedpy-2.24.0-py3-none-any.whl

---
+++ packages/typedpy/typedpy-2.24.0-py3-none-any.whl/typedpy/structures/defaults.py
@@ -0,0 +1,9 @@
+class TypedPyDefaults:
+    additional_properties_default = True
+    compact_serialization_default = False
+    automatic_enum_conversion = True
+    support_json_schema_v6 = False
+    uniqueness_features_enabled = False
+    defensive_copy_on_get = True
+    allow_none_for_optionals = False
+    block_unknown_consts = True
