9c4e890a9e0317af6d10a873f0989a96f5b32ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:38 +0000

    Add package gameyamlspiderandgenerator/1.6.2/gameyamlspiderandgenerator-1.6.2.tar.gz

---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-1.6.2.tar.gz/gameyamlspiderandgenerator-1.6.2/gameyamlspiderandgenerator/__init__.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from loguru import logger
+
+from .plugin import BasePlugin
+from .util.fgi_yaml import YamlData
+from .util.plugin_manager import pkg
+
+
+def verify(url: str):
+    verify_list = [
+        [
+            pkg.plugin[n].verify,
+            pkg.plugin[n],
+        ]
+        for n in pkg.plugin
+    ]
+    return next((cls for func, cls in verify_list if func(url)), None)
+
+
+def produce_yaml(url: str) -> Optional[YamlData]:
+    ret = verify(url)
+    if ret is None:
+        logger.error("URL is invalid")
+        return
+    return ret(url).to_yaml()
