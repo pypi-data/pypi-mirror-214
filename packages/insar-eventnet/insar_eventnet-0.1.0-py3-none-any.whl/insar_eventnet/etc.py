60dd8f42b6ee7e189ac2feec93b54ac3c07d8288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:14 +0000

    Add package insar-eventnet/0.1.0/insar_eventnet-0.1.0-py3-none-any.whl

---
+++ packages/insar-eventnet/insar_eventnet-0.1.0-py3-none-any.whl/insar_eventnet/etc.py
@@ -0,0 +1,27 @@
+"""
+ Summary
+ -------
+ Misc functions.
+
+ Notes
+ -----
+ Created by Jason Herning.
+"""
+
+import random
+import sys
+
+
+def new_seed():
+    """
+    Randomly generates a seed for use in synthetic interferogram dataset generation.
+
+    Returns
+    --------
+    n/a : float
+        Randomly generated seed for use as a random seed.
+    """
+
+    seed_value = random.randrange(sys.maxsize)
+    random.seed(seed_value)
+    return random.randint(100000, 999999)
