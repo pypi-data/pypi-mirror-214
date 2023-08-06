f580f8de520aa005f64bbbad616d376a69fe7e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:34 +0000

    Add package bonn/0.1.3/bonn-0.1.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/bonn/bonn-0.1.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/bonn/__init__.py
@@ -0,0 +1,4 @@
+# import the contents of the Rust library into the Python extension
+# optional: include the documentation from the Rust module
+from .bonn import *
+from .bonn import __all__, __doc__
