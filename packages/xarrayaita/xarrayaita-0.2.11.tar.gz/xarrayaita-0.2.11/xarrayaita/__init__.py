949c4241b21329d42d56702a0cb1ce9e96ce18ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:58 +0000

    Add package xarrayaita/0.2.11/xarrayaita-0.2.11.tar.gz

---
+++ packages/xarrayaita/xarrayaita-0.2.11.tar.gz/xarrayaita-0.2.11/xarrayaita/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .aita import aita
+from .loadData_aita import aita5col
+from .loadData_aita import aita3col
+from .loadData_aita import craft_input
+
+
+__all__ = ['aita', 'aita5col', 'aita3col', 'craft_input']
+
+__version__='0.2.6'
