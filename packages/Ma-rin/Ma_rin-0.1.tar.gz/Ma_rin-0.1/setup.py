835a61522ca551d2f1d265a83d9911d6b9bb0eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package Ma-rin/0.1/Ma_rin-0.1.tar.gz

---
+++ packages/Ma-rin/Ma_rin-0.1.tar.gz/Ma_rin-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ma_rin",
+    version="0.1",
+    license="MIT",
+    description="Diagrama fasorial",
+    author="Anigi marín",
+    packages=find_packages(),
+    url="https://github.com/AMRxviii/A"
+
+)
