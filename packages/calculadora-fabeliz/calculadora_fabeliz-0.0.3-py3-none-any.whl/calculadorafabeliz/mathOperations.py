c1040c6bd115d2f29104d4da9b30cf709970f432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:23 +0000

    Add package calculadora-fabeliz/0.0.3/calculadora_fabeliz-0.0.3-py3-none-any.whl

---
+++ packages/calculadora-fabeliz/calculadora_fabeliz-0.0.3-py3-none-any.whl/calculadorafabeliz/mathOperations.py
@@ -0,0 +1,52 @@
+"""
+modulo para otras operaciones matematicas
+"""
+import math
+
+
+class otrasoperaciones:
+    """
+    Clase para operaciones matematicas de : Log, Valor Absoluto, Raiz cuadrada
+    """
+
+    def logaritmo(self, num1):
+        """
+        Metodo para calcular el logaritmo
+        """
+        return math.log(num1)
+
+    def absoluto(self, num1):
+        """
+        Metodo para calcular el logaritmo
+        """
+        return abs(num1)
+
+    def raizCuadrada(self, num1):
+        """
+        Metodo para calcular el logaritmo
+        """
+        return math.sqrt(num1)
+
+    def seno(self, num1):
+        """
+        Metodo para calcular el seno
+        """
+        return math.sin(num1)
+
+    def coseno(self, num1):
+        """
+        Metodo para calcular el coseno
+        """
+        return math.cos(num1)
+
+    def tangente(self, num1):
+        """
+        Metodo para calcular la tangente
+        """
+        return math.tan(num1)
+
+    def cotangente(self, num1):
+        """
+        Metodo para calcular la tangente
+        """
+        return 1/math.tan(num1)
