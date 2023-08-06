f017593e98f8162a5f6495b717f5e48fd38f6442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:55 +0000

    Add package moneybhaiKV/0.2/moneybhaiKV-0.2.tar.gz

---
+++ packages/moneybhaiKV/moneybhaiKV-0.2.tar.gz/moneybhaiKV-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.2"
+DESCRIPTION = "This is a simple package created to make my school life a lot easier... HOPE U WILL LIKE IT"
+
+setup(name="moneybhaiKV", 
+      version=VERSION, 
+      description=DESCRIPTION, 
+      author="Yatharth Pandey", 
+      author_email="pandeyyatharth2005@gmail.com", 
+      packages=find_packages(), 
+      install_requires=[], 
+      keywords=['python', 'school', 'easy', 'noob', 'school_project'], 
+      classifiers=[
+          "Development Status :: 1 - Planning", 
+          "Intended Audience :: Developers", 
+          "Programming Language :: Python :: 3", 
+          "Operating System :: Unix", 
+          "Operating System :: MacOS :: MacOS X", 
+          "Operating System :: Microsoft :: Windows", 
+          ])
