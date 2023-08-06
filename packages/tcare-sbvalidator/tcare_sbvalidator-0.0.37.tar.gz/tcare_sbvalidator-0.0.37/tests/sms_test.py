8401dda7b155aed4da944230cfb51cb0e1981a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:02 +0000

    Add package tcare-sbvalidator/0.0.37/tcare_sbvalidator-0.0.37.tar.gz

---
+++ packages/tcare-sbvalidator/tcare_sbvalidator-0.0.37.tar.gz/tcare_sbvalidator-0.0.37/tests/sms_test.py
@@ -0,0 +1,24 @@
+import asyncio, sys
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+sys.path.append("..")
+from src.tcare_sbvalidator.sms_handler import Sms
+
+NAMESPACE = config['NAMESPACELOCAL']
+CONNECTION_STRING = config['CONNECTION_STRING']
+SENDER = config['TEST_SMS_SENDER']
+RECIPIENT = config['TEST_SMS_RECIPIENT']
+
+async def main():
+    handler = Sms()
+    handler.connectWithCred(NAMESPACE)
+
+    await handler.send_sms(
+        id="123",
+        sender=SENDER,
+        recipient=RECIPIENT,
+        content="Helloo!"
+    )
+
+asyncio.run(main())
