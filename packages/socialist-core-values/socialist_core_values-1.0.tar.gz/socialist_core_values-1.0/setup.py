29e8cd7354d8b6deafd9e8222f2f1f21e46560c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:45 +0000

    Add package socialist-core-values/1.0/socialist_core_values-1.0.tar.gz

---
+++ packages/socialist-core-values/socialist_core_values-1.0.tar.gz/socialist_core_values-1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+# @Time:  10:08
+# @Author: 浪飒
+# @File: setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='socialist_core_values',
+    version='1.0',
+    author="浪飒",
+    url='https://github.com/langsasec/socialist_core_values',
+    author_email="langsa-hy@qq.com",
+    license="GPL",
+    description='社会主义核心价值观加解密',
+    packages=find_packages()
+    )
+
+
+
+
