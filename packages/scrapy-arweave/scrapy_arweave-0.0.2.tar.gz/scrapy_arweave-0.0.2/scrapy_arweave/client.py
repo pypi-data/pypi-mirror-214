1e99aa98c7f9ad50a3aa459221fd97cfe36e7ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:03 +0000

    Add package scrapy-arweave/0.0.2/scrapy_arweave-0.0.2.tar.gz

---
+++ packages/scrapy-arweave/scrapy_arweave-0.0.2.tar.gz/scrapy_arweave-0.0.2/scrapy_arweave/client.py
@@ -0,0 +1,92 @@
+import hashlib
+import mimetypes
+import os
+from urllib.parse import urljoin
+
+from ar import ANS104DataItemHeader, DataItem, Wallet
+from ar.transaction import Transaction
+from ar.utils.transaction_uploader import create_tag, get_uploader
+from bundlr import Node
+
+
+class ArweaveStorageClient:
+    wallet = None
+
+    def __init__(self, wallet_jwk, gateway_url) -> None:
+        self.GATEWAY_URL = gateway_url
+        self.load_wallet(wallet_jwk)
+        self.node = Node()
+
+    def calculate_hash(self, filepath, algorithm='sha256', chunk_size=65536):
+        """
+        Calculate the hash of a file using the specified algorithm.
+        Returns the hash value as a hexadecimal string.
+        """
+        # Create a hash object using the specified algorithm
+        hash_object = hashlib.new(algorithm)
+
+        # Read the file in chunks and update the hash object
+        with open(filepath, 'rb') as f:
+            while True:
+                data = f.read(chunk_size)
+                if not data:
+                    break
+                hash_object.update(data)
+
+        # Get the hexadecimal representation of the hash value
+        hash_value = hash_object.hexdigest()
+
+        return hash_value
+
+    def load_wallet(self, wallet_jwk):
+        try:
+            if os.path.isfile(wallet_jwk):
+                self.wallet = Wallet(wallet_jwk)
+            else:
+                self.wallet = Wallet.from_data(wallet_jwk)
+            self.wallet.api_url = self.GATEWAY_URL
+        except:
+            raise Exception("Error loading wallet jwk")
+
+    def _get_mime_type(self, file_path):
+        mimetype, _ = mimetypes.guess_type(file_path)
+        if mimetype is None:
+            try:
+                import magic
+
+                mimetype = magic.from_file(file_path, mime=True)
+            except ImportError:
+                pass
+        return mimetype
+
+    def upload(self, file_path, file_buffer):
+        mime_type = self._get_mime_type(file_path)
+        try:
+            tags = [create_tag("Content-Type", mime_type, True)]
+            dataitem = DataItem(data=file_buffer, header=ANS104DataItemHeader(tags=tags))
+            dataitem.sign(self.wallet.rsa)
+            result = self.node.send_tx(dataitem.tobytes())
+            txid = result['id']
+            return txid
+        except:
+            pass
+
+        try:
+            with open(file_path, 'rb', buffering=0) as file_handler:
+                tx = Transaction(self.wallet, file_handler=file_handler, file_path=file_path)
+                tx.api_url = self.GATEWAY_URL
+                tx.add_tag('Content-Type', mime_type)
+                tx.sign()
+
+                uploader = get_uploader(tx, file_handler)
+
+                while not uploader.is_complete:
+                    uploader.upload_chunk()
+                return tx.id
+        except:
+            pass
+
+        raise Exception("Upload Error")
+
+    def get_url(self, tx_id):
+        return urljoin(self.GATEWAY_URL, tx_id)
---
+++ packages/scrapy-arweave/scrapy_arweave-0.0.2.tar.gz/scrapy_arweave-0.0.2/setup.py
@@ -0,0 +1,152 @@
+import io
+import os
+import platform
+import subprocess
+import sys
+from shutil import rmtree
+
+from setuptools import Command, setup
+from setuptools.command.install import install
+
+platform_commands = {
+    'Linux': {
+        'command': ['sudo', 'apt-get', 'install', '-y', 'libmagic1'],
+        'message': 'Failed to install libmagic1. Please install it manually with command: sudo apt-get install libmagic1',
+    },
+    'Windows': {
+        'command': ['pip', 'install', 'python-magic-bin'],
+        'message': 'Failed to install python-magic-bin. Please install it manually with command: pip install python-magic-bin',
+    },
+    'Darwin': {
+        'command': ['brew', 'install', 'libmagic'],
+        'message': 'Failed to install libmagic. Please install it manually with command: brew install libmagic',
+    },
+}
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+        try:
+            current_system = platform.system()
+
+            if current_system in platform_commands:
+                command_info = platform_commands[current_system]
+                try:
+                    subprocess.run(command_info['command'], check=True)
+                except subprocess.CalledProcessError:
+                    print(command_info['message'])
+
+        except:
+            pass
+
+
+# Package meta-data.
+NAME = "scrapy_arweave"
+DESCRIPTION = "Scrapy is a popular open-source and collaborative python framework for extracting the data you need from websites. scrapy-arweave provides scrapy pipelines and feed exports to store items into Arweave."
+URL = "https://github.com/pawanpaudel93/scrapy-arweave"
+EMAIL = "pawanpaudel93@gmail.com"
+AUTHOR = "Pawan Paudel"
+REQUIRES_PYTHON = ">=3.0"
+VERSION = "0.0.2"
+
+REQUIRED = [
+    "python-magic",
+    "pyarweave",
+]
+
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=["scrapy_arweave"],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    packages=["scrapy_arweave"],
+    include_package_data=True,
+    license="ISC",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand, "install": PostInstallCommand},
+)
