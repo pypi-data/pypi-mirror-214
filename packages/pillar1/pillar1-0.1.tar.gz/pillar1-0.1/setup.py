c48590433c4c47c901958c17400bd18f1e95817e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:39 +0000

    Add package pillar1/0.1/pillar1-0.1.tar.gz

---
+++ packages/pillar1/pillar1-0.1.tar.gz/pillar1-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pillar1',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    author='Ayman Hajja',
+    author_email='amhajja@gmail.com',
+    url='https://github.com/amhajja/pillar1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
