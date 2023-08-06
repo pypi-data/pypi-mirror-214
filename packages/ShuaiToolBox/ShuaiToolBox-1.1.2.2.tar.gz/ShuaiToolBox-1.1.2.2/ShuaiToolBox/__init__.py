119c2c78d1038c95bb36f1b12b574490ff8da15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:11 +0000

    Add package ShuaiToolBox/1.1.2.2/ShuaiToolBox-1.1.2.2.tar.gz

---
+++ packages/ShuaiToolBox/ShuaiToolBox-1.1.2.2.tar.gz/ShuaiToolBox-1.1.2.2/ShuaiToolBox/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+from .VideoPro import *
+from .py_plugin import *
+from .cv_plugin import *
+from .FindContours import *
+from .opencv_Functions import *
+from .EORmodule import *
