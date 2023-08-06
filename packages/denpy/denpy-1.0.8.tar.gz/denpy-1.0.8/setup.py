5a9d3b9340c230d272a4fc7182bdb84424f7f26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:58 +0000

    Add package denpy/1.0.8/denpy-1.0.8.tar.gz

---
+++ packages/denpy/denpy-1.0.8.tar.gz/denpy-1.0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='denpy',
+    version='1.0.8',
+    description='My first own package!',
+    author='Денис☻#1489',
+    packages=find_packages()
+)
