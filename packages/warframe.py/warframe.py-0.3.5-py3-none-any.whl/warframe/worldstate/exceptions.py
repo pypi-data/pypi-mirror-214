9989df198c319944a9a03aab3abd227588f87007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:50 +0000

    Add package warframe.py/0.3.5/warframe.py-0.3.5-py3-none-any.whl

---
+++ packages/warframe.py/warframe.py-0.3.5-py3-none-any.whl/warframe/worldstate/exceptions.py
@@ -0,0 +1,41 @@
+from typing import TypeVar
+
+from msgspec import field
+
+from .common import WorldstateObject, SingleQueryModel
+
+__all__ = [
+    "WorldstateAPIError",
+    "WorldstateError",
+    "UnsupportedSingleQueryError",
+]
+
+T = TypeVar("T")
+
+
+class ErrorMessage(WorldstateObject):
+    message: str = field(name="error")
+    code: int
+
+
+class WorldstateError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class WorldstateAPIError(WorldstateError):
+    def __init__(self, error_message: ErrorMessage, *args) -> None:
+        super().__init__(*args)
+        self.error_message: ErrorMessage = error_message
+
+
+class UnsupportedSingleQueryError(WorldstateError):
+    pass
+
+
+class UnsupportedMultiQueryError(WorldstateError):
+    pass
+
+
+class SessionNotFound(WorldstateError):
+    pass
