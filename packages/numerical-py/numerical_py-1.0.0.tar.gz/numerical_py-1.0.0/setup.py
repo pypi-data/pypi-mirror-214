a5c271090b2ac364993128afdcc603856eca8a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:35 +0000

    Add package numerical-py/1.0.0/numerical_py-1.0.0.tar.gz

---
+++ packages/numerical-py/numerical_py-1.0.0.tar.gz/numerical_py-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='numerical_py',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Library for Numerical Methods Exam',
+    author='Aleksey Malakhov',
+    license='MIT',
+)
