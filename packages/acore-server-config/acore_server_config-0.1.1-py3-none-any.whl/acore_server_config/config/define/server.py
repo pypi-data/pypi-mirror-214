b1ff42e696426188a7ff6120a868f0cb957cc6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package acore-server-config/0.1.1/acore_server_config-0.1.1-py3-none-any.whl

---
+++ packages/acore-server-config/acore_server_config-0.1.1-py3-none-any.whl/acore_server_config/config/define/server.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import typing as T
+import dataclasses
+
+if T.TYPE_CHECKING:  # pragma: no cover
+    from .main import Env
+
+
+@dataclasses.dataclass
+class Server:
+    """
+    Per Game Server configuration.
+
+    :param id: Server id, the naming convention is ``${env_name}-${server_name}``
+    :param db_admin_password: the RDS admin password, we need this password
+        to create the database user.
+    :param db_username: the database user for game server
+    :param db_password: the database password for game server
+    """
+    id: T.Optional[str] = dataclasses.field(default=None)
+    db_admin_password: T.Optional[str] = dataclasses.field(default=None)
+    db_username: T.Optional[str] = dataclasses.field(default=None)
+    db_password: T.Optional[str] = dataclasses.field(default=None)
+
+
+@dataclasses.dataclass
+class ServerMixin:
+    servers: T.Dict[str, Server] = dataclasses.field(default_factory=dict)
+
+    @property
+    def server_blue(self) -> Server:
+        return self.servers["blue"]
+
+    @property
+    def server_green(self) -> Server:
+        return self.servers["green"]
