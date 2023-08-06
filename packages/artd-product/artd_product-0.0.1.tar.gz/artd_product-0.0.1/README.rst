63004cadfe9c4af617837b624c2e562c7f7e0747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:16 +0000

    Add package artd-product/0.0.1/artd_product-0.0.1.tar.gz

---
+++ packages/artd-product/artd_product-0.0.1.tar.gz/artd_product-0.0.1/README.rst
@@ -0,0 +1,22 @@
+=================
+Products
+=================
+
+A Django app to create products.
+
+
+Quick start
+-----------
+
+1. Add "product" to your INSTALLED_APPS setting like this:
+    
+        INSTALLED_APPS = [
+            ...
+            'product',
+        ]
+
+2. Run ``python manage.py migrate`` to create the polls models.
+
+3. Run ``python manage.py createsuperuser`` to create a superuser.
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/
---
+++ packages/artd-product/artd_product-0.0.1.tar.gz/artd_product-0.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+setup(
+    name="artd_product",
+    version="0.0.1",
+    include_package_data=True,
+    author="Jonathan Urzola Maladonado",
+    author_email="jonathan@artd.com.co",
+    description="A Django app to create Colombian cities",
+    long_description_content_type="text/markdown",
+    url="https://www.artd.com.co/",
+    long_description=long_description,
+    packages=find_packages(
+        include=[
+            "pillow",
+        ]
+    ),
+    keywords=["pypi", "cicd", "python"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
