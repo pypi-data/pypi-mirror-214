2348f572bf198a393e59e823744f696fd58dcbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:19 +0000

    Add package brainprint/0.4.0/brainprint-0.4.0-py3-none-any.whl

---
+++ packages/brainprint/brainprint-0.4.0-py3-none-any.whl/brainprint/cli/__init__.py
@@ -0,0 +1,11 @@
+"""
+BrainPrint analysis CLI.
+"""
+from ..brainprint import run_brainprint
+from .parser import parse_options
+
+
+def main():
+    options = parse_options()
+    if options is not None:
+        return run_brainprint(**options)
