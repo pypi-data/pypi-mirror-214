ecf2f258648c95d8eb270b87c871ce2ebcc0746e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:07 +0000

    Add package picogo/0.0.9/picogo-0.0.9.tar.gz

---
+++ packages/picogo/picogo-0.0.9.tar.gz/picogo-0.0.9/picogo/__init__.py
@@ -0,0 +1,10 @@
+__name__ = "picogo"
+__package__ = "picogo"
+__version__ = '0.0.5'
+__author__ = "SkySLAM Co., Ltd."
+
+from .Robot import (
+    Robot , 
+)
+
+print( "picogo __init__.py executed." )
---
+++ packages/picogo/picogo-0.0.9.tar.gz/picogo-0.0.9/setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup
+
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+__project__ = 'picogo'
+__packages__ = ['picogo']
+__desc__ = 'A beginner-friendly library for using picogo with the Raspberry Pi Pico. '
+__version__ = '0.0.9'
+__author__ = "SkySLAM Co., Ltd."
+__author_email__ = 'terabuilder@gmail.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/sunabove/PicoGo'
+__keywords__ = [
+    'picogo',
+    'pico',
+    'raspberry',
+    'raspberry pi',
+    'picorun',
+    'skyslam',
+]
+__classifiers__ = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+    ]
+__long_description__ = """A beginner-friendly library for using picogo with the Raspberry Pi Pico.
+
+```python
+from picogo import Robot
+
+robot = Robot()
+robot.forward( speed = 30 )
+
+```
+
+Documentation is available at (https://github.com/sunabove/PicoGo/).
+"""
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        
+        # copy main file
+        self.copy_main_file()
+    pass
+
+    def copy_main_file(self) :
+        file = open( "/myMainFile.py", "w" )
+        file.close()
+    pass
+pass
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__desc__,
+    long_description=__long_description__,
+    long_description_content_type='text/markdown',
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
+    classifiers=__classifiers__,
+    keywords=__keywords__,
+    packages=__packages__,
+
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
