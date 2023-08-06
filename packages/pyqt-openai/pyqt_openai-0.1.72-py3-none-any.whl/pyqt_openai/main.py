4dcd9b0e4d46f723645fc15738bbe6eae62e854f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:40 +0000

    Add package pyqt-openai/0.1.72/pyqt_openai-0.1.72-py3-none-any.whl

---
+++ packages/pyqt-openai/pyqt_openai-0.1.72-py3-none-any.whl/pyqt_openai/main.py
@@ -0,0 +1,305 @@
+import os, sys
+import openai, requests
+
+# Get the absolute path of the current script file
+script_path = os.path.abspath(__file__)
+
+# Get the root directory by going up one level from the script directory
+project_root = os.path.dirname(os.path.dirname(script_path))
+
+sys.path.insert(0, project_root)
+sys.path.insert(0, os.getcwd())  # Add the current directory as well
+
+from qtpy.QtGui import QGuiApplication, QFont, QIcon, QColor
+from qtpy.QtWidgets import QMainWindow, QToolBar, QHBoxLayout, QDialog, QLineEdit, QPushButton, QWidgetAction, QSpinBox, QLabel, QWidget, QApplication, \
+    QComboBox, QSizePolicy, QStackedWidget, QAction, QMenu, QSystemTrayIcon, \
+    QMessageBox, QCheckBox
+from qtpy.QtCore import Qt, QCoreApplication, QSettings
+
+from pyqt_openai.aboutDialog import AboutDialog
+from pyqt_openai.customizeDialog import CustomizeDialog
+from pyqt_openai.svgButton import SvgButton
+from pyqt_openai.image_gen_widget.imageGeneratingToolWidget import ImageGeneratingToolWidget
+from pyqt_openai.openAiChatBotWidget import OpenAIChatBotWidget
+
+# for testing pyside6
+# if you use pyside6 already, you don't have to remove the #
+# os.environ['QT_API'] = 'pyside6'
+
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)  # HighDPI support
+# qt version should be above 5.14
+# todo check the qt version with qtpy
+if os.environ['QT_API'] == 'pyqt5' or os.environ['QT_API'] != 'pyside6':
+    QGuiApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+
+QApplication.setFont(QFont('Arial', 12))
+QApplication.setWindowIcon(QIcon('ico/openai.svg'))
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.__initVal()
+        self.__initUi()
+
+    def __initVal(self):
+        self.__settings_struct = QSettings('pyqt_openai.ini', QSettings.IniFormat)
+
+    def __initUi(self):
+        self.setWindowTitle('PyQt OpenAI Chatbot')
+
+        self.__openAiChatBotWidget = OpenAIChatBotWidget()
+        self.__openAiChatBotWidget.notifierWidgetActivated.connect(self.show)
+        self.__imageGeneratingToolWidget = ImageGeneratingToolWidget()
+        self.__imageGeneratingToolWidget.notifierWidgetActivated.connect(self.show)
+        self.__mainWidget = QStackedWidget()
+        self.__mainWidget.addWidget(self.__openAiChatBotWidget)
+        self.__mainWidget.addWidget(self.__imageGeneratingToolWidget)
+
+        self.__setActions()
+        self.__setMenuBar()
+        self.__setTrayMenu()
+        self.__setToolBar()
+
+        # load ini file
+        self.__loadApiKeyInIni()
+
+        # check if loaded API_KEY from ini file is not empty
+        if openai.api_key:
+            self.__setApi()
+        # if it is empty
+        else:
+            self.__setAIEnabled(False)
+            self.__apiCheckPreviewLbl.hide()
+
+        self.setCentralWidget(self.__mainWidget)
+        self.resize(1024, 768)
+
+    def __setActions(self):
+        # menu action
+        self.__exitAction = QAction("Exit", self)
+        self.__exitAction.triggered.connect(self.close)
+
+        self.__aboutAction = QAction("About...", self)
+        self.__aboutAction.triggered.connect(self.__showAboutDialog)
+
+        # toolbar action
+        self.__chooseAiAction = QWidgetAction(self)
+        self.__chooseAiCmbBox = QComboBox()
+        self.__chooseAiCmbBox.addItems(['Chat', 'Image'])
+        self.__chooseAiCmbBox.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        self.__chooseAiCmbBox.currentIndexChanged.connect(self.__aiTypeChanged)
+        self.__chooseAiAction.setDefaultWidget(self.__chooseAiCmbBox)
+
+        self.__stackAction = QWidgetAction(self)
+        self.__stackBtn = SvgButton()
+        self.__stackBtn.setIcon('ico/stackontop.svg')
+        self.__stackBtn.setCheckable(True)
+        self.__stackBtn.toggled.connect(self.__stackToggle)
+        self.__stackAction.setDefaultWidget(self.__stackBtn)
+        self.__stackBtn.setToolTip('Always On Top')
+
+        self.__customizeAction = QWidgetAction(self)
+        self.__customizeBtn = SvgButton()
+        self.__customizeBtn.setIcon('ico/customize.svg')
+        self.__customizeBtn.clicked.connect(self.__executeCustomizeDialog)
+        self.__customizeAction.setDefaultWidget(self.__customizeBtn)
+        self.__customizeBtn.setToolTip('Customize (working)')
+
+        self.__transparentAction = QWidgetAction(self)
+        self.__transparentSpinBox = QSpinBox()
+        self.__transparentSpinBox.setRange(0, 100)
+        self.__transparentSpinBox.setValue(100)
+        self.__transparentSpinBox.valueChanged.connect(self.__setTransparency)
+        self.__transparentSpinBox.setToolTip('Set Transparency of Window')
+
+        lay = QHBoxLayout()
+        lay.addWidget(QLabel('Window Transparency'))
+        lay.addWidget(self.__transparentSpinBox)
+
+        transparencyActionWidget = QWidget()
+        transparencyActionWidget.setLayout(lay)
+        self.__transparentAction.setDefaultWidget(transparencyActionWidget)
+
+        self.__showAiToolBarAction = QWidgetAction(self)
+        self.__showAiToolBarChkBox = QCheckBox('Show AI Toolbar')
+        self.__showAiToolBarChkBox.setChecked(True)
+        self.__showAiToolBarChkBox.toggled.connect(self.__showAiToolBarChkBoxChecked)
+        self.__showAiToolBarAction.setDefaultWidget(self.__showAiToolBarChkBox)
+
+        self.__apiCheckPreviewLbl = QLabel()
+        self.__apiCheckPreviewLbl.setFont(QFont('Arial', 10))
+
+        apiLbl = QLabel('API')
+
+        self.__apiLineEdit = QLineEdit()
+        self.__apiLineEdit.setPlaceholderText('Write your API Key...')
+        self.__apiLineEdit.returnPressed.connect(self.__setApi)
+        self.__apiLineEdit.setEchoMode(QLineEdit.Password)
+
+        apiBtn = QPushButton('Use')
+        apiBtn.clicked.connect(self.__setApi)
+
+        lay = QHBoxLayout()
+        lay.addWidget(apiLbl)
+        lay.addWidget(self.__apiLineEdit)
+        lay.addWidget(apiBtn)
+        lay.addWidget(self.__apiCheckPreviewLbl)
+        lay.setContentsMargins(0, 0, 0, 0)
+
+        apiWidget = QWidget()
+        apiWidget.setLayout(lay)
+
+        self.__apiAction = QWidgetAction(self)
+        self.__apiAction.setDefaultWidget(apiWidget)
+
+    def __setMenuBar(self):
+        menubar = self.menuBar()
+
+        # create the "File" menu
+        fileMenu = QMenu("File", self)
+        fileMenu.addAction(self.__exitAction)
+        menubar.addMenu(fileMenu)
+
+        # create the "Help" menu
+        helpMenu = QMenu("Help", self)
+        menubar.addMenu(helpMenu)
+
+        helpMenu.addAction(self.__aboutAction)
+
+    def __setTrayMenu(self):
+        # background app
+        menu = QMenu()
+
+        action = QAction("Quit", self)
+        action.setIcon(QIcon('ico/close.svg'))
+
+        action.triggered.connect(app.quit)
+
+        menu.addAction(action)
+
+        tray_icon = QSystemTrayIcon(app)
+        tray_icon.setIcon(QIcon('ico/openai.svg'))
+        tray_icon.activated.connect(self.__activated)
+
+        tray_icon.setContextMenu(menu)
+
+        tray_icon.show()
+
+    def __activated(self, reason):
+        if reason == QSystemTrayIcon.DoubleClick:
+            self.show()
+
+    def __setToolBar(self):
+        aiTypeToolBar = QToolBar()
+        aiTypeToolBar.setMovable(False)
+        aiTypeToolBar.addAction(self.__chooseAiAction)
+
+        windowToolBar = QToolBar()
+        lay = windowToolBar.layout()
+        windowToolBar.addAction(self.__stackAction)
+        windowToolBar.addAction(self.__customizeAction)
+        windowToolBar.addAction(self.__transparentAction)
+        windowToolBar.addAction(self.__showAiToolBarAction)
+        windowToolBar.addAction(self.__apiAction)
+        windowToolBar.setLayout(lay)
+        windowToolBar.setMovable(False)
+        self.addToolBar(aiTypeToolBar)
+        self.addToolBar(windowToolBar)
+        # QToolbar's layout can't be set spacing with lay.setSpacing so i've just did this instead
+        windowToolBar.setStyleSheet('QToolBar { spacing: 2px; }')
+
+    def __setApiKey(self, api_key):
+        # for script
+        openai.api_key = api_key
+        # for subprocess (mostly)
+        os.environ['OPENAI_API_KEY'] = api_key
+        # for showing to the user
+        self.__apiLineEdit.setText(api_key)
+
+    def __loadApiKeyInIni(self):
+        # this api key should be yours
+        if self.__settings_struct.contains('API_KEY'):
+            self.__setApiKey(self.__settings_struct.value('API_KEY'))
+        else:
+            self.__settings_struct.setValue('API_KEY', '')
+
+    def __setAIEnabled(self, f):
+        self.__openAiChatBotWidget.setAIEnabled(f)
+        self.__imageGeneratingToolWidget.setAIEnabled(f)
+
+    def __setApi(self):
+        try:
+            api_key = self.__apiLineEdit.text()
+            response = requests.get('https://api.openai.com/v1/models', headers={'Authorization': f'Bearer {api_key}'})
+            f = response.status_code == 200
+            self.__setAIEnabled(f)
+            if f:
+                self.__setApiKey(api_key)
+                self.__settings_struct.setValue('API_KEY', api_key)
+
+                self.__openAiChatBotWidget.setModelInfoByModel(True)
+
+                self.__apiCheckPreviewLbl.setStyleSheet("color: {}".format(QColor(0, 200, 0).name()))
+                self.__apiCheckPreviewLbl.setText('API key is valid')
+            else:
+                raise Exception
+        except Exception as e:
+            self.__apiCheckPreviewLbl.setStyleSheet("color: {}".format(QColor(255, 0, 0).name()))
+            self.__apiCheckPreviewLbl.setText('API key is invalid')
+            self.__setAIEnabled(False)
+            print(e)
+        finally:
+            self.__apiCheckPreviewLbl.show()
+
+    def __showAboutDialog(self):
+        aboutDialog = AboutDialog()
+        aboutDialog.exec()
+
+    def __stackToggle(self, f):
+        if f:
+            self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+        else:
+            self.setWindowFlags(self.windowFlags() & ~Qt.WindowStaysOnTopHint)
+        self.show()
+
+    def __setTransparency(self, v):
+        self.setWindowOpacity(v / 100)
+
+    def __showAiToolBarChkBoxChecked(self, f):
+        self.__mainWidget.currentWidget().showAiToolBar(f)
+
+    def __executeCustomizeDialog(self):
+        dialog = CustomizeDialog(self)
+        reply = dialog.exec()
+        if reply == QDialog.Accepted:
+            pass
+
+    def __aiTypeChanged(self, i):
+        self.__mainWidget.setCurrentIndex(i)
+
+    def closeEvent(self, e):
+        message = 'The window has been closed. Would you like to continue running this app in the background?'
+        closeMessageBox = QMessageBox(self)
+        closeMessageBox.setWindowTitle('Wait!')
+        closeMessageBox.setText(message)
+        closeMessageBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        reply = closeMessageBox.exec()
+        # Yes
+        if reply == 16384:
+            e.accept()
+        # No
+        elif reply == 65536:
+            app.quit()
+        return super().closeEvent(e)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
