a38b2c5e69fc90c6e136a127dfc981a31b59dd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:41 +0000

    Add package nwon-django-toolbox/0.5.3/nwon_django_toolbox-0.5.3.tar.gz

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.5.3.tar.gz/nwon_django_toolbox-0.5.3/nwon_django_toolbox/tests/celery/celery_folders.py
@@ -0,0 +1,54 @@
+from os import path
+from typing import List, Optional
+
+from django.conf import settings
+from nwon_baseline.directory_helper import clean_directory, create_paths
+
+from nwon_django_toolbox.settings import NWON_DJANGO_SETTINGS
+from nwon_django_toolbox.typings.celery.celery_folders import CeleryFolder
+
+
+def clean_all_celery_messages():
+    for folder in __get_all_celery_message_folder():
+        clean_directory(folder)
+
+
+def create_celery_folder():
+    if "broker_transport_options" in settings.CELERY_SETTINGS:
+        create_paths(__get_all_celery_message_folder())
+
+
+def get_path_to_celery_folder(celery_folder: CeleryFolder) -> Optional[str]:
+    if "broker_transport_options" in settings.CELERY_SETTINGS:
+        folder = settings.CELERY_SETTINGS["broker_transport_options"][
+            celery_folder.value
+        ]
+
+        return (
+            path.join(folder, NWON_DJANGO_SETTINGS.tests.celery_folder_parent)
+            if NWON_DJANGO_SETTINGS.tests
+            and NWON_DJANGO_SETTINGS.tests.celery_folder_parent
+            else folder
+        )
+
+    return None
+
+
+def __get_all_celery_message_folder() -> List[str]:
+    celery_message_folders = [
+        get_path_to_celery_folder(folder)
+        for folder in [
+            CeleryFolder.DataFolderIn,
+            CeleryFolder.DataFolderOut,
+            CeleryFolder.DataFolderProcessed,
+        ]
+    ]
+
+    return [x for x in celery_message_folders if x is not None]
+
+
+__all__ = [
+    "clean_all_celery_messages",
+    "create_celery_folder",
+    "get_path_to_celery_folder",
+]
