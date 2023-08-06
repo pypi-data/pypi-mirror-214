75488bd119d78a0282b57471915aa835a9719f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:41 +0000

    Add package rerun-sdk/0.7.0/rerun_sdk-0.7.0-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/rerun-sdk/rerun_sdk-0.7.0-cp38-abi3-macosx_11_0_arm64.whl/rerun_sdk/rerun/experimental.py
@@ -0,0 +1,13 @@
+"""
+Experimental features for Rerun.
+
+These features are not yet stable and may change in future releases without
+going through the normal deprecation cycle.
+"""
+from __future__ import annotations
+
+from rerun.log.experimental.text import log_text_box
+
+__all__ = [
+    "log_text_box",
+]
