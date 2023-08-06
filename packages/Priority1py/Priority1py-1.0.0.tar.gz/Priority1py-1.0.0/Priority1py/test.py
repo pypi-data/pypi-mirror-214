45e89a7137adc6a342a3af2e5f988ab0067ae9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package Priority1py/1.0.0/Priority1py-1.0.0.tar.gz

---
+++ packages/Priority1py/Priority1py-1.0.0.tar.gz/Priority1py-1.0.0/Priority1py/test.py
@@ -0,0 +1,7 @@
+from Priority1py import Priority1py
+from strings import IDType
+
+if __name__ == '__main__':
+    test = Priority1py('b80ebbe6-d4d7-48ce-ba19-3686d455eac4')
+    tracking = test.get_latest_tracking('DEN2211', IDType.PO)
+    print(tracking)
