d7276715a3defd4376bbcf4ba684d8b64a9a7586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:12 +0000

    Add package bcvContext/0.1.11/bcvcontext-0.1.11-py3-none-any.whl

---
+++ packages/bcvContext/bcvcontext-0.1.11-py3-none-any.whl/bcvContext/bcvContext.py
@@ -0,0 +1,45 @@
+import json
+from subprocess import call
+
+notarizationPath = ""
+isToCache = 1
+
+def init(_notarizationPath, _isToCache) :
+    global notarizationPath, isToCache
+    notarizationPath = _notarizationPath
+    isToCache = int(_isToCache)
+
+def scriptContext(_filename) :
+    return _filename
+
+def notarizationContextRawfile():
+    return "../.." + str(notarizationPath) + "/../rawFile.csv"
+
+def notarizationContext(_filename) :
+    return "../.." + str(notarizationPath) + "/" + str(_filename)
+
+def dayContext(_m, _filename) :
+    if (_m >= 0) : raise Exception("No history at J", str(_m))
+    day = json.load(open("../.." + str(notarizationPath) + "/.config.json"))["day"] + _m
+    if (day < 0) : raise Exception("No history at J", str(day))
+    lastNotarization = json.load(open("../.." + str(notarizationPath) + "/../../Day" + str(day) + "/.dayConfig.json"))["lastNotarization"]
+    return "../.." + str(notarizationPath) + "/../../Day" + str(day) + "/Notarization" + str(lastNotarization) + "/" + str(_filename)
+
+def finish(*args) :
+    call(["rm", "-r","../__cache__"])
+    if isToCache == 1 :
+        call(["mkdir","../__cache__"])
+        for i in args :
+            try :
+                call(["mv",str(i),"../__cache__"])
+            except :
+                print("Warning : ", str(i), "not found")
+        print("Final result at :", "../__cache__")
+
+    else : 
+        for i in args : 
+            try : 
+                call(["mv",str(i),"../.." + str(notarizationPath)])
+            except :
+                print("Warning : ", str(i), "not found")
+        print("Final result at :", "../.." + str(notarizationPath))
