b2474df0a89c6b28756af1815945b0b1a5335222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:54 +0000

    Add package json2schema/0.1.1/json2schema-0.1.1-py3-none-any.whl

---
+++ packages/json2schema/json2schema-0.1.1-py3-none-any.whl/json2schema/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @FileName     :   json2schema.py
+# @Author       :   superhin
+# @CreateTime   :   2022/3/28 12:21
+# @Function     :
+
+
+from .json2schema import json2schema
+from .schema2list import schema2list
