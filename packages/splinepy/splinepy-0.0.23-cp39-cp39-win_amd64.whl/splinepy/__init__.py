c540acad2e2867e4cbdb3d7e2c2f8fff61bcdbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:28 +0000

    Add package splinepy/0.0.23/splinepy-0.0.23-cp39-cp39-win_amd64.whl

---
+++ packages/splinepy/splinepy-0.0.23-cp39-cp39-win_amd64.whl/splinepy/__init__.py
@@ -0,0 +1,52 @@
+from splinepy import (
+    bezier,
+    bspline,
+    helpme,
+    io,
+    load,
+    multipatch,
+    nurbs,
+    rational_bezier,
+    settings,
+    spline,
+    splinepy_core,
+    utils,
+)
+from splinepy._version import __version__
+from splinepy.bezier import Bezier
+from splinepy.bspline import BSpline
+from splinepy.load import load_solution, load_splines
+from splinepy.multipatch import Multipatch
+from splinepy.nurbs import NURBS
+from splinepy.rational_bezier import RationalBezier
+from splinepy.spline import Spline
+
+# set NAME_TO_TYPE
+settings.NAME_TO_TYPE = settings.__splinepy_name_to_type__()
+
+# configure logging
+utils.log.configure()
+
+__all__ = [
+    "__version__",
+    "bezier",
+    "bspline",
+    "io",
+    "load",
+    "multipatch",
+    "nurbs",
+    "rational_bezier",
+    "spline",
+    "splinepy_core",
+    "utils",
+    "settings",
+    "helpme",
+    "Bezier",
+    "BSpline",
+    "Multipatch",
+    "NURBS",
+    "RationalBezier",
+    "Spline",
+    "load_splines",
+    "load_solution",
+]
