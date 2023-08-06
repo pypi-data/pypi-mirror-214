3e7649443c1a43dc83e5064f9dd43b836c7a928c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package pymince/2.8.0/pymince-2.8.0.tar.gz

---
+++ packages/pymince/pymince-2.8.0.tar.gz/pymince-2.8.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+from pymince import __version__, __title__
+
+
+def read(filename):
+    with open(filename, encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name=__title__,
+    version=__version__,
+    description="Python shredded utilities",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+        "Topic :: Utilities",
+    ],
+    keywords="",
+    author="rmoralespp",
+    author_email="rmoralespp@gmail.com",
+    url="https://github.com/rmoralespp/pymince",
+    license="MIT",
+    packages=["pymince"],
+    include_package_data=True,
+    zip_safe=False,  # https://mypy.readthedocs.io/en/latest/installed_packages.html
+    # install_requires=read('requirements.txt'),
+    python_requires=">=3.8",
+)
