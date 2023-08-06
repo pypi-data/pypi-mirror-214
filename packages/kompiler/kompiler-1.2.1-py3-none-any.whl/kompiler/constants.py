be272c63581d5ac33da3b21f47090b6092da439f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:07 +0000

    Add package kompiler/1.2.1/kompiler-1.2.1-py3-none-any.whl

---
+++ packages/kompiler/kompiler-1.2.1-py3-none-any.whl/kompiler/constants.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2021 Garvit Joshi <garvitjoshi9@gmail.com>
+
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+VERSION_NO = "1.2.1"
+
+VERSION = "Kompiler: v"+VERSION_NO
+
+ABOUT = """
+Kompiler: A package for auto compiling C++ files as soon as they are saved.\n
+Source Code: https://github.com/garvit-joshi/kompiler/
+pip: https://pypi.org/project/kompiler/
+Made by Garvit Joshi https://github.com/garvit-joshi
+"""
+
+HELP = """
+Usage: kompiler [File.cpp] [COMMAND...]
+eg:
+    ❯ kompiler hello.cpp
+        Command used for compiling: g++ hello.cpp\n
+    ❯ kompiler hello.cpp -std=c++11 -O2 -Wall hello.cpp -o hello
+        Command used for compiling: g++ -std=c++11 -O2 -Wall hello.cpp -o hello\n
+    ❯ kompiler 3.cpp -std=c++11 -O2 -Wall 3.cpp -o hello "&&" ./hello
+        Command used for compiling: g++ -std=c++11 -O2 -Wall 3.cpp -o hello && ./hello\n
+"""
