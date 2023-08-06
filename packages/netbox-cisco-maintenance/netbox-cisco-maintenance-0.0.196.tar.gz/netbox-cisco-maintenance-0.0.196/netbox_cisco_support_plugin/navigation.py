4c02f19c6145417fe3f3e886c8e210f219219715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:17 +0000

    Add package netbox-cisco-maintenance/0.0.196/netbox-cisco-maintenance-0.0.196.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.196.tar.gz/netbox-cisco-maintenance-0.0.196/netbox_cisco_support_plugin/navigation.py
@@ -0,0 +1,12 @@
+from extras.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link="plugins:netbox_cisco_support_plugin:ciscodevicesupport_list",
+        link_text="Cisco Device Support"
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_cisco_support_plugin:ciscodevicetypesupport_list",
+        link_text="Cisco Device Type Support"
+    ),
+)
