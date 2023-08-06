8ca9a485fe1b91a5403298ec5d869cad6bba86fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:50 +0000

    Add package pytest-yaml-yoyo/1.2.7/pytest-yaml-yoyo-1.2.7.tar.gz

---
+++ packages/pytest-yaml-yoyo/pytest-yaml-yoyo-1.2.7.tar.gz/pytest-yaml-yoyo-1.2.7/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+"""The setup script.
+# 作者-上海悠悠 微信wx:283340479
+# blog地址 https://www.cnblogs.com/yoyoketang/
+"""
+
+setup(
+    name='pytest-yaml-yoyo',
+    url='https://github.com/yoyoketang/pytest-yaml-yoyo',
+    version='v1.2.7',
+    author="上海-悠悠",
+    author_email='283340479@qq.com',
+    description='http/https API run by yaml',
+    long_description=open("README.rst", encoding='utf-8').read(),
+    package_dir={"": "src"},
+    packages=["pytest_yaml_yoyo"],
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 3.8',
+    ],
+    license='proprietary',
+    keywords=[
+        'pytest', 'py.test', 'pytest-yaml', 'pytest-yaml-yoyo',
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        'Jinja2>=3.1.2',
+        'jmespath>=0.9.5',
+        'jsonpath>=0.82',
+        'pytest>=7.2.0',
+        'PyYAML>=6.0',
+        'requests>=2.28.1',
+        'allure-pytest>=2.12.0',
+        'pymysql>=1.0.2',
+        'DingtalkChatbot>=1.5.7',
+        'Faker>=15.3.4',
+        'requests_toolbelt>=0.10.1',
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest-yaml-yoyo = pytest_yaml_yoyo.plugin',
+        ]
+    }
+)
