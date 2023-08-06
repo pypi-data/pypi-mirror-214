2f3c486d261af7379bf881ec197f27bd6289531c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:42 +0000

    Add package ckanext-nhm/5.6.4/ckanext_nhm-5.6.4-py3-none-any.whl

---
+++ packages/ckanext-nhm/ckanext_nhm-5.6.4-py3-none-any.whl/ckanext/nhm/theme/assets/scripts/apps/search/src/store/appState.js
@@ -0,0 +1,56 @@
+let appState = {
+  namespaced: true,
+  state: {
+    app: {
+      loading: false,
+      error: false,
+    },
+    query: {
+      parsingError: {
+        unknown: false,
+        resourceIds: null,
+        queryBody: null,
+      },
+    },
+    status: {
+      resultData: {
+        loading: false,
+        failed: false,
+      },
+      slug: {
+        loading: false,
+        failed: false,
+      },
+      slugEdit: {
+        loading: false,
+        failed: false,
+      },
+      doi: {
+        loading: false,
+        failed: false,
+      },
+      download: {
+        loading: false,
+        failed: false,
+      },
+      resources: {
+        loading: false,
+        failed: false,
+      },
+      queryEdit: {
+        loading: false,
+        failed: false,
+      },
+    },
+  },
+  getters: {
+    app: (state) => {
+      return {
+        loading: state.app.loading || state.status.resources.loading,
+        failed: state.app.error || state.status.resources.failed,
+      };
+    },
+  },
+};
+
+export default appState;
