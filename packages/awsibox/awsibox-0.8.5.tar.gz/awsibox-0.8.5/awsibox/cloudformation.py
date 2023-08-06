f6d2b2bf755716daade6f22b854c0509cbb09225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:04 +0000

    Add package awsibox/0.8.5/awsibox-0.8.5.tar.gz

---
+++ packages/awsibox/awsibox-0.8.5.tar.gz/awsibox-0.8.5/awsibox/cloudformation.py
@@ -0,0 +1,68 @@
+import troposphere.cloudformation as cfm
+
+from .common import *
+from .shared import (
+    Parameter,
+    do_no_override,
+    get_endvalue,
+    get_expvalue,
+    get_subvalue,
+    auto_get_props,
+    get_condition,
+    add_obj,
+    auto_build_obj,
+)
+
+
+def CFM_Parameters(key):
+    auto_build_obj(Parameter(""), getattr(cfg, key))
+
+
+def CFM_Conditions(key):
+    do_no_override(True)
+    for n, v in getattr(cfg, key).items():
+        c_Condition = {n: eval(v)}
+
+        add_obj(c_Condition)
+    do_no_override(False)
+
+
+def CFM_Mappings(key):
+    for n, v in getattr(cfg, key).items():
+        c_Mapping = {n: v}
+
+        cfg.Mappings.update(c_Mapping)
+
+
+def CFM_Outputs(key):
+    auto_build_obj(Output(""), getattr(cfg, key))
+
+
+def CFM_CustomResourceReplicator(key):
+    resname = "CloudFormationCustomResourceStackReplicator"
+    # Parameters
+    P_ReplicateRegions = Parameter(
+        "CCRReplicateRegions",
+        Description="Regions where to replicate - none to disable - empty for default based on env/role",
+        Type="CommaDelimitedList",
+    )
+
+    add_obj(P_ReplicateRegions)
+
+    # Resources
+    R_Replicator = cfm.CustomResource(resname)
+
+    if "LambdaCCRStackReplicator" in cfg.Resources:
+        R_Replicator.DependsOn = "IAMPolicyLambdaCCRStackReplicator"
+        R_Replicator.ServiceToken = GetAtt("LambdaCCRStackReplicator", "Arn")
+    else:
+        R_Replicator.ServiceToken = get_expvalue("LambdaCCRStackReplicator")
+
+    for p, v in cfg.Parameters.items():
+        if not p.startswith("Env"):
+            value = get_endvalue(p)
+        else:
+            value = Ref(p)
+        setattr(R_Replicator, p, value)
+
+    add_obj(R_Replicator)
---
+++ packages/awsibox/awsibox-0.8.5.tar.gz/awsibox-0.8.5/awsibox/common.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import random
+import string
+import copy
+import json
+import logging
+from pprint import pprint, pformat
+from pathlib import PurePath
+from troposphere import validators
+from troposphere.autoscaling import Tags as asgTags
+import troposphere.ssm as ssm
+import troposphere.sso as sso
+
+from troposphere import (
+    And,
+    AWSHelperFn,
+    AWSObject,
+    AWSProperty,
+    PropsDictType,
+    Base64,
+    BaseAWSObject,
+    Condition,
+    Equals,
+    Export,
+    FindInMap,
+    GetAtt,
+    GetAZs,
+    If,
+    ImportValue,
+    Join,
+    Not,
+    Or,
+    Output,
+    Parameter,
+    Ref,
+    Select,
+    Split,
+    Sub,
+    Tags,
+    Template,
+)
+
+from . import cfg
+
+from .cfg import (
+    MAX_SECURITY_GROUPS,
+    SECURITY_GROUPS_DEFAULT,
+)
+
+# Temporary fix for https://github.com/cloudtools/troposphere/issues/2146
+sso.PermissionSet.props["InlinePolicy"] = (str, False)
+
+# Temporary fix for https://github.com/cloudtools/troposphere/issues/1474
+def my_one_of(class_name, properties, property, conditionals):
+    if properties.get(property) not in conditionals and not isinstance(
+        properties.get(property), If
+    ):
+        raise ValueError(
+            # Ensure we handle None as a valid value
+            '%s.%s must be one of: "%s"'
+            % (
+                class_name,
+                property,
+                ", ".join(condition for condition in conditionals if condition),
+            )
+        )
+
+
+def boolean(x):
+    if x in [True, 1, "1", "true", "True"]:
+        return True
+    if x in [False, 0, "0", "false", "False"]:
+        return False
+    raise ValueError
+
+
+# Fix to avoid setting OS variable TROPO_REAL_BOOL
+validators.boolean = boolean
