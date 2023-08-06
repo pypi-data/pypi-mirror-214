629a689dc9fc50db7e063daad33d307da1bfe0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:01 +0000

    Add package kiara-plugin.service/0.4.1/kiara_plugin.service-0.4.1.tar.gz

---
+++ packages/kiara-plugin.service/kiara_plugin.service-0.4.1.tar.gz/kiara_plugin.service-0.4.1/src/kiara_plugin/service/openapi/controllers/pipeline.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+from pydantic import BaseModel, Field
+from starlite import Controller
+
+from kiara.api import KiaraAPI
+from kiara.interfaces.python_api.models.info import PipelineStructureInfo
+from kiara.utils.pipelines import get_pipeline_config
+from kiara_plugin.service.openapi.controllers import get
+
+
+class PipelineMatcher(BaseModel):
+
+    filters: List[str] = Field(
+        description="The (optional) filter strings, a pipeline must match all of them to be included in the result.",
+        default_factory=list,
+    )
+
+
+class PipelineControllerJson(Controller):
+    path = "/"
+
+    @get(path="/structure/{pipeline:str}", api_func=get_pipeline_config)
+    async def get_pipeline_structure(
+        self, kiara_api: KiaraAPI, pipeline: str
+    ) -> PipelineStructureInfo:
+
+        print(f"PIPELINE: {pipeline}")
+        pipeline_config = get_pipeline_config(pipeline=pipeline)
+        info = PipelineStructureInfo.create_from_instance(
+            kiara=kiara_api.context, instance=pipeline_config.structure
+        )
+        return info
