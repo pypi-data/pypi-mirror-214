3f28fe13fb20c371ce2cdf0ad92ad2973ee3dbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:50 +0000

    Add package fal-serverless/0.6.35/fal_serverless-0.6.35-py3-none-any.whl

---
+++ packages/fal-serverless/fal_serverless-0.6.35-py3-none-any.whl/fal_serverless/flags.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import os
+
+
+def bool_envvar(name: str):
+    if name in os.environ:
+        val = os.environ[name].lower().strip()
+        return val != "0" and val != "false" and val != ""
+    return False
+
+
+TEST_MODE = bool_envvar("ISOLATE_TEST_MODE")
+AUTH_DISABLED = bool_envvar("ISOLATE_AUTH_DISABLED")
