d511c3519e2baf3ad2dc938e121ebee3079fd3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:26 +0000

    Add package yid-langchain-extensions/0.4.8/yid_langchain_extensions-0.4.8.tar.gz

---
+++ packages/yid-langchain-extensions/yid_langchain_extensions-0.4.8.tar.gz/yid_langchain_extensions-0.4.8/yid_langchain_extensions/output_parser/thoughts_json_parser.py
@@ -0,0 +1,59 @@
+import json
+from typing import List, Dict, Any
+
+from langchain import PromptTemplate
+from langchain.schema import BaseOutputParser
+from pydantic import BaseModel, validator
+
+FORMAT_PROMPT = """FORMAT:
+------
+You response should be in the following format:
+
+```json
+{
+    {{thoughts}}
+}
+```"""
+
+
+class Thought(BaseModel):
+    name: str
+    description: str
+    type: str = "string"
+
+
+class ThoughtsJSONParser(BaseOutputParser):
+    thoughts: List[Thought]
+    stop_sequences: List[str] = ["}\n```", "}```"]
+
+    @validator("thoughts")
+    def validate_thoughts(cls, v):
+        assert len(v) > 0, "You must have at least one thought"
+        return v
+
+    def parse(self, text: str) -> Dict[str, Any]:
+        text += self.stop_sequences[0]
+        cleaned_output = text.strip()
+        if "```json" in cleaned_output:
+            _, cleaned_output = cleaned_output.split("```json")
+        if "```" in cleaned_output:
+            cleaned_output, _ = cleaned_output.split("```")
+        if cleaned_output.startswith("```json"):
+            cleaned_output = cleaned_output[len("```json"):]
+        if cleaned_output.startswith("```"):
+            cleaned_output = cleaned_output[len("```"):]
+        if cleaned_output.endswith("```"):
+            cleaned_output = cleaned_output[: -len("```")]
+        cleaned_output = cleaned_output.strip()
+        response = json.loads(cleaned_output)
+        return response
+
+    def format_thoughts(self) -> str:
+        return "\n\t".join([
+            f'"{thought.name}": {thought.type} [{thought.description}]' for thought in self.thoughts
+        ])
+
+    def get_format_instructions(self) -> str:
+        format_instructions = PromptTemplate.from_template(FORMAT_PROMPT, template_format="jinja2").format_prompt(
+            thoughts=self.format_thoughts()).to_string()
+        return format_instructions
