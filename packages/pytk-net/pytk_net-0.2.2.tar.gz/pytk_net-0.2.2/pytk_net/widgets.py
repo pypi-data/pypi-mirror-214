2158747124243579f1054e6503557c35b82d7e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:28 +0000

    Add package pytk-net/0.2.2/pytk_net-0.2.2.tar.gz

---
+++ packages/pytk-net/pytk_net-0.2.2.tar.gz/pytk_net-0.2.2/pytk_net/widgets.py
@@ -0,0 +1,7 @@
+from pytk_net.ext.icon import Icon
+from pytk_net.ext.navmenu import NavMenuItem, NavMenu
+from pytk_net.overwrite.DateEntry import DateEntry
+
+ALL_WIDGET = [
+    DateEntry, Icon, NavMenu, NavMenuItem
+]
