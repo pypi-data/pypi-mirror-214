8d9de70b6ba3042de67802868208a54beacf796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:29 +0000

    Add package parangonar/0.1.0/parangonar-0.1.0-py3-none-any.whl

---
+++ packages/parangonar/parangonar-0.1.0-py3-none-any.whl/parangonar/match/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+This module contains methods for note and sequence alignment.
+
+sequence alignment:
+- Dynamic Time Warping
+- Needleman-Wunsch 
+
+note alignment:
+- Hidden Markov Model
+- Greedy note alignment
+- Path-augmented combinatorial alignment
+
+most of the hidden markov machinery comes from 
+https://github.com/neosatrapahereje/hiddenmarkov.git
+this file contains specific variations and additions.
+
+"""
+
+
+from .dtw import DTW
+from .nwtw import NW_DTW, NW
+from .matchers import (AnchorPointNoteMatcher, 
+                       AutomaticNoteMatcher)
+from .utils import (node_array,
+                    save_parangonada_csv)
