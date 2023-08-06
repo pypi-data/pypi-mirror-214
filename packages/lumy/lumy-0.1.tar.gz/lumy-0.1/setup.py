e7a97b3aeb70050d28cafb353f7c326696293ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:44 +0000

    Add package lumy/0.1/lumy-0.1.tar.gz

---
+++ packages/lumy/lumy-0.1.tar.gz/lumy-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="lumy",
+    version="0.1",
+    description="An empty package named Lumy",
+    author="Elian Belot",
+    author_email="contact@lumy.co",
+    packages=["lumy"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
