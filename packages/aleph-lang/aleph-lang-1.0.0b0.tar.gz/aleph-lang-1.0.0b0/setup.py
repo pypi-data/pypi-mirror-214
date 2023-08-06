f4c9785dd3bac9f810ff7705d1082bee0f5b48b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:53 +0000

    Add package aleph-lang/1.0.0b0/aleph-lang-1.0.0b0.tar.gz

---
+++ packages/aleph-lang/aleph-lang-1.0.0b0.tar.gz/aleph-lang-1.0.0b0/setup.py
@@ -0,0 +1,28 @@
+
+import setuptools
+import os
+
+version = os.environ.get('ALEPH_VERSION', '1.0.0b')
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aleph-lang",
+    version=version,
+    author="Andrés Paz",
+    author_email="anpaz@cs.washington.edu",
+    description="Python bindings for aleph, a high level programming model that can be embedded into classical languages to develop large scale quantum hybrid applications, without the quantum mechanics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anpaz/aleph",
+    packages=setuptools.find_namespace_packages(include=["aleph_lang"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ]
+)
