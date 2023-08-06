1d54620d51b65f7d73d9f60b6fdb89f7aac7d9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package certora-cli-alpha-master/20230615.21.19.364056/certora-cli-alpha-master-20230615.21.19.364056.tar.gz

---
+++ packages/certora-cli-alpha-master/certora-cli-alpha-master-20230615.21.19.364056.tar.gz/certora-cli-alpha-master-20230615.21.19.364056/README.md
@@ -0,0 +1,1 @@
+Commit ad6b2ef. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.
---
+++ packages/certora-cli-alpha-master/certora-cli-alpha-master-20230615.21.19.364056.tar.gz/certora-cli-alpha-master-20230615.21.19.364056/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="certora-cli-alpha-master",
+        version="20230615.21.19.364056",
+        author="Certora",
+        author_email="support@certora.com",
+        description="Runner for the Certora Prover",
+        long_description="Commit ad6b2ef. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.",
+        long_description_content_type="text/markdown",
+        url="https://pypi.org/project/certora-cli-alpha-master",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=['click', 'json5', 'pycryptodome', 'requests', 'sly', 'tabulate', 'tqdm'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            "console_scripts": [
+                "certoraRun = certora_cli.certoraRun:entry_point",
+                "certoraMutate = certora_cli.certoraMutate:ext_gambit_entry_point"
+            ]
+        },
+        python_requires='>=3.8',
+    )
+        
