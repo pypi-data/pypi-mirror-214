399ced9c95981ffa8701840a63e2aff3505364f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:35 +0000

    Add package mapbot-frontend/0.1.24/mapbot_frontend-0.1.24.tar.gz

---
+++ packages/mapbot-frontend/mapbot_frontend-0.1.24.tar.gz/mapbot_frontend-0.1.24/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mapbot_frontend",
+    version="0.1.24",
+    description="A frontend package for MapBot",
+    author="Martin Weiss",
+    packages=find_packages(),
+    include_package_data=True,  # Include additional files specified in MANIFEST.in
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
