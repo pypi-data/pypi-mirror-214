c1ecf10a9368fb3e6fe43022b57e266983e3cf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:50 +0000

    Add package CharmCord/0.16.0/CharmCord-0.16.0-py3-none-any.whl

---
+++ packages/CharmCord/CharmCord-0.16.0-py3-none-any.whl/CharmCord/Classes/__init__.py
@@ -0,0 +1,6 @@
+from .CharmCord import CharmClient
+#from .Commands import Commands
+#from .Events import AoiEvents
+from .Cogs import Charmcogs
+from .CommandHandler import load_commands
+
---
+++ packages/CharmCord/CharmCord-0.16.0-py3-none-any.whl/CharmCord/all_functions.py
@@ -0,0 +1,89 @@
+all_Funcs = [
+    "$authorID",
+    "$channelCategoryID",
+    "$channelCategoryName",
+    "$channelCreated",
+    "$channelDelay",
+    "$channelID",
+    "$channelMention",
+    "$channelName",
+    "$channelNsfw",
+    "$channelPosition",
+    "$channelType",
+    "$channelURL",
+    "$count",
+    "$deletedChannel",
+    "$getVar",
+    "$getUserVar",
+    "$getServerVar",
+    "$setServerVar",
+    "$guildID",
+    "$guildName",
+    "$guildTextChannels",
+    "$messageAuthor",
+    "$messageContent",
+    "$oldChannel",
+    "$ping",
+    "$pyEval",
+    "$sendMessage",
+    "$setUserVar",
+    "$setVar",
+    "$slashSend",
+    "$userMention",
+    "$username",
+    "$getVar",
+    "$setUserVar",
+    "$setVar",
+    "$getUserVar",
+    "$wait",
+    "$If",
+    "$ElIf",
+    "$console",
+    "$onlyIf"
+]
+
+argument_funcs = [
+    "$authorID",
+    "$channelCategoryID",
+    "$channelCategoryName",
+    "$channelCreated",
+    "$channelDelay",
+    "$channelMention",
+    "$channelName",
+    "$channelNsfw",
+    "$channelPosition",
+    "$channelType",
+    "$channelURL",
+    "$guildTextChannels",
+    "$guildName",
+    "$messageAuthor",
+    "$messageContent",
+    "$getVar",
+    "$getServerVar",
+    "$getUserVar",
+    "$getGlobalUserVar",
+    "$setVar",
+    "$setServerVar",
+    "$setUserVar",
+    "$setGlobalUserVar",
+    "$console",
+    "$onlyIf"
+]
+
+date_funcs = [
+    "$channelCreated"
+]
+
+ifse = [
+    "$If",
+    "$ElIf"
+]
+
+no_arg_Funcs = [
+    "$authorID",
+    "$channelID",
+    "$guildID",
+    "$ping"
+]
+
+newline_char = "*n*"
