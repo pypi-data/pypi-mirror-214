7a7ed3c6e27d68d24ae41a4ae2b1083b1e28c35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:06 +0000

    Add package findPlate/2.0.0/findPlate-2.0.0.tar.gz

---
+++ packages/findPlate/findPlate-2.0.0.tar.gz/findPlate-2.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+# Setting up
+setup(
+    name="findPlate",
+    version='2.0.0',
+    author="Guido Xhindoli",
+    author_email="<mail@gmail.com>",
+    description='A package finds the car plate',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.0.2",
+        "openpyxl==3.1.2"
+    ],
+    keywords=[ 'SDAfindPlate', 'findPlate', 'find plate'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
