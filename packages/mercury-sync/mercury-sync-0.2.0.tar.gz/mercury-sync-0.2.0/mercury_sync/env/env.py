e43041b4c492c6e1cd2088b66a596cee1a17f49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:13 +0000

    Add package mercury-sync/0.2.0/mercury-sync-0.2.0.tar.gz

---
+++ packages/mercury-sync/mercury-sync-0.2.0.tar.gz/mercury-sync-0.2.0/mercury_sync/env/env.py
@@ -0,0 +1,29 @@
+import psutil
+from pydantic import (
+    BaseModel,
+    StrictStr,
+    StrictInt
+)
+from typing import (
+    Optional, 
+    Dict, 
+    Union,
+    Callable
+)
+
+
+PrimaryType = Union[str, int, float, bytes, bool]
+
+
+class Env(BaseModel):
+    MERCURY_SYNC_CLEANUP_INTERVAL: StrictStr='10s'
+    MERCURY_SYNC_MAX_CONCURRENCY: StrictInt=2048
+    MERCURY_SYNC_AUTH_SECRET: StrictStr
+
+    @classmethod
+    def types_map(self) -> Dict[str, Callable[[str], PrimaryType]]:
+        return {
+            'MERCURY_SYNC_CLEANUP_INTERVAL': str,
+            'MERCURY_SYNC_MAX_CONCURRENCY': int,
+            'MERCURY_SYNC_AUTH_SECRET': str
+        }
