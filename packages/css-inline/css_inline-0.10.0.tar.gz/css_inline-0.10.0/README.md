b3114938396e882706125780d93860d07fe33160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:15 +0000

    Add package css-inline/0.10.0/css_inline-0.10.0.tar.gz

---
+++ packages/css-inline/css_inline-0.10.0.tar.gz/css_inline-0.10.0/README.md
@@ -0,0 +1,229 @@
+# css_inline
+
+[<img alt="build status" src="https://img.shields.io/github/actions/workflow/status/Stranger6667/css-inline/build.yml?style=flat-square&labelColor=555555&logo=github" height="20">](https://github.com/Stranger6667/css-inline)
+[<img alt="pypi" src="https://img.shields.io/pypi/v/css_inline.svg?style=flat-square" height="20">](https://pypi.org/project/css_inline/)
+[<img alt="versions" src="https://img.shields.io/pypi/pyversions/css_inline.svg?style=flat-square" height="20">](https://pypi.org/project/css_inline/)
+[<img alt="license" src="https://img.shields.io/pypi/l/css_inline.svg?style=flat-square" height="20">](https://opensource.org/licenses/MIT)
+[<img alt="codecov.io" src="https://img.shields.io/codecov/c/gh/Stranger6667/css-inline?logo=codecov&style=flat-square&token=tOzvV4kDY0" height="20">](https://app.codecov.io/github/Stranger6667/css-inline)
+[<img alt="gitter" src="https://img.shields.io/gitter/room/Stranger6667/css-inline?style=flat-square" height="20">](https://gitter.im/Stranger6667/css-inline)
+
+`css-inline` inlines CSS into HTML documents, using components from Mozilla's Servo project.
+
+This process is essential for sending HTML emails as you need to use "style" attributes instead of "style" tags.
+
+For instance, the library transforms HTML like this:
+
+```html
+<html>
+    <head>
+        <title>Test</title>
+        <style>h1 { color:blue; }</style>
+    </head>
+    <body>
+        <h1>Big Text</h1>
+    </body>
+</html>
+```
+
+into:
+
+```html
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <h1 style="color:blue;">Big Text</h1>
+    </body>
+</html>
+```
+
+- Uses reliable components from Mozilla's Servo
+- 10-300x faster than alternatives
+- Inlines CSS from `style` and `link` tags
+- Removes `style` and `link` tags
+- Resolves external stylesheets (including local files)
+- Can process multiple documents in parallel
+- Works on Linux, Windows, and macOS
+- Supports HTML5 & CSS3
+
+## Installation
+
+Install with `pip`:
+
+```
+pip install css_inline
+```
+
+Pre-compiled wheels for most popular platforms are provided. If your platform is not supported, you will need
+a Rust compiler to build this package from source. The minimum supported Rust version is 1.60.
+
+## Usage
+
+```python
+import css_inline
+
+HTML = """<html>
+<head>
+    <title>Test</title>
+    <style>h1 { color:blue; }</style>
+</head>
+<body>
+    <h1>Big Text</h1>
+</body>
+</html>"""
+
+inlined = css_inline.inline(HTML)
+# HTML becomes this:
+#
+# <html>
+# <head>
+#    <title>Test</title>
+#    <style>h1 { color:blue; }</style>
+# </head>
+# <body>
+#     <h1 style="color:blue;">Big Text</h1>
+# </body>
+# </html>
+```
+
+If you want to inline many HTML documents, you can utilize `inline_many` that processes the input in parallel.
+
+```python
+import css_inline
+
+css_inline.inline_many(["<...>", "<...>"])
+```
+
+`inline_many` will spawn threads on the Rust level; thus, you can expect it's running faster than `css_inline.inline` via Python's `multiprocessing` or `threading` modules.
+
+### Configuration
+
+For configuration options use the `CSSInliner` class:
+
+```python
+import css_inline
+
+inliner = css_inline.CSSInliner(keep_style_tags=True)
+inliner.inline("...")
+```
+
+- `keep_style_tags`. Specifies whether to keep "style" tags after inlining. Default: `False`
+- `keep_link_tags`. Specifies whether to keep "link" tags after inlining. Default: `False`
+- `base_url`. The base URL used to resolve relative URLs. If you'd like to load stylesheets from your filesystem, use the `file://` scheme. Default: `None`
+- `load_remote_stylesheets`. Specifies whether remote stylesheets should be loaded. Default: `True`
+- `extra_css`. Extra CSS to be inlined. Default: `None`
+- `preallocate_node_capacity`. **Advanced**. Preallocates capacity for HTML nodes during parsing. This can improve performance when you have an estimate of the number of nodes in your HTML document. Default: `32`
+
+You can also skip CSS inlining for an HTML tag by adding the `data-css-inline="ignore"` attribute to it:
+
+```html
+<head>
+    <title>Test</title>
+    <style>h1 { color:blue; }</style>
+</head>
+<body>
+    <!-- The tag below won't receive additional styles -->
+    <h1 data-css-inline="ignore">Big Text</h1>
+</body>
+```
+
+The `data-css-inline="ignore"` attribute also allows you to skip `link` and `style` tags:
+
+```html
+<head>
+    <title>Test</title>
+    <!-- Styles below are ignored -->
+    <style data-css-inline="ignore">h1 { color:blue; }</style>
+</head>
+<body>
+    <h1>Big Text</h1>
+</body>
+```
+
+If you'd like to load stylesheets from your filesystem, use the `file://` scheme:
+
+```python
+import css_inline
+
+# styles/email is relative to the current directory
+inliner = css_inline.CSSInliner(base_url="file://styles/email/")
+inliner.inline("...")
+```
+
+## Standards support & restrictions
+
+`css-inline` is built on top of [html5ever](https://crates.io/crates/html5ever) and [cssparser](https://crates.io/crates/cssparser) and relies on their behavior for HTML & CSS parsing.
+
+- Only HTML 5 is supported, not XHTML.
+- Only CSS 3 is supported.
+- Only UTF-8 encoding for string representation. Other document encodings are not yet supported.
+
+If you'd like to work around some XHTML compatibility issues like closing empty tags (`<hr>` vs. `<hr/>`), you can use the following snippet that involves `lxml`:
+
+```python
+import css_inline
+from lxml import html, etree
+
+document = "..."  # Your HTML document
+inlined = css_inline.inline(document)
+tree = html.fromstring(inlined)
+inlined = etree.tostring(tree).decode(encoding="utf-8")
+```
+
+## Performance
+
+Due to the usage of efficient tooling from Mozilla's Servo project (`html5ever`, `rust-cssparser` and others) this
+library has excellent performance characteristics. In comparison with other Python projects, it is usually >10x faster than the nearest alternative.
+
+For inlining CSS in the html document from the `Usage` section above there is the following breakdown in the benchmarks:
+
+- `css_inline 0.9.0` - 9.72 us
+- `premailer 3.10.0` - 207.95 us (**x21.39**)
+- `toronado 0.1.0` - 1.03 ms (**x106.86**)
+- `inlinestyler 0.2.5` - 1.69 ms (**x174.64**)
+- `pynliner 0.8.0` - 2.05 ms (**x211.89**)
+
+Realistic email 1:
+
+- `css_inline 0.9.0` - 262.33 us
+- `premailer 3.10.0` - 2.11 ms (**x8.08**)
+- `toronado 0.1.0` - 25.51 ms (**x97.28**)
+- `inlinestyler 0.2.5` - 4313 ms (**x164.43**)
+- `pynliner 0.8.0` - 72.55 ms (**x276.57**)
+
+Realistic email 2:
+
+- `css_inline 0.9.0` - 151.83 us
+- `premailer 3.10.0` - 4.12 ms (**x27.18**)
+- `toronado 0.1.0` - `Error: Pseudo-elements are not supported`
+- `inlinestyler 0.2.5` - 28.85 ms (**x190.03**)
+- `pynliner 0.8.0` - `Error: No match was found`
+
+You can take a look at the benchmarks' code at `benches/bench.py` file.
+The results above were measured with stable `rustc 1.70.0` on `Python 3.11.0`.
+
+## Comparison with other libraries
+
+Besides performance, `css-inline` differs from other Python libraries for CSS inlining.
+
+- Generally supports more CSS features than other libraries (for example, `toronado` and `pynliner` do not support pseudo-elements);
+- It has fewer configuration options and not as flexible as `premailer`;
+- Works on fewer platforms than LXML-based libraries (`premailer`, `inlinestyler`, `toronado`, and optionally `pynliner`);
+- Does not have debug logs yet;
+- Supports only HTML 5.
+
+## Python support
+
+`css_inline` supports CPython 3.7, 3.8, 3.9, 3.10, 3.11 and PyPy 3.7, 3.8, 3.9.
+
+## Extra materials
+
+If you want to know how this library was created & how it works internally, you could take a look at these articles:
+
+- [Rust crate](https://dygalo.dev/blog/rust-for-a-pythonista-2/)
+- [Python bindings](https://dygalo.dev/blog/rust-for-a-pythonista-3/)
+
+## License
+
+This project is licensed under the terms of the [MIT license](https://opensource.org/licenses/MIT).
---
+++ packages/css-inline/css_inline-0.10.0.tar.gz/css_inline-0.10.0/local_dependencies/css-inline/src/lib.rs
@@ -0,0 +1,476 @@
+#![doc = include_str!("../README.md")]
+#![warn(
+    clippy::doc_markdown,
+    clippy::redundant_closure,
+    clippy::explicit_iter_loop,
+    clippy::match_same_arms,
+    clippy::needless_borrow,
+    clippy::print_stdout,
+    clippy::integer_arithmetic,
+    clippy::cast_possible_truncation,
+    clippy::unwrap_used,
+    clippy::map_unwrap_or,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::needless_pass_by_value,
+    missing_docs,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    variant_size_differences,
+    rust_2018_idioms,
+    rust_2018_compatibility
+)]
+
+pub mod error;
+mod html;
+mod parser;
+
+pub use error::InlineError;
+use html5ever::tendril::StrTendril;
+use indexmap::IndexMap;
+use smallvec::{smallvec, SmallVec};
+use std::{
+    borrow::Cow,
+    collections::btree_map::Entry,
+    io::{ErrorKind, Write},
+};
+
+use crate::html::{Document, NodeId, Specificity};
+pub use url::{ParseError, Url};
+
+/// Replace double quotes in property values.
+///
+/// This implementation is deliberately simplistic and covers only `font-family`, but escaping
+/// might be needed in other properties that accept strings.
+macro_rules! replace_double_quotes {
+    ($target:expr, $name:expr, $value:expr) => {
+        // Avoid allocation if there is no double quote in the input string
+        if $name.starts_with("font-family") && memchr::memchr(b'"', $value.as_bytes()).is_some() {
+            $target.push_str(&$value.replace('"', "\'"))
+        } else {
+            $target.push_str($value)
+        };
+    };
+}
+
+/// Configuration options for CSS inlining process.
+#[derive(Debug)]
+pub struct InlineOptions<'a> {
+    /// Keep "style" tags after inlining.
+    pub keep_style_tags: bool,
+    /// Keep "link" tags after inlining.
+    pub keep_link_tags: bool,
+    /// Used for loading external stylesheets via relative URLs.
+    pub base_url: Option<Url>,
+    /// Whether remote stylesheets should be loaded or not.
+    pub load_remote_stylesheets: bool,
+    // The point of using `Cow` here is Python bindings, where it is problematic to pass a reference
+    // without dealing with memory leaks & unsafe. With `Cow` we can use moved values as `String` in
+    // Python wrapper for `CSSInliner` and `&str` in Rust & simple functions on the Python side
+    /// Additional CSS to inline.
+    pub extra_css: Option<Cow<'a, str>>,
+    /// Pre-allocate capacity for HTML nodes during parsing.
+    /// It can improve performance when you have an estimate of the number of nodes in your HTML document.
+    pub preallocate_node_capacity: usize,
+}
+
+impl<'a> InlineOptions<'a> {
+    /// Override whether "style" tags should be kept after processing.
+    #[must_use]
+    pub fn keep_style_tags(mut self, keep_style_tags: bool) -> Self {
+        self.keep_style_tags = keep_style_tags;
+        self
+    }
+
+    /// Override whether "link" tags should be kept after processing.
+    #[must_use]
+    pub fn keep_link_tags(mut self, keep_link_tags: bool) -> Self {
+        self.keep_link_tags = keep_link_tags;
+        self
+    }
+
+    /// Set base URL that will be used for loading external stylesheets via relative URLs.
+    #[must_use]
+    pub fn base_url(mut self, base_url: Option<Url>) -> Self {
+        self.base_url = base_url;
+        self
+    }
+
+    /// Override whether remote stylesheets should be loaded.
+    #[must_use]
+    pub fn load_remote_stylesheets(mut self, load_remote_stylesheets: bool) -> Self {
+        self.load_remote_stylesheets = load_remote_stylesheets;
+        self
+    }
+
+    /// Set additional CSS to inline.
+    #[must_use]
+    pub fn extra_css(mut self, extra_css: Option<Cow<'a, str>>) -> Self {
+        self.extra_css = extra_css;
+        self
+    }
+
+    /// Set the initial node capacity for HTML tree.
+    #[must_use]
+    pub fn preallocate_node_capacity(mut self, preallocate_node_capacity: usize) -> Self {
+        self.preallocate_node_capacity = preallocate_node_capacity;
+        self
+    }
+
+    /// Create a new `CSSInliner` instance from this options.
+    #[must_use]
+    pub const fn build(self) -> CSSInliner<'a> {
+        CSSInliner::new(self)
+    }
+}
+
+impl Default for InlineOptions<'_> {
+    #[inline]
+    fn default() -> Self {
+        InlineOptions {
+            keep_style_tags: false,
+            keep_link_tags: false,
+            base_url: None,
+            load_remote_stylesheets: true,
+            extra_css: None,
+            preallocate_node_capacity: 32,
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, InlineError>;
+
+/// Customizable CSS inliner.
+#[derive(Debug)]
+pub struct CSSInliner<'a> {
+    options: InlineOptions<'a>,
+}
+
+impl<'a> CSSInliner<'a> {
+    /// Create a new `CSSInliner` instance with given options.
+    #[must_use]
+    #[inline]
+    pub const fn new(options: InlineOptions<'a>) -> Self {
+        CSSInliner { options }
+    }
+
+    /// Return a default `InlineOptions` that can fully configure the CSS inliner.
+    ///
+    /// # Examples
+    ///
+    /// Get default `InlineOptions`, then change base url
+    ///
+    /// ```rust
+    /// use css_inline::{CSSInliner, Url};
+    /// # use url::ParseError;
+    /// # fn run() -> Result<(), ParseError> {
+    /// let url = Url::parse("https://api.example.com")?;
+    /// let inliner = CSSInliner::options()
+    ///     .base_url(Some(url))
+    ///     .build();
+    /// # Ok(())
+    /// # }
+    /// # run().unwrap();
+    /// ```
+    #[must_use]
+    #[inline]
+    pub fn options() -> InlineOptions<'a> {
+        InlineOptions::default()
+    }
+
+    /// Inline CSS styles from <style> tags to matching elements in the HTML tree and return a
+    /// string.
+    ///
+    /// # Errors
+    ///
+    /// Inlining might fail for the following reasons:
+    ///   - Missing stylesheet file;
+    ///   - Remote stylesheet is not available;
+    ///   - IO errors;
+    ///   - Internal CSS selector parsing error;
+    #[inline]
+    pub fn inline(&self, html: &str) -> Result<String> {
+        // Allocating more memory than the input HTML, as the inlined version is usually bigger
+        let mut out = Vec::with_capacity(html.len().saturating_mul(2));
+        self.inline_to(html, &mut out)?;
+        Ok(String::from_utf8_lossy(&out).to_string())
+    }
+
+    /// Inline CSS & write the result to a generic writer. Use it if you want to write
+    /// the inlined document to a file.
+    ///
+    /// # Errors
+    ///
+    /// Inlining might fail for the following reasons:
+    ///   - Missing stylesheet file;
+    ///   - Remote stylesheet is not available;
+    ///   - IO errors;
+    ///   - Internal CSS selector parsing error;
+    #[inline]
+    pub fn inline_to<W: Write>(&self, html: &str, target: &mut W) -> Result<()> {
+        let document = self.inline_impl(html)?;
+        document.serialize(
+            target,
+            self.options.keep_style_tags,
+            self.options.keep_link_tags,
+        )?;
+        Ok(())
+    }
+
+    /// Non-generic inlining function.
+    fn inline_impl(&self, html: &str) -> Result<Document> {
+        let mut document =
+            Document::parse_with_options(html.as_bytes(), self.options.preallocate_node_capacity);
+        // CSS rules may overlap, and the final set of rules applied to an element depend on
+        // selectors' specificity - selectors with higher specificity have more priority.
+        // Inlining happens in two major steps:
+        //   1. All available styles are mapped to respective elements together with their
+        //      selector's specificity. When two rules overlap on the same declaration, then
+        //      the one with higher specificity replaces another.
+        //   2. Resulting styles are merged into existing "style" tags.
+        let mut styles = IndexMap::with_capacity(128);
+        for style in document.styles() {
+            process_css(&document, style, &mut styles);
+        }
+        if self.options.load_remote_stylesheets {
+            let mut links = document.stylesheets().collect::<Vec<&str>>();
+            links.sort_unstable();
+            links.dedup();
+            for href in &links {
+                let url = self.get_full_url(href);
+                let css = load_external(url.as_ref())?;
+                process_css(&document, css.as_str(), &mut styles);
+            }
+        }
+        if let Some(extra_css) = &self.options.extra_css {
+            process_css(&document, extra_css, &mut styles);
+        }
+        for (node_id, styles) in styles.iter_mut() {
+            let node = &mut document[*node_id];
+            let element = if let Some(element) = node.as_not_ignored_element_mut() {
+                element
+            } else {
+                continue;
+            };
+            styles.sort_unstable_by(|_, (a, _), _, (b, _)| a.cmp(b));
+            let attributes = &mut element.attributes;
+            match attributes.get_style_entry() {
+                Entry::Vacant(entry) => {
+                    let mut final_styles = String::with_capacity(128);
+                    for (name, (_, value)) in styles {
+                        final_styles.push_str(name.as_str());
+                        final_styles.push(':');
+                        replace_double_quotes!(final_styles, name, value);
+                        final_styles.push(';');
+                    }
+                    entry.insert(final_styles.into());
+                }
+                Entry::Occupied(mut entry) => {
+                    let existing_style = entry.get_mut();
+                    merge_styles(existing_style, styles)?;
+                }
+            }
+        }
+        Ok(document)
+    }
+
+    fn get_full_url<'u>(&self, href: &'u str) -> Cow<'u, str> {
+        // Valid absolute URL
+        if Url::parse(href).is_ok() {
+            return Cow::Borrowed(href);
+        };
+        if let Some(base_url) = &self.options.base_url {
+            // Use the same scheme as the base URL
+            if href.starts_with("//") {
+                return Cow::Owned(format!("{}:{}", base_url.scheme(), href));
+            }
+            // Not a URL, then it is a relative URL
+            if let Ok(new_url) = base_url.join(href) {
+                return Cow::Owned(new_url.into());
+            }
+        };
+        // If it is not a valid URL and there is no base URL specified, we assume a local path
+        Cow::Borrowed(href)
+    }
+}
+
+fn load_external(location: &str) -> Result<String> {
+    if location.starts_with("https") | location.starts_with("http") {
+        #[cfg(feature = "http")]
+        {
+            let request = attohttpc::RequestBuilder::try_new(attohttpc::Method::GET, location)?;
+            let response = request.send()?;
+            Ok(response.text()?)
+        }
+
+        #[cfg(not(feature = "http"))]
+        {
+            Err(InlineError::IO(std::io::Error::new(
+                ErrorKind::Unsupported,
+                "Loading external URLs requires the `http` feature",
+            )))
+        }
+    } else {
+        #[cfg(feature = "file")]
+        {
+            let location = location.trim_start_matches("file://");
+            std::fs::read_to_string(location).map_err(|error| match error.kind() {
+                ErrorKind::NotFound => InlineError::MissingStyleSheet {
+                    path: location.to_string(),
+                },
+                _ => InlineError::IO(error),
+            })
+        }
+        #[cfg(not(feature = "file"))]
+        {
+            Err(InlineError::IO(std::io::Error::new(
+                ErrorKind::Unsupported,
+                "Loading local files requires the `file` feature",
+            )))
+        }
+    }
+}
+
+fn process_css(
+    document: &Document,
+    css: &str,
+    styles: &mut IndexMap<NodeId, IndexMap<String, (Specificity, String)>>,
+) {
+    let mut parse_input = cssparser::ParserInput::new(css);
+    let mut parser = cssparser::Parser::new(&mut parse_input);
+    let rule_list =
+        cssparser::RuleListParser::new_for_stylesheet(&mut parser, parser::CSSRuleListParser);
+    for (selectors, declarations) in rule_list.flatten() {
+        // Only CSS Syntax Level 3 is supported, therefore it is OK to split by `,`
+        // With `is` or `where` selectors (Level 4) this split should be done on the parser level
+        for selector in selectors.split(',') {
+            if let Ok(matching_elements) = document.select(selector) {
+                // There is always only one selector applied
+                let specificity = matching_elements.specificity();
+                for matching_element in matching_elements {
+                    let element_styles = styles
+                        .entry(matching_element.node_id)
+                        .or_insert_with(|| IndexMap::with_capacity(8));
+                    // Iterate over pairs of property name & value
+                    // Example: `padding`, `0`
+                    for (name, value) in &declarations {
+                        match element_styles.entry(name.to_string()) {
+                            indexmap::map::Entry::Occupied(mut entry) => {
+                                if entry.get().0 <= specificity {
+                                    entry.insert((specificity, (*value).to_string()));
+                                }
+                            }
+                            indexmap::map::Entry::Vacant(entry) => {
+                                entry.insert((specificity, (*value).to_string()));
+                            }
+                        }
+                    }
+                }
+            }
+            // Skip selectors that can't be parsed
+            // Ignore not parsable entries. E.g. there is no parser for @media queries
+            // Which means that they will fall into this category and will be ignored
+        }
+    }
+}
+
+impl Default for CSSInliner<'_> {
+    #[inline]
+    fn default() -> Self {
+        CSSInliner::new(InlineOptions::default())
+    }
+}
+
+/// Shortcut for inlining CSS with default parameters.
+///
+/// # Errors
+///
+/// Inlining might fail for the following reasons:
+///   - Missing stylesheet file;
+///   - Remote stylesheet is not available;
+///   - IO errors;
+///   - Internal CSS selector parsing error;
+#[inline]
+pub fn inline(html: &str) -> Result<String> {
+    CSSInliner::default().inline(html)
+}
+
+/// Shortcut for inlining CSS with default parameters and writing the output to a generic writer.
+/// # Errors
+///
+/// Inlining might fail for the following reasons:
+///   - Missing stylesheet file;
+///   - Remote stylesheet is not available;
+///   - IO errors;
+///   - Internal CSS selector parsing error;
+#[inline]
+pub fn inline_to<W: Write>(html: &str, target: &mut W) -> Result<()> {
+    CSSInliner::default().inline_to(html, target)
+}
+
+fn merge_styles(
+    existing_style: &mut StrTendril,
+    new_styles: &IndexMap<String, (Specificity, String)>,
+) -> Result<()> {
+    // Parse existing declarations in the "style" attribute
+    let mut input = cssparser::ParserInput::new(existing_style);
+    let mut parser = cssparser::Parser::new(&mut input);
+    let declarations =
+        cssparser::DeclarationListParser::new(&mut parser, parser::CSSDeclarationListParser);
+    // New rules should not override old ones unless !important and we store selectors inline to check the old rules later
+    let mut buffer: SmallVec<[String; 8]> = smallvec![];
+    let mut final_styles: Vec<String> = Vec::new();
+    for declaration in declarations {
+        let (name, value) = declaration?;
+        // Allocate enough space for the new style
+        let mut style =
+            String::with_capacity(name.len().saturating_add(value.len()).saturating_add(2));
+        style.push_str(&name);
+        style.push_str(": ");
+        replace_double_quotes!(style, name, value.trim());
+        final_styles.push(style);
+        // This property won't be taken from new styles unless it's !important
+        buffer.push(name.to_string());
+    }
+    for (property, (_, value)) in new_styles {
+        match (
+            value.strip_suffix("!important"),
+            buffer.iter().position(|r| r == property),
+        ) {
+            // The new rule is `!important` and there is already one in existing styles:
+            // override with the new one.
+            #[allow(clippy::integer_arithmetic)]
+            (Some(value), Some(index)) => {
+                // Reuse existing allocation
+                let target = &mut final_styles[index];
+                // Keep '<rule>: ` (with a space at the end)
+                // NOTE: There will be no overflow as the new len is always smaller than the old one
+                target.truncate(property.len() + 2);
+                // And push the value
+                target.push_str(value.trim());
+            }
+            // No such rules exist - push the version with `!important` trimmed
+            (Some(value), None) => final_styles.push(format!("{}: {}", property, value.trim())),
+            // Completely new rule - write it
+            (None, None) => final_styles.push(format!("{}: {}", property, value.trim())),
+            // Rule exists and the new one is not `!important` - keep the original one
+            (None, Some(_)) => {}
+        }
+    }
+    drop(input);
+    existing_style.clear();
+    let mut first = true;
+    for style in &final_styles {
+        if first {
+            first = false
+        } else {
+            existing_style.push_char(';');
+        }
+        existing_style.push_slice(style);
+    }
+    Ok(())
+}
