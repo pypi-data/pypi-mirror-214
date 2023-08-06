4fcf6848dff7befc441b0d0b3bae9ea522ac9ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:51 +0000

    Add package sqlalchemy-celery-beat/0.4.5/sqlalchemy_celery_beat-0.4.5-py3-none-any.whl

---
+++ packages/sqlalchemy-celery-beat/sqlalchemy_celery_beat-0.4.5-py3-none-any.whl/sqlalchemy_celery_beat/session.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+"""SQLAlchemy session."""
+
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+# from sqlalchemy.schema import CreateSchema  # does not work for SA 1.4
+
+from kombu.utils.compat import register_after_fork
+
+ModelBase = declarative_base()
+
+
+@contextmanager
+def session_cleanup(session):
+    try:
+        yield
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def _after_fork_cleanup_session(session):
+    session._after_fork()
+
+
+class SessionManager(object):
+    """Manage SQLAlchemy sessions."""
+
+    def __init__(self):
+        self._engines = {}
+        self._sessions = {}
+        self.forked = False
+        self.prepared = False
+        if register_after_fork is not None:
+            register_after_fork(self, _after_fork_cleanup_session)
+
+    def _after_fork(self):
+        self.forked = True
+
+    def get_engine(self, dburi, **kwargs):
+        if self.forked:
+            try:
+                return self._engines[dburi]
+            except KeyError:
+                engine = self._engines[dburi] = create_engine(dburi, **kwargs)
+                return engine
+        else:
+            return create_engine(dburi, poolclass=NullPool)
+
+    def create_session(self, dburi, schema=None, short_lived_sessions=False, **kwargs):
+        engine = self.get_engine(dburi, **kwargs)
+        engine = engine.execution_options(schema_translate_map={'celery_schema': schema})
+        if self.forked:
+            if short_lived_sessions or dburi not in self._sessions:
+                self._sessions[dburi] = sessionmaker(bind=engine)
+            return engine, self._sessions[dburi]
+        else:
+            return engine, sessionmaker(bind=engine)
+
+    def prepare_models(self, engine, schema=None):
+        if not self.prepared:
+            if schema:
+                with engine.begin() as connection:
+                    connection.execute(f'CREATE SCHEMA IF NOT EXISTS {schema};')
+            ModelBase.metadata.create_all(engine)
+            self.prepared = True
