f4eec81546711e5f225486764bff54d347676333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:09 +0000

    Add package test-pypi-security/0.1.0/test_pypi_security-0.1.0.tar.gz

---
+++ packages/test-pypi-security/test_pypi_security-0.1.0.tar.gz/test_pypi_security-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='test_pypi_security',
+    version='0.1.0',
+    author='Arthur Le Floch',
+    author_email='alf.github@gmail.com',
+    description='Test of PyPI security',
+    long_description='Test of PyPI security, do not use this package blindly, it deletes a folder called delete_me in the current directory',
+    packages=[''],
+    package_dir={'': '.'},
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
