2b27d9126fa13e70c662dc98bec6419c54189568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:20 +0000

    Add package decore-Base/0.0.22/decore_Base-0.0.22.tar.gz

---
+++ packages/decore-Base/decore_Base-0.0.22.tar.gz/decore_Base-0.0.22/decore_base/sample/bases/company_base.py
@@ -0,0 +1,15 @@
+from decore_base import decore
+from models.company_model import Company_model as Model
+
+@decore.base(p_title='Companies', p_model=Model)
+class Company_base(object):
+    
+    @decore.widget(p_parent_id='per_vi1_di1_wi1', p_title='Companies', p_type='table', p_active_s=Model.field_s)
+    def per_vi1_di1_wi1_wi2():
+        pass
+
+    @decore.view(p_parent_id='Global_management_base', p_title='Companies', p_icon='mdi-domain', p_active_s=Model.field_s)
+    def com_vi1():
+        @decore.dialog(p_title='Company', p_type='standard', p_display='drawer', p_activator='item-click')
+        def com_vi1_di1():
+            pass
