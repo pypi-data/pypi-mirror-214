c2cef84d23b228b2149a93637cb436ad6e7fa97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:36 +0000

    Add package spam-detector-ai/1.0.2/spam-detector-ai-1.0.2.tar.gz

---
+++ packages/spam-detector-ai/spam-detector-ai-1.0.2.tar.gz/spam-detector-ai-1.0.2/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
