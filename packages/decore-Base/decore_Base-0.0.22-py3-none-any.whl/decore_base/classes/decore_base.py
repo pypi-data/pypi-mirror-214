6dbbf9c0d1c266a8d8ddc06f6e6794a185276e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:20 +0000

    Add package decore-Base/0.0.22/decore_Base-0.0.22-py3-none-any.whl

---
+++ packages/decore-Base/decore_Base-0.0.22-py3-none-any.whl/decore_base/classes/decore_base.py
@@ -0,0 +1,43 @@
+from ..globals import globals
+from .decore_object import Decore_object
+
+import secrets
+from pathlib import Path
+from threading import Thread
+
+
+class Decore_base(Decore_object):
+    def __init__(self):
+        Decore_object.__init__(self, p_id=None, p_parent_id='app', p_source_id=None, p_icon=None, p_title=None, p_desc=None, p_doc=None)
+        self.model = None
+        self.field_s = None
+        self.rel_field_s = None
+        self.schema = None
+        self.function_s = []
+
+    def start_shot(self):
+        for i_function in self.function_s:
+            if i_function.type == 'shot':
+                i_function.func(self)
+
+    def start_work(self):
+        for i_function in self.function_s:
+            if i_function.type == 'work':
+                self.worker = Thread(target=self.work, daemon=True, name=self.__class__.__name__)
+                self.worker.start()
+
+    def work(self):
+        while(True):
+            for i_function in self.function_s:
+                if i_function.type == 'worker':
+                    i_function.func(self)
+
+    # def create_secret(self):
+    #     return (secrets.token_hex(2))
+
+    # def create_path(self, p_id, mkdir=False):
+    #     t_root_path = globals.config['default']['data_path']
+    #     r_value: Path = Path(t_root_path).joinpath(self.id, p_id)
+    #     if mkdir == True:
+    #         r_value.mkdir(parents=True)
+    #     return r_value.as_posix()
---
+++ packages/decore-Base/decore_Base-0.0.22-py3-none-any.whl/decore_base/sample/bases/information_stytem_base.py
@@ -0,0 +1,5 @@
+from decore_base import decore
+
+@decore.base(p_title='Information System', p_icon='mdi-information')
+class Information_system_base(object):
+    pass
