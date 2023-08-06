45dcaef025a81c5fa83d9942bf499def07bef344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:45 +0000

    Add package pandasflow/0.7.0/pandasflow-0.7.0.tar.gz

---
+++ packages/pandasflow/pandasflow-0.7.0.tar.gz/pandasflow-0.7.0/pandasflow/metrics/best_thr_f1.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+from sklearn.metrics import f1_score
+from tqdm.notebook import tqdm
+
+
+def best_thr_f1(y_true:pd.Series, score:pd.Series, out:int=1, tqdm_:bool=True):
+	thrs = list(score.unique())
+	thrs = [max(min(thrs) - 0.000001, 0.000001)] + thrs
+	
+	df = pd.concat([y_true, score], axis=1)
+	
+	result = []
+	
+	if tqdm_: thrs = tqdm(thrs)
+	
+	for thr in thrs:
+		df['y_pred_thr'] = (df[score.name] > thr) * 1
+		result.append(
+			(
+				thr,
+				f1_score(df[y_true.name], df['y_pred_thr'])
+			)
+		)
+	
+	result = pd.DataFrame(result, columns=['thr', 'f1'])
+	result = result.sort_values(by='f1', ascending=False)
+	
+	result = result.head(min(out, len(result)))
+	best_thr = result['thr'].loc[result.index[0]]
+	
+	print()
+	print(result)
+	
+	return round(best_thr, 6)
+
+
+
+
