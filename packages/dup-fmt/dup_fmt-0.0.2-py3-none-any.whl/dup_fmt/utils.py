fefd40f6084202357dc9b37524e0a54b365dec6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:35 +0000

    Add package dup-fmt/0.0.2/dup_fmt-0.0.2-py3-none-any.whl

---
+++ packages/dup-fmt/dup_fmt-0.0.2-py3-none-any.whl/dup_fmt/utils.py
@@ -0,0 +1,33 @@
+from typing import Any, Callable, Union
+
+concat: Callable = "".join
+
+
+def remove_pad(value: str) -> str:
+    """Remove zero padding of string
+    .. usage::
+        >>> remove_pad('000')
+        '0'
+
+        >>> remove_pad('010')
+        '10'
+
+        >>> remove_pad('0123')
+        '123'
+    """
+    return value.lstrip("0") or "0"
+
+
+def itself(x: Any = None) -> Any:
+    """Return itself value"""
+    return x
+
+
+def caller(func: Union[Callable, Any]) -> Any:
+    """Call function if it was callable
+    .. usage::
+        >>> some_func = lambda: 100
+        >>> caller(some_func)
+        100
+    """
+    return func() if callable(func) else func
