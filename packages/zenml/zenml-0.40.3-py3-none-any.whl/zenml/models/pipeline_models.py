f69096cdee4cee1a4a8aeb87964aa5a29cc94259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:29 +0000

    Add package zenml/0.40.3/zenml-0.40.3-py3-none-any.whl

---
+++ packages/zenml/zenml-0.40.3-py3-none-any.whl/zenml/models/pipeline_models.py
@@ -0,0 +1,123 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+"""Models representing pipelines."""
+
+from typing import List, Optional, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from zenml.config.pipeline_spec import PipelineSpec
+from zenml.enums import ExecutionStatus
+from zenml.models.base_models import (
+    WorkspaceScopedRequestModel,
+    WorkspaceScopedResponseModel,
+    update_model,
+)
+from zenml.models.constants import STR_FIELD_MAX_LENGTH, TEXT_FIELD_MAX_LENGTH
+from zenml.models.filter_models import WorkspaceScopedFilterModel
+from zenml.models.pipeline_run_models import PipelineRunResponseModel
+
+# ---- #
+# BASE #
+# ---- #
+
+
+class PipelineBaseModel(BaseModel):
+    """Base model for pipelines."""
+
+    name: str = Field(
+        title="The name of the pipeline.",
+        max_length=STR_FIELD_MAX_LENGTH,
+    )
+    version: str = Field(
+        title="The version of the pipeline.",
+        max_length=STR_FIELD_MAX_LENGTH,
+    )
+    version_hash: str = Field(
+        title="The version hash of the pipeline.",
+        max_length=STR_FIELD_MAX_LENGTH,
+    )
+    docstring: Optional[str] = Field(
+        title="The docstring of the pipeline.",
+        max_length=TEXT_FIELD_MAX_LENGTH,
+    )
+    spec: PipelineSpec
+
+
+# -------- #
+# RESPONSE #
+# -------- #
+
+
+class PipelineResponseModel(PipelineBaseModel, WorkspaceScopedResponseModel):
+    """Pipeline response model user, workspace, runs, and status hydrated."""
+
+    runs: Optional[List["PipelineRunResponseModel"]] = Field(
+        default=None, title="A list of the last x Pipeline Runs."
+    )
+    status: Optional[List[ExecutionStatus]] = Field(
+        default=None, title="The status of the last x Pipeline Runs."
+    )
+
+
+# ------ #
+# FILTER #
+# ------ #
+
+
+class PipelineFilterModel(WorkspaceScopedFilterModel):
+    """Model to enable advanced filtering of all Workspaces."""
+
+    name: Optional[str] = Field(
+        default=None,
+        description="Name of the Pipeline",
+    )
+    version: Optional[str] = Field(
+        default=None,
+        description="Version of the Pipeline",
+    )
+    version_hash: Optional[str] = Field(
+        default=None,
+        description="Version hash of the Pipeline",
+    )
+    docstring: Optional[str] = Field(
+        default=None,
+        description="Docstring of the Pipeline",
+    )
+    workspace_id: Optional[Union[UUID, str]] = Field(
+        default=None, description="Workspace of the Pipeline"
+    )
+    user_id: Optional[Union[UUID, str]] = Field(
+        default=None, description="User of the Pipeline"
+    )
+
+
+# ------- #
+# REQUEST #
+# ------- #
+
+
+class PipelineRequestModel(PipelineBaseModel, WorkspaceScopedRequestModel):
+    """Pipeline request model."""
+
+
+# ------ #
+# UPDATE #
+# ------ #
+
+
+@update_model
+class PipelineUpdateModel(PipelineRequestModel):
+    """Pipeline update model."""
