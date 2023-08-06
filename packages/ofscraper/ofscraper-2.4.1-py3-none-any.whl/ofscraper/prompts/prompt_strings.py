6cbf0ea028e1a535a861a604aa699e8ccb1da0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:29 +0000

    Add package ofscraper/2.4.1/ofscraper-2.4.1-py3-none-any.whl

---
+++ packages/ofscraper/ofscraper-2.4.1-py3-none-any.whl/ofscraper/prompts/prompt_strings.py
@@ -0,0 +1,20 @@
+CHECKLISTINSTRUCTIONS=\
+"""
+Keybindings
+Toggle all: Ctrl+R
+Toggle single: spacebar
+
+Press enter when done"
+"""
+FUZZY_INSTRUCTION=\
+"""
+KeyBindings
+Toggle all: Ctrl+r
+Toggle single: Shift +Right /Home +Right / pageDown +Right
+Ctrl-Z: Change Sorting/Filters (keeps selections)
+
+Press Enter When Done
+Parantheses indicates number of selected user
+[Sort-Type: {sort}] [Descending: {desc}] [Account-Type {type}] [Subscription Status {status}] [Renewal {renewal}]
+"""
+
