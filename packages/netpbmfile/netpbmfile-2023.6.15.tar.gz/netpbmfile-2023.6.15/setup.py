1326b2ea1d87124d1129c5c40e7e3210ab7d2020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:24 +0000

    Add package netpbmfile/2023.6.15/netpbmfile-2023.6.15.tar.gz

---
+++ packages/netpbmfile/netpbmfile-2023.6.15.tar.gz/netpbmfile-2023.6.15/setup.py
@@ -0,0 +1,90 @@
+# netpbmfile/setup.py
+
+"""Netpbmfile package Setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup
+
+buildnumber = ''
+
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('netpbmfile/netpbmfile.py') as fh:
+    code = fh.read().replace('\r\n', '\n').replace('\r', '\n')
+
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev')
+version += ('.' + buildnumber) if buildnumber else ''
+
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
+
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+if 'sdist' in sys.argv:
+    # update README and LICENSE files
+
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+    license = search(
+        r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+        code,
+        re.MULTILINE | re.DOTALL,
+    )
+    license = license.replace('# ', '').replace('#', '')
+
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+
+
+setup(
+    name='netpbmfile',
+    version=version,
+    license='BSD',
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/netpbmfile/issues',
+        'Source Code': 'https://github.com/cgohlke/netpbmfile',
+        # 'Documentation': 'https://',
+    },
+    packages=['netpbmfile'],
+    entry_points={
+        'console_scripts': ['netpbmfile = netpbmfile.netpbmfile:main']
+    },
+    python_requires='>=3.8',
+    install_requires=['numpy>=1.19.2'],
+    extras_require={'all': ['tifffile', 'matplotlib']},
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
