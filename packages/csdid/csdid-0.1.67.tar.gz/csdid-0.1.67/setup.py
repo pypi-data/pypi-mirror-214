aadcc20598f6225ca3dfe54d6c828be7be1f9ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:15 +0000

    Add package csdid/0.1.67/csdid-0.1.67.tar.gz

---
+++ packages/csdid/csdid-0.1.67.tar.gz/csdid-0.1.67/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'csdid',
+  version='0.1.67',
+  url='https://github.com/d2cml-ai/csdid',
+  author='D2CML Team, Alexander Quispe, Carlos Guevara, Jhon Flroes',
+  keywords=['Causal inference', 'Research'],
+  license="MIT",
+  description='Difference in Difference in Python',
+  classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering",
+    ],
+  packages=find_packages(),
+  package_data={
+    'data': ['data/*'],
+    'configs': ['configs/*']
+  }
+  
+)
