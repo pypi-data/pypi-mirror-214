8cb96d3eff9250459cd3d7918c66e4f514b66cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:49 +0000

    Add package metlo-python-agent-bindings-common/0.0.1/metlo_python_agent_bindings_common-0.0.1-cp37-none-win_amd64.whl

---
+++ packages/metlo-python-agent-bindings-common/metlo_python_agent_bindings_common-0.0.1-cp37-none-win_amd64.whl/metlo_python_agent_bindings_common/__init__.py
@@ -0,0 +1,5 @@
+from .metlo_python_agent_bindings_common import *
+
+__doc__ = metlo_python_agent_bindings_common.__doc__
+if hasattr(metlo_python_agent_bindings_common, "__all__"):
+    __all__ = metlo_python_agent_bindings_common.__all__
