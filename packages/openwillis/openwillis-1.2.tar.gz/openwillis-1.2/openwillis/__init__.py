1dd0f2c2dd417a7140b8e19ef9d0c29645425a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package openwillis/1.2/openwillis-1.2.tar.gz

---
+++ packages/openwillis/openwillis-1.2.tar.gz/openwillis-1.2/openwillis/__init__.py
@@ -0,0 +1,17 @@
+# author:    Vijay Yadav
+# website:   http://www.bklynhlth.com
+
+# import the required packages
+
+from openwillis.measures.api import (
+    facial_expressivity,
+    emotional_expressivity,
+    vocal_acoustics,
+    speech_transcription,
+    speech_characteristics,
+    speaker_separation,
+    speech_transcription_cloud,
+    to_audio
+)
+
+__all__ = ["facial_expressivity", "vocal_acoustics", "emotional_expressivity", "speech_transcription", "speech_characteristics", "speaker_separation", "speech_transcription_cloud", "to_audio"]
