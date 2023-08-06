6b939e581ebfc72c27751f36e52c3e1937fd0630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:15 +0000

    Add package cond-rnn/3.2.2/cond-rnn-3.2.2.tar.gz

---
+++ packages/cond-rnn/cond-rnn-3.2.2.tar.gz/cond-rnn-3.2.2/setup.py
@@ -0,0 +1,25 @@
+import os
+import platform
+
+from setuptools import setup
+
+tensorflow = 'tensorflow'
+if platform.system() == 'Darwin' and platform.processor() == 'arm':
+    tensorflow = 'tensorflow-macos'
+    # https://github.com/grpc/grpc/issues/25082
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_OPENSSL'] = '1'
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_ZLIB'] = '1'
+
+install_requires = ['numpy', tensorflow, 'tensorflow_addons', "protobuf<=3.20"]
+
+setup(
+    name='cond-rnn',
+    version='3.2.2',
+    description='Conditional RNN',
+    author='Philippe Remy',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md', encoding='utf-8').read(),
+    packages=['cond_rnn'],
+    install_requires=install_requires
+)
