1163b7cf273b513e9117e2adcebef6c94a35c4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:39 +0000

    Add package pbaa/1.0.1/pbaa-1.0.1.tar.gz

---
+++ packages/pbaa/pbaa-1.0.1.tar.gz/pbaa-1.0.1/README.md
@@ -0,0 +1,91 @@
+# pbaa : Prompt-Based Automatic Annotation
+
+[![PyPI - Version](https://img.shields.io/pypi/v/pbaa.svg)](https://pypi.org/project/pbaa)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pbaa.svg)](https://pypi.org/project/pbaa)
+
+Easy inference implementation of [Grounded-SAM](https://github.com/IDEA-Research/Grounded-Segment-Anything) for
+**Prompt-based automatic annotation**
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [Usage](#Usage)
+- [Demo](#Demo)
+- [License](#license)
+
+## Installation
+
+```console
+pip install pbaa
+```
+
+## Usage
+
+### Options
+
+```console
+Usage: pbaa [OPTIONS]
+
+Options:
+  --version                    Show the version and exit.
+  -s, --src TEXT               Source image or directory path  [required]
+  -p, --prompt <TEXT TEXT>...  Space-separated a pair of prompt and target
+                               classe. (Multi)  [required]
+  -b, --box_threshold FLOAT    Threshold for Object Detection (default: 0.25)
+  -n, --nms_threshold FLOAT    Threshold for NMS (default: 0.8)
+  -o, --output_dir TEXT        Path to result data (default: '.')
+  -h, --help                   Show this message and exit.
+```
+
+### CLI
+
+```console
+# pbaa -s <Source> -p <prompt> <class> -p <prompt> <class> ...
+
+pbaa -s source_image.jpg -p "black dog" dog
+pbaa -s source_image.jpg -p "black dog" dog -p "white cat" cat
+```
+
+### Python
+
+```python
+from pbaa import model_init, inference
+
+model_init()
+inference("path/to/source_image.jpg", {"black dog": "dog", "white cat": "cat"})
+```
+
+## Demo
+
+```console
+## Source : assets/demo9.jpg
+## prompts : {"plant" : "plant", "picture" : "picture", "dog": "dog", "lamp" : "lamp", "carpet" : "carpet", "sofa" : "sofa"}
+
+pbaa -s assets/demo9.jpg -p plant plant -p picture picture -p dog dog -p lamp lamp -p carpet carpet -p sofa sofa
+```
+
+| Origin                                                                          | Detection                                                                              | Segmentation                                                                           |
+|---------------------------------------------------------------------------------|----------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------|
+| ![Before](https://github.com/dh031200/pbaa/blob/main/assets/demo9.jpg?raw=true) | ![detection](https://github.com/dh031200/pbaa/blob/main/assets/demo9_det.jpg?raw=true) | ![detection](https://github.com/dh031200/pbaa/blob/main/assets/demo9_seg.jpg?raw=true) |
+### Result data
+[demo9.json](assets/demo9.json)<br>
+```console
+json structure
+
+index
+  ├ cls : class name
+  ├ conf : confidence score
+  ├ box : bounding box coordinates
+  └ poly : polygon coordinates
+```
+## License
+
+`pbaa` is distributed under the terms of the [Apache-2.0](https://spdx.org/licenses/Apache-2.0.html) license.
+
+## Acknowledgements
+
+Grounded-Segment-Anything : [https://github.com/IDEA-Research/Grounded-Segment-Anything](https://github.com/IDEA-Research/Grounded-Segment-Anything)<br>
+Grounding DINO : [https://github.com/IDEA-Research/GroundingDINO](https://github.com/IDEA-Research/GroundingDINO)<br>
+Segment-anything : [https://github.com/facebookresearch/segment-anything](https://github.com/facebookresearch/segment-anything)<br>
---
+++ packages/pbaa/pbaa-1.0.1.tar.gz/pbaa-1.0.1/src/pbaa/core/grounded_sam.py
@@ -0,0 +1,132 @@
+from json import dump
+from pathlib import Path
+
+import cv2
+import numpy as np
+import supervision as sv
+import torch
+import torchvision
+import wget
+from groundingdino.config.GroundingDINO_SwinT_OGC import __file__ as grounding_dino_config_path
+from groundingdino.util.inference import Model
+from loguru import logger
+from segment_anything import SamPredictor, sam_model_registry
+
+HQ = False
+GROUNDING_DINO_CHECKPOINT_PATH = Path("groundingdino_swint_ogc.pth")
+
+if HQ:
+    SAM_CHECKPOINT_PATH = Path("sam_hq_vit_h.pth")
+    url = "https://blueclairvoyancestorage.blob.core.windows.net/package/sam_hq_vit_h.pth"
+else:
+    SAM_CHECKPOINT_PATH = Path("sam_vit_h_4b8939.pth")
+    url = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+
+# Segment-Anything checkpoint
+SAM_ENCODER_VERSION = "vit_h"
+
+
+def model_init():
+    # GroundingDINO config and checkpoint
+    if not GROUNDING_DINO_CHECKPOINT_PATH.exists():
+        logger.warning("GROUNDING_DINO_CHECKPOINT doesn't exist")
+        logger.info("Start download")
+        wget.download(
+            "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
+        )
+    if not SAM_CHECKPOINT_PATH.exists():
+        logger.warning("SAM_CHECKPOINT_PATH doesn't exist")
+        logger.info("Start download")
+        wget.download(url)
+
+
+def segment(sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
+    sam_predictor.set_image(image)
+    result_masks = []
+    for box in xyxy:
+        masks, scores, logits = sam_predictor.predict(box=box, multimask_output=True)
+        index = np.argmax(scores)
+        result_masks.append(masks[index])
+    return np.array(result_masks)
+
+
+def inference(_src, _prompt, box_threshold, nms_threshold, output_dir):
+    src = Path(_src)
+    dst = Path(output_dir)
+    _prompt = {i.lower(): v for i, v in _prompt}
+
+    # Building GroundingDINO inference model
+    grounding_dino_model = Model(
+        model_config_path=grounding_dino_config_path, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH
+    )
+
+    # Building SAM Model and SAM Predictor
+    sam_predictor = SamPredictor(sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH))
+
+    # Predict classes and hyper-param for GroundingDINO
+    prompt = [*map(str.lower, _prompt.keys())]
+
+    # load image
+    image = cv2.imread(_src)
+
+    # detect objects
+    detections = grounding_dino_model.predict_with_classes(
+        image=image, classes=prompt, box_threshold=box_threshold, text_threshold=box_threshold
+    )
+
+    # annotate image with detections
+    box_annotator = sv.BoxAnnotator()
+    labels = [f"{_prompt[prompt[class_id]]} {confidence:0.2f}" for _, _, confidence, class_id, _ in detections]
+    annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
+
+    nms_idx = (
+        torchvision.ops.nms(torch.from_numpy(detections.xyxy), torch.from_numpy(detections.confidence), nms_threshold)
+        .numpy()
+        .tolist()
+    )
+
+    detections.xyxy = detections.xyxy[nms_idx]
+    detections.confidence = detections.confidence[nms_idx]
+    detections.class_id = detections.class_id[nms_idx]
+
+    # Prompting SAM with detected boxes
+    # convert detections to masks
+    detections.mask = segment(
+        sam_predictor=sam_predictor, image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB), xyxy=detections.xyxy
+    )
+
+    confidences = [round(i, 4) for i in detections.confidence.astype(float)]
+    boxes = detections.xyxy.astype(int).tolist()
+    polys = []
+    for mask in detections.mask:
+        canvas = np.zeros((image.shape[:2]), dtype=np.uint8)
+        canvas[mask] = 255
+        poly, _ = cv2.findContours(canvas, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        _polys = []
+        for _poly in poly:
+            _polys.append(_poly.squeeze().astype(int).tolist())
+        polys.append(_polys)
+
+    mask_canvas = np.zeros_like(image, dtype=np.uint8)
+
+    # annotate image with detections
+    box_annotator = sv.BoxAnnotator()
+    mask_annotator = sv.MaskAnnotator()
+    labels = [f"{_prompt[prompt[class_id]]} {confidence:0.2f}" for _, _, confidence, class_id, _ in detections]
+    annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
+    annotated_mask = mask_annotator.annotate(scene=mask_canvas, detections=detections)
+    annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+
+    class_id = detections.class_id.astype(int).tolist()
+    json_data = {}
+    for idx, (_id, conf, box, poly) in enumerate(zip(class_id, confidences, boxes, polys)):
+        json_data[idx] = {"cls": _prompt[prompt[_id]], "conf": conf, "box": box, "poly": poly}
+
+    with open(f"{dst / src.stem}.json", "w") as f:
+        dump(json_data, f, indent=4, ensure_ascii=False)
+
+    # save images
+    cv2.imwrite(f"{dst / src.stem}_det.jpg", annotated_frame)
+    cv2.imwrite(f"{dst / src.stem}_seg.jpg", annotated_image)
+    cv2.imwrite(f"{dst / src.stem}_mask.jpg", annotated_mask)
