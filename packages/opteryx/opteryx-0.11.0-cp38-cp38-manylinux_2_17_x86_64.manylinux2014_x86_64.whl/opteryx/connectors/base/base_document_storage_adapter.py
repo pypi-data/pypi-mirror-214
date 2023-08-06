49aac8abea28e958b589f6a2303913b74f9d5cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:23 +0000

    Add package opteryx/0.11.0/opteryx-0.11.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.11.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/connectors/base/base_document_storage_adapter.py
@@ -0,0 +1,76 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Base Inner Reader for nosql document stores
+"""
+from typing import Iterable
+
+import pyarrow
+
+
+class BaseDocumentStorageAdapter:
+    __mode__ = "Collection"
+
+    def __init__(self, prefix: str, remove_prefix: bool = False) -> None:
+        self._prefix = prefix
+        self._remove_prefix = remove_prefix
+        self.chunk_size = 500
+
+    def chunk_dictset(self, dictset: Iterable[dict], morsel_size: int):
+        """
+        Enables paging through a dictset by returning a chunk of records at a time.
+        Parameters:
+            dictset: iterable of dictionaries:
+                The dictset to process
+            chunk_size: integer:
+                The number of bytes per chunk
+        """
+        chunk_size = self.chunk_size
+        index = -1
+        chunk: list = [{}] * chunk_size
+        for index, record in enumerate(dictset):
+            _id = record.pop("_id", None)
+            record["id"] = None if _id is None else str(_id)
+            if index > 0 and index % chunk_size == 0:
+                morsel = pyarrow.Table.from_pylist(chunk)
+                # from 500 records, estimate the number of records to fill the morsel size
+                # in the unlikely event that the right size for chunks is 500, we calculate
+                # this every cycle
+                if chunk_size == 500 and morsel.nbytes > 0:
+                    chunk_size = int(morsel_size // (morsel.nbytes / 500))
+                    self.chunk_size = chunk_size
+                # yield after the calculation, as you probably expect this to be set after
+                # the first read, not the second read
+                yield morsel
+                chunk = [{}] * chunk_size
+                chunk[0] = record
+            else:
+                chunk[index % chunk_size] = record
+        morsel = pyarrow.Table.from_pylist(chunk[: (index + 1) % chunk_size])
+        yield morsel
+
+    def get_document_count(self, collection) -> int:  # pragma: no cover
+        """
+        Return the count, or an estimate of, the number of documents
+        """
+        raise NotImplementedError("get_document_list not implemented")
+
+    def read_documents(self, collection, morsel_size: int = 500):  # pragma: no cover
+        """
+        Return a morsel of documents
+        """
+        raise NotImplementedError("read_document not implemented")
+
+    @property
+    def can_push_selection(self):
+        return False
---
+++ packages/opteryx/opteryx-0.11.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/operators/__init__.py
@@ -0,0 +1,83 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .base_plan_node import BasePlanNode  # isort: skip
+
+from .aggregate_node import AGGREGATORS
+from .aggregate_node import AggregateNode  # aggregate data
+from .blob_reader_node import BlobReaderNode  # read file/blob datasets
+from .build_statistics_node import BuildStatisticsNode  # Analyze Tables
+from .collection_reader_node import CollectionReaderNode  # reader NoSQL datsets
+from .column_filter_node import ColumnFilterNode  # filter for SHOW COLUMNS
+from .cross_join_node import CrossJoinNode  # CROSS JOIN
+from .distinct_node import DistinctNode  # remove duplicate records
+from .explain_node import ExplainNode  # EXPLAIN queries
+from .file_reader_node import FileReaderNode  # usually on the CLI
+from .function_dataset_node import FunctionDatasetNode  # Dataset Constructors
+from .heap_sort_node import HeapSortNode  # Heap
+from .information_schema_node import InformationSchemaNode  # information_schema
+from .inner_join_node import InnerJoinNode  # INNER JOIN
+from .internal_dataset_node import InternalDatasetNode  # Sample datasets
+from .limit_node import LimitNode  # select the first N records
+from .morsel_defragment_node import MorselDefragmentNode  # consolidate small morsels
+from .noop_node import NoOpNode  # No Operation
+from .offset_node import OffsetNode  # skip a number of records
+from .outer_join_node import OuterJoinNode  # LEFT/RIGHT/FULL OUTER JOIN
+from .projection_node import ProjectionNode  # remove unwanted columns including renames
+from .selection_node import SelectionNode  # filter unwanted rows
+from .show_columns_node import ShowColumnsNode  # column details
+from .show_create_node import ShowCreateNode  # SHOW CREATE TABLE
+from .show_databases_node import ShowDatabasesNode  # SHOW DATABASES
+from .show_functions_node import ShowFunctionsNode  # supported functions
+from .show_value_node import ShowValueNode  # display node for SHOW
+from .show_variables_node import ShowVariablesNode  # SHOW VARIABLES
+from .sort_node import SortNode  # order by selected columns
+from .sql_reader_node import SqlReaderNode  # external SQL databases
+from .v2_scanner_node import V2ScannerNode
+
+# map join types to their implementations
+_join_nodes = {
+    "CrossJoin": CrossJoinNode,
+    "CrossJoinUnnest": CrossJoinNode,
+    "FullOuter": OuterJoinNode,
+    "Inner": InnerJoinNode,
+    "LeftOuter": OuterJoinNode,
+    "RightOuter": OuterJoinNode,
+}
+
+# map reader types to their implementation
+_reader_nodes = {
+    "Blob": BlobReaderNode,  # (disk, gcs, minio, s3)
+    "Collection": CollectionReaderNode,  # (mongodb, firestore)
+    "File": FileReaderNode,  # usually the command line
+    "Function": FunctionDatasetNode,
+    "InformationSchema": InformationSchemaNode,
+    "Internal": InternalDatasetNode,
+    "SubQuery": BlobReaderNode,  # ?? <- this shouldn't be a reader
+    "SQL": SqlReaderNode,
+}
+
+
+def join_factory(mode):
+    return _join_nodes[mode]
+
+
+def reader_factory(mode):
+    return _reader_nodes[mode]
+
+
+def is_aggregator(name):
+    return name in AGGREGATORS
+
+
+def aggregators():
+    return list(AGGREGATORS.keys())
