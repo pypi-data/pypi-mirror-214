a7730316f7c547a1566a7eeb5c37997dd5fc3b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:31 +0000

    Add package vmklib/1.8.3/vmklib-1.8.3.tar.gz

---
+++ packages/vmklib/vmklib-1.8.3.tar.gz/vmklib-1.8.3/setup.py
@@ -0,0 +1,41 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=ab812b89eee40b1fec8ddff61dc0552a
+# =====================================
+
+"""
+vmklib - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from setuptools_wrapper.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from vmklib_bootstrap.setup import setup  # type: ignore
+
+# internal
+from vmklib import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.7",
+        "3.8",
+        "3.9",
+        "3.10",
+        "3.11",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
