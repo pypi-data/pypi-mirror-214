3153da20a30fb6e457afbecdb2daee7ead22f188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:48 +0000

    Add package micropython-stts22h/0.1.1/micropython-stts22h-0.1.1.tar.gz

---
+++ packages/micropython-stts22h/micropython-stts22h-0.1.1.tar.gz/micropython-stts22h-0.1.1/docs/api.rst
@@ -0,0 +1,12 @@
+STTS22H
+=========================================
+
+
+.. automodule:: micropython_stts22h
+    :members:
+
+.. automodule:: micropython_stts22h.stts22h
+    :members:
+
+.. automodule:: micropython_stts22h.i2c_helpers
+    :members:
---
+++ packages/micropython-stts22h/micropython-stts22h-0.1.1.tar.gz/micropython-stts22h-0.1.1/examples/stts22h_output_data_rate.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Jose D. Montoya
+#
+# SPDX-License-Identifier: MIT
+
+import time
+from machine import Pin, I2C
+from micropython_stts22h import stts22h
+
+i2c = I2C(1, sda=Pin(2), scl=Pin(3))  # Correct I2C pins for RP2040
+stts = stts22h.STTS22H(i2c)
+
+stts.output_data_rate = stts22h.ODR_200_HZ
+
+while True:
+    for output_data_rate in stts22h.output_data_rate_values:
+        print("Current Output data rate setting: ", stts.output_data_rate)
+        for _ in range(10):
+            temp = stts.temperature
+            print("Temperature :{:.2f}C".format(temp))
+            time.sleep(0.5)
+        stts.output_data_rate = output_data_rate
