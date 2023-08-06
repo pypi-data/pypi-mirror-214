c041de3d0019ec2b13f39b8cbc5729834d7aa62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:54 +0000

    Add package picogo/0.0.15/picogo-0.0.15-py3-none-any.whl

---
+++ packages/picogo/picogo-0.0.15-py3-none-any.whl/picogo/__init__.py
@@ -0,0 +1,14 @@
+__name__ = "picogo"
+__package__ = "picogo"
+__version__ = '0.0.8'
+__author__ = "SkySLAM Co., Ltd."
+
+print( "picogo __init__.py executed." ) 
+
+from .Robot import (
+    Robot , 
+)
+
+from .MainFileCopy import *
+
+print( "Done. picogo __init__.py executed." ) 
