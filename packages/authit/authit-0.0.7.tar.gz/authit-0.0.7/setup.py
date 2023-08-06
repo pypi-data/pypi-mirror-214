c5d99b63880fdd1b7d9ddbc248adb844de0144b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:07 +0000

    Add package authit/0.0.7/authit-0.0.7.tar.gz

---
+++ packages/authit/authit-0.0.7.tar.gz/authit-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='authit',
+    version='0.0.7',
+    description='An identity platform',
+    author='Muthu',
+    author_email='contact@muthupandian.in',
+    packages=['authit'],
+    url='https://github.com/muthugit/authit',
+    install_requires=[
+        "bcrypt"
+    ],
+)
