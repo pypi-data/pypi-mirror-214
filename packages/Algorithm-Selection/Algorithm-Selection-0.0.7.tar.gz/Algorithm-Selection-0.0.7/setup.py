494309597d92ac51b81032c09f2f3f9dbc5cb9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:52 +0000

    Add package Algorithm-Selection/0.0.7/Algorithm-Selection-0.0.7.tar.gz

---
+++ packages/Algorithm-Selection/Algorithm-Selection-0.0.7.tar.gz/Algorithm-Selection-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='Algorithm-Selection',
+    version='0.0.7',
+    license='MIT License',
+    author='valfridojr',
+    long_description='readme.MD',
+    author_email='juniorprado@alu.ufc.br',
+    keywords='metalearning',
+    description=u'Functions dedicated to The Algoritm Selection problem with a focus on a metalearning approach',
+    packages=['Algorithm-Selection'],
+    install_requires=['requests','numpy','pandas','sklearn','scipy', 'io'],
+    url = 'https://github.com/Junior-Prado/Algorithm-Selection'
+)
