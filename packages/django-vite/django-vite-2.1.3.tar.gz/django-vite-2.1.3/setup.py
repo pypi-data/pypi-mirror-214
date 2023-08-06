f86ed16b77a237982f79c92da8711a669c086b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:32 +0000

    Add package django-vite/2.1.3/django-vite-2.1.3.tar.gz

---
+++ packages/django-vite/django-vite-2.1.3.tar.gz/django-vite-2.1.3/setup.py
@@ -0,0 +1,32 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    README = f.read()
+
+
+setup(
+    name="django-vite",
+    version="2.1.3",
+    description="Integration of ViteJS in a Django project.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="MrBin99",
+    url="https://github.com/MrBin99/django-vite",
+    license="Apache License, Version 2.0",
+    include_package_data=True,
+    packages=find_packages(),
+    requires=[
+        "Django (>=1.11)",
+    ],
+    install_requires=[
+        "Django>=1.11",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    extras_require={"dev": ["black", "flake8"]},
+)
