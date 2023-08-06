d38e95787223f24da22af18bdec8280227324905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:56 +0000

    Add package VLCDA/1.0.0/VLCDA-1.0.0.tar.gz

---
+++ packages/VLCDA/VLCDA-1.0.0.tar.gz/VLCDA-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "VLCDA",
+    version = "1.0.0",
+    description = "Modulo Python dedicado a evolução de circuitos lógicos através de algoritmos genéticos",
+    author = "Humberto Távora, Stefan Blawid, Yasmin Maria",
+    url = 'https://github.com/HumbertoTavora/Logic-Circuits-Evolution',
+    download_url = 'https://github.com/HumbertoTavora/Logic-Circuits-Evolution',
+    keywords = ['Genetic', 'Algorithm', 'Logic Circuits', 'Evolution', 'Optimization'],
+    classifiers=['Programming Language :: Python :: 3','Intended Audience :: Developers'],
+    packages=find_packages()
+    )
+
