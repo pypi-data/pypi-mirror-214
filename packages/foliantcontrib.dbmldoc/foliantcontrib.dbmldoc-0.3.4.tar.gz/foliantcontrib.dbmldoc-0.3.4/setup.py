699794277eea2e4cf4c3a1c8ab180f762e563da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:21 +0000

    Add package foliantcontrib.dbmldoc/0.3.4/foliantcontrib.dbmldoc-0.3.4.tar.gz

---
+++ packages/foliantcontrib.dbmldoc/foliantcontrib.dbmldoc-0.3.4.tar.gz/foliantcontrib.dbmldoc-0.3.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Documentation generator for DBML specification format'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.dbmldoc',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='0.3.4',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    packages=['foliant.preprocessors.dbmldoc'],
+    package_data={'foliant.preprocessors.dbmldoc': ['template/*.j2']},
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.5',
+        'jinja2',
+        'pydbml>=1.0.6',
+        'foliantcontrib.utils>=1.0.2',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
