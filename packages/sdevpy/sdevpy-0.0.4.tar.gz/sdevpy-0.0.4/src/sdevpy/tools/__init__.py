dd63aae3003f4e3e2545558407eea1fc21c9cdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:09 +0000

    Add package sdevpy/0.0.4/sdevpy-0.0.4.tar.gz

---
+++ packages/sdevpy/sdevpy-0.0.4.tar.gz/sdevpy-0.0.4/src/sdevpy/tools/__init__.py
@@ -0,0 +1,6 @@
+from .clipboard import clipboard
+from .constants import constants
+from .filemanager import filemanager
+from .jsonmanager import jsonmanager
+from .timegrids import timegrids
+from .timer import timer
