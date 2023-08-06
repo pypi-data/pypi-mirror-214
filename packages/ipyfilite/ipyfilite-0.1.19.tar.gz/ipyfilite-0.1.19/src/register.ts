4c4693275926483829a4147af67164af599103c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:15 +0000

    Add package ipyfilite/0.1.19/ipyfilite-0.1.19.tar.gz

---
+++ packages/ipyfilite/ipyfilite-0.1.19.tar.gz/ipyfilite-0.1.19/src/register.ts
@@ -0,0 +1,8 @@
+export function registerServiceWorker(): Promise<ServiceWorkerRegistration> {
+  return navigator.serviceWorker.register(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    new URL('./service-worker', import.meta.url),
+    { scope: './download/' }
+  );
+}
