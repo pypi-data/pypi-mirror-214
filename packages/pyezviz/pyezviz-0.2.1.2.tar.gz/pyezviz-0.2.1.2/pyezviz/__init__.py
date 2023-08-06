404a21116896db84d5006f0f239e8f6b1da13bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:41 +0000

    Add package pyezviz/0.2.1.2/pyezviz-0.2.1.2.tar.gz

---
+++ packages/pyezviz/pyezviz-0.2.1.2.tar.gz/pyezviz-0.2.1.2/pyezviz/__init__.py
@@ -0,0 +1,50 @@
+"""init pyezviz."""
+from .camera import EzvizCamera
+from .cas import EzvizCAS
+from .client import EzvizClient
+from .constants import (
+    BatteryCameraWorkMode,
+    DefenseModeType,
+    DeviceCatagories,
+    DeviceSwitchType,
+    IntelligentDetectionMode,
+    MessageFilterType,
+    NightVisionMode,
+    SoundMode,
+    SupportExt,
+)
+from .exceptions import (
+    AuthTestResultFailed,
+    EzvizAuthTokenExpired,
+    EzvizAuthVerificationCode,
+    HTTPError,
+    InvalidHost,
+    InvalidURL,
+    PyEzvizError,
+)
+from .mqtt import MQTTClient
+from .test_cam_rtsp import TestRTSPAuth
+
+__all__ = [
+    "EzvizCamera",
+    "EzvizClient",
+    "PyEzvizError",
+    "InvalidURL",
+    "HTTPError",
+    "InvalidHost",
+    "AuthTestResultFailed",
+    "EzvizAuthTokenExpired",
+    "EzvizAuthVerificationCode",
+    "EzvizCAS",
+    "MQTTClient",
+    "DefenseModeType",
+    "IntelligentDetectionMode",
+    "BatteryCameraWorkMode",
+    "NightVisionMode",
+    "MessageFilterType",
+    "DeviceCatagories",
+    "DeviceSwitchType",
+    "SupportExt",
+    "SoundMode",
+    "TestRTSPAuth",
+]
