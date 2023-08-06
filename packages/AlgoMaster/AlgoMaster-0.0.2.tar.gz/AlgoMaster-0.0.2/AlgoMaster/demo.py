e9eac700f1aa7b86a300fe33620a0bcb900358d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:20 +0000

    Add package AlgoMaster/0.0.2/AlgoMaster-0.0.2.tar.gz

---
+++ packages/AlgoMaster/AlgoMaster-0.0.2.tar.gz/AlgoMaster-0.0.2/AlgoMaster/demo.py
@@ -0,0 +1,14 @@
+# create a new class for print hello world with funtion call
+
+class HelloWorld:
+    def __init__(self):
+        print("Hello World")
+
+    def printHelloWorld(self):
+        print("Hello World")
+    
+    def printHelloWorld2(self):
+        print("Hello World 2")
+    
+    def printHelloWorld3(self):
+        print("Hello World 3")
