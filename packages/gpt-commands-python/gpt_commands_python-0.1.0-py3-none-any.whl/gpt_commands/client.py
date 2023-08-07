d69ec6f1228009a522fd9a68fc83a8973577f44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:23 +0000

    Add package gpt-commands-python/0.1.0/gpt_commands_python-0.1.0-py3-none-any.whl

---
+++ packages/gpt-commands-python/gpt_commands_python-0.1.0-py3-none-any.whl/gpt_commands/client.py
@@ -0,0 +1,294 @@
+import json
+from dataclasses import dataclass
+from enum import Enum
+from logging import getLogger
+from os import getenv
+from typing import AsyncGenerator, Dict, List, Optional
+
+from aiohttp import ClientSession, StreamReader
+from dataclasses_json import dataclass_json
+from dotenv import load_dotenv
+
+from gpt_commands.introspection import Manager, create_manager
+
+
+class Role(str, Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+    FUNCTION = "function"
+
+    def __deepcopy__(self, memo):
+        return self.value
+
+
+@dataclass
+class Message:
+    role: Role
+    content: str
+    name: Optional[str] = None
+
+    def to_request(self) -> dict:
+        result = {
+            "role": self.role.value,
+            "content": self.content,
+        }
+
+        if self.name:
+            result["name"] = self.name
+
+        return result
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class FunctionCall:
+    name: Optional[str] = None
+    arguments: Optional[str] = None
+
+
+@dataclass
+class FunctionExecution:
+    name: str
+    arguments: Dict[str, str]
+
+    def execute(self, manager: Manager) -> Optional[str]:
+        return manager.execute(self.name, self.arguments)
+
+    def has_return(self, manager: Manager) -> bool:
+        function = manager.get_function(self.name)
+        if function is None:
+            return False
+        return function.has_return
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Delta:
+    role: Optional[Role] = None
+    content: Optional[str] = None
+    function_call: Optional[FunctionCall] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class ChatCompletionChoice:
+    index: int
+    delta: Delta
+    finish_reason: Optional[str]
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class ChatCompletionChunk:
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: List[ChatCompletionChoice]
+
+
+@dataclass
+class ResponseData:
+    ready: bool
+    content: str
+    function_name: Optional[str]
+    function_arguments: str
+    delta_text: Optional[str]
+
+    def get_message(self) -> Optional[Message]:
+        if self.ready and self.content:
+            return Message(Role.ASSISTANT, self.content)
+        else:
+            return None
+
+    def get_function_execution(self) -> Optional[FunctionExecution]:
+        if self.ready and self.function_name and self.function_arguments:
+            arguments = {
+                key: json.dumps(value)
+                for key, value in json.loads(
+                    self.function_arguments, strict=False
+                ).items()
+            }
+            return FunctionExecution(self.function_name, arguments)
+        else:
+            return None
+
+
+class GPTCommandsClient:
+    def __init__(
+        self,
+        model: str,
+        system_prompt: str,
+        api_key: Optional[str] = None,
+        api_organization: Optional[str] = None,
+        max_tokens: int = 2000,
+        temperature: float = 0.7,
+    ):
+        load_dotenv()
+        self.logger = getLogger(__name__)
+
+        self.api_key = api_key or getenv("OPENAI_API_KEY")
+        self.organization = api_organization or getenv("OPENAI_ORGANIZATION")
+        self.messages: List[Message] = [Message(Role.SYSTEM, system_prompt)]
+        self.session: Optional[ClientSession] = None
+
+        self.model = model
+        self.system_prompt = system_prompt
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+
+    async def __aenter__(self):
+        self.session = ClientSession(
+            headers={"Authorization": f"Bearer {self.api_key}"}
+        )
+        return self
+
+    async def __aexit__(self, *args):
+        if self.session:
+            await self.session.close()
+
+    def __parse_stream_helper(self, line: bytes) -> Optional[str]:
+        if line:
+            if line.strip() == b"data: [DONE]":
+                return None
+            if line.startswith(b"data: "):
+                line = line[len(b"data: ") :]
+                return line.decode("utf-8")
+            else:
+                return None
+        return None
+
+    async def __parse_stream_async(self, rbody: StreamReader):
+        async for line in rbody:
+            _line = self.__parse_stream_helper(line)
+            if _line is not None:
+                yield _line
+
+    def __process_chunk(
+        self, chunk: ChatCompletionChunk, response: Optional[ResponseData]
+    ) -> ResponseData:
+        delta = chunk.choices[0].delta
+
+        ready = bool(chunk.choices[0].finish_reason)
+
+        text: Optional[str] = None
+        content = response.content if response else ""
+
+        if delta.content:
+            text = delta.content.rstrip("\n")
+            content += text
+
+        function_name = response.function_name if response else None
+        function_arguments = response.function_arguments if response else ""
+
+        if delta.function_call:
+            if delta.function_call.name:
+                function_name = delta.function_call.name
+
+            if delta.function_call.arguments:
+                function_arguments += delta.function_call.arguments
+
+        return ResponseData(
+            ready=ready,
+            content=content,
+            function_name=function_name,
+            function_arguments=function_arguments,
+            delta_text=text,
+        )
+
+    async def __send_message(
+        self, message_to_send: Message, manager: object
+    ) -> AsyncGenerator[str, None]:
+        self.messages.append(message_to_send)
+
+        manager_wrapper = create_manager(manager)
+        messages = [message.to_request() for message in self.messages]
+
+        body = {
+            "model": self.model,
+            "messages": messages,
+            "functions": [
+                function.json_schema()
+                for function in manager_wrapper.functions.values()
+            ],
+            "max_tokens": self.max_tokens,
+            "n": 1,
+            "temperature": self.temperature,
+            "stream": True,
+        }
+        response: Optional[ResponseData] = None
+
+        if self.session:
+            async with self.session.post(
+                "https://api.openai.com/v1/chat/completions", json=body
+            ) as resp:
+                if resp.status != 200:
+                    raise Exception(
+                        f"OpenAI API returned status code {resp.status}: {await resp.text()}"
+                    )
+                async for data in self.__parse_stream_async(resp.content):
+                    json_data = json.loads(data, strict=False)
+
+                    if json_data.get("error"):
+                        raise Exception(
+                            f"OpenAI API returned error: {json_data['error']}"
+                        )
+
+                    chunk: ChatCompletionChunk = ChatCompletionChunk.from_dict(json_data)  # type: ignore
+                    processed_chunk = self.__process_chunk(chunk, response)
+                    if processed_chunk.delta_text:
+                        yield processed_chunk.delta_text
+
+                    response = processed_chunk
+                    if response.ready:
+                        break
+
+        if response and response.ready:
+            message = response.get_message()
+
+            if message:
+                self.logger.info(f"Storing message: {message}")
+                self.messages.append(message)
+
+            call = response.get_function_execution()
+
+            if call:
+                self.logger.info(f"Calling function: {call}")
+                result = call.execute(manager_wrapper)
+                function_result = Message(Role.FUNCTION, result or "true", call.name)
+                async for data in self.__send_message(function_result, manager):
+                    yield data
+
+    async def chat_stream(
+        self, prompt: str, manager: object
+    ) -> AsyncGenerator[str, None]:
+        """
+        Sends a prompt to the OpenAI API and yields the response in chunks
+
+        Args:
+            prompt: User prompt to send to the API
+            manager: The manager object to use for function calls
+
+        Yields:
+            The response from the API in string chunks
+        """
+        message = Message(Role.USER, prompt)
+        async for data in self.__send_message(message, manager):
+            yield data
+
+    async def chat(self, prompt: str, manager: object) -> str:
+        """
+        Sends a prompt to the OpenAI API and returns the response as a string
+
+        Args:
+            prompt: The prompt to send to the API
+            manager: The manager object to use for function calls
+
+        Returns:
+            The response from the API as a string
+        """
+        result = ""
+        async for data in self.chat_stream(prompt, manager):
+            result += data
+        return result
---
+++ packages/gpt-commands-python/gpt_commands_python-0.1.0-py3-none-any.whl/gpt_commands/introspection.py
@@ -0,0 +1,255 @@
+import inspect
+import json
+import typing
+from dataclasses import dataclass
+from inspect import getmembers, ismethod, signature
+from typing import Any, Dict, Optional, Union
+
+from dataclasses_jsonschema import JsonSchemaMixin
+from docstring_parser import parse
+
+
+class UnsupportedTypeException(Exception):
+    def __init__(self, type: type):
+        super().__init__(
+            f"Unsupported type: {type_name(type)}. Only primitive types, lists, dictionaries, Optional and JsonSchemaMixin dataclasses are supported."
+        )
+
+
+class UnsupportedDictionaryKeyTypeException(Exception):
+    def __init__(self, key_type: type):
+        super().__init__(
+            f"Unsupported dictionary key type: {type_name(key_type)}. Only string keys are supported."
+        )
+
+
+def is_optional(hint_type: type) -> bool:
+    return typing.get_origin(hint_type) == Union and type(None) in typing.get_args(
+        hint_type
+    )
+
+
+def type_name(hint_type: type) -> str:
+    return hint_type.__name__ if getattr(hint_type, "__name__") else str(hint_type)
+
+
+def decode_json(json_value: str, hint_type: type) -> object:
+    if is_optional(hint_type):
+        if json_value is None or json_value == "null":
+            return None
+        else:
+            non_none_type = [t for t in typing.get_args(hint_type) if t != type(None)]
+            if len(non_none_type) != 1:
+                raise UnsupportedTypeException(hint_type)
+
+            return decode_json(json_value, typing.get_args(hint_type)[0])
+    elif issubclass(hint_type, JsonSchemaMixin):
+        return hint_type.from_json(json_value)
+    elif typing.get_origin(hint_type) == list:
+        item_type = typing.get_args(hint_type)[0]
+        return [
+            decode_json(
+                item_value if isinstance(item_value, str) else json.dumps(item_value),
+                item_type,
+            )
+            for item_value in json.loads(json_value, strict=False)
+        ]
+    elif typing.get_origin(hint_type) == dict:
+        key_type, value_type = typing.get_args(hint_type)
+        if key_type != str:
+            raise UnsupportedDictionaryKeyTypeException(key_type)
+
+        dictionary: dict = json.loads(json_value, strict=False)
+        return {
+            key: decode_json(
+                value if isinstance(value, str) else json.dumps(value), value_type
+            )
+            for key, value in dictionary.items()
+        }
+    elif hint_type == str:
+        return (
+            json.loads(json_value, strict=False)
+            if json_value.startswith('"')
+            else json_value
+        )
+    elif hint_type == int:
+        return int(json_value)
+    elif hint_type == float:
+        return float(json_value)
+    elif hint_type == bool:
+        return json_value == "true"
+    else:
+        raise UnsupportedTypeException(hint_type)
+
+
+def type_to_json_schema(hint_type: type) -> dict:
+    if is_optional(hint_type):
+        actual_type = typing.get_args(hint_type)[0]
+
+        return type_to_json_schema(actual_type)
+    if hint_type == str:
+        return {"type": "string"}
+    elif hint_type == int:
+        return {"type": "integer"}
+    elif hint_type == float:
+        return {"type": "number"}
+    elif hint_type == bool:
+        return {"type": "boolean"}
+    elif typing.get_origin(hint_type) == list:
+        item = typing.get_args(hint_type)[0]
+        item_type = type_to_json_schema(item)
+        return {"type": "array", "items": item_type}
+    elif typing.get_origin(hint_type) == dict:
+        key, value = typing.get_args(hint_type)
+        key_type = type_to_json_schema(key)["type"]
+
+        if key_type != "string":
+            raise UnsupportedDictionaryKeyTypeException(key)
+
+        value_type = type_to_json_schema(value)
+        return {"type": "object", "additionalProperties": value_type}
+    elif issubclass(hint_type, JsonSchemaMixin):
+        schema = hint_type.json_schema()
+        schema.pop("$schema")
+        return schema
+    else:
+        raise UnsupportedTypeException(hint_type)
+
+
+@dataclass
+class Parameter:
+    name: str
+    type: type
+    optional: bool
+    default_value: Any
+    description: Optional[str]
+
+    def json_schema(self):
+        schema = type_to_json_schema(self.type)
+        schema["description"] = self.description
+        return schema
+
+    def serialize(self, value: object) -> str:
+        return json.dumps(value, default=lambda o: o.to_dict())
+
+    def deserialize(self, value: str) -> object:
+        return decode_json(value, self.type)
+
+
+@dataclass
+class Function:
+    name: str
+    description: Optional[str]
+    parameters: Dict[str, Parameter]
+    has_return: bool
+
+    def json_schema(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    name: parameter.json_schema()
+                    for name, parameter in self.parameters.items()
+                },
+                "required": [
+                    name
+                    for name, parameter in self.parameters.items()
+                    if not parameter.optional
+                ],
+            },
+        }
+
+
+@dataclass
+class Manager:
+    object: object
+    functions: Dict[str, Function]
+
+    def execute(self, function_name: str, arguments: Dict[str, Any]) -> Optional[str]:
+        function = getattr(self.object, function_name, None)
+        function_definition = self.functions[function_name]
+
+        if function_definition is None:
+            raise Exception(f"Function not found: {function_name}")
+
+        if function is None:
+            raise Exception(f"Function not found: {function_name}")
+
+        try:
+            for parameter in function_definition.parameters.values():
+                if parameter.optional:
+                    if parameter.name not in arguments:
+                        arguments[parameter.name] = parameter.default_value
+                        continue
+
+                if parameter.name not in arguments:
+                    raise Exception(
+                        f"Missing argument in {function_name}: {parameter.name}"
+                    )
+
+                arguments[parameter.name] = parameter.deserialize(
+                    arguments[parameter.name]
+                )
+            data = function(**arguments)
+        except Exception as e:
+            raise Exception("Failed to execute function") from e
+
+        if data is None:
+            return None
+
+        json_data = json.dumps(data, default=lambda o: o.to_dict())
+        return json_data
+
+    def get_function(self, function_name: str) -> Optional[Function]:
+        return self.functions.get(function_name, None)
+
+
+def create_manager(object: object) -> Manager:
+    functions: Dict[str, Function] = {}
+
+    for name, method in getmembers(object, predicate=ismethod):
+        if not name.startswith("_"):
+            if method.__doc__ is None:
+                raise Exception(f"Missing docstring for function {name}")
+
+            docstring = parse(method.__doc__)
+            parameters: Dict[str, Parameter] = {}
+            docstring_params = {p.arg_name: p for p in docstring.params} or {}
+            sign = signature(method)
+
+            for parameter_name in sign.parameters:
+                parameter = sign.parameters[parameter_name]
+
+                if parameter.annotation == inspect._empty:
+                    raise Exception(
+                        f"Missing type hint for parameter {parameter_name} in function {name}"
+                    )
+
+                if parameter_name not in docstring_params:
+                    raise Exception(
+                        f"Missing docstring for parameter {parameter_name} in function {name}"
+                    )
+
+                docstring_param = docstring_params[parameter_name]
+
+                parameters[parameter_name] = Parameter(
+                    name=parameter_name,
+                    type=parameter.annotation,
+                    optional=parameter.default != inspect.Parameter.empty,
+                    description=docstring_param.description,
+                    default_value=None
+                    if parameter.default == inspect.Parameter.empty
+                    else parameter.default,
+                )
+
+            functions[name] = Function(
+                name=name,
+                description=docstring.short_description,
+                parameters=parameters,
+                has_return=sign.return_annotation != inspect.Signature.empty
+                and sign.return_annotation != None,
+            )
+
+    return Manager(object, functions)
