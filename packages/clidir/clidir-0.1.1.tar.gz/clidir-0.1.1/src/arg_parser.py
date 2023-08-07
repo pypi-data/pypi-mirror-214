663ec70643ed45ba2135f2349cffb62fed3fa26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:53:04 +0000

    Add package clidir/0.1.1/clidir-0.1.1.tar.gz

---
+++ packages/clidir/clidir-0.1.1.tar.gz/clidir-0.1.1/src/arg_parser.py
@@ -0,0 +1,22 @@
+def parse(args: list[str], command_files: list[str]) -> list[str]:
+    
+    command_path_arg = ""
+    args_start = 0
+
+    for arg_position in range(len(args)):
+
+        path = "./commands/" + '/'.join(args[:arg_position + 1])
+
+        if path + ".py" in command_files:
+            command_path_arg = path + '.py'
+            args_start = arg_position + 1
+            break
+
+        elif any(path in substring for substring in command_files):
+            command_path_arg = path
+            args_start = arg_position + 1
+
+        else:
+            break
+
+    return [command_path_arg] + args[args_start:]
