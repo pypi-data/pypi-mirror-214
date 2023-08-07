dde25082d3416bbcf4612aea369bd45cf3517f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:42 +0000

    Add package taichu-dataflow/1.0.15/taichu-dataflow-1.0.15.tar.gz

---
+++ packages/taichu-dataflow/taichu-dataflow-1.0.15.tar.gz/taichu-dataflow-1.0.15/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    name = 'taichu-dataflow'
+
+    requirements = ['boto==2.49.0', 'esdk-obs-python==3.22.2']
+
+    long_description = ''
+    # with open('README.md', 'r') as f:
+    #     long_description = f.read()
+
+    setup(
+        name=name,
+        version='1.0.15',
+        description='taichu serve is a tool for serving dataflow',
+        long_description=long_description,
+        author='taichu platform team',
+        python_requires=">=3.6.0",
+        url='',
+        keywords='taichu',
+        packages=find_packages(),
+        install_requires=requirements,
+        include_package_data=True,
+        package_data={
+            '': ['*.sh'],
+        }
+    )
---
+++ packages/taichu-dataflow/taichu-dataflow-1.0.15.tar.gz/taichu-dataflow-1.0.15/taichu_dataflow/export_back.py
@@ -0,0 +1,89 @@
+import logging
+import os.path
+import random
+import threading
+import time
+import uuid
+
+from taichu_dataflow.storage import create_storage
+from datetime import datetime
+from taichu_dataflow.storage.env import STORAGE_TYPE, get_service_name
+
+storage_mgr = create_storage(STORAGE_TYPE)
+storage_prefix = 'sys/export_back/' + os.getenv('SERVICE_NAME', 'anonymous')
+
+__all__ = [
+    'export_back', 'export_back_base64', 'export_back_json',
+    'export_back_bytes', 'export_back_string', 'export_back_file'
+]
+
+
+def gen_path(suffix='', service_name=''):
+    suffix = suffix.lower().lstrip('.')
+    date_string = datetime.now().strftime("%Y-%m-%d")
+    rnd_number = str(random.randint(100000, 999999))
+    # 服务下可以根据类型细分
+    return os.path.join(
+        storage_prefix,
+        service_name,
+        date_string,
+        '{}-{}.{}'.format(str(int(time.time())), rnd_number, suffix))
+
+
+# 通用回流函数
+def export_back(export_type, content, suffix='', result_json=None, service_name=''):
+    threading.Thread(target=_export_back_async, args=(export_type, content, suffix, result_json, service_name)).start()
+
+
+def _export_back_async(export_type, content, suffix, result_json=None, service_name=''):
+    key = gen_path(suffix, service_name)
+    func = getattr(storage_mgr, 'write_' + export_type)
+    func(content, key)
+    if result_json:
+        write_json_func = getattr(storage_mgr, 'write_json')
+        write_json_func(result_json, os.path.splitext(key)[0].lower()+'.json')
+
+
+# 以下为辅助函数
+def export_back_base64(content, suffix, result_json=None, service_name=''):
+    _export_back_async('base64', content, suffix, result_json, service_name)
+
+
+def export_back_bytes(content, suffix, result_json=None, service_name=''):
+    _export_back_async('bytes', content, suffix, result_json, service_name)
+
+
+def export_back_string(content, suffix, result_json=None, service_name=''):
+    _export_back_async('string', content, suffix, result_json, service_name)
+
+
+def export_back_json(content, result_json=None, service_name=''):
+    _export_back_async('json', content, 'json', result_json, service_name)
+
+
+def export_back_file(path_to_file, result_json=None, service_name=''):
+    suffix = os.path.splitext(path_to_file)[-1].lower()
+    _export_back_async('file', path_to_file, suffix, result_json, service_name)
+
+
+def _logfmt(suffix, key):
+    logging.info({
+        "id": str(uuid.uuid4()),
+        "app_name": get_service_name(),
+        "create_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        # "user_id": "1889531xx35",
+        "action": "export_back",
+        "action_params": {
+            "suffix": suffix,
+            "key": key
+        }
+    })
+
+
+if __name__ == "__main__":
+    # export_back_string('abc')
+    # global base64_string
+    export_back('string', 'abc', 'txt', {'back_test': 'abc'}, 'mys_service')
+    # export_back('json', {'abc': 123}, 'json')
+    # export_back('base64', base64_string, 'jpg')
+    # export_back('file', '123.xyz', 'xyz', {'back_test': 'abc'})
