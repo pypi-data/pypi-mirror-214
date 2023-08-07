3f66c9c6edae45c2cda826c47e92fd5977e30c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:48 +0000

    Add package autotools/0.1/autotools-0.1.tar.gz

---
+++ packages/autotools/autotools-0.1.tar.gz/autotools-0.1/autotools/__init__.py
@@ -0,0 +1,19 @@
+from .chatgpt import chat
+from .chatgpt import set_api
+from .chatgpt import ask_gpt
+from .chatgpt import set_pov
+from .chatgpt import voice_chat
+from .chatgpt import speak
+from .chatgpt import speech
+from .chatgpt import set_tem
+from .chatgpt import set_token
+from .chatgpt import send
+from .chatgpt import math
+from .chatgpt import sendi
+from .chatgpt import link
+from .chatgpt import synonimize
+from .chatgpt import code
+from .chatgpt import scen
+from .chatgpt import img
+
+
---
+++ packages/autotools/autotools-0.1.tar.gz/autotools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='autotools',
+      version='0.1',
+      description='many autools by automatic',
+      author='automatic',
+      author_email='automatick07@gmail.com',
+      license='OpenSource',
+      packages=['autotools'],
+      install_requires=[],
+      zip_safe=False)
