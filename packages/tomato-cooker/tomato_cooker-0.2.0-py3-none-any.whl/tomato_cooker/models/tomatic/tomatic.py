5cb86cadcdf822c64354ad029ac64e5cda7a6d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:05 +0000

    Add package tomato-cooker/0.2.0/tomato_cooker-0.2.0-py3-none-any.whl

---
+++ packages/tomato-cooker/tomato_cooker-0.2.0-py3-none-any.whl/tomato_cooker/models/tomatic/tomatic.py
@@ -0,0 +1,23 @@
+import dataclasses
+from pathlib import Path
+from ..base import GridProblem
+
+
+MODEL_DEFINITION_PATH = Path(__file__).absolute().parent.joinpath("phone_grill.mzn")
+
+
+@dataclasses.dataclass
+class TomaticProblem(GridProblem):
+    nPersons: int
+    nLines: int
+    nHours: int
+    nNingus: int
+    nDays: int
+    maxTorns: int
+    nTorns: list
+    names: list
+    indisponibilitats: list
+    forcedTurns: list
+
+    def index(self, person, day):
+        return self.nDays*person + day
