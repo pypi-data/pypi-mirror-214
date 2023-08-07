04271119fec968f6b9841dfac3d9d5a26778dbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:46 +0000

    Add package cosched/0.0.0/cosched-0.0.0.tar.gz

---
+++ packages/cosched/cosched-0.0.0.tar.gz/cosched-0.0.0/cosched/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @author: Algorithm Team @ 杉数科技/Cardinal Operations
+# @email: coalgo@shanshu.ai
+# @date: 2023/06
+
+import warnings
+warnings.filterwarnings("ignore")
+
+__version__ = "0.0.0"
+
+print("杉数科技算法工具包的联络信息：coalgo@shanshu.ai")
+
+
+
