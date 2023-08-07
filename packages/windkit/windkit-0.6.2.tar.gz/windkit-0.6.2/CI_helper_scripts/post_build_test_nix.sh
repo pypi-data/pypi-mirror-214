84fe02f067c63b36a40faa8ebf5e12cab68d8fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:12 +0000

    Add package windkit/0.6.2/windkit-0.6.2.tar.gz

---
+++ packages/windkit/windkit-0.6.2.tar.gz/windkit-0.6.2/CI_helper_scripts/post_build_test_nix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for python_ver in "3.8" "3.9"
+do
+echo "======================================================================"
+echo "Building conda enviroment 'test_env' :"
+echo "python="$python_ver
+echo "windkit="$VERSION
+echo "======================================================================"
+echo ""
+
+
+mamba env remove -n test_env
+mamba create -n test_env python=$python_ver pytest
+conda init bash
+source activate test_env
+mamba install -c file://$LOCAL_CONDA_CHANNEL windkit=$VERSION --offline --override-channels
+cd test
+pytest --ignore-glob=*_old.py -v
+cd -
+conda deactivate
+conda env remove -n test_env
+done
+exit 0
