a7a1d600b9bd485a28e33e78161ea14429b827b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:07 +0000

    Add package pytmlib/1.1.0.dev5/pytmlib-1.1.0.dev5.tar.gz

---
+++ packages/pytmlib/pytmlib-1.1.0.dev5.tar.gz/pytmlib-1.1.0.dev5/pytmlib/context.py
@@ -0,0 +1,41 @@
+from typing import List
+from typing import TYPE_CHECKING
+
+from .output import OutputBuilder
+from .serializer import Serializer
+from .types import Entrypoint
+
+if TYPE_CHECKING:
+    from .abstract_exercise import AbstractExercise
+
+
+class Context:
+    def __init__(self, unique_id: str, secret: str, exercise: 'AbstractExercise'):
+        self._unique_id: str = unique_id
+        self._secret: str = secret
+        self._exercise: 'AbstractExercise' = exercise
+        self._entrypoints: List[Entrypoint] = exercise.get_entrypoints()
+
+    @property
+    def secret(self) -> str:
+        return self._secret
+
+    @property
+    def unique_id(self) -> str:
+        return self._unique_id
+
+    @property
+    def serializer(self) -> Serializer:
+        return Serializer(self._secret)
+
+    @property
+    def output(self) -> OutputBuilder:
+        return OutputBuilder(self.serializer)
+
+    @property
+    def exercise(self) -> 'AbstractExercise':
+        return self._exercise
+
+    @property
+    def entrypoints(self) -> List[Entrypoint]:
+        return self._entrypoints
