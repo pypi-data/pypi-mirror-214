23f26edda11e931b6d052de099c0ad72ede0eaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:38 +0000

    Add package kaliapay-modpy-beta-0.0.2/0.0.3/kaliapay-modpy-beta-0.0.2-0.0.3.tar.gz

---
+++ packages/kaliapay-modpy-beta-0.0.2/kaliapay-modpy-beta-0.0.2-0.0.3.tar.gz/kaliapay-modpy-beta-0.0.2-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Module de paiement Kaliapay'
+LONG_DESCRIPTION = "Module de paiement développer pour faciliter l'intégration de Kaliapay dans vos projets."
+
+# Setting up
+setup(
+    name="kaliapay-modpy-beta-0.0.2",
+    version=VERSION,
+    author="Fabio",
+    author_email="<nadefabrice83@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'paiement', 'kaliapay', 'module']
+)
