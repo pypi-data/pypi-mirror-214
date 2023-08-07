e67f5d481c3108e098151fc6f9e0abf36574d18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:03 +0000

    Add package faceid-core/0.0.3/faceid_core-0.0.3.tar.gz

---
+++ packages/faceid-core/faceid_core-0.0.3.tar.gz/faceid_core-0.0.3/faceid_core/face_recognition.py
@@ -0,0 +1,93 @@
+import cv2
+import insightface
+import numpy as np
+from pathlib import Path
+from sklearn import preprocessing
+import deploy_configuration
+
+
+class FaceRecognition:
+    def __init__(self, conf_file):
+        self.config = deploy_configuration.DeployConfig(conf_file)
+        self.model = insightface.app.FaceAnalysis()
+        self.model.prepare(ctx_id=self.config.gpu_id, det_size=(640, 640))
+        self.faces_embedding = list()
+        #
+        self.load_faces(self.config.face_db)
+
+    #
+    def load_faces(self, face_db_path):
+        if not Path(face_db_path).exists():
+            Path.mkdir(face_db_path)
+
+        face_db_path = Path(face_db_path)  # преобразуем строку в объект Path
+        for path in face_db_path.rglob("*"):  # используем .rglob() для рекурсивного обхода всех файлов
+            if path.is_file():  # проверяем, что path является файлом
+                input_image = cv2.imdecode(np.fromfile(path, dtype=np.uint8),
+                                           1)
+                user_id = path.stem
+                print("Loader face: % s" % user_id)
+                face = self.model.get(input_image)[0]
+                embedding = np.array(face.embedding).reshape((1, -1))
+                embedding = preprocessing.normalize(embedding)
+                self.faces_embedding.append({
+                    "user_id": user_id,
+                    "feature": embedding
+                })
+
+    def register(self, image):
+        faces = self.model.get(image)
+        if len(faces) != 1:
+            print("No face, no registration")
+            return None
+        # Judging whether the face exists
+        embedding = np.array(faces[0].embedding).reshape((1, -1))
+        embedding = preprocessing.normalize(embedding)
+        is_exits = False
+        for com_face in self.faces_embedding:
+            r = self.feature_compare(embedding, com_face["feature"], self.config.threshold)
+            if r:
+                is_exits = True
+        if is_exits:
+            print("The face already exists, no registration")
+            return None
+        users_id = [d["user_id"] for d in self.faces_embedding]
+
+        if len(users_id) == 0:
+            user_id = 0
+        else:
+            user_id = int(users_id[-1]) + 1
+
+        cv2.imencode('.jpg', image)[1].tofile(Path.joinpath(self.config.face_db, '%s.jpg' % user_id))
+        self.faces_embedding.append({
+            "user_id": user_id,
+            "feature": embedding
+        })
+        return user_id
+
+    def recognition(self, image):
+        faces = self.model.get(image)
+        results = list()
+        for face in faces:
+            result = dict()
+            # Start face recognition
+            embedding = np.array(face.embedding).reshape((1, -1))
+            embedding = preprocessing.normalize(embedding)
+            result["user_id"] = "unknown"
+            for com_face in self.faces_embedding:
+                is_similar = self.feature_compare(embedding, com_face["feature"], self.config.threshold)
+                if is_similar:
+                    return is_similar
+                result["user_id"] = com_face["user_id"] if not is_similar else is_similar
+            results.append(result)
+        return is_similar
+
+    @staticmethod
+    def feature_compare(feature1, feature2, threshold):
+        diff = np.subtract(feature1, feature2)
+        dist = np.sum(np.square(diff), 1)
+        print("Human Face European Distance:% F" % dist)
+        if dist < threshold:
+            return True
+        else:
+            return False
---
+++ packages/faceid-core/faceid_core-0.0.3.tar.gz/faceid_core-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='faceid_core',
+    version='0.0.3',
+    description='Face Recognition',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    author='Orhan Salahetdinov',
+    author_email='salahetdinovorxan@gmail.com',
+    keywords=['FaceId', 'Face Recognition', 'Python 3'],
+    url='https://github.com/Or1onn/faceid-core',
+    download_url='https://pypi.org/project/faceid-core/'
+)
+
+install_requires = [
+    'opencv-python',
+    'insightface',
+    'numpy',
+    'pathlib',
+    'sklearn',
+
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
