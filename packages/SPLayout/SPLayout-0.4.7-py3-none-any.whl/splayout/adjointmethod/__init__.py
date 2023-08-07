b52a943a7881bd6cbe09ce888fd1dafd65559a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:54 +0000

    Add package SPLayout/0.4.7/SPLayout-0.4.7-py3-none-any.whl

---
+++ packages/SPLayout/SPLayout-0.4.7-py3-none-any.whl/splayout/adjointmethod/__init__.py
@@ -0,0 +1,9 @@
+
+from .shaperegion2d import ShapeOptRegion2D
+from .shaperegion3d import ShapeOptRegion3D
+from .topologyregion2d import TopologyOptRegion2D
+from .topologyregion3d import TopologyOptRegion3D
+from .adjointshapeopt import AdjointForShapeOpt
+from .adjointtopologyopt import AdjointForTO
+
+
