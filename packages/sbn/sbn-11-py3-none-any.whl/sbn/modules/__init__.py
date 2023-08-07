8457994299fbb242f480ce3634cdb16e651a7af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:02 +0000

    Add package sbn/11/sbn-11-py3-none-any.whl

---
+++ packages/sbn/sbn-11-py3-none-any.whl/sbn/modules/__init__.py
@@ -0,0 +1,34 @@
+# This file is placed in the Public Domain.
+#
+# flake8: noqa: F401
+# pylama:ignore=W0611
+
+
+"modules"
+
+
+from . import cmd, err, flt, fnd, irc, log, mdl, mod, req, rss, sts, tdo
+from . import thr, upt, wsd
+
+
+def __dir__():
+    return (
+            "cmd",
+            "err",
+            "flt",
+            "fnd",
+            "irc",
+            "log",
+            'mdl',
+            "mod",
+            'req',
+            "rss",
+            "sts",
+            "tdo",
+            "thr",
+            "upt",
+            'wsd'
+           )
+
+
+__all__ = __dir__()
