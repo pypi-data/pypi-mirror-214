d9fc89033976601e5272053fef23dde993b53dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:39 +0000

    Add package coplaniverse/0.0.0/coplaniverse-0.0.0.tar.gz

---
+++ packages/coplaniverse/coplaniverse-0.0.0.tar.gz/coplaniverse-0.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @author: Algorithm Team @ 杉数科技/Cardinal Operations
+# @email: coalgo@shanshu.ai
+# @date: 2023/06
+
+from setuptools import setup, find_packages
+
+setup(
+    name="coplaniverse",
+    version="0.0.0",
+    description="联络：coalgo@shanshu.ai",
+    author=["coalgo@shanshu.ai"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)
