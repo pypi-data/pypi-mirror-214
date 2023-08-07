b34af199da8e4f3271b9bbed436be3162abf48f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:44 +0000

    Add package tbsync/0.0.1/tbsync-0.0.1-py3-none-any.whl

---
+++ packages/tbsync/tbsync-0.0.1-py3-none-any.whl/tbsync/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for tbsync package.
+"""
+
+#: Title of this project (should be `tbsync`).
+__TITLE__ = 'tbsync'
+
+#: Version of this project.
+__VERSION__ = '0.0.1'
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Sync tools for tensorboard logs.'
+
+#: Author of this project.
+__AUTHOR__ = 'narugo1992'
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = 'narugo@126.com'
