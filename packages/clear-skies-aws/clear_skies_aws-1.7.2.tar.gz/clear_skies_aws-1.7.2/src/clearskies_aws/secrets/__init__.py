e82ffb76ece1d70a4b7041f2108c56114b242deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:01 +0000

    Add package clear-skies-aws/1.7.2/clear_skies_aws-1.7.2.tar.gz

---
+++ packages/clear-skies-aws/clear_skies_aws-1.7.2.tar.gz/clear_skies_aws-1.7.2/src/clearskies_aws/secrets/__init__.py
@@ -0,0 +1,6 @@
+from .parameter_store import ParameterStore
+from .secrets_manager import SecretsManager
+from .akeyless_with_ssm_cache import AkeylessWithSsmCache
+from . import additional_configs
+def akeyless_with_ssm_cache(*args, **kwargs):
+    return BindingConfig(AkeylessWithSsmCache, *args, **kwargs)
