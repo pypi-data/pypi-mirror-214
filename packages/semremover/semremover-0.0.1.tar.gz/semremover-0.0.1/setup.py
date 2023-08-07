487476e256d1b83c960a37399e3c5d516f1cd087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:13 +0000

    Add package semremover/0.0.1/semremover-0.0.1.tar.gz

---
+++ packages/semremover/semremover-0.0.1.tar.gz/semremover-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="semremover",
+    version="0.0.1",
+    packages=find_packages(),
+    package_data={'semremover': ["models/*/*.json, models/*/*.yaml"]},
+    install_required=[
+        "LaMa @ git+https://github.com/jessseee/lama.git",
+        "torch~=2.0",
+        "transformers~=4.30",
+        "omegaconf~=2.3",
+        "opencv-python",
+        "numpy",
+        "pillow",
+        "argparse",
+        "tqdm",
+    ]
+)
