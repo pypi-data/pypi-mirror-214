01da9445923de1f1c319b151bd7e0a27debf75ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:13 +0000

    Add package ms-general-utils/1.4.3/ms_general_utils-1.4.3.tar.gz

---
+++ packages/ms-general-utils/ms_general_utils-1.4.3.tar.gz/ms_general_utils-1.4.3/setup.py
@@ -0,0 +1,22 @@
+"""
+Setup configuration file
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='ms_general_utils',
+    packages=find_packages(),
+    include_package_data=True,
+    version='1.4.3',
+    description='General functions for the implementation of microservices.',
+    authors=[
+        {"name": "Alejandro A. Serrano Correa", "email": "alejandroasc93@gmail.com"},
+        {"name": "Rene Gonzalez Ramos", "email": "rgramos9310@gmail.com"}
+    ],
+    license="GPLv3",
+    url="https://github.com/rgramos/ms-utils.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+)
