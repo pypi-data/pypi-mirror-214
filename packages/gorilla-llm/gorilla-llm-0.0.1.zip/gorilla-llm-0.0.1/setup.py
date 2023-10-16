8f96746c7c0cd79d5ed2988a6e4e51aa6fba61e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:46 +0000

    Add package gorilla-llm/0.0.1/gorilla-llm-0.0.1.zip

---
+++ packages/gorilla-llm/gorilla-llm-0.0.1.zip/gorilla-llm-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gorilla-llm',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://gorilla.cs.berkeley.edu',
+    author='Shishir Patil, Tianjun Zhang',
+    author_email='',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
