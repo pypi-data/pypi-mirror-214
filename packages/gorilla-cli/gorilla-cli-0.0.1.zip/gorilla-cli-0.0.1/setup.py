395a07305744bd5f39a789d537cc9f4c71f52cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:46 +0000

    Add package gorilla-cli/0.0.1/gorilla-cli-0.0.1.zip

---
+++ packages/gorilla-cli/gorilla-cli-0.0.1.zip/gorilla-cli-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sysagi',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://gorilla.cs.berkeley.edu',
+    author='Shishir Patil, Tianjun Zhang',
+    author_email='',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
